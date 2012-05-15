3e26240509ac3037a65e9231a53672d3168005f8
Daniel Meyer <daniel.meyer@camunda.com>
2012-05-15 13:44:59 +0200

    fixing conflicts in PropertyHelper

--- core/service/src/main/java/com/camunda/fox/platform/impl/util/PropertyHelper.java
+++ core/service/src/main/java/com/camunda/fox/platform/impl/util/PropertyHelper.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * Copyright (C) 2011, 2012 camunda services GmbH
  *
@@ -14,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-=======
->>>>>>> bf1847a0643ae882b03606507f3a99c94fa433eb
 package com.camunda.fox.platform.impl.util;
 
 import java.util.Map;
