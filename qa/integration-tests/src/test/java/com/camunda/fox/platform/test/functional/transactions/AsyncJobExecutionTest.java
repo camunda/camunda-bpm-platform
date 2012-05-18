2bc9dd8e0404e9f5da7970cbd5caf8a4f471ee4e
romansmirnov <roman.smirnov@camunda.com>
2012-05-18 16:10:37 +0200

    HEMERA-2207: Added an integrationtestcase

---
+++ qa/integration-tests/src/test/java/com/camunda/fox/platform/test/functional/transactions/AsyncJobExecutionTest.java
@@ -0,0 +1,49 @@
+package com.camunda.fox.platform.test.functional.transactions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.transaction.Status;
+import javax.transaction.UserTransaction;
+
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.Assert;
+
+import com.camunda.fox.platform.test.util.AbstractFoxPlatformIntegrationTest;
+
+@RunWith(Arquillian.class)
+public class AsyncJobExecutionTest extends AbstractFoxPlatformIntegrationTest {
+
+  @Deployment
+  public static WebArchive processArchive() {
+    return initWebArchiveDeployment()
+            .addClass(GetVersionInfoDelegate.class)
+            .addClass(UpdateRouterConfiguration.class)
+            .addAsResource("com/camunda/fox/platform/test/functional/transactions/AsyncJobExecutionTest.testAsyncServiceTasks.bpmn20.xml")
+            .addAsWebInfResource("persistence.xml", "classes/META-INF/persistence.xml");
+  }
+
+  @Inject
+  private RuntimeService runtimeService;
+
+  @Test
+  public void testAsyncServiceTasks() {
+    try {
+      HashMap<String, Object> variables = new HashMap<String, Object>();
+      variables.put("serialnumber", "23");
+      runtimeService.startProcessInstanceByKey("configure-router", variables);
+      
+      waitForJobExecutorToProcessAllJobs(2000, 200);
+      
+    } catch (Exception ex) {
+      Assert.fail("Unexpected exception!");
+    }
+  }
+}
