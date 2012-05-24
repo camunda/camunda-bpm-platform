e18b05424685a91d98a2d83500f520e63b754d07
Nils Preusker <n.preusker@googlemail.com>
2012-05-24 18:02:26 +0200

    adding generation of task form link, updating layout
    adding method to determine ProcessArchive and read context path from
    properties, updating layout and adding lots of dummy stuff that still
    needs to be made dynamic.

--- webapps/fox-platform-tasklist/src/main/java/com/camunda/fox/platform/tasklist/TaskList.java
+++ webapps/fox-platform-tasklist/src/main/java/com/camunda/fox/platform/tasklist/TaskList.java
@@ -3,14 +3,20 @@ package com.camunda.fox.platform.tasklist;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.activiti.engine.FormService;
+import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.TaskService;
 import org.activiti.engine.form.TaskFormData;
 import org.activiti.engine.task.Task;
+
+import com.camunda.fox.client.impl.ProcessArchiveSupport;
+import com.camunda.fox.platform.api.ProcessArchiveService;
+import com.camunda.fox.platform.spi.ProcessArchive;
 
 @SessionScoped
 @Named
@@ -24,6 +30,12 @@ public class TaskList implements Serializable {
   @Inject
   private FormService formService;
 
+  @EJB(lookup = ProcessArchiveSupport.PROCESS_ARCHIVE_SERVICE_NAME)
+  private ProcessArchiveService processArchiveService;
+
+  @Inject
+  private ProcessEngine processEngine;
+
   public void update() {
     // do nothing here, since a refreh trigger a reload of the list anyway
   }
@@ -32,14 +44,18 @@ public class TaskList implements Serializable {
     return taskService.createTaskQuery().list();
   }
 
-  public String getFormKey(Task task) {
+  public String getTaskFormUrl(Task task) {
+    String formKey, taskFormUrl = "";
     TaskFormData taskFormData = formService.getTaskFormData(task.getId());
-    if (taskFormData!=null) {
-      return taskFormData.getFormKey();
-    }
-    else {
-      // we do not want to fail just because we have tasks in the task list without form data (typically manually created tasks)
+    if (taskFormData == null || taskFormData.getFormKey() == null) {
       return null;
     }
+    formKey = taskFormData.getFormKey();
+    ProcessArchive processArchive = processArchiveService.getProcessArchiveByProcessDefinitionId(task.getProcessDefinitionId(), processEngine.getName());
+    String contextPath = (String) processArchive.getProperties().get(ProcessArchive.PROP_SERVLET_CONTEXT_PATH);
+
+    taskFormUrl = "../" + contextPath + "/" + formKey + ".jsf?taskId=" + task.getId();
+
+    return taskFormUrl;
   }
 }
