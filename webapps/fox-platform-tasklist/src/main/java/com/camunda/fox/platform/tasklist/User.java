3e051070e39562a7c5f83099335ed8e13694c171
Nils Preusker <n.preusker@googlemail.com>
2012-05-31 18:24:59 +0200

    starting to make design dynamic
    Adding (fake) sign-in and action listener for "My Tasks" menu

---
+++ webapps/fox-platform-tasklist/src/main/java/com/camunda/fox/platform/tasklist/User.java
@@ -0,0 +1,37 @@
+package com.camunda.fox.platform.tasklist;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+  private String username;
+  private String password;
+
+  public User() {
+    this.username = "";
+    this.password = "";
+  }
+
+  public User(String username, String password) {
+    super();
+    this.username = username;
+    this.password = password;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+}
