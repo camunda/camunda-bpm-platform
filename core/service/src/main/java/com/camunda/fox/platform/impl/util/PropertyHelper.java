73927a62ffe30e214de248618bb4fad652b2ddf1
Andreas Drobisch <andreas.drobisch@camunda.com>
2012-06-07 11:40:00 +0200

    fix property evaluation, set isAutoSchemaUpdate default value to "false" , fixes HEMERA-2307

--- core/service/src/main/java/com/camunda/fox/platform/impl/util/PropertyHelper.java
+++ core/service/src/main/java/com/camunda/fox/platform/impl/util/PropertyHelper.java
@@ -16,16 +16,34 @@
 package com.camunda.fox.platform.impl.util;
 
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * 
  * @author Daniel Meyer
  */
 public class PropertyHelper {
-  
+
+  static Logger logger = Logger.getLogger(PropertyHelper.class.getName());
+
   public static <T> T getProperty(Map<String, Object> configMap, String propertyName, T defaultValue) {
-    if(configMap.containsKey(propertyName)) {
-      return (T) configMap.get(propertyName);
+    if (configMap.containsKey(propertyName)) {
+        if (defaultValue instanceof Boolean) {
+          return (T) Boolean.valueOf(""+configMap.get(propertyName));
+        } else if (defaultValue instanceof Integer) {
+          try {
+            return (T) Integer.valueOf(""+configMap.get(propertyName));
+          } catch (NumberFormatException nfe) {
+            logger.log(
+                    Level.WARNING,
+                    String.format("Could not parse property %s with value %s, is the value formatted correctly?", propertyName,
+                            "" + configMap.get(propertyName)));
+            return defaultValue;
+          }
+        } else {
+          return (T) configMap.get(propertyName);
+        }
     } else {
       return defaultValue;
     }
