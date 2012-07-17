bcfc64aeee997be95bccdb1c3d41b6576f97b842
Bernd Ruecker <bernd.ruecker@camunda.com>
2012-07-17 16:20:30 +0200

    moved some parts to the api module to allow easy extension of the tasklist for LDAP logins

--- webapps/tasklist/tasklist-webapp/src/main/java/com/camunda/fox/tasklist/UnassignedTasksLink.java
+++ webapps/tasklist/tasklist-webapp/src/main/java/com/camunda/fox/tasklist/UnassignedTasksLink.java
@@ -1,5 +1,7 @@
 package com.camunda.fox.tasklist;
 
+import com.camunda.fox.tasklist.api.TaskNavigationLink;
+
 public class UnassignedTasksLink extends TaskNavigationLink {
 
   public UnassignedTasksLink(String label, long count, boolean active) {
