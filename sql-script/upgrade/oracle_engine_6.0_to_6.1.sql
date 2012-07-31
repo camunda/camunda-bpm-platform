0e99c102f7cf4ce1eb1575663262e9a71518291d
polenz <kristin.polenz@camunda.com>
2012-07-31 17:09:58 +0200

    HEMERA-2185: Added create and upgrade script to distro.

---
+++ sql-script/upgrade/oracle_engine_6.0_to_6.1.sql
@@ -0,0 +1,8 @@
+alter table ACT_RU_EXECUTION add CACHED_ENT_STATE_ INTEGER;
+update ACT_RU_EXECUTION set CACHED_ENT_STATE_ = 7;
+
+create index ACT_IDX_HI_DETAIL_TASK_ID on ACT_HI_DETAIL(TASK_ID_);
+
+alter table ACT_RE_PROCDEF
+    add constraint ACT_UNIQ_PROCDEF
+    unique (KEY_,VERSION_);
