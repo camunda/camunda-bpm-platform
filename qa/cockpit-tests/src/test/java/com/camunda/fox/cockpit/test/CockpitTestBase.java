bc53e2b8019b251ff1dba4e8875133c4f1a2d5ff
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2012-08-10 16:27:53 +0200

    Add cockpit core platform tests

--- qa/cockpit-tests/src/test/java/com/camunda/fox/cockpit/test/CockpitTestBase.java
+++ qa/cockpit-tests/src/test/java/com/camunda/fox/cockpit/test/CockpitTestBase.java
@@ -1,15 +1,12 @@
 package com.camunda.fox.cockpit.test;
 
 
-import javax.enterprise.inject.spi.Extension;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.Rule;
 
 import com.camunda.fox.cdi.BeanManagerLookup;
 import com.camunda.fox.cdi.FoxProcessEngineLookup;
-import com.camunda.fox.cdi.LocalBeanManagerLookupExtension;
 import com.camunda.fox.cdi.ProgrammaticBeanLookup;
 import com.camunda.fox.cdi.persistence.EntityManagerFactories;
 import com.camunda.fox.cockpit.service.AuditService;
@@ -17,8 +14,8 @@ import com.camunda.fox.cockpit.service.ExecutionService;
 import com.camunda.fox.cockpit.service.JobService;
 import com.camunda.fox.cockpit.service.ProcessDefinitionService;
 import com.camunda.fox.cockpit.service.ProcessService;
-import com.camunda.fox.cockpit.service.producer.ProcessServicesProducer;
 import com.camunda.fox.cockpit.service.VariableService;
+import com.camunda.fox.cockpit.service.producer.ProcessServicesProducer;
 import com.camunda.fox.cockpit.service.query.impl.MyBatisService;
 import com.camunda.fox.cockpit.spi.engine.impl.PlatformProcessEngineLookup;
 import com.camunda.fox.cockpit.spi.engine.impl.PlatformProcessEngines;
@@ -85,10 +82,4 @@ public class CockpitTestBase {
           "com.camunda.fox.cockpit.test.delegate",
           "com.camunda.fox.cockpit.test.util");
   }
-  
-//  @Rule
-//  public TransactionRule transactionRule = new TransactionRule();
-//  
-//  @Rule
-//  public CockpitCleanupRule cockpitCleanupRule = new CockpitCleanupRule();
 }
