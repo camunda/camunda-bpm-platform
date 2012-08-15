31f304160da4a46b79f671c36d389bb812e708b5
Christian Lipphardt <christian.lipphardt@camunda.com>
2012-08-15 15:10:34 +0200

    Added migration sql for new history level

--- sql-script/upgrade/mssql_engine_6.0_to_6.1.sql
+++ sql-script/upgrade/mssql_engine_6.0_to_6.1.sql
@@ -16,13 +16,9 @@ alter table ACT_RE_PROCDEF
 create table ACT_HI_PROCVARIABLE (
     ID_ nvarchar(64) not null,
     PROC_INST_ID_ nvarchar(64) not null,
-    EXECUTION_ID_ nvarchar(64) not null,
-    TASK_ID_ nvarchar(64),
-    ACT_INST_ID_ nvarchar(64),
     NAME_ nvarchar(255) not null,
     VAR_TYPE_ nvarchar(255),
     REV_ int,
-    TIME_ datetime not null,
     BYTEARRAY_ID_ nvarchar(64),
     DOUBLE_ double precision,
     LONG_ numeric(19,0),
@@ -33,6 +29,30 @@ create table ACT_HI_PROCVARIABLE (
 
 create index ACT_IDX_HI_PROCVAR_PROC_INST on ACT_HI_PROCVARIABLE(PROC_INST_ID_);
 create index ACT_IDX_HI_PROCVAR_NAME_TYPE on ACT_HI_PROCVARIABLE(NAME_, VAR_TYPE_);
+
+/**  fill table ACT_HI_PROCVARIABLE when HISTORY_LEVEL FULL is set, could take a long time depending on the amount of data! */
+insert into ACT_HI_PROCVARIABLE
+  (ID_,PROC_INST_ID_,EXECUTION_ID_,TASK_ID_,ACT_INST_ID_,NAME_,VAR_TYPE_,REV_,TIME_,BYTEARRAY_ID_,DOUBLE_,LONG_,TEXT_,TEXT2_)
+  select d.ID_,d.PROC_INST_ID_,d.EXECUTION_ID_,d.TASK_ID_,d.ACT_INST_ID_,d.NAME_,d.VAR_TYPE_,d.REV_,d.TIME_,d.BYTEARRAY_ID_,d.DOUBLE_,d.LONG_,d.TEXT_,d.TEXT2_
+  from ACT_HI_DETAIL d
+  inner join
+    (
+      select de.PROC_INST_ID_, de.NAME_, MAX(de.TIME_) as MAXTIME
+      from ACT_HI_DETAIL de
+      inner join ACT_HI_PROCINST p on de.PROC_INST_ID_ = p.ID_
+      where p.END_TIME_ is not NULL
+      group by de.PROC_INST_ID_, de.NAME_
+    )
+  groupeddetail on d.PROC_INST_ID_ = groupeddetail.PROC_INST_ID_
+  and d.NAME_ = groupeddetail.NAME_
+  and d.TIME_ = groupeddetail.MAXTIME
+  and (select prop.VALUE_ from ACT_GE_PROPERTY prop where prop.NAME_ = 'historyLevel') = 3;
+
+/** update history level to retain old history level */
+update ACT_GE_PROPERTY
+set VALUE_ = VALUE_ + 1,
+    REV_ = REV_ + 1
+where NAME_ = 'historyLevel' and VALUE_ >= 2;
 
 alter table ACT_HI_ACTINST
 add TASK_ID_ nvarchar(64), CALL_PROC_INST_ID_ nvarchar(64);
