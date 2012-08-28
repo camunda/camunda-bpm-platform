252af69262393724b45996e3dd1c4aaa5b301b1f
Andreas Drobisch <andreas.drobisch@camunda.com>
2012-08-28 18:20:16 +0200

    WIP HEMERA-2727

--- cycle/src/main/java/com/camunda/fox/cycle/connector/VfsConnector.java
+++ cycle/src/main/java/com/camunda/fox/cycle/connector/VfsConnector.java
@@ -1,6 +1,13 @@
 package com.camunda.fox.cycle.connector;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.commons.vfs2.FileObject;
+import org.apache.commons.vfs2.FileSystemException;
+import org.apache.commons.vfs2.FileSystemManager;
+import org.apache.commons.vfs2.VFS;
 
 import com.camunda.fox.cycle.api.connector.Connector;
 import com.camunda.fox.cycle.api.connector.ConnectorFolder;
@@ -10,7 +17,32 @@ public class VfsConnector extends Connector {
 
   @Override
   public List<ConnectorNode> getChildren(ConnectorFolder folder) {
-    return null;
-  }
+    List<ConnectorNode> nodes = new ArrayList<ConnectorNode>();
+    
+    try {
+      FileSystemManager fsManager = VFS.getManager();
+      FileObject jarFile;
+      
+      String path = folder.getPath();
+      if (path.equals("root")) {
+        path = "";
+      }
+      
+      jarFile = fsManager.resolveFile( "file://"+System.getProperty("user.home")+File.pathSeparator + path);
+      // List the children of the Jar file
+      FileObject[] children = jarFile.getChildren();
+      for ( int i = 0; i < children.length; i++ )
+      {
+          String baseName = children[ i ].getName().getBaseName();
+          nodes.add(new ConnectorNode(folder.getPath()+File.pathSeparator+baseName, baseName));
+      }
+      
+      return nodes;
+      
+    } catch (FileSystemException e) {
+      e.printStackTrace();
+      return nodes;
+    }
 
+  }
 }
