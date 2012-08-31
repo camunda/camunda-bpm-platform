a3c5e3b4c2a7190cc25fd7bd1ec2651f56378a5a
romansmirnov <roman.smirnov@camunda.com>
2012-08-31 10:31:07 +0200

    resolved merge conflicts

--- cycle/src/main/java/com/camunda/fox/cycle/api/connector/ConnectorNode.java
+++ cycle/src/main/java/com/camunda/fox/cycle/api/connector/ConnectorNode.java
@@ -80,4 +80,30 @@ public class ConnectorNode implements Comparable<ConnectorNode> {
   public int compareTo(ConnectorNode o) {
     return o.label.compareTo(label);
   }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    ConnectorNode other = (ConnectorNode) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+  
 }
