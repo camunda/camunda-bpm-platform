c4a4b1f98dfd093d5f9ebe7160574bf5403111fe
Andreas Drobisch <andreas.drobisch@camunda.com>
2012-09-05 13:16:02 +0200

    added e2e test for HEMERA-2549

--- cycle/src/main/webapp/assets/js/test/scenarios.js
+++ cycle/src/main/webapp/assets/js/test/scenarios.js
@@ -29,10 +29,18 @@ describe('cycle', function() {
     it('should contain add roundtrip button', function() {
       expect(element('a.btn[ng-click="createNew()"]').count()).toBe(1);
     });
-
     
-    it('should contain additional tests', function() {
-      expect(true).toBe(false);
+    it('should add a new roundtrip to the list when clicking on button', function() {
+      var testRoundtripName="Roundtrip " +Math.floor(Math.random()*10000);
+      
+      element('a.btn[ng-click="createNew()"]').click();
+      input('name').enter(testRoundtripName);
+      sleep(2);
+      element('#saveRoundtripButton').click();
+      sleep(2);
+      // There should be 2 links now, own in the bread crumb, one in the list
+      expect(element('a:contains('+testRoundtripName+')').count()).toBe(2);
     });
+    
   });
 });
