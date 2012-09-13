a2b43080853cd88ebe78558e67a2aabdf56d350d
Andreas Drobisch <andreas.drobisch@camunda.com>
2012-09-13 09:56:03 +0200

    add http client dependencies, changed id generation strategy

--- cycle/src/main/java/com/camunda/fox/cycle/entity/AbstractEntity.java
+++ cycle/src/main/java/com/camunda/fox/cycle/entity/AbstractEntity.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.TableGenerator;
 
 /**
  * Abstract super class for all entities.
@@ -18,7 +19,12 @@ public abstract class AbstractEntity implements Serializable {
   private static final long serialVersionUID = 1L;
   
   @Id
-  @GeneratedValue(strategy=GenerationType.AUTO)
+  @GeneratedValue(strategy=GenerationType.TABLE, generator="cycleIdGenerator")  
+  @TableGenerator(name="cycleIdGenerator", table="TableID",  
+                  pkColumnName="tablename", // TableID.TableName (value = table_name, test_table, etc.)  
+                  valueColumnName="id", // TableID.ID (value = 1,2,3,etc.)  
+                  allocationSize=1 // flush every 1 insert  
+  )  
   private Long id;
   
   public Long getId() {
