27dca9aa5f558f95d884eefb1862818aba198cb7
polenz <kristin.polenz@camunda.com>
2012-09-24 14:15:27 +0200

    Fixed imports.

--- cycle/src/main/java/com/camunda/fox/cycle/roundtrip/BpmnProcessModelUtil.java
+++ cycle/src/main/java/com/camunda/fox/cycle/roundtrip/BpmnProcessModelUtil.java
@@ -18,6 +18,7 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
+import org.springframework.stereotype.Component;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
