0e5486853b4e0f9c1b2fca0944a07d793abae988
romansmirnov <roman.smirnov@camunda.com>
2012-10-02 18:06:50 +0200

    Loading emptyJson from classLoader

--- cycle/src/main/java/com/camunda/fox/cycle/connector/signavio/SignavioConnector.java
+++ cycle/src/main/java/com/camunda/fox/cycle/connector/signavio/SignavioConnector.java
@@ -568,6 +568,7 @@ public class SignavioConnector extends Connector {
 
   @Override
   public ConnectorNode createNode(String parentId, String label, ConnectorNodeType type) {
+    InputStream emptyJson = null;
     try {
       String response = "";
       ConnectorNode result = null;
@@ -585,7 +586,10 @@ public class SignavioConnector extends Connector {
           newModelForm.setComment("");
           newModelForm.setDescription("");
           newModelForm.setParent(parentId);
-          newModelForm.setJsonXml(IoUtil.readFileAsString("com/camunda/fox/cycle/connector/emptyProcessModelTemplate.json"));
+          
+          emptyJson = getClass().getClassLoader().getResourceAsStream("com/camunda/fox/cycle/connector/emptyProcessModelTemplate.json");
+          
+          newModelForm.setJsonXml(new String(IoUtil.readInputStream(emptyJson, "emptyProcessModelTemplate.json"), UTF_8));
           newModelForm.setSVG_XML("<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:oryx=\"http://oryx-editor.org\" id=\"sid-80D82B67-3B30-4B35-A6CB-16EEE17A719F\" width=\"50\" height=\"50\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:svg=\"http://www.w3.org/2000/svg\"><defs/><g stroke=\"black\" font-family=\"Verdana, sans-serif\" font-size-adjust=\"none\" font-style=\"normal\" font-variant=\"normal\" font-weight=\"normal\" line-heigth=\"normal\" font-size=\"12\"><g class=\"stencils\" transform=\"translate(25, 25)\"><g class=\"me\"/><g class=\"children\"/><g class=\"edge\"/></g></g></svg>");
           
           response = this.signavioClient.createModel(newModelForm);
@@ -601,6 +605,8 @@ public class SignavioConnector extends Connector {
     } catch (Exception e) {
       logger.fine("Failed to create a new connector node '" + label + "'.");
       throw new CycleException("Failed to create a new connector node '" + label + "'.", e);
+    } finally {
+      IoUtil.closeSilently(emptyJson);
     }
   }
   
