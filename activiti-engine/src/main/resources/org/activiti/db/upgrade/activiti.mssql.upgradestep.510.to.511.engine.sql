09cf9a6c0e5a4c27ec72f523a4d754e7439913f3
Daniel Meyer <daniel.meyer@camunda.com>
2012-10-12 15:32:21 +0200

    [#HEMERA-2935] delete activiti upgrade sql scripts

--- activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.mssql.upgradestep.510.to.511.engine.sql
+++
@@ -1,5 +0,0 @@
-alter table ACT_RE_PROCDEF
-    alter column KEY_ nvarchar(255) not null;
-
-alter table ACT_RE_PROCDEF
-    alter column VERSION_ int not null;
--- activiti-engine/src/main/resources/org/activiti/db/upgrade/activiti.oracle.upgradestep.510.to.511.engine.sql
+++
@@ -1,5 +0,0 @@
-alter table ACT_RE_PROCDEF
-    modify KEY_ not null;
-
-alter table ACT_RE_PROCDEF
-    modify VERSION_ not null;
