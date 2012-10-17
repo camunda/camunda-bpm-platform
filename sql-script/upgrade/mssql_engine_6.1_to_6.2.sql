d36f4e9ea7244078d912165b69483fe254e99853
Stefan Hentschel <stefan.hentschel@camunda.com>
2012-10-17 11:36:00 +0200

    #HEMERA-2876: fixed migration script for mssql

--- sql-script/upgrade/mssql_engine_6.1_to_6.2.sql
+++ sql-script/upgrade/mssql_engine_6.1_to_6.2.sql
@@ -22,14 +22,14 @@ insert into ACT_HI_VARINST
 	select ID_, PROC_INST_ID_, NULL, NULL, NAME_, VAR_TYPE_, REV_, BYTEARRAY_ID_, DOUBLE_, LONG_, TEXT_, TEXT2_ FROM ACT_HI_PROCVARIABLE;
 
 -- drop old indices --
-drop index ACT_IDX_HI_PROCVAR_PROC_INST;
-drop index ACT_IDX_HI_PROCVAR_NAME_TYPE;
+drop index ACT_IDX_HI_PROCVAR_PROC_INST on ACT_HI_PROCVARIABLE;
+drop index ACT_IDX_HI_PROCVAR_NAME_TYPE on ACT_HI_PROCVARIABLE;
 
 -- drop old table --
 drop table ACT_HI_PROCVARIABLE;
 
 -- create new indices --
-create index ACT_IDX_HI_PROCVAR_PROC_INST on ACT_HI_VARINST(PROC_INST_ID_)
+create index ACT_IDX_HI_PROCVAR_PROC_INST on ACT_HI_VARINST(PROC_INST_ID_);
 create index ACT_IDX_HI_PROCVAR_NAME_TYPE on ACT_HI_VARINST(NAME_, VAR_TYPE_);
 
 -- not migrating --
@@ -59,12 +59,12 @@ alter table ACT_RU_TASK
 -------------------------------------------
 -- change column types in ACT_RU_IDENTITYLINK --
 
-drop index ACT_IDX_IDENT_LNK_USER;
+drop index ACT_IDX_IDENT_LNK_USER on ACT_RU_IDENTITYLINK;
 alter table ACT_RU_IDENTITYLINK 
 	alter column USER_ID_ varchar(255);
 create index ACT_IDX_IDENT_LNK_USER on ACT_RU_IDENTITYLINK(USER_ID_);
 
-drop index ACT_IDX_IDENT_LNK_GROUP;
+drop index ACT_IDX_IDENT_LNK_GROUP on ACT_RU_IDENTITYLINK;
 alter table ACT_RU_IDENTITYLINK 
 	alter column GROUP_ID_ varchar(255);
 create index ACT_IDX_IDENT_LNK_GROUP on ACT_RU_IDENTITYLINK(GROUP_ID_);
@@ -72,7 +72,7 @@ create index ACT_IDX_IDENT_LNK_GROUP on ACT_RU_IDENTITYLINK(GROUP_ID_);
 ----------------------------------------------
 -- change column types in ACT_RU_JOB (remove NOT NULL constraint)
 alter table ACT_RU_JOB 
-	alter column DUEDATE_ SET DATA TYPE timestamp;
+	alter column DUEDATE_ datetime2;
 
 ----------------------------------------------
 -- revert introduction of new history level --
@@ -91,7 +91,7 @@ alter table ACT_RE_DEPLOYMENT ALTER COLUMN DEPLOY_TIME_ datetime2;
 alter table ACT_RU_JOB ALTER COLUMN LOCK_EXP_TIME_ datetime2;
 alter table ACT_RU_JOB ALTER COLUMN DUEDATE_ datetime2 NULL;
 
-drop index ACT_RU_TASK.ACT_IDX_TASK_CREATE;
+drop index ACT_IDX_TASK_CREATE on ACT_RU_TASK;
 alter table ACT_RU_TASK ALTER COLUMN CREATE_TIME_ datetime2;
 create index ACT_IDX_TASK_CREATE on ACT_RU_TASK(CREATE_TIME_);
 alter table ACT_RU_TASK ALTER COLUMN DUE_DATE_ datetime2;
@@ -101,15 +101,15 @@ alter table ACT_RU_EVENT_SUBSCR ALTER COLUMN CREATED_ datetime2;
 --history
 alter table ACT_HI_PROCINST ALTER COLUMN START_TIME_ datetime2;
 
-drop index ACT_HI_PROCINST.ACT_IDX_HI_PRO_INST_END;
+drop index ACT_IDX_HI_PRO_INST_END on ACT_HI_PROCINST;
 alter table ACT_HI_PROCINST ALTER COLUMN END_TIME_ datetime2;
 create index ACT_IDX_HI_PRO_INST_END on ACT_HI_PROCINST(END_TIME_);
 
-drop index ACT_HI_ACTINST.ACT_IDX_HI_ACT_INST_START;
+drop index ACT_IDX_HI_ACT_INST_START on ACT_HI_ACTINST;
 alter table ACT_HI_ACTINST ALTER COLUMN START_TIME_ datetime2 not null;
 create index ACT_IDX_HI_ACT_INST_START on ACT_HI_ACTINST(START_TIME_);
 
-drop index ACT_HI_ACTINST.ACT_IDX_HI_ACT_INST_END;
+drop index ACT_IDX_HI_ACT_INST_END on ACT_HI_ACTINST;
 alter table ACT_HI_ACTINST ALTER COLUMN END_TIME_ datetime2;
 create index ACT_IDX_HI_ACT_INST_END on ACT_HI_ACTINST(END_TIME_);
 
@@ -117,5 +117,7 @@ alter table ACT_HI_TASKINST ALTER COLUMN START_TIME_ datetime2 not null;
 alter table ACT_HI_TASKINST ALTER COLUMN END_TIME_ datetime2;
 alter table ACT_HI_TASKINST ALTER COLUMN DUE_DATE_ datetime2;
 
+drop index ACT_IDX_HI_DETAIL_TIME on ACT_HI_DETAIL;
 alter table ACT_HI_DETAIL ALTER COLUMN TIME_ datetime2 not null;
+create index ACT_IDX_HI_DETAIL_TIME on ACT_HI_DETAIL(TIME_);
 alter table ACT_HI_COMMENT ALTER COLUMN TIME_ datetime2 not null;
