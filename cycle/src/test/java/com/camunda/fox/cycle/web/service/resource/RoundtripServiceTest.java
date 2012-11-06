a01e597a539a6dd73d788dc70c10313acc781720
Christian Lipphardt <christian.lipphardt@camunda.com>
2012-11-06 12:18:11 +0100

    HEMERA-3073 Added comment why there is a Thread.sleep

--- cycle/src/test/java/com/camunda/fox/cycle/web/service/resource/RoundtripServiceTest.java
+++ cycle/src/test/java/com/camunda/fox/cycle/web/service/resource/RoundtripServiceTest.java
@@ -166,6 +166,8 @@ public class RoundtripServiceTest {
     RoundtripDTO testRoundtrip = createAndFlushRoundtrip();
     BpmnDiagramDTO rightHandSide = testRoundtrip.getRightHandSide();
     
+    // needs to be here because invoke of diagramService.getImage(...), expects
+    // the image date to be 'now' + 5 secs. 'now' is set in roundtripService.doSynchronize(...)
     Thread.sleep(6000);
     
     roundtripService.doSynchronize(SyncMode.LEFT_TO_RIGHT, testRoundtrip.getId());
