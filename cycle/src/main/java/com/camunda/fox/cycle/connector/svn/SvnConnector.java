ec787c656914cc97f440132510df36e5bf621b83
Christian Lipphardt <christian.lipphardt@camunda.com>
2012-11-12 11:23:36 +0100

    Cleaned import

--- cycle/src/main/java/com/camunda/fox/cycle/connector/svn/SvnConnector.java
+++ cycle/src/main/java/com/camunda/fox/cycle/connector/svn/SvnConnector.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.locks.ReentrantLock;
