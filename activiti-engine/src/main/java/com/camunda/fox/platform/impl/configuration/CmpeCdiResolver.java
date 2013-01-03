571cd85984d4a60b2b32fe91a213e713f845030a
Daniel Meyer <daniel.meyer@camunda.com>
2013-01-03 06:27:18 +0100

    moved platform core service and api to engine module

---
+++ activiti-engine/src/main/java/com/camunda/fox/platform/impl/configuration/CmpeCdiResolver.java
@@ -0,0 +1,105 @@
+/**
+ * Copyright (C) 2011, 2012 camunda services GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.camunda.fox.platform.impl.configuration;
+
+import java.beans.FeatureDescriptor;
+import java.util.Iterator;
+
+import javax.enterprise.inject.spi.BeanManager;
+
+import org.activiti.engine.impl.javax.el.ELContext;
+
+import com.camunda.fox.platform.impl.configuration.tmp.CdiResolver;
+import com.camunda.fox.platform.impl.context.spi.ProcessArchiveServices;
+
+/**
+ * 
+ * @author Daniel Meyer
+ */
+public class CmpeCdiResolver extends CdiResolver {
+    
+  private final ProcessArchiveServices paServices;
+  
+  public CmpeCdiResolver(ProcessArchiveServices processArchiveServices) {
+    paServices = processArchiveServices;
+  }
+  
+  @Override
+  protected BeanManager getBeanManager() {
+    return paServices.getBeanManager();
+  }
+  
+  protected boolean skip() {
+    return getBeanManager() == null;
+  }
+  
+  @Override
+  public Class< ? > getCommonPropertyType(ELContext context, Object base) {
+    if(skip()) {
+      return null;
+    }
+    return super.getCommonPropertyType(context, base);
+  }
+
+  @Override
+  public Iterator<FeatureDescriptor> getFeatureDescriptors(ELContext context, Object base) {
+    if(skip()) {
+      return null;
+    }
+    return super.getFeatureDescriptors(context, base);
+  }
+
+  @Override
+  public Class< ? > getType(ELContext context, Object base, Object property) {
+    if(skip()) {
+      return null;
+    }
+    return super.getType(context, base, property);
+  }
+
+  @Override
+  public Object getValue(ELContext context, Object base, Object property) {
+    if(skip()) {
+      return null;
+    }
+    return super.getValue(context, base, property);
+  }
+
+  @Override
+  public boolean isReadOnly(ELContext context, Object base, Object property) {
+    if(skip()) {
+      return true;
+    }
+    return super.isReadOnly(context, base, property);
+  }
+
+  @Override
+  public void setValue(ELContext context, Object base, Object property, Object value) {
+    if(skip()) {
+      return;
+    }
+    super.setValue(context, base, property, value);
+  }
+
+  @Override
+  public Object invoke(ELContext context, Object base, Object method, java.lang.Class< ? >[] paramTypes, Object[] params) {
+    if(skip()) {
+      return null;
+    }
+    return super.invoke(context, base, method, paramTypes, params);
+  }
+
+}
---
+++ activiti-engine/src/main/java/com/camunda/fox/platform/impl/deployment/ActivateDeploymentCmd.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (C) 2011, 2012 camunda services GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.camunda.fox.platform.impl.deployment;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.activiti.engine.ActivitiException;
+import org.activiti.engine.impl.ProcessDefinitionQueryImpl;
+import org.activiti.engine.impl.cfg.ProcessEngineConfigurationImpl;
+import org.activiti.engine.impl.cmd.ActivateProcessDefinitionCmd;
+import org.activiti.engine.impl.context.Context;
+import org.activiti.engine.impl.interceptor.Command;
+import org.activiti.engine.impl.interceptor.CommandContext;
+import org.activiti.engine.impl.persistence.deploy.DeploymentCache;
+import org.activiti.engine.repository.ProcessDefinition;
+
+/**
+ * For each process definition part of this deployment: 
+ *  - add the key of the process definition to the classloader
+ *  - get all process definitions with the same key
+ *  - then, for each process definition with that key:
+ *    - activate the process definition (resumes job execution) 
+ *    - add the process definition to the activiti deployment cache 
+ * 
+ * @author Daniel Meyer
+ * @author nico.rehwaldt@camunda.com
+ */
+public class ActivateDeploymentCmd implements Command<Void> {
+
+  private static Logger log = Logger.getLogger(ActivateDeploymentCmd.class.getName());
+  
+  protected final String deploymentId;
+  protected final ClassLoader classloader;
+
+  public ActivateDeploymentCmd(String deploymentId, ClassLoader classloader) {
+    this.deploymentId = deploymentId;
+    this.classloader = classloader;
+  }
+
+  public Void execute(CommandContext commandContext) {
+
+    final ProcessEngineConfigurationImpl processEngineConfiguration = Context.getProcessEngineConfiguration();
+    final DeploymentCache deploymentCache = processEngineConfiguration.getDeploymentCache();
+
+    final List<ProcessDefinition> processDefinitions = new ProcessDefinitionQueryImpl(commandContext).deploymentId(deploymentId).list();
+
+    ClassLoader customClassLoader = processEngineConfiguration.getClassLoader();
+
+    if (customClassLoader == null) {
+      customClassLoader = new ProcessArchiveClassLoader();
+      processEngineConfiguration.setClassLoader(customClassLoader);
+    }
+
+    if (!(customClassLoader instanceof ProcessArchiveClassLoader)) {
+      throw new ActivitiException("the custom classloader set on the process engine configuration is not a DeploymentClassLoader");
+    }
+
+    ProcessArchiveClassLoader deploymentClassLoader = (ProcessArchiveClassLoader) customClassLoader;
+
+    for (ProcessDefinition processDefinition : processDefinitions) {
+      // activate all process definitions with the same key
+      List<ProcessDefinition> processDefinitionsToActivate = new ProcessDefinitionQueryImpl(commandContext).processDefinitionKey(processDefinition.getKey()).list();
+
+      for (ProcessDefinition definitionToActivate : processDefinitionsToActivate) {
+
+        log.log(
+          Level.INFO, "Deploying process definition  ''{0}[definitionId={1}, version={2}] to the cache.", 
+          new Object[]{ processDefinition.getKey(), definitionToActivate.getId(), definitionToActivate.getVersion() });
+        
+        //  assert that processes are parsed and deployed to the cache:
+        deploymentCache.findDeployedProcessDefinitionById(processDefinition.getId());
+
+        if (definitionToActivate.isSuspended()) {
+          log.log(
+            Level.INFO, "Activating process definition ''{0}[definitionId={1}, version={2}]", 
+            new Object[]{ processDefinition.getKey(), definitionToActivate.getId(), definitionToActivate.getVersion() });
+          
+          new ActivateProcessDefinitionCmd(definitionToActivate.getId(), null).execute(commandContext);
+        }
+      }
+
+      deploymentClassLoader.registerProcessDefinition(processDefinition.getKey(), classloader);
+    }
+    
+    return null;
+  }
+}
