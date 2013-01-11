9120f667c89de39b5b52b4c5b018b70a6bfb7214
Daniel Meyer <daniel.meyer@camunda.com>
2013-01-11 09:34:35 +0100

    CAM-67 moved fox platform jobexecutor core to process engine

--- activiti-engine/src/main/java/com/camunda/fox/platform/jobexecutor/impl/DefaultPlatformJobExecutor.java
+++ activiti-engine/src/main/java/com/camunda/fox/platform/jobexecutor/impl/DefaultPlatformJobExecutor.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
