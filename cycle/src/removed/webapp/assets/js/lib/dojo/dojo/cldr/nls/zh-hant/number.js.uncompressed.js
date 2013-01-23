9f90b9dd0967d01b3c3c874faa60683caaaa61e9
Christian Lipphardt <christian.lipphardt@camunda.com>
2013-01-23 11:43:00 +0100

    Removed old removed folder - we do not need it anymore.

--- cycle/src/removed/webapp/assets/js/lib/dojo/dojo/cldr/nls/zh-hant/number.js.uncompressed.js
+++
@@ -1,9 +0,0 @@
-define(
-"dojo/cldr/nls/zh-hant/number", //begin v1.x content
-{
-	"nan": "非數值",
-	"decimalFormat-short": "000兆",
-	"currencyFormat": "¤#,##0.00"
-}
-//end v1.x content
-);
--- cycle/src/removed/webapp/assets/js/lib/dojo/dojo/cldr/nls/zh-hk/number.js.uncompressed.js
+++
@@ -1,7 +0,0 @@
-define(
-"dojo/cldr/nls/zh-hk/number", //begin v1.x content
-{
-	"nan": "非數值"
-}
-//end v1.x content
-);
