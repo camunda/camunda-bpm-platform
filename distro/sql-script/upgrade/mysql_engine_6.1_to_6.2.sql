a2e88bfea642850f5b7099fc225afdab2900c727
Daniel Meyer <daniel.meyer@camunda.com>
2013-01-23 12:07:08 +0100

    HEMERA-3572 Activiti merge: added upgrade statements for new default value for Job Timestamps introduced in 9aaf14fbece10a82e674f381b23400a71d0775be

--- distro/sql-script/upgrade/mysql_engine_6.1_to_6.2.sql
+++ distro/sql-script/upgrade/mysql_engine_6.1_to_6.2.sql
@@ -87,3 +87,9 @@ update ACT_GE_PROPERTY
 -- Additional index on PROC_INST_ID_ and ACT_ID_ for historic activity
 
 create index ACT_IDX_HI_ACT_INST_PROCINST on ACT_HI_ACTINST(PROC_INST_ID_, ACT_ID_);
+
+-----------------------------------------------
+-- http://jira.codehaus.org/browse/ACT-1424    
+alter table ACT_RU_JOB 
+    MODIFY LOCK_EXP_TIME_ timestamp null;
+	
