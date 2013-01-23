4fdc03c2b85b940b5c66904548c6e387cf31c058
Daniel Meyer <daniel.meyer@camunda.com>
2013-01-23 12:00:18 +0100

    HEMERA-3572 Activiti merge: added upgrade statements for new index statements introduced in d399624b8023776ba595b46ff82c91024147861b

--- distro/sql-script/upgrade/db2_engine_6.1_to_6.2.sql
+++ distro/sql-script/upgrade/db2_engine_6.1_to_6.2.sql
@@ -81,3 +81,8 @@ update ACT_GE_PROPERTY
   set VALUE_ = VALUE_ - 1,
       REV_ = REV_ + 1
   where NAME_ = 'historyLevel' and VALUE_ >= 2;
+  
+-----------------------------------------------
+-- Additional index on PROC_INST_ID_ and ACT_ID_ for historic activity
+
+create index ACT_IDX_HI_ACT_INST_PROCINST on ACT_HI_ACTINST(PROC_INST_ID_, ACT_ID_);
