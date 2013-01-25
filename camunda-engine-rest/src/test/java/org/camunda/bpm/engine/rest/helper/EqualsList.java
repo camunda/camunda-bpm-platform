d22b3d27613c90a10f8d13c3a0354f017c828f36
Thorben Lindhauer <thorben.lindhauer@student.hpi.uni-potsdam.de>
2013-01-25 16:20:35 +0100

    [#CAM-78] Refactored the list equals matcher implementation.

--- camunda-engine-rest/src/test/java/org/camunda/bpm/engine/rest/helper/EqualsList.java
+++ camunda-engine-rest/src/test/java/org/camunda/bpm/engine/rest/helper/EqualsList.java
@@ -1,11 +1,10 @@
 package org.camunda.bpm.engine.rest.helper;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import org.mockito.ArgumentMatcher;
-
-import com.google.common.collect.Sets;
 
 public class EqualsList extends ArgumentMatcher<List<String>> {
 
@@ -19,12 +18,15 @@ public class EqualsList extends ArgumentMatcher<List<String>> {
   public boolean matches(Object list) {
     List<String> argumentList = (List<String>) list;
     
-    Set<String> setToCompare = Sets.newHashSet(listToCompare);
-    Set<String> argumentSet = Sets.newHashSet(argumentList);
+    Set<String> setToCompare = new HashSet<String>(listToCompare);
+    int initialSize = setToCompare.size();
     
-    Set<String> union = Sets.union(setToCompare, argumentSet);
+    Set<String> argumentSet = new HashSet<String>(argumentList);
     
-    if (union.size() == setToCompare.size() && union.size() == argumentSet.size()) {
+    // intersection of the two sets
+    setToCompare.retainAll(argumentSet);
+    
+    if (initialSize == setToCompare.size()) {
       return true;
     } else {
       return false;
