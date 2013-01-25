1e836d78cf8e6918635ea1652fe3b27e37c57593
Thorben Lindhauer <thorben.lindhauer@student.hpi.uni-potsdam.de>
2013-01-25 11:53:53 +0100

    [#CAM-78] Added pagination support.

--- camunda-engine-rest/src/main/java/org/camunda/bpm/engine/rest/impl/TaskRestServiceImpl.java
+++ camunda-engine-rest/src/main/java/org/camunda/bpm/engine/rest/impl/TaskRestServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.activiti.engine.TaskService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.runtime.ProcessInstanceQuery;
 import org.activiti.engine.task.Task;
 import org.activiti.engine.task.TaskQuery;
 import org.camunda.bpm.engine.rest.TaskRestService;
@@ -13,11 +15,17 @@ import org.camunda.bpm.engine.rest.dto.TaskQueryDto;
 public class TaskRestServiceImpl extends AbstractEngineService implements TaskRestService {
 
   @Override
-  public List<TaskDto> getTasks(TaskQueryDto queryDto) {
+  public List<TaskDto> getTasks(TaskQueryDto queryDto,
+      Integer firstResult, Integer maxResults) {
     TaskService taskService = processEngine.getTaskService();
     TaskQuery query = queryDto.toQuery(taskService);
     
-    List<Task> matchingTasks = query.list();
+    List<Task> matchingTasks;
+    if (firstResult != null || maxResults != null) {
+      matchingTasks = executePaginatedQuery(query, firstResult, maxResults);
+    } else {
+      matchingTasks = query.list();
+    }
     
     List<TaskDto> tasks = new ArrayList<TaskDto>();
     for (Task task : matchingTasks) {
@@ -26,6 +34,16 @@ public class TaskRestServiceImpl extends AbstractEngineService implements TaskRe
     }
     
     return tasks;
+  }
+  
+  private List<Task> executePaginatedQuery(TaskQuery query, Integer firstResult, Integer maxResults) {
+    if (firstResult == null) {
+      firstResult = 0;
+    }
+    if (maxResults == null) {
+      maxResults = Integer.MAX_VALUE;
+    }
+    return query.listPage(firstResult, maxResults); 
   }
 
 }
