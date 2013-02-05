80723132d951cf0bd7d433475fcc2d12ef313de8
Daniel Meyer <daniel.meyer@camunda.com>
2013-02-05 06:05:10 +0100

    Fix for ACT-1259

--- activiti-engine/src/test/java/org/activiti/engine/test/bpmn/event/timer/IntermediateTimerEventTest.java
+++ activiti-engine/src/test/java/org/activiti/engine/test/bpmn/event/timer/IntermediateTimerEventTest.java
@@ -12,13 +12,17 @@
  */
 package org.activiti.engine.test.bpmn.event.timer;
 
-import java.util.Date;
-
 import org.activiti.engine.impl.test.PluggableActivitiTestCase;
 import org.activiti.engine.impl.util.ClockUtil;
+import org.activiti.engine.runtime.Job;
 import org.activiti.engine.runtime.JobQuery;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.test.Deployment;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class IntermediateTimerEventTest extends PluggableActivitiTestCase {
@@ -44,39 +48,34 @@ public class IntermediateTimerEventTest extends PluggableActivitiTestCase {
 
   }
 
-  // SEE HEMERA-2931
-//  
-//  @Deployment 
-//  public void testExpression() {
-//    // Set the clock fixed
-//    Date currentTime = ClockUtil.getCurrentTime();
-//
-//    HashMap<String, Object> variables1 = new HashMap<String, Object>();
-//    variables1.put("dueDate", currentTime);
-//    
-//    HashMap<String, Object> variables2 = new HashMap<String, Object>();
-//    variables2.put("dueDate", new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss").format(currentTime));
-//    
-//    // After process start, there should be timer created    
-//    ProcessInstance pi1 = runtimeService.startProcessInstanceByKey("intermediateTimerEventExample", variables1);
-//    ProcessInstance pi2 = runtimeService.startProcessInstanceByKey("intermediateTimerEventExample", variables2);
-//    
-//    assertEquals(1, managementService.createJobQuery().processInstanceId(pi1.getId()).count());
-//    assertEquals(1, managementService.createJobQuery().processInstanceId(pi2.getId()).count());
-//
-//    // After setting the clock to 5 seconds in the future the timers should fire
-//    ClockUtil.setCurrentTime(new Date(currentTime.getTime() + 5000));
-//    
-//    List<Job> jobs = managementService.createJobQuery().executable().list();
-//    for (Job job : jobs) {
-//      managementService.executeJob(job.getId());
-//    }
-//    
-//    assertEquals(0, managementService.createJobQuery().processInstanceId(pi1.getId()).count());
-//    assertEquals(0, managementService.createJobQuery().processInstanceId(pi2.getId()).count());
-//
-//    assertProcessEnded(pi1.getProcessInstanceId());
-//    assertProcessEnded(pi2.getProcessInstanceId());    
-//  }
+  @Deployment 
+  public void testExpression() {
+    // Set the clock fixed
+    HashMap<String, Object> variables1 = new HashMap<String, Object>();
+    variables1.put("dueDate", new Date());
+    
+    HashMap<String, Object> variables2 = new HashMap<String, Object>();
+    variables2.put("dueDate", new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(new Date()));
+    
+    // After process start, there should be timer created    
+    ProcessInstance pi1 = runtimeService.startProcessInstanceByKey("intermediateTimerEventExample", variables1);
+    ProcessInstance pi2 = runtimeService.startProcessInstanceByKey("intermediateTimerEventExample", variables2);
+    
+    assertEquals(1, managementService.createJobQuery().processInstanceId(pi1.getId()).count());
+    assertEquals(1, managementService.createJobQuery().processInstanceId(pi2.getId()).count());
+
+    // After setting the clock to one second in the future the timers should fire
+    List<Job> jobs = managementService.createJobQuery().executable().list();
+    assertEquals(2, jobs.size());
+    for (Job job : jobs) {
+      managementService.executeJob(job.getId());
+    }
+    
+    assertEquals(0, managementService.createJobQuery().processInstanceId(pi1.getId()).count());
+    assertEquals(0, managementService.createJobQuery().processInstanceId(pi2.getId()).count());
+
+    assertProcessEnded(pi1.getProcessInstanceId());
+    assertProcessEnded(pi2.getProcessInstanceId());    
+  }
 
 }
