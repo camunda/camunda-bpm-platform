5e9b1a99754d3cf6ff265cbed13ff4ced590043a
Daniel Meyer <daniel.meyer@camunda.com>
2013-03-06 15:12:13 +0100

    refactor(spring): renaming packages and classes
    Packages
    - org.activiti.spring -> org.camunda.bpm.engine.spring
    
    Resources
    - Schema activiti-5.0.xsd -> camunda-7.0.xsd
    
    Classes
    - Activiti-Component -> ProcessEngineComponent
    - SpringActivitiTestCase -> SpringProcessEngineTestCase
    
    related to CAM-56

---
+++ activiti-spring/src/test/java/org/camunda/bpm/engine/spring/test/servicetask/DelegateExpressionBean.java
@@ -0,0 +1,48 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.engine.spring.test.servicetask;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.impl.el.FixedValue;
+
+/**
+ * @author Joram Barrez
+ * @author Bernd Ruecker (camunda)
+ */
+public class DelegateExpressionBean implements JavaDelegate {
+
+  private SentenceGenerator sentenceGenerator;
+
+  private FixedValue someField;
+
+  public void execute(DelegateExecution execution) throws Exception {
+    execution.setVariable("myVar", sentenceGenerator.getSentence());
+    if (someField != null) {
+      execution.setVariable("fieldInjection", someField.getValue(execution));
+    }
+  }
+
+  public void setSentenceGenerator(SentenceGenerator sentenceGenerator) {
+    this.sentenceGenerator = sentenceGenerator;
+  }
+
+  public FixedValue getSomeField() {
+    return someField;
+  }
+
+  public void setSomeField(FixedValue someField) {
+    this.someField = someField;
+  }
+
+}
