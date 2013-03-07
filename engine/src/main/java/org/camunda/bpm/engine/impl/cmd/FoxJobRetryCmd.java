ce0a10f53947d76ec925991ebd904ca07eda6de7
Daniel Meyer <daniel.meyer@camunda.com>
2013-03-07 13:52:25 +0100

    merge(master)

--- engine/src/main/java/org/camunda/bpm/engine/impl/cmd/FoxJobRetryCmd.java
+++ engine/src/main/java/org/camunda/bpm/engine/impl/cmd/FoxJobRetryCmd.java
@@ -2,6 +2,8 @@ package org.camunda.bpm.engine.impl.cmd;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.camunda.bpm.engine.ProcessEngineException;
 import org.camunda.bpm.engine.impl.bpmn.parser.FoxFailedJobParseListener;
@@ -26,6 +28,8 @@ import org.camunda.bpm.engine.impl.pvm.process.ActivityImpl;
 
 public class FoxJobRetryCmd implements Command<Object> {
 
+  private static final Logger log = Logger.getLogger(FoxJobRetryCmd.class.getName());
+
   protected String jobId;
   protected Throwable exception;
 
@@ -42,93 +46,67 @@ public class FoxJobRetryCmd implements Command<Object> {
     ActivityImpl activity = getCurrentActivity(commandContext, job);
     
     if (activity == null) {
+      log.log(Level.SEVERE, "Failure while executing " + FoxJobRetryCmd.class.getName() + " for job id '" + jobId + "'. Falling back to standard job retry strategy.");
       executeStandardStrategy(commandContext);
     } else {
-      executeCustomStrategy(commandContext, job, activity);
+      try {
+        executeCustomStrategy(commandContext, job, activity);
+      } catch (Exception e) {
+        log.log(Level.SEVERE, "Failure while executing " + FoxJobRetryCmd.class.getName() + " for job id '" + jobId + "'. Falling back to standard job retry strategy.", e);
+        executeStandardStrategy(commandContext);
+      }
     }
-//  else if (type.equals(ProcessEventJobHandler.TYPE)) {
-//  EventSubscriptionEntity eventSubscription = Context.getCommandContext()
-//                                                     .getEventSubscriptionManager()
-//                                                     .findEventSubscriptionbyId(job.getJobHandlerConfiguration());
-//  activity = eventSubscription.getActivity();
-//}
     
     return null;
   }
 
-  private void executeCustomStrategy(CommandContext commandContext, JobEntity job, ActivityImpl activity) {
+  private void executeCustomStrategy(CommandContext commandContext, JobEntity job, ActivityImpl activity) throws Exception {
     String failedJobRetryTimeCycle = (String) activity.getProperty(FoxFailedJobParseListener.FOX_FAILED_JOB_CONFIGURATION);
-    if (failedJobRetryTimeCycle != null) {
-      try {
-        DurationHelper durationHelper = new DurationHelper(failedJobRetryTimeCycle);
-        job.setLockExpirationTime(durationHelper.getDateAfter());
-        // TODO: why isn't the lockOwner set to null like in @DecrementJobRetriesCmd
-        // TODO: why revision == 2, isn't it used for optimistic locking?
-        if (job.getRevision() == 2) {
-          int times = durationHelper.getTimes();
-          if (times > 0) {
-            job.setRetries(durationHelper.getTimes());
-          } else {
-            throw new ProcessEngineException("Cannot parse the amount of retries.");
-          }
-        }
-        job.setRetries(job.getRetries() - 1);
-      } catch (Exception e) {
-        executeStandardStrategy(commandContext);
-        throw new ProcessEngineException("Due to parsing failure the default retry strategy has been chosen.", e);
-      }
-      if (exception != null) {
-        job.setExceptionMessage(exception.getMessage());
-        job.setExceptionStacktrace(getExceptionStacktrace());
-      }
-      JobExecutor jobExecutor = Context.getProcessEngineConfiguration().getJobExecutor();
-      MessageAddedNotification messageAddedNotification = new MessageAddedNotification(jobExecutor);
-      TransactionContext transactionContext = commandContext.getTransactionContext();
-      transactionContext.addTransactionListener(TransactionState.COMMITTED, messageAddedNotification);
-    } else {
-      executeStandardStrategy(commandContext);
+    
+    DurationHelper durationHelper = new DurationHelper(failedJobRetryTimeCycle);
+    job.setLockExpirationTime(durationHelper.getDateAfter());
+    
+    // check if this is jobs' first execution, then change default retries
+    if (job.getRevision() == 2) {
+      job.setRetries(durationHelper.getTimes());
     }
+    job.setRetries(job.getRetries() - 1);
+    
+    if (exception != null) {
+      job.setExceptionMessage(exception.getMessage());
+      job.setExceptionStacktrace(getExceptionStacktrace());
+    }
+    
+    JobExecutor jobExecutor = Context.getProcessEngineConfiguration().getJobExecutor();
+    MessageAddedNotification messageAddedNotification = new MessageAddedNotification(jobExecutor);
+    TransactionContext transactionContext = commandContext.getTransactionContext();
+    transactionContext.addTransactionListener(TransactionState.COMMITTED, messageAddedNotification);
   }
 
   private ActivityImpl getCurrentActivity(CommandContext commandContext, JobEntity job) {
     String type = job.getJobHandlerType();
     ActivityImpl activity = null;
-    if (type.equals(TimerExecuteNestedActivityJobHandler.TYPE)
-            || type.equals(TimerCatchIntermediateEventJobHandler.TYPE)) {
-      String activityId = job.getJobHandlerConfiguration();
+    
+    if (TimerExecuteNestedActivityJobHandler.TYPE.equals(type) ||
+        TimerCatchIntermediateEventJobHandler.TYPE.equals(type)) {
       ExecutionEntity execution = fetchExecutionEntity(job.getExecutionId());
-      if (execution == null) {
-        executeStandardStrategy(commandContext);
-        throw new ProcessEngineException("No execution found for key '" + job.getJobHandlerConfiguration() + "'");
+      if (execution != null) {
+        activity = execution.getProcessDefinition().findActivity(job.getJobHandlerConfiguration());
       }
-      activity = execution.getProcessDefinition().findActivity(activityId);
-      
-    } else if (type.equals(TimerStartEventJobHandler.TYPE)) {
+    } else if (TimerStartEventJobHandler.TYPE.equals(type)) {
       DeploymentCache deploymentCache = Context.getProcessEngineConfiguration().getDeploymentCache();
-
       ProcessDefinitionEntity processDefinition = deploymentCache.findDeployedLatestProcessDefinitionByKey(job.getJobHandlerConfiguration());
-      if (processDefinition == null) {
-        executeStandardStrategy(commandContext);
-        throw new ProcessEngineException("No process definition found for key '" + job.getJobHandlerConfiguration() + "'");
+      if (processDefinition != null) {
+        activity = processDefinition.getInitial();
       }
-
-      activity = processDefinition.getInitial();
-
-    } else if (type.equals(AsyncContinuationJobHandler.TYPE)) {
-      ExecutionEntity execution = this.fetchExecutionEntity(job.getExecutionId());
-      if (execution == null) {
-        executeStandardStrategy(commandContext);
-        throw new ProcessEngineException("No execution found for key '" + job.getJobHandlerConfiguration() + "'");
+    } else if (AsyncContinuationJobHandler.TYPE.equals(type)) {
+      ExecutionEntity execution = fetchExecutionEntity(job.getExecutionId());
+      if (execution != null) {
+        activity = execution.getActivity();
       }
-      activity = execution.getActivity();
-      
+    } else {
+      // nop, because activity type is not supported
     }
-//  else if (type.equals(ProcessEventJobHandler.TYPE)) {
-//  EventSubscriptionEntity eventSubscription = Context.getCommandContext()
-//                                                     .getEventSubscriptionManager()
-//                                                     .findEventSubscriptionbyId(job.getJobHandlerConfiguration());
-//  activity = eventSubscription.getActivity();
-//}
     
     return activity;
   }
