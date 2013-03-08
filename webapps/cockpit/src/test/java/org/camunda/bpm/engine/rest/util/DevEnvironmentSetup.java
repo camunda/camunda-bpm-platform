1c9da5d00c6378acb6575129f331082505ac7acf
Daniel Meyer <daniel.meyer@camunda.com>
2013-03-08 13:57:34 +0100

    fix(build) updating DevEnvironment Setup

--- webapps/cockpit/src/test/java/org/camunda/bpm/engine/rest/util/DevEnvironmentSetup.java
+++ webapps/cockpit/src/test/java/org/camunda/bpm/engine/rest/util/DevEnvironmentSetup.java
@@ -12,8 +12,8 @@ import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
 import org.camunda.bpm.engine.impl.persistence.StrongUuidGenerator;
 import org.camunda.bpm.engine.repository.ProcessDefinition;
-import org.camunda.bpm.engine.rest.impl.ProcessDefinitionServiceImpl;
-import org.camunda.bpm.engine.rest.impl.ProcessInstanceServiceImpl;
+import org.camunda.bpm.engine.rest.impl.ProcessDefinitionRestServiceImpl;
+import org.camunda.bpm.engine.rest.impl.ProcessInstanceRestServiceImpl;
 import org.camunda.bpm.engine.rest.impl.TaskRestServiceImpl;
 import org.camunda.bpm.engine.rest.mapper.EngineQueryDtoGetReader;
 import org.camunda.bpm.engine.rest.mapper.JacksonConfigurator;
@@ -78,8 +78,8 @@ public class DevEnvironmentSetup implements ProcessEngineProvider {
 
     server.setPort(8081);
 
-    server.getDeployment().getActualResourceClasses().add(ProcessDefinitionServiceImpl.class);
-    server.getDeployment().getActualResourceClasses().add(ProcessInstanceServiceImpl.class);
+    server.getDeployment().getActualResourceClasses().add(ProcessDefinitionRestServiceImpl.class);
+    server.getDeployment().getActualResourceClasses().add(ProcessInstanceRestServiceImpl.class);
     server.getDeployment().getActualResourceClasses().add(TaskRestServiceImpl.class);
 
     server.getDeployment().getActualProviderClasses().add(EngineQueryDtoGetReader.class);
