2b664210a5b607f17e5503196031519ac104b6a1
Daniel Meyer <daniel.meyer@camunda.com>
2013-03-09 09:36:20 +0100

    chore(javadoc) javadoc in EjbProcessApplication

--- engine/src/main/java/org/camunda/bpm/application/impl/EjbProcessApplication.java
+++ engine/src/main/java/org/camunda/bpm/application/impl/EjbProcessApplication.java
@@ -12,14 +12,87 @@
  */
 package org.camunda.bpm.application.impl;
 
+import java.util.concurrent.Callable;
+
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import org.camunda.bpm.application.AbstractProcessApplication;
+import org.camunda.bpm.application.ProcessApplicationExecutionException;
 import org.camunda.bpm.application.ProcessApplicationReference;
+import org.camunda.bpm.engine.ManagementService;
 import org.camunda.bpm.engine.ProcessEngineException;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
 
 /**
+ * <p>Common base class for writing EJB process applications.</p>
+ * 
+ * <p>An EJB Process Application exposes itself as a Session Bean Component inside the EJB container.
+ * This determines the invocation semantics when invoking code from the process application and the 
+ * nature of the {@link ProcessApplicationReference} held by the process engine.</p>
+ * 
+ * <h2>Usage</h2>
+ * <p>In order to add an EJB process application to your application, extend this class and configure 
+ * it as follows:</p>
+ * <pre>
+ * {@literal @}ProcessApplication("my process application")
+ * {@literal @}Singleton
+ * {@literal @}Startup 
+ * {@literal @}ConcurrencyManagement(ConcurrencyManagementType.BEAN) 
+ * {@literal @}TransactionAttribute(TransactionAttributeType.SUPPORTS)
+ * public class DefaultEjbProcessApplication extends EjbProcessApplication {
+ *   
+ *   {@literal @}PostConstruct
+ *   public void deploy() {
+ *     super.deploy();
+ *   }
+ * 
+ *   {@literal @}PreDestroy
+ *   public void undeploy() {
+ *     super.undeploy();
+ *   }
+ * }
+ * </pre>
+ * <p>(the same Metadata can of course be provided using an XML-based <code>ejb-jar.xml</code> 
+ * deployment descriptor</p> 
+ *  
+ * <h2>Invocation Semantics</h2>
+ * <p>This allows the process engine as well as other applications to invoke this EJB Process 
+ * Application and get EJB invocation semantics for the invocation. For example, if your
+ * process application provides a {@link JavaDelegate} implementation, the process engine
+ * will call the {@link EjbProcessApplication EjbProcessApplication's} 
+ * {@link #execute(java.util.concurrent.Callable)} Method and from that method invoke 
+ * the {@link JavaDelegate}. This makes sure that 
+ * <ul>
+ * <li>the call is intercepted by the EJB container and "enters" the process application legally.</li>
+ * <li>the {@link JavaDelegate} may take advantage of the {@link EjbProcessApplication}'s invocation context 
+ * and resolve resources form the component's Environment (such as a <code>java:comp/BeanManager</code>).
+ * </ul>
+ * </p>
+ * 
+ * 
+ * <pre>
+ *                        Big pile of EJB interceptors
+ *                                     |
+ *                                     |  +--------------------+    
+ *                                     |  |Process Application |
+ *                       invoke        v  |                    |
+ *      ProcessEngine ----------------OOOOO--> Java Delegate   |
+ *                                        |                    |
+ *                                        |                    |
+ *                                        +--------------------+
+ *                                        
+ * </pre>
+ * 
+ * <h2>Process Application Reference</h2>
+ * <p>When the process application registers with a process engine 
+ * (see {@link ManagementService#registerProcessApplication(String, ProcessApplicationReference)}, 
+ * the process application passes a reference to itself to the process engine. This reference allows the 
+ * process engine to reference the process application. The {@link EjbProcessApplication} takes advantage 
+ * of the Ejb Containers naming context and passes a reference containing the EJBProcessApplication's 
+ * Component Name to the process engine. Whenever the process engine needs access to process application, 
+ * the actual component instance is looked up and invoked.</p>
+ *
  * @author Daniel Meyer
  * 
  */
@@ -55,6 +128,14 @@ public class EjbProcessApplication extends AbstractProcessApplication {
     return eeModulePath;
   }
   
+  public <T> T execute(Callable<T> callable) throws ProcessApplicationExecutionException {
+    try {
+      return callable.call();
+    } catch(Exception e) {
+      throw new ProcessApplicationExecutionException(e);
+    }
+  }
+  
   protected String composeSessionObjectName() {
     
     try {
@@ -66,7 +147,9 @@ public class EjbProcessApplication extends AbstractProcessApplication {
 
       if (moduleName != null && !moduleName.equals(appName)) {
         // make sure that if an EAR carries multiple PAs, they are correctly
-        // identified by appName + moduleName
+        // identified by appName + moduleName        
+        // NOTE: this may be broken on IBM since there WARs or EJBs 
+        // are always wrapped inside an EAR deployment. 
         eeModulePath = appName + "/" + moduleName;
       } else {
         eeModulePath = appName;
