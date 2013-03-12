24e7cb8e981fd0ccc8fd754713092ea1d19c6f75
Daniel Meyer <daniel.meyer@camunda.com>
2013-03-12 12:53:30 +0100

    feat(welcome) added welcome page

--- distro/tomcat/assembly/src/start-camunda.bat
+++ distro/tomcat/assembly/src/start-camunda.bat
@@ -3,7 +3,8 @@
 echo "starting camunda BPM platform ${project.version} on Apache Tomcat ${tomcat.version}"
 
 cd server\apache-tomcat-${tomcat.version}\bin\
-startup.bat
+start startup.bat
 
-start http://localhost:8080/
+ping -n 5 localhost > NULL
+start http://localhost:8080/camunda-welcome/index.html
  
