4076d06cd5c5a66e8117f4240d0e6116e664c223
Stefan Hentschel <stefan.hentschel@camunda.com>
2013-03-28 11:41:24 +0100

    fix(distro): repair shell script and start server in foreground

--- distro/jbossas71/assembly/src/start-camunda.sh
+++ distro/jbossas71/assembly/src/start-camunda.sh
@@ -1,21 +1,18 @@
 #! /bin/sh
-echo "starting camunda BPM platform ${project.version} on JBoss Application Server ${version.jboss.as}"
+echo "starting camunda BPM platform on JBoss Application Server";
 
-if [ `which firefox` = "/usr/bin/firefox" ]; then
+if [ "`which firefox`" = "/usr/bin/firefox" ]; then
   BROWSER="/usr/bin/firefox";
-elif [ `which chromium-browser` = "/usr/bin/chromium-browser" ]; then
+elif [ "`which chromium-browser`" = "/usr/bin/chromium-browser" ]; then
   BROWSER="/usr/bin/chromium-browser";
 else
   BROWSER="empty";
 fi
 
-./server/jboss-as-${version.jboss.as}/bin/standalone.sh &
-sleep 5
-
-if [ $BROWSER = "empty" ]; then
-  echo "We are sorry... We tried all we could do but we couldn't locate your default browser...";
-  echo "If you want to see our default website please open your browser and insert this URL:";
-  echo "http://localhost:8080/";
+if [ "$BROWSER" = "empty" ]; then
+( sleep 15;  echo "We are sorry... We tried all we could do but we couldn't locate your default browser... \nIf you want to see our default website please open your browser and insert this URL:\nhttp://localhost:8080/camunda-welcome/index.html";) &
 else
-  $BROWSER "http://localhost:8080/camunda-welcome/index.html";
+  (sleep 5; $BROWSER "http://localhost:8080/camunda-welcome/index.html";) &
 fi
+
+/bin/sh ./server/jboss-as-${version.jboss.as}/bin/standalone.sh
