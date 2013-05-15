5d003608f4eec3852e4ff2a74f6a9ef0e6ae8c31
Andreas Drobisch <andreas.drobisch@camunda.com>
2013-05-15 14:45:25 +0200

    fix(tests): add missing APP_NAME method to all TestContainer classes

--- qa/integration-tests-engine/src/test/java-glassfish-servlet/org/camunda/bpm/integrationtest/util/TestContainer.java
+++ qa/integration-tests-engine/src/test/java-glassfish-servlet/org/camunda/bpm/integrationtest/util/TestContainer.java
@@ -26,9 +26,11 @@ public class TestContainer {
   public final static String PROCESS_APPLICATION_SERVICE_JNDI_NAME = BpmPlatform.PROCESS_APPLICATION_SERVICE_JNDI_NAME;
   
   public static void addContainerSpecificResources(WebArchive archive) {
-    
     archive.addClass(TestProcessApplication.class);
-    
+  }
+
+  public static String getAppName() {
+    return APP_NAME;
   }
   
   public static void addContainerSpecificResourcesForNonPa(WebArchive webArchive) {
