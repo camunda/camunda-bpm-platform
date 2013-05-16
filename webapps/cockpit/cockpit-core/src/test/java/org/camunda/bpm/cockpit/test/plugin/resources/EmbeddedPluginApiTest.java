3b7b0becb61da750be52b925d1b2b3caa0f5f968
romansmirnov <roman.smirnov@camunda.com>
2013-05-16 13:45:20 +0200

    fix(license): Add license header to classes

--- webapps/cockpit/cockpit-core/src/test/java/org/camunda/bpm/cockpit/test/plugin/resources/EmbeddedPluginApiTest.java
+++ webapps/cockpit/cockpit-core/src/test/java/org/camunda/bpm/cockpit/test/plugin/resources/EmbeddedPluginApiTest.java
@@ -1,3 +1,15 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.camunda.bpm.cockpit.test.plugin.resources;
 
 import static org.fest.assertions.Assertions.assertThat;
@@ -9,7 +21,6 @@ import javax.ws.rs.core.MediaType;
 import org.camunda.bpm.cockpit.plugin.spi.CockpitPlugin;
 import org.camunda.bpm.cockpit.test.sample.plugin.embedded.EmbeddedPlugin;
 import org.camunda.bpm.cockpit.test.util.AbstractCockpitCoreTest;
-import org.camunda.bpm.cockpit.test.util.DeploymentHelper;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
