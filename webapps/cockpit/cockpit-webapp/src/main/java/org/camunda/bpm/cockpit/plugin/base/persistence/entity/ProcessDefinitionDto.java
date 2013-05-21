06f0704045fc988db7d086ace037ed2751717b67
romansmirnov <roman.smirnov@camunda.com>
2013-05-21 12:25:50 +0200

    feat(plugin): Add table with deployed processes
    - The table contains a list of all deployed processes
    
    closes CAM-691

--- webapps/cockpit/cockpit-webapp/src/main/java/org/camunda/bpm/cockpit/plugin/base/persistence/entity/ProcessDefinitionDto.java
+++ webapps/cockpit/cockpit-webapp/src/main/java/org/camunda/bpm/cockpit/plugin/base/persistence/entity/ProcessDefinitionDto.java
@@ -17,10 +17,6 @@ public class ProcessDefinitionDto {
   private String id;
   private String name;
   private String key;
-  private String deploymentId;
-  private long suspensionState;
-  private long failedJobs;
-  private long version;
   
   public ProcessDefinitionDto() {}
 
@@ -48,36 +44,4 @@ public class ProcessDefinitionDto {
     this.key = key;
   }
 
-  public String getDeploymentId() {
-    return deploymentId;
-  }
-
-  public void setDeploymentId(String deploymentId) {
-    this.deploymentId = deploymentId;
-  }
-
-  public long getSuspensionState() {
-    return suspensionState;
-  }
-
-  public void setSuspensionState(long suspensionState) {
-    this.suspensionState = suspensionState;
-  }
-
-  public long getFailedJobs() {
-    return failedJobs;
-  }
-
-  public void setFailedJobs(long failedJobs) {
-    this.failedJobs = failedJobs;
-  }
-
-  public long getVersion() {
-    return version;
-  }
-
-  public void setVersion(long version) {
-    this.version = version;
-  }
-  
 }
