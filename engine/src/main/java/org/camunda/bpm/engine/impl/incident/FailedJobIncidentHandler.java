c7f43ede0a95f48369e60004431e2abdce5724a0
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2013-05-31 11:40:36 +0200

    docs(engine): add docs for incident handlers
    Related to CAM-798

--- engine/src/main/java/org/camunda/bpm/engine/impl/incident/FailedJobIncidentHandler.java
+++ engine/src/main/java/org/camunda/bpm/engine/impl/incident/FailedJobIncidentHandler.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,39 +18,53 @@ import org.camunda.bpm.engine.impl.context.Context;
 import org.camunda.bpm.engine.impl.persistence.entity.IncidentEntity;
 import org.camunda.bpm.engine.runtime.Incident;
 
+/**
+ * A incident handler that logs incidents of type <code>failedJob</code>
+ * as instances of {@link Incident} to the engine database.
+ *
+ * <p>
+ *
+ * This incident handler is active by default and must be disabled
+ * via {@link org.camunda.bpm.engine.ProcessEngineConfiguration#setCreateIncidentOnFailedJobEnabled(boolean)}.
+ *
+ * @see IncidentHandler
+ * 
+ * @author nico.rehwaldt
+ * @author roman.smirnow
+ */
 public class FailedJobIncidentHandler implements IncidentHandler {
 
   public final static String INCIDENT_HANDLER_TYPE = "failedJob";
-  
+
   public String getIncidentHandlerType() {
     return INCIDENT_HANDLER_TYPE;
   }
 
   public void handleIncident(String processDefinitionId, String activityId, String executionId, String jobId) {
-    
+
     if(executionId != null) {
       IncidentEntity newIncident = IncidentEntity.createAndInsertIncident(INCIDENT_HANDLER_TYPE, executionId, jobId);
       newIncident.createRecursiveIncidents();
-      
-    } else {      
+
+    } else {
       IncidentEntity.createAndInsertIncident(INCIDENT_HANDLER_TYPE, processDefinitionId, activityId, jobId);
-      
+
     }
-    
+
   }
-  
+
   public void resolveIncident(String processDefinitionId, String activityId, String executionId, String jobId) {
 
     List<Incident> incidents = Context
         .getCommandContext()
         .getIncidentManager()
         .findIncidentByConfiguration(jobId);
-    
+
     for (Incident currentIncident : incidents) {
       IncidentEntity incident = (IncidentEntity) currentIncident;
       incident.delete();
     }
-        
+
   }
 
 }
