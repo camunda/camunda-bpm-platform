1da8bf9a1716f525a3220759436ad9ed459b2f49
romansmirnov <roman.smirnov@camunda.com>
2013-06-05 17:24:39 +0200

    feat(circle): Add directive to create a circle
    - add directive to create circle which will be green or red.
    - the circle will green, if no incident were found and red if a incident
      was found
    - add some testcase to test circle directive
    
    related to CAM-706

--- webapps/cockpit/cockpit-webapp/src/main/webapp/plugin/base/app/pages/dashboardProcessDefinitionController.js
+++ webapps/cockpit/cockpit-webapp/src/main/webapp/plugin/base/app/pages/dashboardProcessDefinitionController.js
@@ -1,46 +1,118 @@
 ngDefine('cockpit.plugin.base.pages', function(module) {
 
   var Controller = function($scope, ProcessDefinitionResource) {
+    
+    var failedJobIncidentType = 'failedJob';
+    $scope.incidentTypes = [ failedJobIncidentType ];
 
-    ProcessDefinitionResource.queryStatistics(
-        {"failedJobs": true},
-        function(data){
-          $scope.statistics = getStatisticsResult(data);
-    });
-
-    var getStatisticsResult = function(statistics) {
+    ProcessDefinitionResource.queryStatistics({
+	      incidentsForType: failedJobIncidentType
+	    }).$then(function (data) {
+	      $scope.statistics = aggregateStatistics(data.resource);
+	    });
+	  
+    /**
+     * Returns an aggregated list over the statistics.
+     * 
+     * Summarize the statistics of process definitions 
+     * which have the same process definition key to
+     * one item. This item contains the latest process
+     * definition name (if there does not exist a name
+     * the process definition key will be used as the
+     * process definition name).
+     * 
+     * Furthermore, the number of instances, failed jobs
+     * and incidents will be aggregated over all versions
+     * of a process definition key. 
+     *  
+     */
+    var aggregateStatistics = function(statistics) {
+      // represents a map which contains to definition key
+      // the corresponding statistics. 
       var statisticsResult = [];
+      // the result to return
       var result = [];
 
+      // iterate over assigned statistics
       angular.forEach(statistics, function (currentStatistic) {
+        // get the statistic to the definition key of the current item
         var statistic = statisticsResult[currentStatistic.definition.key];
 
         if (!statistic) {
+          // if there does not exists a statistic to the definition key
+          // then create a copy of the current item (currentStatistic).
           statistic = angular.copy(currentStatistic);
+          
           if (!statistic.definition.name) {
+            // if there does not exist a name then set definition key
+            // as the name of the definition.
             statistic.definition.name = statistic.definition.key;
           }
+          
+          // put the statistic into the map of statistics
           statisticsResult[statistic.definition.key] = statistic;
 
+          // add the statistic to the result set
           result.push(statistic);
 
         } else {
+          // First save the values of instances, failedJobs and incidents. 
+          var currentInstances = statistic.instances;
+          var currentFailedJobs = statistics.failedJobs;
+          var currentIncidents = angular.copy(statistic.incidents);
+          
           if (currentStatistic.definition.version > statistic.definition.version) {
-            var currentInstances = statistic.instances;
-            var currentFailedJobs = statistic.failedJobs;
+            // if the version of the current statistic, then create copy from them.
             angular.copy(currentStatistic, statistic);
+            
             if (!statistic.definition.name) {
+              // if there does not exist a name then set definition key
+              // as the name of the definition.
               statistic.definition.name = statistic.definition.key;
             }
-            statistic.instances = currentInstances + currentStatistic.instances;
-            statistic.failedJobs = currentFailedJobs + currentStatistic.failedJobs;
           }
+          
+          statistic.instances = currentInstances + currentStatistic.instances;
+          statistic.failedJobs = currentFailedJobs + currentStatistic.failedJobs;
+          
+          angular.forEach(currentIncidents, function (incident) {
+            var incidentType = incident.incidentType;
+            var incidentCount = incident.incidentCount;
+            
+            var newIncident = true;
+            for(var i = 0; i < statistic.incidents.length; i++) {
+              var statisticIncident = statistic.incidents[i];
+              if (statisticIncident.incidentType == incidentType) {
+                statisticIncident.incidentCount = incidentCount + statisticIncident.incidentCount;
+                newIncident = false;
+              }
+            }
+            
+            if (!!newIncident) {
+              statistic.incidents.push(incident);
+            }
+            
+          });
+          
         }
       });
 
       return result;
     };
-
+    
+    $scope.hasFailedJobIncidents = function(statistics) {
+      var result = false;
+      angular.forEach(statistics.incidents, function (incident) {
+        if (incident.incidentType == failedJobIncidentType) {
+          if (incident.incidentCount > 0) {
+            result = true;
+          }
+          return;
+        }
+      });
+      return result;
+    };
+    
   };
 
   Controller.$inject = ["$scope", "ProcessDefinitionResource"];
