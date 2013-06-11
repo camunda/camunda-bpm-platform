8c5e6290b816c2b1726c673824ae56685c401e40
Daniel Meyer <daniel.meyer@camunda.com>
2013-06-11 11:10:28 +0200

    fix h2 history drop script

--- engine/src/main/resources/org/camunda/bpm/engine/db/drop/activiti.h2.drop.history.sql
+++ engine/src/main/resources/org/camunda/bpm/engine/db/drop/activiti.h2.drop.history.sql
@@ -1,8 +1,6 @@
 drop index ACT_IDX_HI_PRO_INST_END;
 drop index ACT_IDX_HI_PRO_I_BUSKEY;
 drop index ACT_IDX_HI_ACT_INST_START;
-drop index ACT_IDX_HI_ACT_INST_END;
-drop index ACT_IDX_HI_ACT_INST_COMP;
 drop index ACT_IDX_HI_DETAIL_PROC_INST;
 drop index ACT_IDX_HI_DETAIL_ACT_INST;
 drop index ACT_IDX_HI_DETAIL_TIME;
@@ -11,9 +9,6 @@ drop index ACT_IDX_HI_DETAIL_TASK_ID;
 drop index ACT_IDX_HI_PROCVAR_PROC_INST;
 drop index ACT_IDX_HI_PROCVAR_NAME_TYPE;
 drop index ACT_IDX_HI_ACT_INST_PROCINST;
-
-alter table ACT_HI_PROCINST
-    drop constraint ACT_UNIQ_HI_BUS_KEY;
     
 drop table ACT_HI_PROCINST if exists;
 drop table ACT_HI_ACTINST if exists;
