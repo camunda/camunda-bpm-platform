9c0760dd646c1e6ad49b3a236a76ae6d63079ca1
Daniel Meyer <daniel.meyer@camunda.com>
2013-06-17 12:04:17 +0200

    adjust mysql create/drop

--- engine/src/main/resources/org/camunda/bpm/engine/db/create/activiti.mysql.create.history.sql
+++ engine/src/main/resources/org/camunda/bpm/engine/db/create/activiti.mysql.create.history.sql
@@ -1,19 +1,15 @@
 create table ACT_HI_PROCINST (
     ID_ varchar(64) not null,
     PROC_INST_ID_ varchar(64) not null,
-    BUSINESS_KEY_ varchar(255),
     PROC_DEF_ID_ varchar(64) not null,
-    START_TIME_ datetime not null,
-    END_TIME_ datetime,
-    DURATION_ bigint,
+    TIMESTAMP_ datetime not null,
+    ACT_ID_ varchar(255),
+    EVENT_TYPE_ varchar(255) not null,
+    BUSINESS_KEY_ varchar(255),
     START_USER_ID_ varchar(255),
-    START_ACT_ID_ varchar(255),
-    END_ACT_ID_ varchar(255),
     SUPER_PROCESS_INSTANCE_ID_ varchar(64),
     DELETE_REASON_ varchar(4000),
-    primary key (ID_),
-    unique (PROC_INST_ID_),
-    unique ACT_UNIQ_HI_BUS_KEY (PROC_DEF_ID_, BUSINESS_KEY_)
+    primary key (ID_)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
 
 create table ACT_HI_ACTINST (
@@ -21,51 +17,36 @@ create table ACT_HI_ACTINST (
     PROC_DEF_ID_ varchar(64) not null,
     PROC_INST_ID_ varchar(64) not null,
     EXECUTION_ID_ varchar(64) not null,
+    TIMESTAMP_ datetime not null,
     ACT_ID_ varchar(255) not null,
+    ACT_NAME_ varchar(255),
+    ACT_TYPE_ varchar(255),
+    ACT_INST_ID_ varchar(255) not null,
+    PARENT_ACT_INST_ID_ varchar(255) not null,
+    EVENT_TYPE_ varchar(255) not null,
     TASK_ID_ varchar(64),
     CALL_PROC_INST_ID_ varchar(64),
-    ACT_NAME_ varchar(255),
-    ACT_TYPE_ varchar(255) not null,
     ASSIGNEE_ varchar(64),
-    START_TIME_ datetime not null,
-    END_TIME_ datetime,
-    DURATION_ bigint,
     primary key (ID_)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
 
 create table ACT_HI_TASKINST (
     ID_ varchar(64) not null,
+    TASK_ID_ varchar(64) not null,
     PROC_DEF_ID_ varchar(64),
     TASK_DEF_KEY_ varchar(255),
     PROC_INST_ID_ varchar(64),
     EXECUTION_ID_ varchar(64),
+    TIMESTAMP_ datetime not null,
     NAME_ varchar(255),
     PARENT_TASK_ID_ varchar(64),
     DESCRIPTION_ varchar(4000),
     OWNER_ varchar(255),
     ASSIGNEE_ varchar(255),
-    START_TIME_ datetime not null,
-    END_TIME_ datetime,
-    DURATION_ bigint,
+    EVENT_TYPE_ varchar(255) not null,
     DELETE_REASON_ varchar(4000),
     PRIORITY_ integer,
-    DUE_DATE_ datetime,
-    primary key (ID_)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
-
-create table ACT_HI_VARINST (
-    ID_ varchar(64) not null,
-    PROC_INST_ID_ varchar(64),
-    EXECUTION_ID_ varchar(64),
-    TASK_ID_ varchar(64),
-    NAME_ varchar(255) not null,
-    VAR_TYPE_ varchar(100),
-    REV_ integer,
-    BYTEARRAY_ID_ varchar(64),
-    DOUBLE_ double,
-    LONG_ bigint,
-    TEXT_ varchar(4000),
-    TEXT2_ varchar(4000),
+    DUE_DATE_ timestamp,
     primary key (ID_)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
 
@@ -74,12 +55,15 @@ create table ACT_HI_DETAIL (
     TYPE_ varchar(255) not null,
     PROC_INST_ID_ varchar(64),
     EXECUTION_ID_ varchar(64),
+    PROC_DEF_ID_ varchar(64),
+    TIMESTAMP_ datetime not null,
+    EVENT_TYPE_ varchar(64) not null,
     TASK_ID_ varchar(64),
     ACT_INST_ID_ varchar(64),
+    VAR_INST_ID_ varchar(64),
     NAME_ varchar(255) not null,
     VAR_TYPE_ varchar(255),
     REV_ integer,
-    TIME_ datetime not null,
     BYTEARRAY_ID_ varchar(64),
     DOUBLE_ double,
     LONG_ bigint,
@@ -115,16 +99,12 @@ create table ACT_HI_ATTACHMENT (
     primary key (ID_)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
 
-create index ACT_IDX_HI_PRO_INST_END on ACT_HI_PROCINST(END_TIME_);
+create index ACT_IDX_HI_PRO_INST_END on ACT_HI_PROCINST(TIMESTAMP_);
 create index ACT_IDX_HI_PRO_I_BUSKEY on ACT_HI_PROCINST(BUSINESS_KEY_);
-create index ACT_IDX_HI_ACT_INST_START on ACT_HI_ACTINST(START_TIME_);
-create index ACT_IDX_HI_ACT_INST_END on ACT_HI_ACTINST(END_TIME_);
-create index ACT_IDX_HI_ACT_INST_COMP on ACT_HI_ACTINST(EXECUTION_ID_, ACT_ID_, END_TIME_, ID_);
+create index ACT_IDX_HI_ACT_INST_START on ACT_HI_ACTINST(TIMESTAMP_);
 create index ACT_IDX_HI_DETAIL_PROC_INST on ACT_HI_DETAIL(PROC_INST_ID_);
 create index ACT_IDX_HI_DETAIL_ACT_INST on ACT_HI_DETAIL(ACT_INST_ID_);
-create index ACT_IDX_HI_DETAIL_TIME on ACT_HI_DETAIL(TIME_);
+create index ACT_IDX_HI_DETAIL_TIME on ACT_HI_DETAIL(TIMESTAMP_);
 create index ACT_IDX_HI_DETAIL_NAME on ACT_HI_DETAIL(NAME_);
 create index ACT_IDX_HI_DETAIL_TASK_ID on ACT_HI_DETAIL(TASK_ID_);
-create index ACT_IDX_HI_PROCVAR_PROC_INST on ACT_HI_VARINST(PROC_INST_ID_);
-create index ACT_IDX_HI_PROCVAR_NAME_TYPE on ACT_HI_VARINST(NAME_, VAR_TYPE_);
 create index ACT_IDX_HI_ACT_INST_PROCINST on ACT_HI_ACTINST(PROC_INST_ID_, ACT_ID_);
