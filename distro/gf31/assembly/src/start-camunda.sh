0bc491e34fec9e4a1124cd8c1c16b2960e233998
romansmirnov <roman.smirnov@camunda.com>
2013-07-16 15:35:21 +0200

    fix(start-script): Change .bat to .sh

--- distro/gf31/assembly/src/start-camunda.sh
+++ distro/gf31/assembly/src/start-camunda.sh
@@ -15,4 +15,4 @@ else
   (sleep 5; $BROWSER "http://localhost:8080/camunda-welcome/index.html";) &
 fi
 
-/bin/sh ./server/glassfish3/glassfish/bin/startserv.bat
+/bin/sh ./server/glassfish3/glassfish/bin/startserv.sh
