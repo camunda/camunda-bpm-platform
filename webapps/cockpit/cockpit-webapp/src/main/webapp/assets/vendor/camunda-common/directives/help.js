1dcf29f7e57cf758fbcb41016213672368fe21c2
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2013-07-18 16:58:58 +0200

    fix(webapps/help): fix help directive tooltip
    - trigger help tooltip manually to better deal with special
    circumstances
      (i.e. tooltips on menus and selects)

--- webapps/cockpit/cockpit-webapp/src/main/webapp/assets/vendor/camunda-common/directives/help.js
+++ webapps/cockpit/cockpit-webapp/src/main/webapp/assets/vendor/camunda-common/directives/help.js
@@ -11,7 +11,34 @@ ngDefine('camunda.common.directives', [ 'jquery' ], function(module, $) {
           p = scope.$eval(attrs.helpPlacement);
         }
 
-        $(element).tooltip({ title: help, placement: p });
+        var shown;
+
+        function show() {
+          var e = this;
+
+          // optimization to work correctly with menues
+          if ($(e).is('.open')) {
+            return;
+          }
+
+          var shown = true;
+          
+          setTimeout(function() {
+            if (shown) {
+              $(e).tooltip('show');
+            }
+          }, 200);
+        }
+
+        function hide() {
+          $(this).tooltip('hide');
+          shown = false;
+        }
+
+        $(element)
+          .tooltip({ title: help, placement: p, trigger: 'manual' })
+          .click(hide)
+          .hover(show, hide);
       }
     };
   };
