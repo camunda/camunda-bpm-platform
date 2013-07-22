33a6f52e0b5690c8eb390682afdded725c040e93
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2013-07-22 15:32:20 +0200

    chore(webapps/debouncer): rename debounce directive

--- webapps/cockpit/cockpit-webapp/src/main/webapp/assets/vendor/camunda-common/services/main.js
+++ webapps/cockpit/cockpit-webapp/src/main/webapp/assets/vendor/camunda-common/services/main.js
@@ -1,6 +1,6 @@
 ngDefine('camunda.common.services', [
   'module:camunda.common.services.authentication:./Authentication',
-  './debouncer',
+  'module:camunda.common.services.debounce:./debounce',
   'module:camunda.common.services.notifications:./Notifications',
   'module:camunda.common.services.resolver:./ResourceResolver',
   'module:camunda.common.services.uri:./uri',
