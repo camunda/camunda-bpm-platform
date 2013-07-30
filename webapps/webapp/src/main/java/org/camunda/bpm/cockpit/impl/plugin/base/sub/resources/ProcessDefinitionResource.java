aad78229f5f54570fbe48345cc5843bccfecfa17
romansmirnov <roman.smirnov@camunda.com>
2013-07-30 17:57:01 +0200

    feat(variables): Add variables to query
    - now it is possible to query after called process definitions which
      parent process instances (who called the process definitions) contains
      the passed variables with the assigned value
    
    related to CAM-910

--- webapps/webapp/src/main/java/org/camunda/bpm/cockpit/impl/plugin/base/sub/resources/ProcessDefinitionResource.java
+++ webapps/webapp/src/main/java/org/camunda/bpm/cockpit/impl/plugin/base/sub/resources/ProcessDefinitionResource.java
@@ -1,3 +1,15 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.camunda.bpm.cockpit.impl.plugin.base.sub.resources;
 
 import java.util.List;
@@ -14,6 +26,8 @@ import javax.ws.rs.core.UriInfo;
 import org.camunda.bpm.cockpit.impl.plugin.base.dto.ProcessDefinitionDto;
 import org.camunda.bpm.cockpit.impl.plugin.base.dto.query.ProcessDefinitionQueryDto;
 import org.camunda.bpm.cockpit.plugin.resource.AbstractPluginResource;
+import org.camunda.bpm.engine.impl.ProcessEngineImpl;
+import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
 
 public class ProcessDefinitionResource extends AbstractPluginResource {
 
@@ -38,8 +52,20 @@ public class ProcessDefinitionResource extends AbstractPluginResource {
   @Consumes(MediaType.APPLICATION_JSON)
   public List<ProcessDefinitionDto> queryCalledProcessDefinitions(ProcessDefinitionQueryDto queryParameter) {  
     queryParameter.setParentProcessDefinitionId(id);
+    injectEngineConfig(queryParameter);
     List<ProcessDefinitionDto> result = getQueryService().executeQuery("selectCalledProcessDefinitions", queryParameter);
+
     return result;
+  }
+  
+  private void injectEngineConfig(ProcessDefinitionQueryDto parameter) {
+
+    ProcessEngineConfigurationImpl processEngineConfiguration = ((ProcessEngineImpl) getProcessEngine()).getProcessEngineConfiguration();
+    if (processEngineConfiguration.getHistoryLevel() == 0) {
+      parameter.setHistoryEnabled(false);
+    }
+
+    parameter.initQueryVariableValues(processEngineConfiguration.getVariableTypes());
   }
   
 }
