dae078c9a760ce21b30502ac4ed63caccf163e11
Christian Lipphardt <christian.lipphardt@googlemail.com>
2013-07-31 14:19:31 +0200

    chore(webapps-it): add screenshot rule for selenium testing

--- qa/integration-tests-webapps/src/test/java/org/camunda/bpm/cockpit/test/DashboardIT.java
+++ qa/integration-tests-webapps/src/test/java/org/camunda/bpm/cockpit/test/DashboardIT.java
@@ -2,10 +2,13 @@ package org.camunda.bpm.cockpit.test;
 
 
 import org.camunda.bpm.TestProperties;
+import org.camunda.bpm.util.SeleniumScreenshotRule;
 import org.camunda.bpm.util.TestUtil;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Rule;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -16,18 +19,20 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DashboardIT {
 
-  protected WebDriver driver;
+  protected static WebDriver driver = new FirefoxDriver();
   protected String appUrl;
 
   protected TestProperties testProperties;
 
   private TestUtil testUtil;
 
+  @Rule
+  public SeleniumScreenshotRule screenshotRule = new SeleniumScreenshotRule(driver);
+
   @Before
   public void before() throws Exception {
     testProperties = new TestProperties(48080);
     appUrl = testProperties.getApplicationPath("/camunda/app/cockpit");
-    driver = new FirefoxDriver();
 
     testUtil = new TestUtil(testProperties);
 
@@ -58,7 +63,10 @@ public class DashboardIT {
   public void after() {
     testUtil.deleteUser("admin");
     testUtil.destroy();
+  }
 
+  @AfterClass
+  public static void cleanup() {
     driver.close();
   }
 }
