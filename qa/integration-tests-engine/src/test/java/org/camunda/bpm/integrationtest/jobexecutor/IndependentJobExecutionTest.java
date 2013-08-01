cc9ebd131b9746779b39aecd7070a13c22c223c9
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2013-08-01 16:34:36 +0200

    chore(integration-test): rename IndependentJobExecutorTest resource

--- qa/integration-tests-engine/src/test/java/org/camunda/bpm/integrationtest/jobexecutor/IndependentJobExecutionTest.java
+++ qa/integration-tests-engine/src/test/java/org/camunda/bpm/integrationtest/jobexecutor/IndependentJobExecutionTest.java
@@ -42,16 +42,16 @@ public class IndependentJobExecutionTest extends AbstractFoxPlatformIntegrationT
   @Deployment(order = 0, name="pa1")
   public static WebArchive processArchive1() {    
     
-    return initWebArchiveDeployment("pa1.war", "org/camunda/bpm/integrationtest/jobexecutor/pa1.xml")
-        .addAsResource("org/camunda/bpm/integrationtest/jobexecutor/archive1process.bpmn20.xml");
+    return initWebArchiveDeployment("pa1.war", "org/camunda/bpm/integrationtest/jobexecutor/IndependentJobExecutionTest.pa1.xml")
+        .addAsResource("org/camunda/bpm/integrationtest/jobexecutor/IndependentJobExecutionTest.process1.bpmn20.xml");
             
   }
   
   @Deployment(order = 1, name="pa2")
   public static WebArchive processArchive2() {    
     
-    return initWebArchiveDeployment("pa2.war", "org/camunda/bpm/integrationtest/jobexecutor/pa2.xml")
-        .addAsResource("org/camunda/bpm/integrationtest/jobexecutor/archive2process.bpmn20.xml");
+    return initWebArchiveDeployment("pa2.war", "org/camunda/bpm/integrationtest/jobexecutor/IndependentJobExecutionTest.pa2.xml")
+        .addAsResource("org/camunda/bpm/integrationtest/jobexecutor/IndependentJobExecutionTest.process2.bpmn20.xml");
   }
   
   @OperateOnDeployment("pa1")
