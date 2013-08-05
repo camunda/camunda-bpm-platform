8f01c4f42b4523a60468293cc24f98c91e4a94b0
Christian Lipphardt <christian.lipphardt@googlemail.com>
2013-08-05 12:56:43 +0200

    chore(engine): remove business key db unique index/constraint
    related to CAM-849

--- engine/src/main/resources/org/camunda/bpm/engine/db/create/activiti.h2.create.engine.sql
+++ engine/src/main/resources/org/camunda/bpm/engine/db/create/activiti.h2.create.engine.sql
@@ -209,11 +209,7 @@ alter table ACT_RU_EXECUTION
 alter table ACT_RU_EXECUTION
     add constraint ACT_FK_EXE_PROCDEF 
     foreign key (PROC_DEF_ID_) 
-    references ACT_RE_PROCDEF (ID_);    
-    
-alter table ACT_RU_EXECUTION
-    add constraint ACT_UNIQ_RU_BUS_KEY
-    unique(PROC_DEF_ID_, BUSINESS_KEY_);
+    references ACT_RE_PROCDEF (ID_);
     
 alter table ACT_RU_IDENTITYLINK
     add constraint ACT_FK_TSKASS_TASK
