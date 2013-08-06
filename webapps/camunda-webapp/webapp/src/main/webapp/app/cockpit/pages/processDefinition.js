c8152718e0eb517d350557256a48e3c334328ea6
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2013-08-06 17:33:38 +0200

    chore(merge): merge from upstream

--- webapps/camunda-webapp/webapp/src/main/webapp/app/cockpit/pages/processDefinition.js
+++ webapps/camunda-webapp/webapp/src/main/webapp/app/cockpit/pages/processDefinition.js
@@ -1,75 +1,171 @@
-ngDefine('cockpit.pages', [ 'angular' ], function(module, angular) {
+ngDefine('cockpit.pages.processDefinition', [ 
+  'angular',
+  'module:dataDepend:angular-data-depend'
+], function(module, angular) {
 
-  var Controller = function($scope, $rootScope, $location, Notifications, ProcessDefinitionResource, ProcessInstanceResource, Views, Transform, processDefinition) {
+  var Controller = [
+    '$scope', '$rootScope', 'search', '$q', 'Notifications', 'ProcessDefinitionResource', 'ProcessInstanceResource', 'Views', 'Transform', 'dataDependFactory', 'processDefinition',
+    function($scope, $rootScope, search, $q, Notifications, ProcessDefinitionResource, ProcessInstanceResource, Views, Transform, dataDependFactory, processDefinition) {
 
-    $scope.processDefinition = processDefinition;
-    $scope.currentVersion = processDefinition.version;
-
-    var parentProcessDefinitionId = $location.search().parentProcessDefinitionId || null;
-
-    $scope.processDefinitions;
-
-    $scope.selection = {};
+    var processData = $scope.processData = dataDependFactory.create($scope);
 
 
-    $scope.$on('$routeChangeStart', function () {
-      $rootScope.clearBreadcrumbs();
-    });
-
-    // add process definition breadcrumb
-    if (!parentProcessDefinitionId) {
-      $rootScope.addBreadcrumb({'type': 'processDefinition', 'processDefinition': processDefinition});  
-    } else {
-      ProcessDefinitionResource.get({ id : parentProcessDefinitionId }).$then(function(response) {
-        $rootScope.addBreadcrumb({'type': 'processDefinition', 'processDefinition': response.data});  
-        $rootScope.addBreadcrumb({'type': 'processDefinition', 'processDefinition': processDefinition});  
-      });
-    }
-  
-    $scope.processDefinitionVars = { read: [ 'processDefinition', 'selection' ] };
-    $scope.processDefinitionViews = Views.getProviders({ component: 'cockpit.processDefinition.view' });
-
-    $scope.selectView = function(view) {
-      $scope.selectedView = view;
-      $location.search('detailsTab', view.id);
-    };
-
-    function setDefaultTab(tabs) {
-      var selectedTabId = $location.search().detailsTab;
-
-      if (!tabs.length) {
-        return;
-      }
-
-      if (selectedTabId) {
-        var provider = Views.getProvider({ component: 'cockpit.processDefinition.view', id: selectedTabId });
-        if (provider && tabs.indexOf(provider) != -1) {
-          $scope.selectedView = provider;
-          return;
-        }
-      }
-
-      $location.search('detailsTab', null);
-      $scope.selectedView = tabs[0];
-    }
-
-    setDefaultTab($scope.processDefinitionViews);
-
-    ProcessInstanceResource.count({ processDefinitionKey : processDefinition.key }).$then(function(response) {
-      $scope.processDefinitionTotalCount = response.data;
-    });
-
-    ProcessDefinitionResource.query({ 'key' : processDefinition.key, 'sortBy': 'version', 'sortOrder': 'asc' }).$then(function(response) {
-      $scope.processDefinitions = response.resource;
-    });
-
-    ProcessInstanceResource.count({ processDefinitionId : processDefinition.id }).$then(function(response) {
-      $scope.processDefinitionCurrentVersionCount = response.data;
-    });
+    // utilities ///////////////////////
     
-    ProcessDefinitionResource.getBpmn20Xml({ id : processDefinition.id}).$then(function(response) {
-      $scope.semantic = Transform.transformBpmn20Xml(response.data.bpmn20Xml);
-      $scope.processDefinition.bpmnElements = getBpmnElements(processDefinition, $scope.semantic);
+    var internalUpdateLocation;
+
+    $scope.$on('$routeChanged', function() {
+      processData.set('filter', parseFilterFromUri());
+    });
+
+    function collect(elements, fn) {
+      var result = [];
+
+      angular.forEach(elements, function(e) {
+        try {
+          var c = fn(e);
+
+          if (c !== undefined) {
+            result.push(c);
+          }
+        } catch (e) {
+          ; // safe collect -> error skips element
+        }
+      });
+
+      return result;
+    }
+
+    var currentFilter = null;
+
+    function parseFilterFromUri() {
+
+      var params = search();
+
+      function parseArray(str) {
+        if (!str) {
+          return [];
+        }
+
+        return str.split(/,/);
+      }
+
+      function parseVariables(vars) {
+        return collect(vars, Variables.parse);
+      }
+
+      currentFilter = {
+        activityIds: parseArray(params.activity), 
+        parentProcessDefinitionId: params.parentProcessDefinitionId,
+        businessKey: params.businessKey, 
+        variables: parseVariables(parseArray(params.variables)),
+        page: parseInt(params.page) || undefined
+      };
+
+      return currentFilter;
+    }
+
+    function serializeFilterToUri(filter) {
+      var businessKey = filter.businessKey,
+          activityIds = filter.activityIds,
+          parentProcessDefinitionId = filter.parentProcessDefinitionId,
+          variables = filter.variables;
+
+      function nonEmpty(array) {
+        return array && array.length;
+      }
+
+      search.updateSilently({
+        businessKey: businessKey || null,
+        activityIds: nonEmpty(activityIds) ? activityIds.join(',') : null,
+        variables: nonEmpty(variables) ? collect(variables, Variables.toString).join(',') : null,
+        parentProcessDefinitionId: parentProcessDefinitionId || null
+      });
+
+      currentFilter = filter;
+    }
+
+    // end utilities ///////////////////////
+
+
+    // begin data definition //////////////////////
+
+    processData.set('processDefinition', processDefinition);
+
+    processData.set('filter', parseFilterFromUri());
+
+    processData.set('parentId', [ 'filter', function(filter) { return filter.parentProcessDefinitionId; } ]);
+
+    processData.set('parent', [ 'parentId', function(parentId) {
+      if (!parentId) {
+        return null;
+      } else {
+        return ProcessDefinitionResource.get({ id : parentId }).$promise;
+      }
+    }]);
+
+    processData.set('instances.all', [ 'processDefinition', function(definition) {
+      return ProcessInstanceResource.count({ processDefinitionKey : definition.key }).$promise;
+    }]);
+
+    processData.set('instances.current', [ 'processDefinition', function(definition) {
+      return ProcessInstanceResource.count({ processDefinitionId : definition.id }).$promise;
+    }]);
+
+    processData.set('semantic', [ 'processDefinition', function(definition) {
+      return ProcessDefinitionResource.getBpmn20Xml({ id : definition.id}).$promise.then(function(value) {
+        return Transform.transformBpmn20Xml(value.bpmn20Xml);
+      });
+    }]);
+
+    processData.set('bpmnElements', [ 'processDefinition', 'semantic', function(definition, semantic) {
+      return getBpmnElements(definition, semantic);
+    }]);
+
+    processData.set('allProcessDefinitions', [ 'processDefinition', function(definition) {
+      return ProcessDefinitionResource.query({ 'key' : definition.key, 'sortBy': 'version', 'sortOrder': 'asc' }).$promise;
+    }]);
+
+    // end data definition /////////////////////////
+
+
+    // begin data usage ////////////////////////////
+
+    $scope.breadcrumbData = processData.get([ 'processDefinition', 'parent' ], function(definition, parent) {
+      $rootScope.clearBreadcrumbs();
+
+      if (parent) {
+        $rootScope.addBreadcrumb({ type: 'processDefinition', processDefinition: parent });
+      }
+
+      $rootScope.addBreadcrumb({ type: 'processDefinition', processDefinition: definition });  
+    });
+
+    $scope.instanceStatistics = processData.get([ 'instances.all', 'instances.current' ], function(allCount, currentCount) {
+      $scope.instanceStatistics.all = allCount;
+      $scope.instanceStatistics.current = currentCount;
+    });
+
+    // processDiagram
+    
+    $scope.processDiagramData = processData.get([ 'semantic', 'processDefinition', 'bpmnElements', function (semantic, processDefinition, bpmnElements) {
+      var diagram = $scope.processDiagram = $scope.processDiagram || {};
+
+      angular.extend(diagram, {
+        semantic: semantic,
+        processDefinition: processDefinition,
+        bpmnElements: bpmnElements
+      });
+
+      return diagram;
+    }]);
+
+    processData.get('filter', function(filter) {
+      if (filter != currentFilter) {
+        console.log('filter changed -> ', filter);
+        
+        serializeFilterToUri(filter);
+      }
     });
 
     function getBpmnElements (processDefinition, semantic) {
@@ -103,7 +199,6 @@ ngDefine('cockpit.pages', [ 'angular' ], function(module, angular) {
           getBpmnElementsHelper(baseElement, result);
         });
       }
-
     }
     
     ProcessDefinitionResource.queryActivityStatistics({ id : processDefinition.id, incidents: true }).$then(function(response) {
@@ -121,10 +216,357 @@ ngDefine('cockpit.pages', [ 'angular' ], function(module, angular) {
       });
       
     });
-    
-  };
 
-  Controller.$inject = [ '$scope', '$rootScope', '$location', 'Notifications', 'ProcessDefinitionResource', 'ProcessInstanceResource', 'Views', 'Transform', 'processDefinition' ];
+    $scope.processDefinition = processDefinition;
+
+    $scope.selection = {};
+
+    $scope.processDefinitionVars = { read: [ 'processDefinition', 'selection', 'processData' ] };
+    $scope.processDefinitionViews = Views.getProviders({ component: 'cockpit.processDefinition.view' });
+
+    $scope.selectView = function(view) {
+      $scope.selectedView = view;
+
+      search.updateSilently({
+        detailsTab: view.id
+      });
+    };
+
+    function setDefaultTab(tabs) {
+      var selectedTabId = search().detailsTab;
+
+      if (!tabs.length) {
+        return;
+      }
+
+      if (selectedTabId) {
+        var provider = Views.getProvider({ component: 'cockpit.processDefinition.view', id: selectedTabId });
+        if (provider && tabs.indexOf(provider) != -1) {
+          $scope.selectedView = provider;
+          return;
+        }
+      }
+
+      search.updateSilently({
+        detailsTab: null
+      });
+
+      $scope.selectedView = tabs[0];
+    }
+
+    setDefaultTab($scope.processDefinitionViews);
+  }];
+  
+  var ProcessDefinitionFilterController = [ '$scope', 'debounce', function($scope, debounce) {
+
+    var processData = $scope.processData,
+        filterData,
+        cachedFilter;
+
+    function createRefs(elements) {
+      var result = [];
+
+      angular.forEach(elements, function(e, i) {
+        result.push({
+          value: e
+        });
+      });
+
+      return result;
+    }
+
+    processData.set('filterData', [ 'processDefinition', 'allProcessDefinitions', 'filter', 'parent', function(definition, allDefinitions, filter, parent) {
+
+      if (!filterData || filterData.filter != filter) {
+        return {
+          definition: definition,
+          allDefinitions: allDefinitions,
+          businessKey: filter.businessKey ? { value: filter.businessKey } : null,
+          parent: parent, 
+          filter: filter,
+          variables: createRefs(filter.variables),
+          activityIds: filter.activityIds
+        };
+      } else {
+        return filterData;
+      }
+    }]);
+
+    processData.get([ 'filterData' ], function(_filterData) {
+      $scope.filterData = filterData = _filterData;
+    });
+
+    $scope.operators = Variables.operators;
+
+    $scope.filterChanged = debounce(function() {
+
+      if ($scope.filterForm.$invalid) {
+        return;
+      }
+
+      var variables = filterData.variables,
+          activityIds = filterData.activityIds,
+          parent = filterData.parent,
+          businessKey = filterData.businessKey,
+          newFilterVariables = [], 
+          newFilter = {};
+
+      // business key
+      if (businessKey) {
+        newFilter.businessKey = businessKey.value;
+      }
+
+      // variables
+      angular.forEach(variables, function(v) {
+        if (v.value) {
+          newFilterVariables.push(v.value);
+        }
+      });
+
+      if (newFilterVariables.length) {
+        newFilter.variables = newFilterVariables;
+      }
+
+      // parentId
+      if (parent) {
+        newFilter.parentProcessDefinitionId = parent.id;
+      }
+
+      // activityIds
+      newFilter.activityIds = activityIds;
+
+      // update cached filter
+      filterData.filter = newFilter;
+
+      processData.set('filter', newFilter);
+    }, 2000);
+
+    $scope.toggleVariableFilterHelp = function() {
+      $scope.showVariableFilterHelp = !$scope.showVariableFilterHelp;
+    }
+    
+    $scope.addVariableFilter = function() {
+      filterData.variables.push({});
+    };
+
+    $scope.addBusinessKeyFilter = function() {
+      $scope.filterData.businessKey = { };
+    };
+
+    $scope.removeBusinessKeyFilter = function() {
+      filterData.businessKey = null;
+      $scope.filterChanged();
+    };
+
+    $scope.removeParentFilter = function() {
+      filterData.parent = null;
+      $scope.filterChanged();
+    };
+
+    $scope.removeVariableFilter = function(variable) {
+      var variables = filterData.variables,
+          idx = variables.indexOf(variable);
+
+      if (idx !== -1) {
+        variables.splice(idx, 1);
+      }
+
+      $scope.filterChanged();
+    };
+  }];
+
+
+  function ProcessVariableFilter() {
+
+    return {
+
+      require: 'ngModel',
+      link: function (scope, element, attrs, ngModel) {
+
+        function parseText(text) {
+          var variable;
+
+          try {
+            variable = Variables.parse(text);
+          } catch (e) {
+            ; // ok, failed to parse variable
+          }
+
+          ngModel.$setValidity('processVariableFilter', !!variable);
+          return variable;
+        }
+
+        ngModel.$parsers.push(parseText);
+        ngModel.$formatters.push(Variables.toString);
+      }
+    }
+  }
+
+  var Variables = (function() {
+
+    // variable specific stuff //////////////
+    
+    function reverse(hash) {
+      var result = {};
+
+      for (var key in hash) {
+        result[hash[key]] = key;
+      }
+
+      return result;
+    }
+
+    function keys(hash) {
+      var keys = [];
+
+      for (var key in hash) {
+        keys.push(key);
+      }
+
+      return keys;
+    }
+
+    var OPS = {
+      eq: '=',
+      neq: '!=',
+      gt : '>',
+      gteq : '>=',
+      lt : '<',
+      lteq : '<=',
+      like: 'like'
+    };
+
+    var SYM_TO_OPS = reverse(OPS);
+    
+    function operatorName(op) {
+      return OPS[op];
+    }
+
+    var PATTERN = new RegExp('^(\\S+)\\s(' + keys(SYM_TO_OPS).join('|') + ')\\s(.+)$');
+
+    /**
+     * Tries to guess the type of the input string
+     * and returns the appropriate representation 
+     * in the guessed type.
+     *
+     * @param value {string}
+     * @return value {string|boolean|number} the interpolated value
+     */
+    function typed(value) {
+
+      // is a string ( "asdf" )
+      if (/^".*"\s*$/.test(value)) {
+        return value.substring(1, value.length - 1);
+      }
+
+      if ((parseFloat(value) + '') === value) {
+        return parseFloat(value);
+      }
+
+      if (value === 'true' || value === 'false') {
+        return value === 'true';
+      }
+
+      throw new Error('Cannot infer type of value ' + value);
+    }
+
+    function typedString(value) {
+
+      if (!value) {
+        return value;
+      }
+
+      if (typeof value === 'string') {
+        return '"' + value + '"';
+      }
+
+      if (typeof value === 'boolean') {
+        return value ? 'true' : 'false';
+      }
+
+      if (typeof value === 'number') {
+        return value;
+      }
+
+
+      throw new Error('Cannot infer type of value ' + value);
+    }
+
+    /**
+     * Public API of Variables utility
+     */
+    return {
+
+      /**
+       * Parse a string into a variableFilter { name: ..., operator: ..., value: ... }
+       * @param  {string} str the string to parse
+       * @return {object}     the parsed variableFilter object
+       */
+      parse: function(str) {
+
+        var match = PATTERN.exec(str),
+            value;
+
+        if (!match) {
+          throw new Error('Invalid variable syntax: ' + str);
+        }
+
+        value = typed(match[3]);
+
+        return {
+          name: match[1],
+          operator: SYM_TO_OPS[match[2]],
+          value: value
+        };
+      },
+
+      toString: function(variable) {
+        if (!variable) {
+          return '';
+        }
+
+        return variable.name + ' ' + operatorName(variable.operator) + ' ' + typedString(variable.value);
+      },
+
+      operators: keys(SYM_TO_OPS)
+    };
+  })();
+
+  var SearchFactory = [ '$location', '$rootScope', function($location, $rootScope) {
+
+    var silent = false;
+
+    $rootScope.$on('$routeUpdate', function(e, lastRoute) {
+      if (silent) {
+        console.log('silenced $routeUpdate');
+        silent = false;
+      } else {
+        $rootScope.$broadcast('$routeChanged', lastRoute);
+      }
+    });
+
+    $rootScope.$on('$routeChangeSuccess', function(e, lastRoute) {
+      silent = false;
+    });
+
+    var search = function() {
+      var args = Array.prototype.slice(arguments);
+
+      return $location.search.apply($location, arguments);
+    }
+
+    search.updateSilently = function(params) {
+      angular.forEach(params, function(value, key) {
+        $location.search(key, value);
+      });
+
+      silent = true;
+    };
+
+    return search;
+  }];
+
+  module.factory('search', SearchFactory);
 
   var RouteConfig = [ '$routeProvider', function($routeProvider) {
     $routeProvider.when('/process-definition/:processDefinitionId', {
@@ -146,5 +588,7 @@ ngDefine('cockpit.pages', [ 'angular' ], function(module, angular) {
   }];
 
   module
+    .controller('ProcessDefinitionFilterController', ProcessDefinitionFilterController)
+    .directive('processVariableFilter', ProcessVariableFilter)
     .config(RouteConfig);
 });
--- webapps/camunda-webapp/webapp/src/main/webapp/plugin/base/app/views/processDefinition/processInstanceTable.js
+++ webapps/camunda-webapp/webapp/src/main/webapp/plugin/base/app/views/processDefinition/processInstanceTable.js
@@ -1,98 +1,44 @@
 ngDefine('cockpit.plugin.base.views', function(module) {
 
-  function reverse(hash) {
-    var result = {};
+  var Controller = [ '$scope', 'search', 'debounce', 'PluginProcessInstanceResource', 'Uri', 'ProcessInstanceResource',
+      function ($scope, search, debounce, PluginProcessInstanceResource, Uri, ProcessInstanceResource) {
 
-    for (var key in hash) {
-      result[hash[key]] = key;
-    }
+    var processData = $scope.processData;
 
-    return result;
-  }
+    var processDefinition = $scope.processDefinition;
+    
+    var DEFAULT_PAGES = { size: 2, total: 0, current: 1 };
 
-  function keys(hash) {
-    var keys = [];
+    var pages = $scope.pages = angular.copy(DEFAULT_PAGES);
 
-    for (var key in hash) {
-      keys.push(key);
-    }
+    var filter = null;
 
-    return keys;
-  }
-
-  var OPS = {
-    eq: '=',
-    neq: '!=',
-    gt : '>',
-    gteq : '>=',
-    lt : '<',
-    lteq : '<=',
-    like: 'like'
-  };
-
-  var SYM_TO_OPS = reverse(OPS);
-
-  var Controller = function ($scope, $routeParams, $location, $http, debounce, PluginProcessInstanceResource, Uri, ProcessInstanceResource) {
-
-    // input processDefinition, selection
-
-    var processDefinitionId = $scope.processDefinition.id;
-    var parentProcessDefinitionId = $location.search().parentProcessDefinitionId || null;
-    var activityIds = null;
-    var alreadyUpdated = false;
-
-    var pages = $scope.pages = { size: 50, total: 0 };
-
-    var filter = $scope.filter = {
-      active: false,
-      search: {},
-      variables: [],
-    };
-
-    $scope.$watch(function() { return $location.search().page; }, function(newValue) {
-      pages.current = parseInt(newValue) || 1;
-    });
-
-    $scope.$watch('pages.current', function(newValue) {
-      var currentPage = newValue || 1;
-      var search = $location.search().page;
-
-      if (search || currentPage !== 1) {
-        $location.search('page', currentPage);
-        updateView(currentPage);
-      }
-      
-    });
-
-    $scope.$watch(function() {return $location.search().bpmnElements; }, function (newValue) {
-      if (!newValue) {
-        activityIds = [];
-      } else if (angular.isString(newValue)) {
-        activityIds = newValue.split(',');
-      } else if (angular.isArray(newValue)) {
-        activityIds = newValue;
+    $scope.$watch('pages.current', function(newValue, oldValue) {
+      if (newValue == oldValue) {
+        return;
       }
 
-      updateView();
+      search('page', !newValue || newValue == 1 ? null : newValue);
     });
 
-    function deferredUpdateView(delay) {
-      return debounce(updateView, delay);
-    };
+    processData.get('filter', function(newFilter) {
+      pages.current = newFilter.page || 1;
 
-    $scope.$watch('filter.search.businessKey', deferredUpdateView(500));
-    $scope.$watch('filter.search.variables', deferredUpdateView(1000));
+      updateView(newFilter);
+    });
 
-    function updateView() {
+    function updateView(newFilter) {
+
+      filter = angular.copy(newFilter);
+
+      delete filter.page;
 
       var page = pages.current,
           count = pages.size,
           firstResult = (page - 1) * count;
 
       var defaultParams = {
-        processDefinitionId: processDefinitionId,
-        parentProcessDefinitionId: parentProcessDefinitionId,
-        activityIdIn: activityIds
+        processDefinitionId: processDefinition.id
       };
 
       var pagingParams = {
@@ -102,22 +48,17 @@ ngDefine('cockpit.plugin.base.views', function(module) {
         sortOrder: 'asc'
       };
 
-      var clear = function(obj) {
-        for (var key in obj) {
-          if (!obj[key]) {
-            delete obj[key];
-          }
-        }
+      var countParams = angular.extend({}, filter, defaultParams);
 
-        return obj;
-      }
+      // fix missmatch -> activityIds -> activityIdIn
+      countParams.activityIdIn = countParams.activityIds;
+      delete countParams.activityIds;
 
-      var countParams = angular.extend({}, clear(filter.search || {}), defaultParams);
-      var params = angular.extend(countParams, pagingParams);
+      var params = angular.extend({}, countParams, pagingParams);
 
       $scope.processInstances = null;
 
-      PluginProcessInstanceResource.query(params).$then(function(data) {
+      PluginProcessInstanceResource.query(pagingParams, params).$then(function(data) {
         $scope.processInstances = data.resource;
       });
 
@@ -125,151 +66,7 @@ ngDefine('cockpit.plugin.base.views', function(module) {
         pages.total = Math.ceil(data.data.count / pages.size);
       });
     };
-
-    $scope.toggleFilter = function() {
-      filter.open = !filter.open;
-    };
-
-    $scope.applyFilter = function() {
-      filter.active = true;
-    };
-
-    $scope.clearFilter = function() {
-      filter.active = false;
-      filter.search = null;
-    };
-
-    $scope.addVariable = function() {
-      filter.variables.push({});
-    };
-
-    $scope.removeVariable = function(variable) {
-      var variables = filter.variables,
-          idx = variables.indexOf(variable);
-
-      if (idx !== -1) {
-        variables.splice(idx, 1);
-      }
-
-      $scope.variablesChanged();
-    };
-
-    $scope.operators = keys(SYM_TO_OPS).join(', ');
-    
-    $scope.variablesChanged = debounce(function() {
-      var variables = filter.variables,
-          filterVars = [];
-
-      angular.forEach(variables, function(v) {
-        if (v.input) {
-          filterVars.push(v.input);
-        }
-      });
-
-      $scope.filter.search.variables = filterVars;
-    }, 500);
-  };
-
-  Controller.$inject = [ '$scope', '$routeParams', '$location', '$http', 'debounce', 'PluginProcessInstanceResource', 'Uri', 'ProcessInstanceResource' ];
-
-  module.directive('processVariableFilter', function() {
-
-    return {
-
-      require: 'ngModel',
-      link: function (scope, element, attrs, ngModel) {
-
-        var PATTERN = new RegExp('^(\\S+)\\s(' + keys(SYM_TO_OPS).join('|') + ')\\s(.+)$');
-
-        /**
-         * Tries to guess the type of the input string
-         * and returns the appropriate representation 
-         * in the guessed type.
-         *
-         * @param value {string}
-         * @return value {string|boolean|number} the interpolated value
-         */
-        function typed(value) {
-
-          // is a string ( "asdf", 'asdf' )
-          if (/^".*"\s*|'.*'\s*$/.test(value)) {
-            return value.substring(1, value.length - 2);
-          }
-
-          if ((parseFloat(value) + "") === value) {
-            return parseFloat(value);
-          }
-
-          if (value === 'true' || value === 'false') {
-            return value === 'true';
-          }
-
-          throw new Error('Cannot infer type of value ' + value);
-        }
-
-        function typedString(value) {
-
-          if (!value) {
-            return value;
-          }
-
-          if (typeof value === "string") {
-            return '"' + value + '"';
-          }
-
-          if (typeof value === "boolean") {
-            return value ? 'true' : 'false';
-          }
-
-          if (typeof value === "number") {
-            return value;
-          }
-
-
-          throw new Error('Cannot infer type string of value ' + value);
-        }
-
-        function parseText(text) {
-          var match = PATTERN.exec(text),
-              value;
-
-          ngModel.$setValidity('processVariableFilter', !!match);
-
-          if (!match) {
-            return;
-          }
-
-          try {
-            value = typed(match[3]);
-          } catch (e) {
-            ngModel.$setValidity('processVariableFilter', false);
-            return;
-          }
-
-          return {
-            name: match[1],
-            operator: SYM_TO_OPS[match[2]],
-            value: value
-          };
-        }
-
-        function operatorName(op) {
-          return OPS[op];
-        }
-
-        function formatFilter(filter) {
-          if (!filter) {
-            return '';
-          }
-
-          return filter.name + ' ' + operatorName(filter.operator) + ' ' + typedString(filter.value);
-        }
-
-        ngModel.$parsers.push(parseText);
-        ngModel.$formatters.push(formatFilter);
-      }
-    }
-  });
+  }];
 
   var Configuration = function PluginConfiguration(ViewsProvider) {
 
