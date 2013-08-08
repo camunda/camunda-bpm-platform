d3b68e94b4b92dd67603f24d47a972fe028bf0ab
Stefan Hentschel <stefan.hentschel@camunda.com>
2013-08-08 09:49:15 +0200

    fix(placeholder): replace jquery solution with polyfill
    Closes: #CAM-1083

--- webapps/webapp/src/main/webapp/assets/vendor/camunda-common/pages/login.js
+++ webapps/webapp/src/main/webapp/assets/vendor/camunda-common/pages/login.js
@@ -3,58 +3,6 @@ ngDefine('camunda.common.pages.login', [ 'angular', 'require', 'module:camunda.c
 
   var Controller = ['$scope', 'Authentication', 'Notifications', '$location',
            function ($scope, Authentication, Notifications, $location) {
-    
-    (function ($) {
-      $.extend($.fn, {
-        placeholder: function (options) {
-          var defaults = {placeholderClass: 'placeholder'};
-
-          options = $.extend(defaults, options);
-
-          return this.each(function () {
-            var input = $(this).addClass(options.placeholderClass);
-            var form  = input.parents('form:first');
-            var text  = input.val() || input.attr('placeholder');
-
-            if (text) {
-              input.val(text);
-
-              input.focus(function () {
-                clearInput();
-              }).blur(function () {
-                unclearInput();
-              });
-
-              form.submit(function() {
-                if (input.hasClass(options.placeholderClass)) {
-                  input.val('');
-                }
-              });
-
-              input.blur();
-            }
-
-            function clearInput() {
-              if (input.val() === text) {
-                input.val('');
-              }
-
-              input.removeClass(options.placeholderClass);
-            }
-
-            function unclearInput() {
-              if (input.val() === '') {
-                input.addClass(options.placeholderClass).val(text);
-              }
-            }
-          });
-        }
-      });
-    })(jQuery);
-
-   jQuery(document).ready(function ($) {
-      $('[placeholder]:not([type="password"])').placeholder();
-    });
 
     if (Authentication.username()) {
       $location.path("/");
