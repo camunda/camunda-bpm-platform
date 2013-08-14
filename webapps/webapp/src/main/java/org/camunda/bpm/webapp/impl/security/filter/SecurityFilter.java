ec7492dd78d337893e52d20b28e5d34665fea347
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2013-08-14 13:34:46 +0200

    fix(webapps): allow cockpit plugins to work with authorization
    Closes CAM-1089

--- webapps/webapp/src/main/java/org/camunda/bpm/webapp/impl/security/filter/SecurityFilter.java
+++ webapps/webapp/src/main/java/org/camunda/bpm/webapp/impl/security/filter/SecurityFilter.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,97 +27,77 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.camunda.bpm.engine.impl.util.IoUtil;
-import org.camunda.bpm.engine.impl.util.ReflectUtil;
-import org.camunda.bpm.webapp.impl.security.filter.SecurityFilterConfig.PathFilterConfig;
-import org.camunda.bpm.webapp.impl.security.filter.SecurityFilterConfig.PathMatcherConfig;
-import org.codehaus.jackson.map.ObjectMapper;
+import org.camunda.bpm.webapp.impl.security.filter.util.FilterRules;
 
 
 /**
- * <p>Simple filter implementation which delegates to a list of {@link SecurityFilterRule FilterRules}, 
- * evaluating their {@link SecurityFilterRule#isRequestAuthorized(HttpServletRequest)} condition 
- * for the given request. Each rule may veto the request by returing 'false'. If the request is 
+ * <p>Simple filter implementation which delegates to a list of {@link SecurityFilterRule FilterRules},
+ * evaluating their {@link SecurityFilterRule#isRequestAuthorized(HttpServletRequest)} condition
+ * for the given request. Each rule may veto the request by returing 'false'. If the request is
  * vetoed, a 401 (unauthorized) response is returned.</p>
- * 
+ *
  * <p>This filter must be configured using a init-param in the web.xml file. The parameter must be named
  * "configFile" and point to the configuration file located in the servlet context.</p>
- * 
+ *
  * @author Daniel Meyer
  *
  */
 public class SecurityFilter implements Filter {
-  
+
   public List<SecurityFilterRule> filterRules = new ArrayList<SecurityFilterRule>();
 
+  @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-    
-    for (SecurityFilterRule filterRule : filterRules) {
-      if(!filterRule.isRequestAuthorized((HttpServletRequest) request)) {
-        // send unauthorized response
-        ((HttpServletResponse) response).sendError(401);
-        // break
-        return;
-      } 
+    doFilterSecure((HttpServletRequest) request, (HttpServletResponse) response, chain);
+  }
+
+  public void doFilterSecure(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    if (!isAuthorized(request, filterRules)) {
+      response.sendError(401);
+      return;
     }
-    
-    // if all rules evaluate to true, continue chain.
+
+    // if request is authorized
     chain.doFilter(request, response);
   }
 
+  @Override
   public void init(FilterConfig filterConfig) throws ServletException {
-    String configFileName = filterConfig.getInitParameter("configFile");
-    InputStream configFileResource = filterConfig.getServletContext().getResourceAsStream(configFileName);
-    if(configFileResource == null) {
-      throw new ServletException("Could not read security filter config file '"+configFileName+"': no such resource in servlet context.");      
-    } else {      
-      ObjectMapper objectMapper = new ObjectMapper();
-      try {
-        SecurityFilterConfig config = objectMapper.readValue(configFileResource, SecurityFilterConfig.class);
-        createFilterRules(config);
-        
-      } catch(Exception e) {
-        throw new ServletException("Exception while parsing '"+configFileName+"'", e);
-        
-      } finally {
-        IoUtil.closeSilently(configFileResource);
-        
-      }
-    }
+    loadFilterRules(filterConfig);
   }
 
-  protected void createFilterRules(SecurityFilterConfig config) {
-    PathFilterConfig pathFilter = config.getPathFilter();
-    filterRules.add(createPathFilterRule(pathFilter));
-  }
-
-  protected PathFilterRule createPathFilterRule(PathFilterConfig pathFilter) {
-    PathFilterRule pathFilterRule = new PathFilterRule();
-    
-    for (PathMatcherConfig pathMatcherConfig : pathFilter.getDeniedPaths()) {
-      pathFilterRule.getDeniedPaths().add(transformPathMatcher(pathMatcherConfig));
-    }
-    
-    for (PathMatcherConfig pathMatcherConfig : pathFilter.getAllowedPaths()) {
-      pathFilterRule.getAllowedPaths().add(transformPathMatcher(pathMatcherConfig));
-    } 
-    
-    return pathFilterRule;
-  }
-
-  protected RequestMatcher transformPathMatcher(PathMatcherConfig pathMatcherConfig) {
-    RequestMatcher requestMatcher = null;
-    if(pathMatcherConfig.getMatcher() == null || pathMatcherConfig.getMatcher().isEmpty()) {
-      requestMatcher = new RequestMatcher(pathMatcherConfig.getPath(), pathMatcherConfig.getParsedMethods());
-    } else {
-      String matcher = pathMatcherConfig.getMatcher();
-      Object[] params = new Object[]{pathMatcherConfig.getPath(), pathMatcherConfig.getParsedMethods()};
-      requestMatcher = (RequestMatcher) ReflectUtil.instantiate(matcher, params);
-    }
-    return requestMatcher;
-  }
-
+  @Override
   public void destroy() {
 
   }
 
+  /**
+   * Iterate over a number of filter rules and match them against
+   * the specified request.
+   *
+   * @param request
+   * @param filterRules
+   *
+   * @return true if the request is authorized against all filter rules, false otherwise
+   */
+  public static boolean isAuthorized(HttpServletRequest request, List<SecurityFilterRule> filterRules) {
+    return FilterRules.isAuthorized(request, filterRules);
+  }
+
+  protected void loadFilterRules(FilterConfig filterConfig) throws ServletException {
+    String configFileName = filterConfig.getInitParameter("configFile");
+    InputStream configFileResource = filterConfig.getServletContext().getResourceAsStream(configFileName);
+    if (configFileResource == null) {
+      throw new ServletException("Could not read security filter config file '"+configFileName+"': no such resource in servlet context.");
+    } else {
+      try {
+        filterRules = FilterRules.load(configFileResource);
+      } catch (Exception e) {
+        throw new RuntimeException("Exception while parsing '" + configFileName + "'", e);
+      } finally {
+        IoUtil.closeSilently(configFileResource);
+      }
+    }
+  }
 }
