c2903550300f20e7abb92ec4b262d3d98642275b
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2013-08-20 16:20:56 +0200

    chore(webapps/modal): use angular-ui for dialogs
    Instead on relying on the self-written and outdated modalDialog
    directive,
    we are using the angular-ui to open and interact with dialogs.
    
    - migrate all dialogs and move them to external templates
    - remove modalDialog directive
    
    additional cleanup
    - remove angular cookie dependency
    
    Closes CAM-1150

--- webapps/webapp/src/main/webapp/app/admin/admin.js
+++ webapps/webapp/src/main/webapp/app/admin/admin.js
@@ -12,9 +12,9 @@
     'module:camunda.common.extensions:camunda-common/extensions/main',
     'module:camunda.common.resources:camunda-common/resources/main',
     'module:camunda.common.services:camunda-common/services/main',
-    'module:camunda.common.pages:camunda-common/pages/main'];
+    'module:camunda.common.pages:camunda-common/pages/main' ];
 
-  var dependencies = [ 'jquery', 'angular', 'module:ng', 'module:ngResource', 'module:ngCookies' ].concat(commons, adminCore);
+  var dependencies = [ 'jquery', 'angular', 'module:ng', 'module:ngResource', 'module:ui.bootstrap:angular-ui' ].concat(commons, adminCore);
 
   ngDefine('admin', dependencies, function(module, $, angular) {
   
