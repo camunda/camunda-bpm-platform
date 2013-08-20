5342e758686067d6315dfe075460ebc9f3ce9447
Daniel Meyer <daniel.meyer@camunda.com>
2013-08-20 11:53:18 +0200

    feat(ldap): add ldap library and config to distros
    - add ldap library to all distributions
    - make sure jboss subsystem instantiates process engine plugins
    
    related to #CAM-978

--- webapps/webapp/src/main/java/org/camunda/bpm/webapp/impl/security/auth/AuthenticationFilter.java
+++ webapps/webapp/src/main/java/org/camunda/bpm/webapp/impl/security/auth/AuthenticationFilter.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,40 +33,40 @@ import org.camunda.bpm.webapp.impl.security.SecurityActions.SecurityAction;
 /**
  * <p>Servlet {@link Filter} implementation responsible for poulating the
  * {@link Authentications#getCurrent()} thread-local (ie. binding the current
- * set of authentications to the current thread so that it may esily be obtained 
+ * set of authentications to the current thread so that it may esily be obtained
  * by application parts not having access to the current session.</p>
- * 
+ *
  * @author Daniel Meyer
- * 
+ *
  */
 public class AuthenticationFilter implements Filter {
 
   public void init(FilterConfig filterConfig) throws ServletException {
-    
+
   }
 
   public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
-    
+
     final HttpServletRequest req = (HttpServletRequest) request;
     final StatusAwareServletResponse resp = new StatusAwareServletResponse((HttpServletResponse) response);
-    
+
     // get authentication from session
-    Authentications authentications = Authentications.getFromSession(req.getSession());      
+    Authentications authentications = Authentications.getFromSession(req.getSession());
     Authentications.setCurrent(authentications);
-    
+
     try {
-      
+
       SecurityActions.runWithAuthentications(new SecurityAction<Void>() {
         public Void execute() {
-          try {            
-            chain.doFilter(request, response);            
+          try {
+            chain.doFilter(request, response);
           } catch(Exception e) {
             throw new RuntimeException(e);
           }
           return null;
         }
       }, authentications);
-    } finally {      
+    } finally {
       if(resp.getStatus() == 401) {
         // update cookie
         AuthenticationCookie.updateCookie(resp, req.getSession());
@@ -74,10 +74,10 @@ public class AuthenticationFilter implements Filter {
       Authentications.clearCurrent();
       Authentications.updateSession(req.getSession(), authentications);
     }
-    
+
   }
 
-  protected void setProcessEngineAuthentications(Authentications authentications) {    
+  protected void setProcessEngineAuthentications(Authentications authentications) {
     for (Authentication authentication : authentications.getAuthentications()) {
       ProcessEngine processEngine = Cockpit.getProcessEngine(authentication.getProcessEngineName());
       if(processEngine != null) {
@@ -86,22 +86,22 @@ public class AuthenticationFilter implements Filter {
         if(authentication instanceof UserAuthentication) {
           groupIds = ((UserAuthentication) authentication).getGroupIds();
         }
-        processEngine.getIdentityService().setAuthentication(identityId, groupIds);      
-      }            
+        processEngine.getIdentityService().setAuthentication(identityId, groupIds);
+      }
     }
   }
-  
-  protected void clearProcessEngineAuthentications(Authentications authentications) {    
+
+  protected void clearProcessEngineAuthentications(Authentications authentications) {
     for (Authentication authentication : authentications.getAuthentications()) {
       ProcessEngine processEngine = Cockpit.getProcessEngine(authentication.getProcessEngineName());
       if(processEngine != null) {
-        processEngine.getIdentityService().clearAuthentication();      
-      }            
+        processEngine.getIdentityService().clearAuthentication();
+      }
     }
   }
 
   public void destroy() {
-    
+
   }
 
 }
