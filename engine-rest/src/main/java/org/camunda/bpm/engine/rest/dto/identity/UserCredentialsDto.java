c6b9b4ea20689daf19efeaa5cac00cff0cf32d4c
romansmirnov <roman.smirnov@camunda.com>
2013-08-23 11:22:20 +0200

    feat(admin/password): Add confirmation password
    related to CAM-1111

--- engine-rest/src/main/java/org/camunda/bpm/engine/rest/dto/identity/UserCredentialsDto.java
+++ engine-rest/src/main/java/org/camunda/bpm/engine/rest/dto/identity/UserCredentialsDto.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,24 +19,32 @@ import org.camunda.bpm.engine.identity.User;
  *
  */
 public class UserCredentialsDto {
-  
+
   protected String password;
-  
+  protected String authenticatedUserPassword;
+
   // transformers //////////////////////////////////
-  
+
   public static UserCredentialsDto fromUser(User user) {
     UserCredentialsDto result = new UserCredentialsDto();
     result.setPassword(user.getPassword());
     return result;
   }
-  
+
   // getters / setters /////////////////////////////
-  
+
   public String getPassword() {
     return password;
   }
   public void setPassword(String password) {
     this.password = password;
+  }
+
+  public String getAuthenticatedUserPassword() {
+    return authenticatedUserPassword;
+  }
+  public void setAuthenticatedUserPassword(String authenticatedUserPassword) {
+    this.authenticatedUserPassword = authenticatedUserPassword;
   }
 
 }
