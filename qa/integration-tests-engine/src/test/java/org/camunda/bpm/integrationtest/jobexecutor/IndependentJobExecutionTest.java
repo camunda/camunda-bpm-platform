a799e74f5e8bf071ba6a3417f9e5c16162f873f6
Daniel Meyer <daniel.meyer@camunda.com>
2013-08-27 13:48:02 +0200

    impr(engine): jobEx deploymentAware in distro
    enable jobExecutorDeploymentAware in distribution by default
    
    related to #CAM-1178 #CAM-49

--- qa/integration-tests-engine/src/test/java/org/camunda/bpm/integrationtest/jobexecutor/IndependentJobExecutionTest.java
+++ qa/integration-tests-engine/src/test/java/org/camunda/bpm/integrationtest/jobexecutor/IndependentJobExecutionTest.java
@@ -29,10 +29,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
 public class IndependentJobExecutionTest extends AbstractFoxPlatformIntegrationTest {
-  
+
   private ProcessEngine engine1;
   private ProcessEngineConfigurationImpl engine1Configuration;
-  
+
   @Before
   public void setEngines() {
     ProcessEngineService engineService = BpmPlatform.getProcessEngineService();
@@ -41,91 +41,96 @@ public class IndependentJobExecutionTest extends AbstractFoxPlatformIntegrationT
   }
 
   @Deployment(order = 0, name="pa1")
-  public static WebArchive processArchive1() {    
-    
+  public static WebArchive processArchive1() {
+
     WebArchive deployment = initWebArchiveDeployment("pa1.war", "org/camunda/bpm/integrationtest/jobexecutor/IndependentJobExecutionTest.pa1.xml")
         .addAsResource("org/camunda/bpm/integrationtest/jobexecutor/IndependentJobExecutionTest.process1.bpmn20.xml");
-    
+
     TestContainer.addContainerSpecificProcessEngineConfigurationClass(deployment);
-    
+
     return deployment;
-            
+
   }
-  
+
   @Deployment(order = 1, name="pa2")
-  public static WebArchive processArchive2() {    
-    
+  public static WebArchive processArchive2() {
+
     return initWebArchiveDeployment("pa2.war", "org/camunda/bpm/integrationtest/jobexecutor/IndependentJobExecutionTest.pa2.xml")
         .addAsResource("org/camunda/bpm/integrationtest/jobexecutor/IndependentJobExecutionTest.process2.bpmn20.xml");
   }
-  
+
   @OperateOnDeployment("pa1")
   @Test
   public void testDeploymentRegistration() {
     Set<String> registeredDeploymentsForEngine1 = engine1.getManagementService().getRegisteredDeployments();
     Set<String> registeredDeploymentsForDefaultEngine = processEngine.getManagementService().getRegisteredDeployments();
-    
+
     ProcessApplicationInfo pa1Info = getProcessApplicationDeploymentInfo("pa1");
-    
+
     List<ProcessApplicationDeploymentInfo> pa1DeploymentInfo = pa1Info.getDeploymentInfo();
-    
+
     Assert.assertEquals(1, pa1DeploymentInfo.size());
     Assert.assertTrue(registeredDeploymentsForEngine1.contains(pa1DeploymentInfo.get(0).getDeploymentId()));
-    
+
     ProcessApplicationInfo pa2Info = getProcessApplicationDeploymentInfo("pa2");
-    
+
     List<ProcessApplicationDeploymentInfo> pa2DeploymentInfo = pa2Info.getDeploymentInfo();
     Assert.assertEquals(1, pa2DeploymentInfo.size());
     Assert.assertTrue(registeredDeploymentsForDefaultEngine.contains(pa2DeploymentInfo.get(0).getDeploymentId()));
   }
-  
+
   private ProcessApplicationInfo getProcessApplicationDeploymentInfo(String applicationName) {
     ProcessApplicationInfo processApplicationInfo = BpmPlatform.getProcessApplicationService().getProcessApplicationInfo(applicationName);
     if (processApplicationInfo == null) {
       processApplicationInfo = BpmPlatform.getProcessApplicationService().getProcessApplicationInfo("/" + applicationName);
     }
     return processApplicationInfo;
-    
+
   }
-  
+
   @OperateOnDeployment("pa1")
   @Test
   public void testDeploymentAwareJobAcquisition() {
     JobExecutor jobExecutor1 = engine1Configuration.getJobExecutor();
-    
+
     ProcessInstance instance1 = engine1.getRuntimeService().startProcessInstanceByKey("archive1Process");
     ProcessInstance instance2 = processEngine.getRuntimeService().startProcessInstanceByKey("archive2Process");
-    
+
     Job job1 = managementService.createJobQuery().processInstanceId(instance1.getId()).singleResult();
     Job job2 = managementService.createJobQuery().processInstanceId(instance2.getId()).singleResult();
-    
-    
+
+
     // the deployment aware configuration should only return the jobs of the registered deployments
     CommandExecutor commandExecutor = engine1Configuration.getCommandExecutorTxRequired();
     AcquiredJobs acquiredJobs = commandExecutor.execute(new AcquireJobsCmd(jobExecutor1));
-    
+
     Assert.assertEquals(1, acquiredJobs.size());
     Assert.assertTrue(acquiredJobs.contains(job1.getId()));
     Assert.assertFalse(acquiredJobs.contains(job2.getId()));
   }
-  
+
   @OperateOnDeployment("pa1")
   @Test
   public void testDeploymentUnawareJobAcquisition() {
     JobExecutor defaultJobExecutor = processEngineConfiguration.getJobExecutor();
-    
+
     ProcessInstance instance1 = engine1.getRuntimeService().startProcessInstanceByKey("archive1Process");
     ProcessInstance instance2 = processEngine.getRuntimeService().startProcessInstanceByKey("archive2Process");
-    
+
     Job job1 = managementService.createJobQuery().processInstanceId(instance1.getId()).singleResult();
     Job job2 = managementService.createJobQuery().processInstanceId(instance2.getId()).singleResult();
-    
+
     // the deployment unaware configuration should return both jobs
     CommandExecutor commandExecutor = processEngineConfiguration.getCommandExecutorTxRequired();
-    AcquiredJobs acquiredJobs = commandExecutor.execute(new AcquireJobsCmd(defaultJobExecutor));
-    
-    Assert.assertEquals(2, acquiredJobs.size());
-    Assert.assertTrue(acquiredJobs.contains(job1.getId()));
-    Assert.assertTrue(acquiredJobs.contains(job2.getId()));
+    processEngineConfiguration.setJobExecutorDeploymentAware(false);
+    try {
+      AcquiredJobs acquiredJobs = commandExecutor.execute(new AcquireJobsCmd(defaultJobExecutor));
+
+      Assert.assertEquals(2, acquiredJobs.size());
+      Assert.assertTrue(acquiredJobs.contains(job1.getId()));
+      Assert.assertTrue(acquiredJobs.contains(job2.getId()));
+    } finally {
+      processEngineConfiguration.setJobExecutorDeploymentAware(true);
+    }
   }
 }
