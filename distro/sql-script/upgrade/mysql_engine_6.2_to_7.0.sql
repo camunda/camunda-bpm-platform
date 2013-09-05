f5e6ea7a02a5b38c70fd2101fa5fdc3cea5c4840
Christian Lipphardt <christian.lipphardt@googlemail.com>
2013-09-05 10:36:51 +0200

    chore(upgrade-sql): remove no longer required create index statement

--- distro/sql-script/upgrade/mysql_engine_6.2_to_7.0.sql
+++ distro/sql-script/upgrade/mysql_engine_6.2_to_7.0.sql
@@ -40,7 +40,7 @@ alter table ACT_RU_INCIDENT
     foreign key (ROOT_CAUSE_INCIDENT_ID_) 
     references ACT_RU_INCIDENT (ID_) on delete cascade on update cascade;
     
-create index ACT_IDX_HI_ACT_INST_COMP on ACT_HI_ACTINST(EXECUTION_ID_, ACT_ID_, END_TIME_, ID_);
+
 
 /** add ACT_INST_ID_ column to execution table */
 alter table ACT_RU_EXECUTION
