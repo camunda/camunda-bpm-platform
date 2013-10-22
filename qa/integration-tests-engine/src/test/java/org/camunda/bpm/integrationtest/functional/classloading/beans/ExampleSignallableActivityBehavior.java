72cb1de698a74d68757debeced742fdf9435522e
Daniel Meyer <daniel.meyer@camunda.com>
2013-10-22 08:37:59 +0200

    fix(engine): use PA-Classloader on signal()
    - use process application classloader when processing BPMN signals
    
    related to #CAM-1371

---
+++ qa/integration-tests-engine/src/test/java/org/camunda/bpm/integrationtest/functional/classloading/beans/ExampleSignallableActivityBehavior.java
@@ -0,0 +1,39 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.integrationtest.functional.classloading.beans;
+
+import org.camunda.bpm.engine.impl.bpmn.behavior.AbstractBpmnActivityBehavior;
+import org.camunda.bpm.engine.impl.pvm.delegate.ActivityExecution;
+
+/**
+ * @author Daniel Meyer
+ *
+ */
+public class ExampleSignallableActivityBehavior extends AbstractBpmnActivityBehavior {
+
+  @Override
+  public void execute(ActivityExecution execution) throws Exception {
+
+    // do nothing = waitstate
+
+  }
+
+  @Override
+  public void signal(ActivityExecution execution, String signalEvent, Object signalData) throws Exception {
+
+    // leave waitstate
+    leave(execution);
+
+  }
+
+}
