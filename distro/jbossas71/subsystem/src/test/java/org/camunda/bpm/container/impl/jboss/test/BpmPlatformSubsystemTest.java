3dd29e3804c3b74d8b4504bd85f749572ce24e63
Christian Lipphardt <christian.lipphardt@googlemail.com>
2013-10-24 11:50:48 +0200

    refactor(jboss-subsystem): remove override method
    related to CAM-1334

--- distro/jbossas71/subsystem/src/test/java/org/camunda/bpm/container/impl/jboss/test/BpmPlatformSubsystemTest.java
+++ distro/jbossas71/subsystem/src/test/java/org/camunda/bpm/container/impl/jboss/test/BpmPlatformSubsystemTest.java
@@ -15,18 +15,11 @@
  */
 package org.camunda.bpm.container.impl.jboss.test;
 
-import java.io.IOException;
-import java.util.List;
-
-import org.junit.Assert;
-
 import org.camunda.bpm.container.impl.jboss.extension.BpmPlatformExtension;
 import org.camunda.bpm.container.impl.jboss.extension.ModelConstants;
-import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
 import org.jboss.as.subsystem.test.AbstractSubsystemBaseTest;
-import org.jboss.as.subsystem.test.AdditionalInitialization;
-import org.jboss.as.subsystem.test.KernelServices;
-import org.jboss.dmr.ModelNode;
+
+import java.io.IOException;
 
 
 /**
@@ -49,57 +42,8 @@ public class BpmPlatformSubsystemTest extends AbstractSubsystemBaseTest {
     } catch (Exception e) {
       e.printStackTrace();
     }
+
     return null;
   }
-  
-  @Override
-  public void testSubsystem() throws Exception {
-    final String configId = null;
-    final AdditionalInitialization additionalInit = createAdditionalInitialization();
 
-    // Parse the subsystem xml and install into the first controller
-    final String subsystemXml = configId == null ? getSubsystemXml() : getSubsystemXml(configId);
-    final KernelServices servicesA = createKernelServicesBuilder(additionalInit)
-                                        .setSubsystemXml(subsystemXml)
-                                        .build();
-    Assert.assertNotNull(servicesA);
-    //Get the model and the persisted xml from the first controller
-    final ModelNode modelA = servicesA.readWholeModel();
-    Assert.assertNotNull(modelA);
-
-    // Test marshalling
-    final String marshalled = servicesA.getPersistedSubsystemXml();
-    servicesA.shutdown();
-
-
-    // validate the the normalized xmls
-    String normalizedSubsystem = normalizeXML(subsystemXml);
-    compareXml(configId, normalizedSubsystem, normalizeXML(marshalled));
-
-    //Install the persisted xml from the first controller into a second controller
-    final KernelServices servicesB = createKernelServicesBuilder(additionalInit)
-                                        .setSubsystemXml(marshalled)
-                                        .build();
-    final ModelNode modelB = servicesB.readWholeModel();
-
-    //Make sure the models from the two controllers are identical
-    compare(modelA, modelB);
-
-    // Test the describe operation
-    final ModelNode operation = createDescribeOperation();
-    final ModelNode result = servicesB.executeOperation(operation);
-    Assert.assertTrue("the subsystem describe operation has to generate a list of operations to recreate the subsystem",
-            !result.hasDefined(ModelDescriptionConstants.FAILURE_DESCRIPTION));
-    final List<ModelNode> operations = result.get(ModelDescriptionConstants.RESULT).asList();
-    servicesB.shutdown();
-
-    final KernelServices servicesC = createKernelServicesBuilder(additionalInit)
-                                        .setBootOperations(operations)
-                                        .build();
-    final ModelNode modelC = servicesC.readWholeModel();
-
-    compare(modelA, modelC);
-
-    assertRemoveSubsystemResources(servicesA);
-  }
 }
