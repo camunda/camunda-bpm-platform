274759edef70507f9ccb71c62362113751ed1315
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2013-11-11 13:31:11 +0100

    fix(cockpit/plugins): expose TestProcessEngineProvider

--- webapps/core/src/main/java/org/camunda/bpm/cockpit/plugin/test/application/TestProcessEngineProvider.java
+++ webapps/core/src/main/java/org/camunda/bpm/cockpit/plugin/test/application/TestProcessEngineProvider.java
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.camunda.bpm.cockpit.test.sample.application;
+package org.camunda.bpm.cockpit.plugin.test.application;
 
 import java.util.Set;
 
