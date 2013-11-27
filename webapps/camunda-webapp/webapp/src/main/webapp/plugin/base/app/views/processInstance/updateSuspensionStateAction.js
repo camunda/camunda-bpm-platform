33eca2f14e4673be6678ea1db8c19ce42d9c06f4
romansmirnov <roman.smirnov@camunda.com>
2013-11-27 14:36:37 +0100

    feat(suspension/procInst): Suspend or activate a process instance
    - it is possible to activate a suspended process instance via an action
      button (and vice versa)
    
    related to CAM-1403

---
+++ webapps/camunda-webapp/webapp/src/main/webapp/plugin/base/app/views/processInstance/updateSuspensionStateAction.js
@@ -0,0 +1,45 @@
+ngDefine('cockpit.plugin.base.views', ['require'], function(module, require) {
+
+  var Controller = [ '$scope', '$dialog',
+    function($scope, $dialog) {
+
+    $scope.openDialog = function () {
+      var dialog = $dialog.dialog({
+        resolve: {
+          processData: function() { return $scope.processData; },
+          processInstance: function() { return $scope.processInstance; }
+        },
+        controller: 'UpdateProcessInstanceSuspensionStateController',
+        templateUrl: require.toUrl('./update-suspension-state-dialog.html')
+      });
+
+      dialog.open().then(function(result) {
+
+        // dialog closed. YEA!
+        if (result.status === 'SUCCESS') {
+          $scope.processInstance.suspended = result.suspended;
+
+          $scope.processData.set('filter', angular.extend({}, $scope.filter));
+        }
+
+      });
+    };
+
+  }];
+
+  var Configuration = function PluginConfiguration(ViewsProvider) {
+
+    ViewsProvider.registerDefaultView('cockpit.processInstance.action', {
+      id: 'update-suspension-state-action',
+      label: 'Update Suspension State',
+      url: 'plugin://base/static/app/views/processInstance/update-suspension-state-action.html',
+      controller: Controller,
+      priority: 50
+    });
+  };
+
+  Configuration.$inject = ['ViewsProvider'];
+
+  module.config(Configuration);
+
+});
