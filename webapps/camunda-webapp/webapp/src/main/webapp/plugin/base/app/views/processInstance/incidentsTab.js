e9dd92a1b6de9e7ad810ad73e1984411f362e875
romansmirnov <roman.smirnov@camunda.com>
2013-12-06 11:31:56 +0100

    feat(increment/job/retry): Increment retries of specific failed job
    - Increment the number of retries of a corresponding failed job of a
      selected incident from type "failedJob"
    
    related to CAM-1558

--- webapps/camunda-webapp/webapp/src/main/webapp/plugin/base/app/views/processInstance/incidentsTab.js
+++ webapps/camunda-webapp/webapp/src/main/webapp/plugin/base/app/views/processInstance/incidentsTab.js
@@ -1,93 +1,107 @@
-ngDefine('cockpit.plugin.base.views', function(module) {
+ngDefine('cockpit.plugin.base.views', ['require'], function(module, require) {
 
-   function IncidentsController ($scope, $http, search, Uri) {
+  var Controller = [ '$scope', '$http', '$dialog', 'search', 'Uri',
+    function ($scope, $http, $dialog, search, Uri) {
 
-    // input: processInstance, processData
+      // input: processInstance, processData
 
-    var incidentData = $scope.processData.newChild($scope);
-    var processInstance = $scope.processInstance;
+      var incidentData = $scope.processData.newChild($scope);
+      var processInstance = $scope.processInstance;
 
-    var DEFAULT_PAGES = { size: 50, total: 0, current: 1 };
+      var DEFAULT_PAGES = { size: 50, total: 0, current: 1 };
 
-    var pages = $scope.pages = angular.copy(DEFAULT_PAGES);
+      var pages = $scope.pages = angular.copy(DEFAULT_PAGES);
 
-    var filter = null;
+      var filter = null;
 
-    $scope.$watch('pages.current', function(newValue, oldValue) {
-      if (newValue == oldValue) {
-        return;
-      }
+      $scope.$watch('pages.current', function(newValue, oldValue) {
+        if (newValue == oldValue) {
+          return;
+        }
 
-      search('page', !newValue || newValue == 1 ? null : newValue);
-    });
-
-    incidentData.observe([ 'filter', 'bpmnElements', 'activityIdToInstancesMap' ], function(newFilter, bpmnElements, activityIdToInstancesMap) {
-      pages.current = newFilter.page || 1;
-
-      updateView(newFilter, bpmnElements, activityIdToInstancesMap);
-    });
-
-    function updateView (newFilter, bpmnElements, activityIdToInstancesMap) {
-      filter = angular.copy(newFilter);
-
-      delete filter.page;
-      delete filter.activityInstanceIds;
-      delete filter.scrollToBpmnElement;
-
-      var page = pages.current,
-          count = pages.size,
-          firstResult = (page - 1) * count;
-
-      var defaultParams = {
-        processInstanceIdIn: [ processInstance.id ]
-      };
-
-      var pagingParams = {
-        firstResult: firstResult,
-        maxResults: count
-      };
-
-      var params = angular.extend({}, filter, defaultParams);
-
-      // fix missmatch -> activityIds -> activityIdIn
-      params.activityIdIn = params.activityIds;
-      delete params.activityIds;
-
-      $scope.incidents = null;
-
-      // get the 'count' of incidents
-      $http.post(Uri.appUri('plugin://base/:engine/incident/count'), params).success(function(data) {
-        pages.total = Math.ceil(data.count / pages.size);
+        search('page', !newValue || newValue == 1 ? null : newValue);
       });
 
-      // get the incidents
-      $http.post(Uri.appUri('plugin://base/:engine/incident'), params, {params: pagingParams }).success(function(data) {
-        angular.forEach(data, function (incident) {
-          var activityId = incident.activityId;
-          var bpmnElement = bpmnElements[activityId];
-          incident.activityName = bpmnElement.name || bpmnElement.id;
-          incident.linkable = bpmnElements[activityId] && activityIdToInstancesMap[activityId].length > 0;
+      incidentData.observe([ 'filter', 'bpmnElements', 'activityIdToInstancesMap' ], function(newFilter, bpmnElements, activityIdToInstancesMap) {
+        pages.current = newFilter.page || 1;
+
+        updateView(newFilter, bpmnElements, activityIdToInstancesMap);
+      });
+
+      function updateView (newFilter, bpmnElements, activityIdToInstancesMap) {
+        filter = angular.copy(newFilter);
+
+        delete filter.page;
+        delete filter.activityInstanceIds;
+        delete filter.scrollToBpmnElement;
+
+        var page = pages.current,
+            count = pages.size,
+            firstResult = (page - 1) * count;
+
+        var defaultParams = {
+          processInstanceIdIn: [ processInstance.id ]
+        };
+
+        var pagingParams = {
+          firstResult: firstResult,
+          maxResults: count
+        };
+
+        var params = angular.extend({}, filter, defaultParams);
+
+        // fix missmatch -> activityIds -> activityIdIn
+        params.activityIdIn = params.activityIds;
+        delete params.activityIds;
+
+        $scope.incidents = null;
+
+        // get the 'count' of incidents
+        $http.post(Uri.appUri('plugin://base/:engine/incident/count'), params).success(function(data) {
+          pages.total = Math.ceil(data.count / pages.size);
         });
 
-        $scope.incidents = data;
-      });
-    };
+        // get the incidents
+        $http.post(Uri.appUri('plugin://base/:engine/incident'), params, {params: pagingParams }).success(function(data) {
+          angular.forEach(data, function (incident) {
+            var activityId = incident.activityId;
+            var bpmnElement = bpmnElements[activityId];
+            incident.activityName = bpmnElement.name || bpmnElement.id;
+            incident.linkable = bpmnElements[activityId] && activityIdToInstancesMap[activityId].length > 0;
+          });
 
-    $scope.getIncidentType = function (incident) {
-      if (incident.incidentType === 'failedJob') {
-        return 'Failed Job';
-      }
+          $scope.incidents = data;
+        });
+      };
 
-      return incident.incidentType;
-    };
+      $scope.getIncidentType = function (incident) {
+        if (incident.incidentType === 'failedJob') {
+          return 'Failed Job';
+        }
 
-    $scope.getJobStacktraceUrl = function (incident) {
-      return Uri.appUri('engine://engine/:engine/job/' + incident.rootCauseIncidentConfiguration + '/stacktrace');
-    };
+        return incident.incidentType;
+      };
 
-  };
+      $scope.getJobStacktraceUrl = function (incident) {
+        return Uri.appUri('engine://engine/:engine/job/' + incident.rootCauseIncidentConfiguration + '/stacktrace');
+      };
 
-  module.controller('IncidentsController', [ '$scope', '$http', 'search', 'Uri', IncidentsController ]);
+      $scope.openJobRetryDialog = function(incident) {
+        var dialog = $dialog.dialog({
+          resolve: {
+            incident: function() { return incident; }
+          },
+          controller: 'JobRetryController',
+          templateUrl: require.toUrl('./job-retry-dialog.html')
+        });
+
+        dialog.open().then(function(result) {
+          // dialog closed. YEA!
+        });      
+
+      };
+
+  }];
 
   var Configuration = function PluginConfiguration(ViewsProvider) {
 
@@ -95,7 +109,7 @@ ngDefine('cockpit.plugin.base.views', function(module) {
       id: 'incidents-tab',
       label: 'Incidents',
       url: 'plugin://base/static/app/views/processInstance/incidents-tab.html',
-      controller: 'IncidentsController',
+      controller: Controller,
       priority: 15
     });
   };
