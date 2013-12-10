5bbeb4e361575df6dc3078de33784303d794b6ed
Sebastian Menski <sebastian.menski@googlemail.com>
2013-12-10 16:37:56 +0100

    fix(engine): Properly end pruned execution after parallel gateway join
    - use the ExecutionEntity end() instead of remove() function to clean pruned
      executions
    - the end() function will correctly call the AtomicOperation ACTIVITY_END,
      which will trigger a database update for the endtime
    - add test case and process from  https://app.camunda.com/jira/browse/CAM-1537
    - set isEnded flag in ExectuinEntity deleteCascade() function to
      prevent an additional deletion attempt
    
    Closes #CAM-1537

--- engine/src/test/java/org/camunda/bpm/engine/test/bpmn/gateway/ParallelGatewayTest.java
+++ engine/src/test/java/org/camunda/bpm/engine/test/bpmn/gateway/ParallelGatewayTest.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,6 +15,7 @@ package org.camunda.bpm.engine.test.bpmn.gateway;
 
 import java.util.List;
 
+import org.camunda.bpm.engine.history.HistoricActivityInstanceQuery;
 import org.camunda.bpm.engine.impl.test.PluggableProcessEngineTestCase;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.task.Task;
@@ -33,51 +34,51 @@ public class ParallelGatewayTest extends PluggableProcessEngineTestCase {
    */
   @Deployment
   public void testSplitMergeNoWaitstates() {
-    ProcessInstance processInstance = 
+    ProcessInstance processInstance =
       runtimeService.startProcessInstanceByKey("forkJoinNoWaitStates");
     assertTrue(processInstance.isEnded());
   }
-  
+
   @Deployment
   public void testUnstructuredConcurrencyTwoForks() {
-    ProcessInstance processInstance = 
+    ProcessInstance processInstance =
       runtimeService.startProcessInstanceByKey("unstructuredConcurrencyTwoForks");
     assertTrue(processInstance.isEnded());
   }
-  
+
   @Deployment
   public void testUnstructuredConcurrencyTwoJoins() {
-    ProcessInstance processInstance = 
+    ProcessInstance processInstance =
       runtimeService.startProcessInstanceByKey("unstructuredConcurrencyTwoJoins");
     assertTrue(processInstance.isEnded());
   }
-  
+
   @Deployment
   public void testForkFollowedByOnlyEndEvents() {
-    ProcessInstance processInstance = 
+    ProcessInstance processInstance =
       runtimeService.startProcessInstanceByKey("forkFollowedByEndEvents");
     assertTrue(processInstance.isEnded());
   }
-  
+
   @Deployment
   public void testNestedForksFollowedByEndEvents() {
-    ProcessInstance processInstance = 
+    ProcessInstance processInstance =
       runtimeService.startProcessInstanceByKey("nestedForksFollowedByEndEvents");
     assertTrue(processInstance.isEnded());
   }
-  
+
   // ACT-482
   @Deployment
   public void testNestedForkJoin() {
    String pid = runtimeService.startProcessInstanceByKey("nestedForkJoin").getId();
-   
+
    // After process startm, only task 0 should be active
-   TaskQuery query = taskService.createTaskQuery().orderByTaskName().asc(); 
+   TaskQuery query = taskService.createTaskQuery().orderByTaskName().asc();
    List<Task> tasks = query.list();
    assertEquals(1, tasks.size());
    assertEquals("Task 0", tasks.get(0).getName());
    assertEquals(1, runtimeService.getActivityInstance(pid).getChildActivityInstances().length);
-   
+
    // Completing task 0 will create Task A and B
    taskService.complete(tasks.get(0).getId());
    tasks = query.list();
@@ -85,7 +86,7 @@ public class ParallelGatewayTest extends PluggableProcessEngineTestCase {
    assertEquals("Task A", tasks.get(0).getName());
    assertEquals("Task B", tasks.get(1).getName());
    assertEquals(2, runtimeService.getActivityInstance(pid).getChildActivityInstances().length);
-   
+
    // Completing task A should not trigger any new tasks
    taskService.complete(tasks.get(0).getId());
    tasks = query.list();
@@ -100,7 +101,7 @@ public class ParallelGatewayTest extends PluggableProcessEngineTestCase {
    assertEquals("Task B1", tasks.get(0).getName());
    assertEquals("Task B2", tasks.get(1).getName());
    assertEquals(3, runtimeService.getActivityInstance(pid).getChildActivityInstances().length);
-   
+
    // Completing B1 and B2 will activate both joins, and process reaches task C
    taskService.complete(tasks.get(0).getId());
    taskService.complete(tasks.get(1).getId());
@@ -109,33 +110,59 @@ public class ParallelGatewayTest extends PluggableProcessEngineTestCase {
    assertEquals("Task C", tasks.get(0).getName());
    assertEquals(1, runtimeService.getActivityInstance(pid).getChildActivityInstances().length);
   }
-  
+
   /**
    * http://jira.codehaus.org/browse/ACT-1222
    */
   @Deployment
   public void testReceyclingExecutionWithCallActivity() {
     String processInstanceId = runtimeService.startProcessInstanceByKey("parent-process").getId();
-    
+
     // After process start we have two tasks, one from the parent and one from the sub process
-    TaskQuery query = taskService.createTaskQuery().orderByTaskName().asc(); 
+    TaskQuery query = taskService.createTaskQuery().orderByTaskName().asc();
     List<Task> tasks = query.list();
     assertEquals(2, tasks.size());
     assertEquals("Another task", tasks.get(0).getName());
     assertEquals("Some Task", tasks.get(1).getName());
-    
-    // we complete the task from the parent process, the root execution is receycled, the task in the sub process is still there 
+
+    // we complete the task from the parent process, the root execution is receycled, the task in the sub process is still there
     taskService.complete(tasks.get(1).getId());
     tasks = query.list();
     assertEquals(1, tasks.size());
     assertEquals("Another task", tasks.get(0).getName());
 
-    // we end the task in the sub process and the sub process instance end is propagated to the parent process 
+    // we end the task in the sub process and the sub process instance end is propagated to the parent process
     taskService.complete(tasks.get(0).getId());
-    assertEquals(0, taskService.createTaskQuery().count()); 
-    
+    assertEquals(0, taskService.createTaskQuery().count());
+
     // There is a QA config without history, so we cannot work with this:
-    //assertEquals(1, historyService.createHistoricProcessInstanceQuery().processInstanceId(processInstanceId).finished().count());    
+    //assertEquals(1, historyService.createHistoricProcessInstanceQuery().processInstanceId(processInstanceId).finished().count());
   }
-  
+
+  /**
+   * https://app.camunda.com/jira/browse/CAM-1537
+   */
+  @Deployment
+  public void testGatewayEndTimes() {
+    ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("gatewayEndTimes");
+
+    TaskQuery query = taskService.createTaskQuery().orderByTaskName().asc();
+    List<Task> tasks = query.list();
+    taskService.complete(tasks.get(0).getId());
+    taskService.complete(tasks.get(1).getId());
+
+    // process instance should have finished
+    assertNotNull(historyService.createHistoricProcessInstanceQuery().processInstanceId(processInstance.getId()).singleResult().getEndTime());
+    // gateways should have end timestamps
+    assertNotNull(historyService.createHistoricActivityInstanceQuery().activityId("Gateway_0").singleResult().getEndTime());
+
+    // there exists two historic activity instances for "Gateway_1" (parallel join)
+    HistoricActivityInstanceQuery historicActivityInstanceQuery = historyService.createHistoricActivityInstanceQuery().activityId("Gateway_1");
+
+    assertEquals(2, historicActivityInstanceQuery.count());
+    // they should have an end timestamp
+    assertNotNull(historicActivityInstanceQuery.list().get(0).getEndTime());
+    assertNotNull(historicActivityInstanceQuery.list().get(1).getEndTime());
+  }
+
 }
