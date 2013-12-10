a267a43cd6af9630764ad2bca4493b4964f9bdd3
romansmirnov <roman.smirnov@camunda.com>
2013-12-10 16:42:48 +0100

    chore(cdi/jsf/test): Provide test case for right bean resolving
    related to CAM-1609

--- engine-cdi/src/main/java/org/camunda/bpm/engine/cdi/compat/FoxTaskForm.java
+++ engine-cdi/src/main/java/org/camunda/bpm/engine/cdi/compat/FoxTaskForm.java
@@ -4,10 +4,12 @@ import javax.enterprise.context.ConversationScoped;
 import javax.enterprise.inject.Typed;
 import javax.inject.Named;
 
+import org.camunda.bpm.engine.cdi.jsf.TaskForm;
+
 @ConversationScoped
 @Named("fox.taskForm")
 @Typed({FoxTaskForm.class})
-public class FoxTaskForm extends org.camunda.bpm.engine.cdi.jsf.TaskForm {
+public class FoxTaskForm extends TaskForm {
 
   private static final long serialVersionUID = 9042602064970870095L;
 }
---
+++ engine-cdi/src/test/java/org/camunda/bpm/engine/cdi/test/jsf/TaskFormTest.java
@@ -0,0 +1,52 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.engine.cdi.test.jsf;
+
+import java.util.Set;
+
+import javax.enterprise.inject.AmbiguousResolutionException;
+import javax.enterprise.inject.spi.Bean;
+
+import org.camunda.bpm.engine.cdi.compat.FoxTaskForm;
+import org.camunda.bpm.engine.cdi.jsf.TaskForm;
+import org.camunda.bpm.engine.cdi.test.CdiProcessEngineTestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Daniel Meyer
+ */
+public class TaskFormTest extends CdiProcessEngineTestCase {
+
+  @Test
+  public void testTaskFormInjectable() throws Exception {
+
+    Set<Bean<?>> taskForm = beanManager.getBeans(TaskForm.class);
+    try {
+      Bean<? extends Object> bean = beanManager.resolve(taskForm);
+      Assert.assertNotNull(bean);
+    }catch(AmbiguousResolutionException e) {
+      Assert.fail("Injection of TaskForm is ambiguous.");
+    }
+
+    Set<Bean<?>> foxTaskForm = beanManager.getBeans(FoxTaskForm.class);
+    try {
+      Bean<? extends Object> bean = beanManager.resolve(foxTaskForm);
+      Assert.assertNotNull(bean);
+    }catch(AmbiguousResolutionException e) {
+      Assert.fail("Injection of TaskForm is ambiguous.");
+    }
+
+  }
+
+}
