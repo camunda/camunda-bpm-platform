a8c020e3e361510bed146e55ff0e5fa58f5e5274
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2014-01-09 13:57:15 +0100

    test(cycle/roundtrip): additional extension element tests
    Related to CAM-1635

--- webapps/cycle/cycle/src/test/java/org/camunda/bpm/cycle/roundtrip/BpmnProcessModelUtilTest.java
+++ webapps/cycle/cycle/src/test/java/org/camunda/bpm/cycle/roundtrip/BpmnProcessModelUtilTest.java
@@ -10,15 +10,13 @@ import org.apache.commons.io.IOUtils;
 import org.camunda.bpm.cycle.util.IoUtil;
 import org.junit.Test;
 
-public class BpmnProcessModelUtilTest {
-
-  private BpmnProcessModelUtil cycleRoundtripUtil = new BpmnProcessModelUtil();
+public class BpmnProcessModelUtilTest extends AbstractRoundtripTest {
 
   @Test
   public void testReplaceDeveloperFriendlyIds() {
     String sourceModel = IoUtil.readFileAsString("org/camunda/bpm/cycle/roundtrip/repository/test-lhs.bpmn");
 
-    String resultModel = cycleRoundtripUtil.replaceDeveloperFriendlyIds(sourceModel);
+    String resultModel = roundtripUtil.replaceDeveloperFriendlyIds(sourceModel);
     Assert.assertTrue(resultModel.contains("Process_Engine"));
   }
 
@@ -26,7 +24,7 @@ public class BpmnProcessModelUtilTest {
   public void testReplaceDeveloperFriendlyIdsWithReplacePoolId() {
     String sourceModel = IoUtil.readFileAsString("org/camunda/bpm/cycle/roundtrip/repository/test-lhs.bpmn");
 
-    String resultModel = cycleRoundtripUtil.replaceDeveloperFriendlyIds(sourceModel,"My Custom Pool Name");
+    String resultModel = roundtripUtil.replaceDeveloperFriendlyIds(sourceModel,"My Custom Pool Name");
     Assert.assertTrue(resultModel.contains("My Custom Pool Name"));
   }
 
@@ -34,7 +32,7 @@ public class BpmnProcessModelUtilTest {
   public void testExtractPool() throws IOException {
     InputStream sourceModel = new FileInputStream(IoUtil.getFile("org/camunda/bpm/cycle/roundtrip/repository/test-lhs.bpmn"));
 
-    String resultModel = IOUtils.toString(cycleRoundtripUtil.extractExecutablePool(sourceModel), "UTF-8");
+    String resultModel = IOUtils.toString(roundtripUtil.extractExecutablePool(sourceModel), "UTF-8");
     Assert.assertFalse(resultModel.contains("Mensch"));
   }
 
@@ -42,7 +40,7 @@ public class BpmnProcessModelUtilTest {
   public void testShouldRemoveCollapsedPool() throws IOException {
     InputStream sourceModel = new FileInputStream(IoUtil.getFile("org/camunda/bpm/cycle/roundtrip/repository/test-lhs-with-collapsed-pool.bpmn"));
 
-    String resultModel = IOUtils.toString(cycleRoundtripUtil.extractExecutablePool(sourceModel), "UTF-8");
+    String resultModel = IOUtils.toString(roundtripUtil.extractExecutablePool(sourceModel), "UTF-8");
     Assert.assertFalse(resultModel.contains("A collapsed Pool"));
     Assert.assertFalse(resultModel.contains("sid-099E1FA7-EDE2-48DB-B6FF-D06E54C58C70"));
   }
@@ -52,7 +50,7 @@ public class BpmnProcessModelUtilTest {
     String sourceModel = IOUtils.toString(new FileInputStream(IoUtil.getFile("org/camunda/bpm/cycle/roundtrip/repository/test-rhs-with-collapsed-pool.bpmn")), "UTF-8");
     String targetModel = IOUtils.toString(new FileInputStream(IoUtil.getFile("org/camunda/bpm/cycle/roundtrip/repository/test-lhs-with-collapsed-pool.bpmn")), "UTF-8");
 
-    String resultModel = cycleRoundtripUtil.importChangesFromExecutableBpmnModel(sourceModel, targetModel);
+    String resultModel = roundtripUtil.importChangesFromExecutableBpmnModel(sourceModel, targetModel);
     Assert.assertTrue(resultModel.contains("A collapsed Pool"));
     Assert.assertTrue(resultModel.contains("sid-099E1FA7-EDE2-48DB-B6FF-D06E54C58C70"));
     Assert.assertTrue(resultModel.contains("A new Service Task"));
