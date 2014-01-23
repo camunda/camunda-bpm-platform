a14c9fcaca6e263def0c81cb43d8ea703793af5b
Valentin Vago <zeropaper@irata.ch>
2014-01-23 14:32:30 +0100

    fix(cockpit/page reload): This commit improve several is supposed to fix the scope lost and page reload encounter when a link in the tabs is clicked.
    It also improves (a bit) the javascript build tools and processes:
    
    - use the [newer](http://npmjs.org/package/grunt-newer) to speed up the copying of watched files
    - prevent the tests to be automatically runned when a file is changed
    - allow livereload feature to be runned on a different port
    - prevent the livereload snippet in require-conf.js to be parsed as a jsDoc comment block
    - refactor the bootstraping modules of cockpit, admin and tasklist by moving some functions in the require-conf.js

--- webapps/cockpit-bootstrap.js
+++ webapps/cockpit-bootstrap.js
@@ -1,46 +1,19 @@
-/**
- * bootstrap script of the cockpit application
- */
-
 (function(document, window, require) {
+  /**
+   * A helper module to prepare the cockpit plugin for bootstrap.
+   * @module cockpit-bootstrap
+   */
 
   var baseUrl = '../../../';
   var APP_NAME = 'cockpit';
   var pluginPackages = window.PLUGIN_PACKAGES || [];
 
-  /**
-   * @see http://stackoverflow.com/questions/15499997/how-to-use-angular-scenario-with-requirejs
-   */
-  function ensureScenarioCompatibility() {
-
-    var html = document.getElementsByTagName('html')[0];
-
-    html.setAttribute('ng-app', APP_NAME);
-    if (html.dataset) {
-      html.dataset.ngApp = APP_NAME;
-    }
-
-    if (top !== window) {
-      window.parent.postMessage({ type: 'loadamd' }, '*');
-    }
-  }
-
-  /**
-   * Bootstrap the angular application
-   */
-  function bootstrapApp(angular) {
-    angular.bootstrap(document, [ APP_NAME ]);
-
-    // ensure compatibility with scenario runner
-    ensureScenarioCompatibility();
-  }
-
   require([baseUrl +'require-conf'], function(rjsConf) {
     require({
-      baseUrl: baseUrl,
-      paths: rjsConf.paths,
-      shim: rjsConf.shim,
-      packages: rjsConf.packages.concat(pluginPackages)
+      baseUrl:    baseUrl,
+      paths:      rjsConf.paths,
+      shim:       rjsConf.shim,
+      packages:   rjsConf.packages.concat(pluginPackages)
     });
 
     require([
@@ -56,7 +29,7 @@
         APP_NAME,
         'domReady!'
       ], function() {
-        bootstrapApp(angular);
+        rjsConf.utils.bootAngular(angular, APP_NAME);
       });
     });
   });
