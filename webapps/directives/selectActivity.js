fedf922e6a37af5eab73b773f33d7e04053aa3e8
Roman Smirnov <roman.smirnov@camunda.com>
2014-02-19 09:33:07 +0100

    fix(cockpit): refresh view on selection in a proper way in cockpit
    related to CAM-1864

--- webapps/directives/selectActivity.js
+++ webapps/directives/selectActivity.js
@@ -34,4 +34,34 @@ ngDefine('cockpit.directives', [ 'angular' ], function(module, angular) {
     };
   });
 
+  module.directive('camSelectActivityInstance', function() {
+    return {
+      link: function(scope, element, attrs) {
+
+        var processData = scope.processData;
+        var selectedActivityInstanceQuery = element.attr('cam-select-activity-instance');
+
+        if (!processData) {
+          throw new Error('No processData defined in scope');
+        }
+
+        if (!selectedActivityInstanceQuery) {
+          throw new Error('No activity instance id query given in @cam-select-activity');
+        }
+
+        element.on('click', function(event) {
+
+          event.preventDefault();
+
+          scope.$apply(function() {
+            // refresh view with selected activity instance id
+            processData.set('filter', {
+              activityInstanceIds: [scope.$eval(selectedActivityInstanceQuery)]
+            });
+          });
+        });
+      }
+    };
+  });
+
 });
