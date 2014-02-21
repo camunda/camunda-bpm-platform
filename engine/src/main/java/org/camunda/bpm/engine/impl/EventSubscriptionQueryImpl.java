30e4e32dbb75c829d7abaf0384a96bef8924f86a
Roman Smirnov <roman.smirnov@camunda.com>
2014-02-21 09:34:03 +0100

    fix(engine/eventSubQuery): assert given id is not null
    related to CAM-1539

--- engine/src/main/java/org/camunda/bpm/engine/impl/EventSubscriptionQueryImpl.java
+++ engine/src/main/java/org/camunda/bpm/engine/impl/EventSubscriptionQueryImpl.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,7 +16,6 @@ package org.camunda.bpm.engine.impl;
 import java.io.Serializable;
 import java.util.List;
 
-import org.camunda.bpm.engine.ProcessEngineException;
 import org.camunda.bpm.engine.impl.interceptor.CommandContext;
 import org.camunda.bpm.engine.impl.interceptor.CommandExecutor;
 import org.camunda.bpm.engine.runtime.EventSubscription;
@@ -26,12 +25,12 @@ import org.camunda.bpm.engine.runtime.EventSubscriptionQuery;
 /**
  * @author Daniel Meyer
  */
-public class EventSubscriptionQueryImpl 
-                extends AbstractQuery<EventSubscriptionQuery, EventSubscription> 
+public class EventSubscriptionQueryImpl
+                extends AbstractQuery<EventSubscriptionQuery, EventSubscription>
                 implements Serializable, EventSubscriptionQuery {
 
   private static final long serialVersionUID = 1L;
-  
+
   protected String eventSubscriptionId;
   protected String eventName;
   protected String eventType;
@@ -42,94 +41,53 @@ public class EventSubscriptionQueryImpl
   public EventSubscriptionQueryImpl(CommandContext commandContext) {
     super(commandContext);
   }
-  
+
   public EventSubscriptionQueryImpl(CommandExecutor commandExecutor) {
     super(commandExecutor);
   }
 
-  /* (non-Javadoc)
-   * @see org.camunda.bpm.engine.impl.EventSubscriptionQuery#eventSubscriptionId(java.lang.String)
-   */
-  @Override
   public EventSubscriptionQuery eventSubscriptionId(String id) {
-    if (eventSubscriptionId == null) {
-      throw new ProcessEngineException("Provided svent subscription id is null");
-    }
+    assertParamNotNull("event subscription id", id);
     this.eventSubscriptionId = id;
     return this;
   }
 
-  /* (non-Javadoc)
-   * @see org.camunda.bpm.engine.impl.EventSubscriptionQuery#eventName(java.lang.String)
-   */
-  @Override
   public EventSubscriptionQuery eventName(String eventName) {
-    if (eventName == null) {
-      throw new ProcessEngineException("Provided event name is null");
-    }
+    assertParamNotNull("event name", eventName);
     this.eventName = eventName;
     return this;
   }
 
-  /* (non-Javadoc)
-   * @see org.camunda.bpm.engine.impl.EventSubscriptionQuery#executionId(java.lang.String)
-   */
-  @Override
   public EventSubscriptionQueryImpl executionId(String executionId) {
-    if (executionId == null) {
-      throw new ProcessEngineException("Provided execution id is null");
-    }
+    assertParamNotNull("execution id", executionId);
     this.executionId = executionId;
     return this;
   }
 
-  /* (non-Javadoc)
-   * @see org.camunda.bpm.engine.impl.EventSubscriptionQuery#processInstanceId(java.lang.String)
-   */
-  @Override
   public EventSubscriptionQuery processInstanceId(String processInstanceId) {
-    if (processInstanceId == null) {
-      throw new ProcessEngineException("Provided process instance id is null");
-    }
+    assertParamNotNull("process instance id", processInstanceId);
     this.processInstanceId = processInstanceId;
     return this;
   }
 
-  /* (non-Javadoc)
-   * @see org.camunda.bpm.engine.impl.EventSubscriptionQuery#activityId(java.lang.String)
-   */
-  @Override
   public EventSubscriptionQueryImpl activityId(String activityId) {
-    if (activityId == null) {
-      throw new ProcessEngineException("Provided activity id is null");
-    }
+    assertParamNotNull("activity id", activityId);
     this.activityId = activityId;
     return this;
   }
-  
-  /* (non-Javadoc)
-   * @see org.camunda.bpm.engine.impl.EventSubscriptionQuery#eventType(java.lang.String)
-   */
-  @Override
+
   public EventSubscriptionQueryImpl eventType(String eventType) {
-    if (eventType == null) {
-      throw new ProcessEngineException("Provided event type is null");
-    }
+    assertParamNotNull("event type", eventType);
     this.eventType = eventType;
     return this;
   }
-  
-  /* (non-Javadoc)
-   * @see org.camunda.bpm.engine.impl.EventSubscriptionQuery#orderByCreated()
-   */
-  @Override
+
   public EventSubscriptionQuery orderByCreated() {
     return orderBy(EventSubscriptionQueryProperty.CREATED);
   }
-  
+
   //results //////////////////////////////////////////
 
-  @Override
   public long executeCount(CommandContext commandContext) {
     checkQueryOk();
     return commandContext
@@ -137,17 +95,16 @@ public class EventSubscriptionQueryImpl
       .findEventSubscriptionCountByQueryCriteria(this);
   }
 
-  @Override
   public List<EventSubscription> executeList(CommandContext commandContext, Page page) {
     checkQueryOk();
     return commandContext
       .getEventSubscriptionManager()
       .findEventSubscriptionsByQueryCriteria(this,page);
   }
-  
+
   //getters //////////////////////////////////////////
-  
-   
+
+
   public String getEventSubscriptionId() {
     return eventSubscriptionId;
   }
--- engine/src/test/java/org/camunda/bpm/engine/test/api/runtime/EventSubscriptionQueryTest.java
+++ engine/src/test/java/org/camunda/bpm/engine/test/api/runtime/EventSubscriptionQueryTest.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,6 +17,7 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
 
+import org.camunda.bpm.engine.ProcessEngineException;
 import org.camunda.bpm.engine.impl.EventSubscriptionQueryImpl;
 import org.camunda.bpm.engine.impl.interceptor.Command;
 import org.camunda.bpm.engine.impl.interceptor.CommandContext;
@@ -25,6 +26,7 @@ import org.camunda.bpm.engine.impl.persistence.entity.MessageEventSubscriptionEn
 import org.camunda.bpm.engine.impl.persistence.entity.SignalEventSubscriptionEntity;
 import org.camunda.bpm.engine.impl.test.PluggableProcessEngineTestCase;
 import org.camunda.bpm.engine.runtime.EventSubscription;
+import org.camunda.bpm.engine.runtime.EventSubscriptionQuery;
 import org.camunda.bpm.engine.runtime.Execution;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
@@ -35,100 +37,150 @@ import org.junit.Assert;
  * @author Daniel Meyer
  */
 public class EventSubscriptionQueryTest extends PluggableProcessEngineTestCase {
-  
-  public void testQueryByEventName() {
-    
+
+  public void testQueryByEventSubscriptionId() {
     createExampleEventSubscriptions();
-    
+
+    List<EventSubscription> list = runtimeService.createEventSubscriptionQuery()
+        .eventName("messageName2")
+        .list();
+    assertEquals(1, list.size());
+
+    EventSubscription eventSubscription = list.get(0);
+
+    EventSubscriptionQuery query = runtimeService.createEventSubscriptionQuery()
+        .eventSubscriptionId(eventSubscription.getId());
+
+    assertEquals(1, query.count());
+    assertEquals(1, query.list().size());
+    assertNotNull(query.singleResult());
+
+    try {
+      runtimeService.createEventSubscriptionQuery().eventSubscriptionId(null).list();
+      fail("Expected ProcessEngineException");
+    } catch (ProcessEngineException e) {
+    }
+
+    cleanDb();
+  }
+
+  public void testQueryByEventName() {
+
+    createExampleEventSubscriptions();
+
     List<EventSubscription> list = runtimeService.createEventSubscriptionQuery()
       .eventName("messageName")
       .list();
     assertEquals(2, list.size());
-    
+
     list = runtimeService.createEventSubscriptionQuery()
       .eventName("messageName2")
       .list();
     assertEquals(1, list.size());
-    
+
+    try {
+      runtimeService.createEventSubscriptionQuery().eventName(null).list();
+      fail("Expected ProcessEngineException");
+    } catch (ProcessEngineException e) {
+    }
+
     cleanDb();
-    
+
   }
-  
+
   public void testQueryByEventType() {
-    
+
     createExampleEventSubscriptions();
-    
+
     List<EventSubscription> list = runtimeService.createEventSubscriptionQuery()
       .eventType("signal")
       .list();
     assertEquals(1, list.size());
-    
+
     list = runtimeService.createEventSubscriptionQuery()
       .eventType("message")
       .list();
     assertEquals(2, list.size());
-    
+
+    try {
+      runtimeService.createEventSubscriptionQuery().eventType(null).list();
+      fail("Expected ProcessEngineException");
+    } catch (ProcessEngineException e) {
+    }
+
     cleanDb();
-    
+
   }
-  
+
   public void testQueryByActivityId() {
-    
+
     createExampleEventSubscriptions();
-    
+
     List<EventSubscription> list = runtimeService.createEventSubscriptionQuery()
       .activityId("someOtherActivity")
       .list();
     assertEquals(1, list.size());
-    
+
     list = runtimeService.createEventSubscriptionQuery()
       .activityId("someActivity")
       .eventType("message")
       .list();
     assertEquals(2, list.size());
-    
+
+    try {
+      runtimeService.createEventSubscriptionQuery().activityId(null).list();
+      fail("Expected ProcessEngineException");
+    } catch (ProcessEngineException e) {
+    }
+
     cleanDb();
-    
+
   }
-  
+
   @Deployment
   public void testQueryByExecutionId() {
-    
+
     // starting two instances:
     ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("catchSignal");
     runtimeService.startProcessInstanceByKey("catchSignal");
-    
+
     // test query by process instance id
     EventSubscription subscription = runtimeService.createEventSubscriptionQuery()
       .processInstanceId(processInstance.getId())
       .singleResult();
     assertNotNull(subscription);
-    
+
     Execution executionWaitingForSignal = runtimeService.createExecutionQuery()
       .activityId("signalEvent")
       .processInstanceId(processInstance.getId())
       .singleResult();
-    
+
     // test query by execution id
     EventSubscription signalSubscription = runtimeService.createEventSubscriptionQuery()
       .executionId(executionWaitingForSignal.getId())
       .singleResult();
     assertNotNull(signalSubscription);
-    
+
     assertEquals(signalSubscription, subscription);
-    
+
+    try {
+      runtimeService.createEventSubscriptionQuery().executionId(null).list();
+      fail("Expected ProcessEngineException");
+    } catch (ProcessEngineException e) {
+    }
+
     cleanDb();
-    
+
   }
-  
+
   public void testQuerySorting() {
     createExampleEventSubscriptions();
     List<EventSubscription> eventSubscriptions = runtimeService.createEventSubscriptionQuery().orderByCreated().asc().list();
     Assert.assertEquals(3, eventSubscriptions.size());
-    
+
     Assert.assertTrue(eventSubscriptions.get(0).getCreated().compareTo(eventSubscriptions.get(1).getCreated()) < 0);
     Assert.assertTrue(eventSubscriptions.get(1).getCreated().compareTo(eventSubscriptions.get(2).getCreated()) < 0);
-    
+
     cleanDb();
   }
 
@@ -138,35 +190,35 @@ public class EventSubscriptionQueryTest extends PluggableProcessEngineTestCase {
     .execute(new Command<Void>() {
       public Void execute(CommandContext commandContext) {
         Calendar calendar = new GregorianCalendar();
-        
-        
+
+
         MessageEventSubscriptionEntity messageEventSubscriptionEntity1 = new MessageEventSubscriptionEntity();
-        messageEventSubscriptionEntity1.setEventName("messageName");        
+        messageEventSubscriptionEntity1.setEventName("messageName");
         messageEventSubscriptionEntity1.setActivityId("someActivity");
         calendar.set(2001, 1, 1);
         messageEventSubscriptionEntity1.setCreated(calendar.getTime());
         messageEventSubscriptionEntity1.insert();
-        
+
         MessageEventSubscriptionEntity messageEventSubscriptionEntity2 = new MessageEventSubscriptionEntity();
         messageEventSubscriptionEntity2.setEventName("messageName");
         messageEventSubscriptionEntity2.setActivityId("someActivity");
         calendar.set(2000, 1, 1);
         messageEventSubscriptionEntity2.setCreated(calendar.getTime());
         messageEventSubscriptionEntity2.insert();
-        
+
         SignalEventSubscriptionEntity signalEventSubscriptionEntity3 = new SignalEventSubscriptionEntity();
         signalEventSubscriptionEntity3.setEventName("messageName2");
         signalEventSubscriptionEntity3.setActivityId("someOtherActivity");
         calendar.set(2002, 1, 1);
         signalEventSubscriptionEntity3.setCreated(calendar.getTime());
         signalEventSubscriptionEntity3.insert();
-        
+
         return null;
       }
     });
   }
-  
-  protected void cleanDb() {    
+
+  protected void cleanDb() {
     processEngineConfiguration.getCommandExecutorTxRequired()
     .execute(new Command<Void>() {
       public Void execute(CommandContext commandContext) {
@@ -177,8 +229,8 @@ public class EventSubscriptionQueryTest extends PluggableProcessEngineTestCase {
         return null;
       }
     });
-    
+
   }
-  
+
 
 }
