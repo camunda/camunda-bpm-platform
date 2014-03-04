78e09669912b2c80493e340e5e67b96026c44883
Mehran Hatami <mehranhatamie@gmail.com>
2014-03-05 02:46:20 +0330

    final steps to fix formatting issues and use camunda's paging component.

--- examples/cockpit-failed-jobs-plugin/src/main/resources/plugin-webapp/failed-jobs-plugin/app/plugin.js
+++ examples/cockpit-failed-jobs-plugin/src/main/resources/plugin-webapp/failed-jobs-plugin/app/plugin.js
@@ -1,85 +1,74 @@
 ngDefine('cockpit.plugin.failed-jobs-plugin', function(module) {
 
-	var DashboardController = function($scope, $http, Uri) {
+  var DashboardController = function($scope, $http, Uri) {
+    
+    $scope.livePath = function(instanceId) {
+      return Uri.appUri("app://#/process-instance/" + instanceId + "/live");
+    };
+    
+    $scope.processName = function(job) {
+      return job.processDefinitionId.split(":")[0];
+    };
 
-		$scope.livePath = function(instanceId) {
-			return "/camunda/app/cockpit/default/#/process-instance/"
-					+ instanceId + "/live";
-		};
+    $scope.jobRestUrl = Uri.appUri("engine://engine/default/job");
+    $scope.totalPages = 0;
+    $scope.pageNumber = 1;
+    $scope.failedJobsCount = 0;
 
-		$scope.jobRestUrl = Uri.appUri("engine://engine/default/job");
-		$scope.totalPages = 0;
-		$scope.failedJobsCount = 0;
-		$scope.loading = true;
-		$scope.pageLoading = false;
-		$scope.failedJobs = [];
-		$scope.pageNumber = 1;
-		$scope.filterCriteria = {
-			firstResult : 0,
-			maxResults : 10,
-			withException : true
-		};
-		
-		$scope.fetchResult = function() {
-			$http.get($scope.jobRestUrl, { params : $scope.filterCriteria }).success(function(data) {
-				$scope.loading = false;
-				$scope.pageLoading = false;
-				$scope.failedJobs = data;
-			});
-		};
-		
-		var loadPage = function() {
-			if($scope.pageNumber < 1) $scope.pageNumber = 1;
-			$scope.pageLoading = true;
-			$scope.filterCriteria.firstResult = ($scope.pageNumber - 1) * 10;
-			$scope.fetchResult();
-		};
-		
-		$scope.goToPage = function(ev) {
-			var pg;
-			if(!$scope.pageNumber || isNaN(pg = parseInt($scope.pageNumber)) || pg < 1) $scope.pageNumber = 1;
-			else if($scope.pageNumber > $scope.totalPages) $scope.pageNumber = $scope.totalPages;
-			loadPage();
-		};
-		
-		$scope.goToNext = function() {
-			if($scope.pageNumber < $scope.totalPages){
-				$scope.pageNumber++;
-				loadPage();
-			}
-		};
-		
-		$scope.goToPrevious = function() {
-			if($scope.pageNumber > 1){
-				$scope.pageNumber--;
-				loadPage();
-			}
-		};
-		
-		$http.get($scope.jobRestUrl + "/count", {params : {withException : true}}).success(function(data) {
-			var pg = parseInt(($scope.failedJobsCount = data.count) / 10);
-			$scope.totalPages = (data.count % 10) ? (pg + 1) : pg;
-		});
-		
-		$scope.fetchResult();
-	};
+    $scope.loading = true;
+    $scope.failedJobs = [];
 
-	DashboardController.$inject = [ "$scope", "$http", "Uri" ];
+    $scope.filterCriteria = {
+      firstResult : 0,
+      maxResults : 10,
+      withException : true
+    };
 
-	var Configuration = function Configuration(ViewsProvider) {
+    var loadPage = function() {
+      $scope.loading = true;
+      $scope.filterCriteria.firstResult = ($scope.pageNumber - 1) * 10;
+      $http.get($scope.jobRestUrl, {
+        params : $scope.filterCriteria
+      }).success(function(data) {
+        $scope.loading = false;
+        $scope.failedJobs = data;
+      });
+    };
 
-		ViewsProvider.registerDefaultView('cockpit.dashboard', {
-			id : 'failed-jobs',
-			label : 'Failed Jobs',
-			url : 'plugin://failed-jobs-plugin/static/app/dashboard.html',
-			controller : DashboardController,
-			priority : 15
-		});
-	};
+    $scope.$watch('pageNumber', function(newValue, oldValue) {
+      if (newValue == oldValue)
+        return;
+      loadPage();
+    });
 
-	Configuration.$inject = [ 'ViewsProvider' ];
+    $http.get($scope.jobRestUrl + "/count", {
+      params : {
+        withException : true
+      }
+    }).success(function(data) {
+      var pg = parseInt(($scope.failedJobsCount = data.count) / 10);
+      $scope.totalPages = (data.count % 10) ? (pg + 1) : pg;
+    });
 
-	module.config(Configuration);
+    loadPage();
+  };
 
-	return module;
+  DashboardController.$inject = [ "$scope", "$http", "Uri" ];
+
+  var Configuration = function Configuration(ViewsProvider) {
+
+    ViewsProvider.registerDefaultView('cockpit.dashboard', {
+      id : 'failed-jobs',
+      label : 'Failed Jobs',
+      url : 'plugin://failed-jobs-plugin/static/app/dashboard.html',
+      controller : DashboardController,
+      priority : 15
+    });
+  };
+
+  Configuration.$inject = [ 'ViewsProvider' ];
+
+  module.config(Configuration);
+
+  return module;
 });
