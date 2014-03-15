044c75ea2391c6941ff7a757ffa714894f551721
Bernd Ruecker <bernd.ruecker@camunda.com>
2014-03-15 13:22:16 +0100

    fix (engine): changed default in memory database name to camunda (according to current docs)

--- engine/src/main/java/org/camunda/bpm/engine/impl/cfg/StandaloneInMemProcessEngineConfiguration.java
+++ engine/src/main/java/org/camunda/bpm/engine/impl/cfg/StandaloneInMemProcessEngineConfiguration.java
@@ -21,6 +21,6 @@ public class StandaloneInMemProcessEngineConfiguration extends StandaloneProcess
 
   public StandaloneInMemProcessEngineConfiguration() {
     this.databaseSchemaUpdate = DB_SCHEMA_UPDATE_CREATE_DROP;
-    this.jdbcUrl = "jdbc:h2:mem:activiti";
+    this.jdbcUrl = "jdbc:h2:mem:camunda";
   }
 }
