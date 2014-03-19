ebd89d8b4245524cae52f18652d9553f97568225
Valentin Vago <zeropaper@irata.ch>
2014-03-19 16:39:02 +0100

    fix(cockpit/breadcrumbs): retrieve the correct breadcrumbs information
    related to CAM-1865
    additionally look at CAM-2017

---
+++ webapps/camunda-webapp/webapp/src/main/webapp/app/cockpit/services/breadcrumbTrails.js
@@ -0,0 +1,50 @@
+/* global ngDefine: false */
+ngDefine('cockpit.services.breadcrumbTrails', function(module) {
+  'use strict';
+
+  module.factory('breadcrumbTrails', [
+    'ProcessDefinitionResource',
+    'page',
+  function(ProcessDefinitionResource, page) {
+    function breadcrumbTrails(processInstance, fetchSuperInstance, trail, index, urlSuffix) {
+      trail = trail || [];
+
+      function handleSuperProcessInstance(err, superProcessInstance) {
+
+        if (!superProcessInstance) {
+          page.breadcrumbsInsertAt(index, trail);
+          return;
+        }
+
+        // ... and fetch its process definition
+        ProcessDefinitionResource
+        .get({
+          // TODO: CAM-2017 API definition cleanup
+          id: superProcessInstance.processDefinitionId || superProcessInstance.definitionId
+        })
+        .$then(function (response) {
+          var superProcessDefinition = response.data;
+
+          // ... PREpend the breadcrumbs
+          trail = [
+            {
+              href: '#/process-definition/'+ superProcessDefinition.id +(urlSuffix ? '/'+ urlSuffix : ''),
+              label: superProcessDefinition.name || superProcessDefinition.key
+            },
+            {
+              divider: ':',
+              href: '#/process-instance/'+ superProcessInstance.id +(urlSuffix ? '/'+ urlSuffix : ''),
+              label: superProcessInstance.id.slice(0, 8) +'…'
+            }
+          ].concat(trail);
+
+          breadcrumbTrails(superProcessInstance, fetchSuperInstance, trail, index, urlSuffix);
+        });
+      }
+
+      fetchSuperInstance(processInstance, handleSuperProcessInstance);
+    }
+
+    return breadcrumbTrails;
+  }]);
+});
