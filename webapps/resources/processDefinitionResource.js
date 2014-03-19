535ee73bfe6eb17cb1330901ef69feb093699d9a
Valentin Vago <zeropaper@irata.ch>
2014-03-19 16:39:02 +0100

    fix(cockpit/breadcrumbs): retrieve the correct breadcrumbs information
    related to CAM-1865
    additionally look at CAM-2017

--- webapps/resources/processDefinitionResource.js
+++ webapps/resources/processDefinitionResource.js
@@ -4,10 +4,21 @@ ngDefine('cockpit.resources', function(module) {
 
   var Resource = [ '$resource', 'Uri', function ($resource, Uri) {
 
-    return $resource(Uri.appUri('engine://engine/:engine/process-definition/:id/:action'), { id: '@id' }, {
-      queryStatistics: { method: 'GET', isArray: true, params: { id: 'statistics' }},
-      queryActivityStatistics: { method: 'GET', isArray: true, params: { action: 'statistics' }},
-      getBpmn20Xml: { method: 'GET', params: { action: 'xml' }, cache: true }
+    return $resource(Uri.appUri('engine://engine/:engine/process-definition/:id/:action'), {
+      id: '@id'
+    }, {
+      queryStatistics: {
+        method: 'GET', isArray: true, params: {
+          id: 'statistics'
+        }
+      },
+      queryActivityStatistics: {
+        method: 'GET', isArray: true, params: {
+          action: 'statistics'
+        }
+      },
+      getBpmn20Xml: {
+        method: 'GET', params: { action: 'xml' }, cache: true }
     });
   }];
 
