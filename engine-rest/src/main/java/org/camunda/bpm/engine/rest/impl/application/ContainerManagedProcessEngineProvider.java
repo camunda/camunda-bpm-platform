85c4c4003bf906da43c4f6204d23e2fb9479bbf5
Daniel Meyer <daniel.meyer@camunda.com>
2014-03-26 15:37:36 +0100

    feat(webapp): provide standalone distributions for camunda webapp
    related to #CAM-2057

--- engine-rest/src/main/java/org/camunda/bpm/engine/rest/impl/application/ContainerManagedProcessEngineProvider.java
+++ engine-rest/src/main/java/org/camunda/bpm/engine/rest/impl/application/ContainerManagedProcessEngineProvider.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,28 +17,34 @@ import java.util.Set;
 import org.camunda.bpm.BpmPlatform;
 import org.camunda.bpm.ProcessEngineService;
 import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngines;
 import org.camunda.bpm.engine.rest.spi.ProcessEngineProvider;
 
 /**
  * <p>Uses the {@link ProcessEngineService} and exposes the default process engine</p>
- * 
+ *
  * @author Daniel Meyer
  */
 public class ContainerManagedProcessEngineProvider implements ProcessEngineProvider {
 
   @Override
   public ProcessEngine getDefaultProcessEngine() {
-    return BpmPlatform.getDefaultProcessEngine();
+    ProcessEngine defaultProcessEngine = BpmPlatform.getDefaultProcessEngine();
+    if(defaultProcessEngine != null) {
+      return defaultProcessEngine;
+    } else {
+      return ProcessEngines.getDefaultProcessEngine(false);
+    }
   }
 
   @Override
   public ProcessEngine getProcessEngine(String name) {
-    return BpmPlatform.getProcessEngineService().getProcessEngine(name);
+    return ProcessEngines.getProcessEngine(name);
   }
 
   @Override
   public Set<String> getProcessEngineNames() {
-    return BpmPlatform.getProcessEngineService().getProcessEngineNames();
+    return ProcessEngines.getProcessEngines().keySet();
   }
 
 }
--- engine/src/main/java/org/camunda/bpm/engine/ProcessEngines.java
+++ engine/src/main/java/org/camunda/bpm/engine/ProcessEngines.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -41,43 +41,47 @@ import org.camunda.bpm.engine.impl.util.ReflectUtil;
  * All created {@link ProcessEngine}s will be registered with this class.
  * <br>
  * The activiti-webapp-init webapp will
- * call the {@link #init()} method when the webapp is deployed and it will call the 
- * {@link #destroy()} method when the webapp is destroyed, using a context-listener 
- * (<code>org.camunda.bpm.engine.test.impl.servlet.listener.ProcessEnginesServletContextListener</code>).  That way, 
- * all applications can just use the {@link #getProcessEngines()} to 
+ * call the {@link #init()} method when the webapp is deployed and it will call the
+ * {@link #destroy()} method when the webapp is destroyed, using a context-listener
+ * (<code>org.camunda.bpm.engine.test.impl.servlet.listener.ProcessEnginesServletContextListener</code>).  That way,
+ * all applications can just use the {@link #getProcessEngines()} to
  * obtain pre-initialized and cached process engines. <br>
  * <br>
- * Please note that there is <b>no lazy initialization</b> of process engines, so make sure the 
+ * Please note that there is <b>no lazy initialization</b> of process engines, so make sure the
  * context-listener is configured or {@link ProcessEngine}s are already created so they were registered
  * on this class.<br>
  * <br>
- * The {@link #init()} method will try to build one {@link ProcessEngine} for 
+ * The {@link #init()} method will try to build one {@link ProcessEngine} for
  * each camunda.cfg.xml file found on the classpath.  If you have more then one,
  * make sure you specify different process.engine.name values.
- *  
+ *
  * @author Tom Baeyens
  * @author Joram Barrez
  */
 public abstract class ProcessEngines {
-  
+
   private static Logger log = Logger.getLogger(ProcessEngines.class.getName());
-  
+
   public static final String NAME_DEFAULT = "default";
-  
-  protected static boolean isInitialized = false; 
+
+  protected static boolean isInitialized = false;
   protected static Map<String, ProcessEngine> processEngines = new HashMap<String, ProcessEngine>();
   protected static Map<String, ProcessEngineInfo> processEngineInfosByName = new HashMap<String, ProcessEngineInfo>();
   protected static Map<String, ProcessEngineInfo> processEngineInfosByResourceUrl = new HashMap<String, ProcessEngineInfo>();
   protected static List<ProcessEngineInfo> processEngineInfos = new ArrayList<ProcessEngineInfo>();
-  
-  /** Initializes all process engines that can be found on the classpath for 
+
+  public synchronized static void init() {
+    init(true);
+  }
+
+  /** Initializes all process engines that can be found on the classpath for
    * resources <code>camunda.cfg.xml</code> (plain Activiti style configuration)
    * and for resources <code>activiti-context.xml</code> (Spring style configuration). */
-  public synchronized static void init() {
+  public synchronized static void init(boolean forceCreate) {
     if (!isInitialized) {
       if(processEngines == null) {
         // Create new map to store process-engines if current map is null
-        processEngines = new HashMap<String, ProcessEngine>();        
+        processEngines = new HashMap<String, ProcessEngine>();
       }
       ClassLoader classLoader = ReflectUtil.getClassLoader();
       Enumeration<URL> resources = null;
@@ -87,10 +91,14 @@ public abstract class ProcessEngines {
         try {
           resources = classLoader.getResources("activiti.cfg.xml");
         } catch(IOException ex) {
-          throw new ProcessEngineException("problem retrieving camunda.cfg.xml and activiti.cfg.xml resources on the classpath: "+System.getProperty("java.class.path"), ex);
+          if(forceCreate) {
+            throw new ProcessEngineException("problem retrieving camunda.cfg.xml and activiti.cfg.xml resources on the classpath: "+System.getProperty("java.class.path"), ex);
+          } else {
+            return;
+          }
         }
       }
-      
+
       // Remove duplicated configuration URL's using set. Some classloaders may return identical URL's twice, causing duplicate startups
       Set<URL> configUrls = new HashSet<URL>();
       while (resources.hasMoreElements()) {
@@ -100,11 +108,15 @@ public abstract class ProcessEngines {
         URL resource = iterator.next();
         initProcessEngineFromResource(resource);
       }
-      
+
       try {
         resources = classLoader.getResources("activiti-context.xml");
       } catch (IOException e) {
-        throw new ProcessEngineException("problem retrieving activiti-context.xml resources on the classpath: "+System.getProperty("java.class.path"), e);
+        if(forceCreate) {
+          throw new ProcessEngineException("problem retrieving activiti-context.xml resources on the classpath: "+System.getProperty("java.class.path"), e);
+        } else {
+          return;
+        }
       }
       while (resources.hasMoreElements()) {
         URL resource = resources.nextElement();
@@ -122,26 +134,26 @@ public abstract class ProcessEngines {
       Class< ? > springConfigurationHelperClass = ReflectUtil.loadClass("org.camunda.bpm.engine.test.spring.SpringConfigurationHelper");
       Method method = springConfigurationHelperClass.getMethod("buildProcessEngine", new Class<?>[]{URL.class});
       ProcessEngine processEngine = (ProcessEngine) method.invoke(null, new Object[]{resource});
-      
+
       String processEngineName = processEngine.getName();
       ProcessEngineInfo processEngineInfo = new ProcessEngineInfoImpl(processEngineName, resource.toString(), null);
       processEngineInfosByName.put(processEngineName, processEngineInfo);
       processEngineInfosByResourceUrl.put(resource.toString(), processEngineInfo);
-      
+
     } catch (Exception e) {
       throw new ProcessEngineException("couldn't initialize process engine from spring configuration resource "+resource.toString()+": "+e.getMessage(), e);
-    } 
+    }
   }
- 
+
   /**
-   * Registers the given process engine. No {@link ProcessEngineInfo} will be 
+   * Registers the given process engine. No {@link ProcessEngineInfo} will be
    * available for this process engine. An engine that is registered will be closed
    * when the {@link ProcessEngines#destroy()} is called.
    */
   public static void registerProcessEngine(ProcessEngine processEngine) {
     processEngines.put(processEngine.getName(), processEngine);
   }
-  
+
   /**
    * Unregisters the given process engine.
    */
@@ -194,14 +206,14 @@ public abstract class ProcessEngines {
       inputStream = resource.openStream();
       ProcessEngineConfiguration processEngineConfiguration = ProcessEngineConfiguration.createProcessEngineConfigurationFromInputStream(inputStream);
       return processEngineConfiguration.buildProcessEngine();
-      
+
     } catch (IOException e) {
       throw new ProcessEngineException("couldn't open resource stream: "+e.getMessage(), e);
     } finally {
       IoUtil.closeSilently(inputStream);
     }
   }
-  
+
   /** Get initialization results. */
   public static List<ProcessEngineInfo> getProcessEngineInfos() {
     return processEngineInfos;
@@ -216,18 +228,26 @@ public abstract class ProcessEngines {
   }
 
   public static ProcessEngine getDefaultProcessEngine() {
-    return getProcessEngine(NAME_DEFAULT);
+    return getDefaultProcessEngine(true);
   }
 
-  /** obtain a process engine by name.  
-   * @param processEngineName is the name of the process engine or null for the default process engine.  */
+  public static ProcessEngine getDefaultProcessEngine(boolean forceCreate) {
+    return getProcessEngine(NAME_DEFAULT, forceCreate);
+  }
+
   public static ProcessEngine getProcessEngine(String processEngineName) {
+    return getProcessEngine(processEngineName, true);
+  }
+
+  /** obtain a process engine by name.
+   * @param processEngineName is the name of the process engine or null for the default process engine.  */
+  public static ProcessEngine getProcessEngine(String processEngineName, boolean forceCreate) {
     if (!isInitialized) {
-      init();
+      init(forceCreate);
     }
     return processEngines.get(processEngineName);
   }
-  
+
   /** retries to initialize a process engine that previously failed.
    */
   public static ProcessEngineInfo retry(String resourceUrl) {
@@ -238,20 +258,20 @@ public abstract class ProcessEngines {
       throw new ProcessEngineException("invalid url: "+resourceUrl, e);
     }
   }
-  
-  /** provides access to process engine to application clients in a 
-   * managed server environment.  
+
+  /** provides access to process engine to application clients in a
+   * managed server environment.
    */
   public static Map<String, ProcessEngine> getProcessEngines() {
     return processEngines;
   }
-  
+
   /** closes all process engines.  This method should be called when the server shuts down. */
   public synchronized static void destroy() {
     if (isInitialized) {
       Map<String, ProcessEngine> engines = new HashMap<String, ProcessEngine>(processEngines);
       processEngines = new HashMap<String, ProcessEngine>();
-      
+
       for (String processEngineName: engines.keySet()) {
         ProcessEngine processEngine = engines.get(processEngineName);
         try {
@@ -260,12 +280,13 @@ public abstract class ProcessEngines {
           log.log(Level.SEVERE, "exception while closing "+(processEngineName==null ? "the default process engine" : "process engine "+processEngineName), e);
         }
       }
-      
+
       processEngineInfosByName.clear();
       processEngineInfosByResourceUrl.clear();
       processEngineInfos.clear();
-      
+
       isInitialized = false;
     }
   }
+
 }
