da5b5a49385823bf4769a2ea54cf8dbec48f4e3d
Daniel Meyer <daniel.meyer@camunda.com>
2014-03-28 11:00:39 +0100

    doc(engine): javadoc for BadUserRequestException
    related to #CAM-1598

--- engine/src/main/java/org/camunda/bpm/engine/BadUserRequestException.java
+++ engine/src/main/java/org/camunda/bpm/engine/BadUserRequestException.java
@@ -14,9 +14,22 @@
 package org.camunda.bpm.engine;
 
 /**
+ * <p>Exception resulting from a bad user request. A bad user request is
+ * an interaction where the user requests some non-existing state or
+ * attempts to perform an illegal action on some entity.</p>
+ *
+ * <p><strong>Examples:</strong>
+ * <ul>
+ *  <li>cancelling a non-existing process instance</li>
+ *  <li>triggering a suspended execution...</li>
+ * </ul>
+ * </p>
+ *
  * @author Sebastian Menski
  */
 public class BadUserRequestException extends ProcessEngineException {
+
+  private static final long serialVersionUID = 1L;
 
   public BadUserRequestException() {
   }
