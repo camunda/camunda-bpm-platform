0cc901dedd6f2b3f253452d98e8118aee03b0de4
Valentin Vago <zeropaper@irata.ch>
2014-04-02 09:09:33 +0200

    fix(cockpit): coorect the baseUrl for require.js
    This commit also corrects some paths to polyfills used for IE9 compatibility
    
    Related to CAM-2091, CAM-2092

--- webapps/webapp/src/main/webapp/app/tasklist/tasklist-bootstrap.js
+++ webapps/webapp/src/main/webapp/app/tasklist/tasklist-bootstrap.js
@@ -1,11 +1,11 @@
 (function(document, window, require) {
   'use strict';
 
-  var baseUrl = '../../../';
+  var baseUrl = document.getElementsByTagName('base')[0].getAttribute('app-root') +'/';
   var APP_NAME = 'tasklist';
   var pluginPackages = window.PLUGIN_PACKAGES || [];
 
-  require([baseUrl +'require-conf'], function(rjsConf) {
+  require([baseUrl +'require-conf.js'], function(rjsConf) {
     require({
       baseUrl:    baseUrl,
       urlArgs:    rjsConf.urlArgs,
