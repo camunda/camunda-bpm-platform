3867d7412adf9f9c530ca785ffb830a741a54b29
Valentin Vago <zeropaper@irata.ch>
2014-07-23 11:31:42 +0200

    fix(pages): solve dependencies for pages

--- webapps/client/scripts/pages/main.js
+++ webapps/client/scripts/pages/main.js
@@ -1,17 +1,17 @@
 ngDefine('admin.pages', [
+  'module:ngRoute:angular-route',
+  'module:cam.commons:camunda-commons-ui',
   './authorizations',
   './authorizationCreate',
   './authorizationDeleteConfirm',
-  './users', 
-  './userCreate', 
-  './userEdit', 
+  './users',
+  './userCreate',
+  './userEdit',
   './groups',
   './groupCreate',
   './groupEdit',
   './groupMembershipsCreate',
   './setup',
   './system',
-  './systemSettingsGeneral',
-], function(module) {
-
-});
+  './systemSettingsGeneral'
+], function(module) {});
