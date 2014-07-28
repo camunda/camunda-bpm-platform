1d1cc3d225f270eca2e214546eb49c271d989ff6
Valentin Vago <zeropaper@irata.ch>
2014-07-28 08:02:34 +0200

    refactor(misc): little changes

--- webapps/Gruntfile.js
+++ webapps/Gruntfile.js
@@ -56,13 +56,13 @@ module.exports = function(grunt) {
     mode = mode || 'prod';
 
     grunt.config.data.buildTarget = (mode === 'prod' ? config.prodTarget : config.devTarget);
-    grunt.log.subhead('Will build the project in "'+ mode +'" mode and place it in "'+ grunt.config('buildTarget') +'"');
+    grunt.log.subhead('Will build the "'+ pkg.name +'" project in "'+ mode +'" mode and place it in "'+ grunt.config('buildTarget') +'"');
     if (mode === 'dev') {
       grunt.log.writeln('Will serve on port "'+
-        config.connectPort
-        +'" and liverreload available on port "'+
-        config.livereloadPort
-        +'"');
+        config.connectPort +
+        '" and liverreload available on port "'+
+        config.livereloadPort +
+        '"');
     }
 
     var tasks = [
