38942a0fb78cc50fa45119fd9131d0f6446966f3
Valentin Vago <zeropaper@irata.ch>
2014-07-28 07:57:49 +0200

    fix(build): correct the var name of livereload port

--- webapps/grunt/config/copy.js
+++ webapps/grunt/config/copy.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/* jshint node: true */
 var commentLineExp =  /^[\s]*<!-- (\/|#) (CE|EE)/;
 var requireConfExp =  /require-conf.js$/;
 
@@ -21,7 +21,7 @@ function distFileProcessing(content, srcpath) {
 
 function devFileProcessing(content, srcpath) {
   /* jshint validthis: true */
-  var liveReloadPort = this.config.get('app.liveReloadPort');
+  var liveReloadPort = this.config.get('pkg.gruntConfig.livereloadPort');
   /* jshint validthis: false */
 
   if (requireConfExp.test(srcpath)) {
