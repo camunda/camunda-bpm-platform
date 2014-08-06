77e051f718e7952ec89ba0a6ba991e7fad0a5216
Valentin Vago <zeropaper@irata.ch>
2014-08-06 15:23:45 +0200

    refactor(tasks list): simplify and improve the listing of tasks

--- webapps/client/scripts/pile/directives/cam-tasklist-tasks.js
+++ webapps/client/scripts/pile/directives/cam-tasklist-tasks.js
@@ -44,15 +44,51 @@ define([
 
         scope.sorting = angular.element('[cam-sorting-choices]').scope();
 
-        scope.sorting.$on('sorting.by.change', function(/*info, value*/) {
-          // console.info('sorting.by.change', value);
-          loadItems();
-        });
-        scope.sorting.$on('sorting.order.change', function(/*info, value*/) {
-          // console.info('sorting.order.change', value);
-          loadItems();
-        });
+        scope.sorting.$on('sorting.by.change', loadItems);
 
+        scope.sorting.$on('sorting.order.change', loadItems);
+
+        function setCurrentTask(task) {
+          task._embedded = task._embedded || {};
+          // if (!task._embedded.processDefinition) {
+          //   task._embedded.processDefinition = taskProcessDefinition(task);
+          // }
+          $rootScope.currentTask = task;
+          $rootScope.$broadcast('tasklist.task.current');
+        }
+
+        // function taskProcessDefinition(task) {
+        //   if (task._embedded && task._embedded.processDefinition) {
+        //     return task._embedded.processDefinition;
+        //   }
+
+        //   for (var p in scope.processDefinitions) {
+        //     var procDef = scope.processDefinitions[p];
+        //     if (procDef.id === task.processDefinitionId) {
+        //       return procDef;
+        //     }
+        //   }
+        // }
+
+        function loadItems() {
+          scope.loading = true;
+          scope.tasks = [];
+
+          var where = buildWhere(scope.sorting.order, scope.sorting.by);
+
+          Task.list(where, function(err, res) {
+            scope.loading = false;
+            if (err) { throw err; }
+
+            scope.totalItems = res.count;
+            scope.processDefinitions = res._embedded.processDefinition;
+            // TODO: refactor that when #CAM-2550 done
+            scope.tasks = res._embedded.task || res._embedded.tasks;
+            // angular.forEach(scope.tasks, function(task, t) {
+            //   // taskProcessDefinition(task);
+            // });
+          });
+        }
 
         function buildWhere(order, by) {
           var where = {};
@@ -78,6 +114,10 @@ define([
           return where;
         }
 
+
+        scope.pageChange = loadItems;
+
+
         scope.lookupTask = function(val) {
           var deferred = $q.defer();
 
@@ -102,43 +142,15 @@ define([
 
 
         scope.selectedTask = function($item) {
-          console.info('scope', scope);
-          $rootScope.currentTask = $item;
-          $rootScope.$broadcast('tasklist.task.current');
+          setCurrentTask($item);
           scope.searchTask = '';
         };
 
-        function loadItems() {
-          scope.loading = true;
-          scope.tasks = [];
-
-          var where = buildWhere(scope.sorting.order, scope.sorting.by);
-
-          Task.list(where, function(err, res) {
-            scope.loading = false;
-
-            if (err) {
-              throw err;
-            }
-
-            scope.totalItems = res.count;
-            scope.tasks = res._embedded.tasks;
-          });
-        }
-
-
-        scope.pageChange = loadItems;
-
 
         scope.focus = function(delta) {
-          $rootScope.currentTask = scope.tasks[delta];
-          $rootScope.$broadcast('tasklist.task.current');
+          setCurrentTask(scope.tasks[delta]);
         };
 
-
-        // scope.batchOperationSelect = function() {
-        //   console.info('selected task', this);
-        // };
 
         scope.$on('tasklist.pile.current', function() {
           if (
