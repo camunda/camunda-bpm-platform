a3aa673b95fb9c03cc38164a21884e8683bccc66
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2014-09-01 10:27:45 +0200

    chore(engine-rest): test and fix EqualsList and EqualsMap matchers

--- engine-rest/src/test/java/org/camunda/bpm/engine/rest/helper/EqualsMap.java
+++ engine-rest/src/test/java/org/camunda/bpm/engine/rest/helper/EqualsMap.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,36 +21,28 @@ import org.mockito.ArgumentMatcher;
 public class EqualsMap extends ArgumentMatcher<Map<String, Object>> {
 
   private Map<String, Object> mapToCompare;
-  
+
   public EqualsMap(Map<String, Object> mapToCompare) {
     this.mapToCompare = mapToCompare;
   }
-  
+
   @Override
   public boolean matches(Object argument) {
-    if (argument == null) {
-      if (mapToCompare == null) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-    
-    Map<String, Object> argumentMap = (Map<String, Object>) argument;
-    
-    Set<Entry<String, Object>> setToCompare = mapToCompare.entrySet();
-    int initialSize = setToCompare.size();
-    
-    Set<Entry<String, Object>> argumentSet = argumentMap.entrySet();
-    
-    // intersection of the two sets
-    setToCompare.retainAll(argumentSet);
-    
-    if (initialSize == setToCompare.size()) {
-      return true;
-    } else {
+    if ((argument == null && mapToCompare != null) ||
+        (argument != null && mapToCompare == null)) {
       return false;
     }
+
+    if (argument == null && mapToCompare == null) {
+      return true;
+    }
+
+    Map<String, Object> argumentMap = (Map<String, Object>) argument;
+
+    Set<Entry<String, Object>> setToCompare = mapToCompare.entrySet();
+    Set<Entry<String, Object>> argumentSet = argumentMap.entrySet();
+
+    return setToCompare.equals(argumentSet);
   }
 
 }
