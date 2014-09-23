5c2b71f8aa415855e77f22557d3b603ba66d3b48
Roman Smirnov <roman.smirnov@camunda.com>
2014-09-23 11:17:55 +0200

    chore(defaultRetries): provide a further test case
    related to CAM-2539

--- engine/src/test/java/org/camunda/bpm/engine/test/standalone/initialization/ProcessEngineInitializationTest.java
+++ engine/src/test/java/org/camunda/bpm/engine/test/standalone/initialization/ProcessEngineInitializationTest.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,10 +13,13 @@
 package org.camunda.bpm.engine.test.standalone.initialization;
 
 import org.camunda.bpm.engine.ProcessEngineConfiguration;
+import org.camunda.bpm.engine.impl.persistence.entity.JobEntity;
 import org.camunda.bpm.engine.impl.test.PvmTestCase;
 
 /**
  * @author Tom Baeyens
+ * @author Stefan Hentschel
+ * @author Roman Smirnov
  */
 public class ProcessEngineInitializationTest extends PvmTestCase {
 
@@ -30,6 +33,20 @@ public class ProcessEngineInitializationTest extends PvmTestCase {
       // OK
       assertTextPresent("no activiti tables in db", e.getMessage());
     }
+  }
+
+  public void testDefaultRetries() {
+    ProcessEngineConfiguration configuration = ProcessEngineConfiguration
+      .createProcessEngineConfigurationFromResource("org/camunda/bpm/engine/test/standalone/initialization/defaultretries.camunda.cfg.xml");
+
+    assertEquals(JobEntity.DEFAULT_RETRIES, configuration.getDefaultNumberOfRetries());
+  }
+
+  public void testCustomDefaultRetries() {
+    ProcessEngineConfiguration configuration = ProcessEngineConfiguration
+      .createProcessEngineConfigurationFromResource("org/camunda/bpm/engine/test/standalone/initialization/customdefaultretries.camunda.cfg.xml");
+
+    assertEquals(5, configuration.getDefaultNumberOfRetries());
   }
 
 }
