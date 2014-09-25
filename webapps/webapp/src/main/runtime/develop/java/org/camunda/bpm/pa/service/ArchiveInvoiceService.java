710f9b96366d182816c35da5f1c1a41daaa85cc4
Roman Smirnov <roman.smirnov@camunda.com>
2014-09-25 14:01:25 +0200

    fix(demo): change package declaration

--- webapps/webapp/src/main/runtime/develop/java/org/camunda/bpm/pa/service/ArchiveInvoiceService.java
+++ webapps/webapp/src/main/runtime/develop/java/org/camunda/bpm/pa/service/ArchiveInvoiceService.java
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.camunda.bpm.example.invoice.service;
+package org.camunda.bpm.pa.service;
 
 import java.util.logging.Logger;
 
