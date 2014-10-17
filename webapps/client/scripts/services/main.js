ab58d9c5baf120ccde69d3ed056d25de49ecbbfb
Roman Smirnov <roman.smirnov@camunda.com>
2014-10-17 16:51:14 +0200

    chore(service): remove 'search' service

--- webapps/client/scripts/services/main.js
+++ webapps/client/scripts/services/main.js
@@ -7,7 +7,6 @@ ngDefine('cockpit.services', [
   //'./activityInstance',
   './transform',
   './variables',
-  './search',
   'module:cockpit.services.page:./page',
   'module:cockpit.services.breadcrumbTrails:./breadcrumbTrails'
 ], function() {});
