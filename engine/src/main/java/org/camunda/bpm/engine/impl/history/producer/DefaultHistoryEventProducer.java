4906e371aa0602ce9983483d2b86309609a2ef31
John D. Ament <johndament@apache.org>
2014-10-27 13:26:16 -0400

    bug(minor) : check for null when calling to string on property values.  Fix for CAM-2962

--- engine/src/main/java/org/camunda/bpm/engine/impl/history/producer/DefaultHistoryEventProducer.java
+++ engine/src/main/java/org/camunda/bpm/engine/impl/history/producer/DefaultHistoryEventProducer.java
@@ -499,7 +499,9 @@ public class DefaultHistoryEventProducer implements HistoryEventProducer {
     historicFormPropertyEntity.setProcessDefinitionId(execution.getProcessDefinitionId());
     historicFormPropertyEntity.setProcessInstanceId(execution.getProcessInstanceId());
     historicFormPropertyEntity.setPropertyId(propertyId);
-    historicFormPropertyEntity.setPropertyValue(propertyValue.toString());
+    if (propertyValue != null) {
+      historicFormPropertyEntity.setPropertyValue(propertyValue.toString());
+    }
     historicFormPropertyEntity.setTaskId(taskId);
 
     return historicFormPropertyEntity;
