f10f2e3d7584e4b6ea42f3779cc85997b7121d82
Valentin Vago <zeropaper@irata.ch>
2014-10-30 11:07:21 +0100

    impr(layout): only the toggler should trigger collapse / expand
    Related to: CAM-2943

--- webapps/client/scripts/navigation/controllers/cam-layout-ctrl.js
+++ webapps/client/scripts/navigation/controllers/cam-layout-ctrl.js
@@ -7,25 +7,6 @@ define([
   var $ = angular.element;
   var $bdy = $('body');
 
-  function navsOpen() {
-    return $bdy.hasClass('filters-column-open') ||
-           $bdy.hasClass('list-column-open');
-  }
-
-  function isNavChild(el) {
-    return $(el).parents('.task-filters, .task-list').length > 0;
-  }
-
-
-  $bdy.click(function(evt) {
-    if (navsOpen() &&
-        !isNavChild(evt.target) &&
-        !evt.isDefaultPrevented()) {
-      $bdy.removeClass('filters-column-open list-column-open');
-    }
-  });
-
-
   return [
     '$scope',
   function(
