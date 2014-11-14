4e340b7a588ce9f26649b48ab8e39c6c984a98c2
Roman Smirnov <roman.smirnov@camunda.com>
2014-11-14 11:38:20 +0100

    chore(clean-up): remove dead code

--- webapps/client/scripts/navigation/directives/cam-tasklist-navigation.js
+++ webapps/client/scripts/navigation/directives/cam-tasklist-navigation.js
@@ -9,69 +9,7 @@ define([
 
       template: template,
 
-      controller: [
-        '$rootScope',
-        '$scope',
-        'Uri',
-      function(
-        $rootScope,
-        $scope,
-        Uri
-      ) {
-        var auth;
-
-        function reset() {
-          $scope.links = [
-            {
-              title: '',
-              icon: 'home',
-              links: []
-            }
-          ];
-
-          $scope.username = null;
-        }
-
-
-        function refresh() {
-          auth = $rootScope.authentication;
-
-          if (!auth) {
-            return reset();
-          }
-
-          if ($scope.username === auth.name) {
-            return;
-          }
-
-          reset();
-
-          $scope.username = auth.name;
-
-          var appLinks = {
-            admin: {
-              title: 'Admin',
-              href: Uri.appUri('adminbase://:engine/#')
-            },
-            cockpit: {
-              title: 'Cockpit',
-              href: Uri.appUri('cockpitbase://:engine/#')
-            }
-          };
-
-          angular.forEach(appLinks, function(info, appName) {
-            if (auth.canAccess(appName)) {
-              $scope.links[0].links.push(info);
-            }
-          });
-        }
-
-        $rootScope.$on('authentication.login.success', refresh);
-        $rootScope.$on('authentication.logout.success', refresh);
-
-        // initializes...
-        refresh();
-      }]
+      controller: [function() {}]
     };
   };
 });
