b2505ccc66a581d34c58dd628eac4bb62a7e67c9
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2014-12-02 14:13:41 +0100

    test(cdi): bean resolution during deployment
    - e.g. for timer start events
    
    test case for CAM-3163

---
+++ qa/integration-tests-engine/src/test/java/org/camunda/bpm/integrationtest/functional/cdi/beans/TimerStartBean.java
@@ -0,0 +1,45 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.integrationtest.functional.cdi.beans;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+/**
+ * @author Thorben Lindhauer
+ *
+ */
+@Named
+@ApplicationScoped
+public class TimerStartBean {
+
+  public String getCycle() {
+    Calendar calendar = Calendar.getInstance();
+    calendar.setTime(new Date());
+    calendar.add(Calendar.SECOND, 10);
+    Date startTime = calendar.getTime();
+    return iso8601(startTime);
+  }
+
+  private String iso8601(Date date) {
+    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+    String dateStr = dateFormat.format(date);
+    System.out.println("Date: " + dateStr);
+    return dateStr;
+  }
+}
