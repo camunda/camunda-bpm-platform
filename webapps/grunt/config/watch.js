e48d9c40d0b4cb7b8db3899fe228c62d9ffa921d
Valentin Vago <zeropaper@irata.ch>
2015-01-28 10:57:34 +0100

    style(cleanup): remove some comments

--- webapps/grunt/config/watch.js
+++ webapps/grunt/config/watch.js
@@ -34,51 +34,9 @@ module.exports = function(config) {
         '<%= pkg.gruntConfig.clientDir %>/scripts/**/*.{js,html}'
       ],
       tasks: [
-        // 'newer:jshint:scripts',
-        // 'requirejs:dependencies',
         'requirejs:scripts'
       ]
     },
-
-    // sdk: {
-    //   files: [
-    //     'node_modules/camunda-bpm-sdk-js/dist/**/*.js'
-    //   ],
-    //   tasks: [
-    //     'copy:sdk',
-    //     'requirejs:scripts'
-    //   ]
-    // },
-
-    // commons: {
-    //   files: [
-    //     'node_modules/camunda-commons/lib/**/*.{js,html}'
-    //   ],
-    //   tasks: [
-    //     'requirejs:scripts'
-    //   ]
-    // },
-
-    // unitTest: {
-    //   files: [
-    //     'grunt/config/jasmine_node.js',
-    //     'test/unit/**/*Spec.js'
-    //   ],
-    //   tasks: [
-    //     'jasmine_node:unit'
-    //   ]
-    // },
-
-    // integrationTest: {
-    //   files: [
-    //     'grunt/config/karma.js',
-    //     'test/integration/main.js',
-    //     'test/integration/**/*Spec.js'
-    //   ],
-    //   tasks: [
-    //     'karma:integration'
-    //   ]
-    // },
 
     served: {
       options: {
