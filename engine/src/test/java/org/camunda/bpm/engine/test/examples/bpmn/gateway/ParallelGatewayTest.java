88e7d17060602d72bef384c20fb8b7cd2ff111aa
Filip Hrisafov <filip.hrisafov@gmail.com>
2015-02-18 23:02:33 +0100

    add failing test according to the description in https://github.com/camunda/camunda-bpm-platform/pull/137#discussion_r24885042

--- engine/src/test/java/org/camunda/bpm/engine/test/examples/bpmn/gateway/ParallelGatewayTest.java
+++ engine/src/test/java/org/camunda/bpm/engine/test/examples/bpmn/gateway/ParallelGatewayTest.java
@@ -15,7 +15,13 @@ package org.camunda.bpm.engine.test.examples.bpmn.gateway;
 
 import java.util.List;
 
+import org.camunda.bpm.engine.impl.interceptor.Command;
+import org.camunda.bpm.engine.impl.interceptor.CommandContext;
+import org.camunda.bpm.engine.impl.interceptor.CommandExecutor;
+import org.camunda.bpm.engine.impl.persistence.entity.ExecutionEntity;
+import org.camunda.bpm.engine.impl.persistence.entity.ExecutionManager;
 import org.camunda.bpm.engine.impl.test.PluggableProcessEngineTestCase;
+import org.camunda.bpm.engine.runtime.Execution;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.task.TaskQuery;
@@ -139,4 +145,48 @@ public class ParallelGatewayTest extends PluggableProcessEngineTestCase {
 	assertEquals("The parallel gateway should have been passed now", 2, tasks.size());
   }
   
+  /**
+   * https://app.camunda.com/jira/browse/CAM-3472
+   */
+  @Test
+  @Deployment
+  public void testParallelGatewaySpecFinishOneFlowAfterUpgrade() {
+    // Start the process
+    ProcessInstance processInstance = runtimeService
+        .startProcessInstanceByKey("ParallelGatewayOneAfterUpgrade");
+
+    Task task = taskService.createTaskQuery()
+               .processInstanceId(processInstance.getId())
+               .taskDefinitionKey("task1").singleResult();
+    taskService.complete(task.getId());
+
+  
+    Execution execution = runtimeService.createExecutionQuery().activityId("join").singleResult();
+    final String executionId = execution.getId();
+    //Set the transitionId to null in the DB
+  
+    Command<Void> setTransitionNullCmd = new Command<Void>() {
+      @Override
+      public Void execute(CommandContext commandContext) {
+        ExecutionManager executionManager = commandContext.getExecutionManager();
+        ExecutionEntity executionEntity = executionManager.findExecutionById(executionId);
+        executionEntity.setTransitionId(null);
+        return null;
+      }
+    };
+  
+    CommandExecutor executor = processEngineConfiguration.getCommandExecutorTxRequired();
+    executor.execute(setTransitionNullCmd);
+
+    task = taskService.createTaskQuery()
+            .processInstanceId(processInstance.getId())
+            .taskDefinitionKey("task2").singleResult();
+    taskService.complete(task.getId());
+
+    List<Task> tasks  = taskService.createTaskQuery()
+            .processInstanceId(processInstance.getId())
+            .taskDefinitionKey("finalTask").list();
+    // After the completion of the task2 the parallel gateway would be passed
+    assertEquals("The parallel gateway should have been passed", 1, tasks.size());
+  }
 }
