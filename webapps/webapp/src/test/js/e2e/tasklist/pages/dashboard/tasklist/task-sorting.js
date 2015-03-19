de28e02f688d39fa3737028192ab0dc70fd812ab
Sebastian Stamm <sebastian.stamm@camunda.com>
2015-03-19 11:30:27 +0100

    chore(tests): adapt tests for tasklist sorting
    related to CAM-3567, CAM-3595

--- webapps/webapp/src/test/js/e2e/tasklist/pages/dashboard/tasklist/task-sorting.js
+++ webapps/webapp/src/test/js/e2e/tasklist/pages/dashboard/tasklist/task-sorting.js
@@ -79,11 +79,12 @@ module.exports = Page.extend({
       varInput = true;
     }
     this.addSortingButton().click();
-    this.newSortingSelectionListElement(sortingType).click().then(function() {
+    var listElement = this.newSortingSelectionListElement(sortingType);
+    listElement.click().then(function() {
       if (varInput) {
-        element(by.css('.variable-inputs')).element(by.model('variable')).sendKeys(variableName);
-        element(by.css('.variable-inputs')).element(by.cssContainingText('.ng-scope', variableType)).click();
-        element(by.css('.variable-inputs')).element(by.css('[ng-click="applySorting($event)"]')).click();
+        listElement.element(by.xpath('..')).element(by.model('variable')).sendKeys(variableName);
+        listElement.element(by.xpath('..')).element(by.cssContainingText('.ng-scope', variableType)).click();
+        listElement.element(by.xpath('..')).element(by.css('[ng-click="applySorting($event)"]')).click();
       }
     });
   },
