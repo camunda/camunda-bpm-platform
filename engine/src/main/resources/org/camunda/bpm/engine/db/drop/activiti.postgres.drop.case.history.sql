b1091b70d4226f0644472dbce7606c8df4907b72
Sebastian Menski <sebastian.menski@googlemail.com>
2015-04-02 13:52:32 +0200

    chore(db): add missing index to h2 and postgresql scripts
    related to #CAM-3711

--- engine/src/main/resources/org/camunda/bpm/engine/db/drop/activiti.postgres.drop.case.history.sql
+++ engine/src/main/resources/org/camunda/bpm/engine/db/drop/activiti.postgres.drop.case.history.sql
@@ -2,6 +2,7 @@ drop index ACT_IDX_HI_CAS_I_CLOSE;
 drop index ACT_IDX_HI_CAS_I_BUSKEY;
 drop index ACT_IDX_HI_CAS_A_I_CREATE;
 drop index ACT_IDX_HI_CAS_A_I_END;
+drop index ACT_IDX_HI_CAS_A_I_COMP;
 
 drop table ACT_HI_CASEINST;
 drop table ACT_HI_CASEACTINST;
