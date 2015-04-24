8d16bce451c32b452edf0d17e0f9e055c210a091
Christian Lipphardt <christian.lipphardt@googlemail.com>
2015-04-24 10:47:53 +0200

    refact(qa): refactor instance migration test infrastructure
    related to #CAM-3787

---
+++ qa/test-db-instance-migration/test-fixture-old/src/main/java/org/camunda/bpm/qa/upgrade/TestFixtureOld.java
@@ -0,0 +1,131 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.qa.upgrade;
+
+
+import org.camunda.bpm.engine.*;
+import org.camunda.bpm.engine.impl.ProcessEngineImpl;
+import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
+import org.camunda.bpm.engine.impl.interceptor.Command;
+import org.camunda.bpm.engine.impl.interceptor.CommandContext;
+import org.camunda.bpm.engine.repository.Deployment;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Task;
+import org.camunda.bpm.engine.task.TaskQuery;
+import org.junit.Assert;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author Daniel Meyer
+ *
+ * Drops and creates the old database.
+ *
+ */
+public class TestFixtureOld {
+
+  private final static Logger LOGGER = Logger.getLogger(TestFixtureOld.class.getName());
+
+  protected ProcessEngine processEngine;
+  protected RepositoryService repositoryService;
+  protected RuntimeService runtimeService;
+  protected ManagementService managementService;
+  protected TaskService taskService;
+
+  public TestFixtureOld(ProcessEngine processEngine) {
+    this.processEngine = processEngine;
+    repositoryService = processEngine.getRepositoryService();
+    runtimeService = processEngine.getRuntimeService();
+    managementService = processEngine.getManagementService();
+    taskService = processEngine.getTaskService();
+  }
+
+  public static void main(String[] args) {
+
+    ProcessEngineConfigurationImpl processEngineConfiguration = (ProcessEngineConfigurationImpl) ProcessEngineConfiguration
+      .createProcessEngineConfigurationFromResource("process-engine-config-old.xml");
+    ProcessEngine processEngine = processEngineConfiguration.buildProcessEngine();
+
+    dropCreateDatabase(processEngine);
+
+    TestFixtureOld fixture = new TestFixtureOld(processEngine);
+
+    // userTask migration
+    fixture.startTestUserTaskMigration();
+
+    processEngine.close();
+
+  }
+
+  protected static void dropCreateDatabase(ProcessEngine processEngine) {
+    // delete all deployments
+    RepositoryService repositoryService = processEngine.getRepositoryService();
+    List<Deployment> deployments = repositoryService
+      .createDeploymentQuery()
+      .list();
+    for (Deployment deployment : deployments) {
+      LOGGER.info("deleting deployment " + deployment.getId());
+      repositoryService.deleteDeployment(deployment.getId(), true);
+    }
+
+    try {
+      ((ProcessEngineImpl)processEngine).getProcessEngineConfiguration()
+        .getCommandExecutorTxRequired()
+        .execute(new Command<Void>() {
+          public Void execute(CommandContext commandContext) {
+            commandContext.getDbSqlSession().dbSchemaDrop();
+            return null;
+          }
+        });
+    } catch(Exception e) {
+      LOGGER.log(Level.WARNING, "Could not drop schema: " + e.getMessage());
+    }
+
+    ((ProcessEngineImpl)processEngine).getProcessEngineConfiguration()
+      .getCommandExecutorTxRequired()
+      .execute(new Command<Void>() {
+        public Void execute(CommandContext commandContext) {
+          commandContext.getDbSqlSession().dbSchemaCreate();
+          return null;
+        }
+      });
+  }
+
+  public void startTestUserTaskMigration() {
+    deploy("org/camunda/bpm/qa/upgrade/TestFixtureOld.testUserTaskMigration.bpmn20.xml");
+
+    Map<String, Object> params = Collections.<String, Object>singletonMap("aStartVariableName", "aStartVariableValue");
+    ProcessInstance pi = runtimeService
+        .startProcessInstanceByKey("TestFixtureOld.testUserTaskMigration", params);
+
+    TaskQuery taskQuery = taskService.createTaskQuery().processInstanceId(pi.getId());
+
+    // get current task
+    Task task = taskQuery.singleResult();
+    Assert.assertNotNull(task);
+  }
+
+  public Deployment deploy(String processOrCaseXml) {
+    Deployment deployment = repositoryService
+        .createDeployment()
+        .addClasspathResource(processOrCaseXml)
+        .deploy();
+
+    return deployment;
+  }
+
+}
