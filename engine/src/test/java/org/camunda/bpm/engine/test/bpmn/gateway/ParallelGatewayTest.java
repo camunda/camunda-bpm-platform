daae8a603e13003050951f7b121dd779ce078ab7
Ronny Bräunlich <ronny.braeunlich@camunda.com>
2015-06-02 15:07:31 +0200

    test(engine): test instance tree in subprocess with parallel gateway
    Related to CAM-3574

--- engine/src/test/java/org/camunda/bpm/engine/test/bpmn/gateway/ParallelGatewayTest.java
+++ engine/src/test/java/org/camunda/bpm/engine/test/bpmn/gateway/ParallelGatewayTest.java
@@ -13,10 +13,17 @@
 
 package org.camunda.bpm.engine.test.bpmn.gateway;
 
+import static org.hamcrest.CoreMatchers.either;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.junit.Assert.assertThat;
+
 import java.util.List;
 
 import org.camunda.bpm.engine.impl.test.PluggableProcessEngineTestCase;
 import org.camunda.bpm.engine.management.JobDefinition;
+import org.camunda.bpm.engine.runtime.ActivityInstance;
 import org.camunda.bpm.engine.runtime.Job;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.task.Task;
@@ -35,81 +42,76 @@ public class ParallelGatewayTest extends PluggableProcessEngineTestCase {
    */
   @Deployment
   public void testSplitMergeNoWaitstates() {
-    ProcessInstance processInstance =
-      runtimeService.startProcessInstanceByKey("forkJoinNoWaitStates");
+    ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("forkJoinNoWaitStates");
     assertTrue(processInstance.isEnded());
   }
 
   @Deployment
   public void testUnstructuredConcurrencyTwoForks() {
-    ProcessInstance processInstance =
-      runtimeService.startProcessInstanceByKey("unstructuredConcurrencyTwoForks");
+    ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("unstructuredConcurrencyTwoForks");
     assertTrue(processInstance.isEnded());
   }
 
   @Deployment
   public void testUnstructuredConcurrencyTwoJoins() {
-    ProcessInstance processInstance =
-      runtimeService.startProcessInstanceByKey("unstructuredConcurrencyTwoJoins");
+    ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("unstructuredConcurrencyTwoJoins");
     assertTrue(processInstance.isEnded());
   }
 
   @Deployment
   public void testForkFollowedByOnlyEndEvents() {
-    ProcessInstance processInstance =
-      runtimeService.startProcessInstanceByKey("forkFollowedByEndEvents");
+    ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("forkFollowedByEndEvents");
     assertTrue(processInstance.isEnded());
   }
 
   @Deployment
   public void testNestedForksFollowedByEndEvents() {
-    ProcessInstance processInstance =
-      runtimeService.startProcessInstanceByKey("nestedForksFollowedByEndEvents");
+    ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("nestedForksFollowedByEndEvents");
     assertTrue(processInstance.isEnded());
   }
 
   // ACT-482
   @Deployment
   public void testNestedForkJoin() {
-   String pid = runtimeService.startProcessInstanceByKey("nestedForkJoin").getId();
+    String pid = runtimeService.startProcessInstanceByKey("nestedForkJoin").getId();
 
-   // After process startm, only task 0 should be active
-   TaskQuery query = taskService.createTaskQuery().orderByTaskName().asc();
-   List<Task> tasks = query.list();
-   assertEquals(1, tasks.size());
-   assertEquals("Task 0", tasks.get(0).getName());
-   assertEquals(1, runtimeService.getActivityInstance(pid).getChildActivityInstances().length);
+    // After process startm, only task 0 should be active
+    TaskQuery query = taskService.createTaskQuery().orderByTaskName().asc();
+    List<Task> tasks = query.list();
+    assertEquals(1, tasks.size());
+    assertEquals("Task 0", tasks.get(0).getName());
+    assertEquals(1, runtimeService.getActivityInstance(pid).getChildActivityInstances().length);
 
-   // Completing task 0 will create Task A and B
-   taskService.complete(tasks.get(0).getId());
-   tasks = query.list();
-   assertEquals(2, tasks.size());
-   assertEquals("Task A", tasks.get(0).getName());
-   assertEquals("Task B", tasks.get(1).getName());
-   assertEquals(2, runtimeService.getActivityInstance(pid).getChildActivityInstances().length);
+    // Completing task 0 will create Task A and B
+    taskService.complete(tasks.get(0).getId());
+    tasks = query.list();
+    assertEquals(2, tasks.size());
+    assertEquals("Task A", tasks.get(0).getName());
+    assertEquals("Task B", tasks.get(1).getName());
+    assertEquals(2, runtimeService.getActivityInstance(pid).getChildActivityInstances().length);
 
-   // Completing task A should not trigger any new tasks
-   taskService.complete(tasks.get(0).getId());
-   tasks = query.list();
-   assertEquals(1, tasks.size());
-   assertEquals("Task B", tasks.get(0).getName());
-   assertEquals(2, runtimeService.getActivityInstance(pid).getChildActivityInstances().length);
+    // Completing task A should not trigger any new tasks
+    taskService.complete(tasks.get(0).getId());
+    tasks = query.list();
+    assertEquals(1, tasks.size());
+    assertEquals("Task B", tasks.get(0).getName());
+    assertEquals(2, runtimeService.getActivityInstance(pid).getChildActivityInstances().length);
 
-   // Completing task B creates tasks B1 and B2
-   taskService.complete(tasks.get(0).getId());
-   tasks = query.list();
-   assertEquals(2, tasks.size());
-   assertEquals("Task B1", tasks.get(0).getName());
-   assertEquals("Task B2", tasks.get(1).getName());
-   assertEquals(3, runtimeService.getActivityInstance(pid).getChildActivityInstances().length);
+    // Completing task B creates tasks B1 and B2
+    taskService.complete(tasks.get(0).getId());
+    tasks = query.list();
+    assertEquals(2, tasks.size());
+    assertEquals("Task B1", tasks.get(0).getName());
+    assertEquals("Task B2", tasks.get(1).getName());
+    assertEquals(3, runtimeService.getActivityInstance(pid).getChildActivityInstances().length);
 
-   // Completing B1 and B2 will activate both joins, and process reaches task C
-   taskService.complete(tasks.get(0).getId());
-   taskService.complete(tasks.get(1).getId());
-   tasks = query.list();
-   assertEquals(1, tasks.size());
-   assertEquals("Task C", tasks.get(0).getName());
-   assertEquals(1, runtimeService.getActivityInstance(pid).getChildActivityInstances().length);
+    // Completing B1 and B2 will activate both joins, and process reaches task C
+    taskService.complete(tasks.get(0).getId());
+    taskService.complete(tasks.get(1).getId());
+    tasks = query.list();
+    assertEquals(1, tasks.size());
+    assertEquals("Task C", tasks.get(0).getName());
+    assertEquals(1, runtimeService.getActivityInstance(pid).getChildActivityInstances().length);
   }
 
   /**
@@ -117,27 +119,31 @@ public class ParallelGatewayTest extends PluggableProcessEngineTestCase {
    */
   @Deployment
   public void testReceyclingExecutionWithCallActivity() {
-    String processInstanceId = runtimeService.startProcessInstanceByKey("parent-process").getId();
+    runtimeService.startProcessInstanceByKey("parent-process").getId();
 
-    // After process start we have two tasks, one from the parent and one from the sub process
+    // After process start we have two tasks, one from the parent and one from
+    // the sub process
     TaskQuery query = taskService.createTaskQuery().orderByTaskName().asc();
     List<Task> tasks = query.list();
     assertEquals(2, tasks.size());
     assertEquals("Another task", tasks.get(0).getName());
     assertEquals("Some Task", tasks.get(1).getName());
 
-    // we complete the task from the parent process, the root execution is receycled, the task in the sub process is still there
+    // we complete the task from the parent process, the root execution is
+    // receycled, the task in the sub process is still there
     taskService.complete(tasks.get(1).getId());
     tasks = query.list();
     assertEquals(1, tasks.size());
     assertEquals("Another task", tasks.get(0).getName());
 
-    // we end the task in the sub process and the sub process instance end is propagated to the parent process
+    // we end the task in the sub process and the sub process instance end is
+    // propagated to the parent process
     taskService.complete(tasks.get(0).getId());
     assertEquals(0, taskService.createTaskQuery().count());
 
     // There is a QA config without history, so we cannot work with this:
-    //assertEquals(1, historyService.createHistoricProcessInstanceQuery().processInstanceId(processInstanceId).finished().count());
+    // assertEquals(1,
+    // historyService.createHistoricProcessInstanceQuery().processInstanceId(processInstanceId).finished().count());
   }
 
   @Deployment
@@ -158,8 +164,7 @@ public class ParallelGatewayTest extends PluggableProcessEngineTestCase {
     assertFalse(processInstance.isEnded());
 
     // there are two jobs to continue the gateway:
-    List<Job> list = managementService.createJobQuery()
-      .list();
+    List<Job> list = managementService.createJobQuery().list();
     assertEquals(2, list.size());
 
     managementService.executeJob(list.get(0).getId());
@@ -178,8 +183,7 @@ public class ParallelGatewayTest extends PluggableProcessEngineTestCase {
     taskService.complete(task.getId());
 
     // there are two jobs to continue the gateway:
-    List<Job> list = managementService.createJobQuery()
-      .list();
+    List<Job> list = managementService.createJobQuery().list();
     assertEquals(2, list.size());
 
     managementService.executeJob(list.get(0).getId());
@@ -193,5 +197,26 @@ public class ParallelGatewayTest extends PluggableProcessEngineTestCase {
     ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("process");
 
     assertTrue(processInstance.isEnded());
+  }
+
+  @Deployment
+  public void testParallelGatewayBeforeAndInSubProcess() {
+    ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("process");
+    List<Task> tasks = taskService.createTaskQuery().list();
+    assertThat(tasks, hasSize(3));
+
+    ActivityInstance instance = runtimeService.getActivityInstance(processInstance.getId());
+    assertThat(instance.getActivityName(), is("Process1"));
+    ActivityInstance[] childActivityInstances = instance.getChildActivityInstances();
+    for (ActivityInstance activityInstance : childActivityInstances) {
+      if (activityInstance.getActivityId().equals("SubProcess_1")) {
+        ActivityInstance[] instances = activityInstance.getChildActivityInstances();
+        for (ActivityInstance activityInstance2 : instances) {
+          assertThat(activityInstance2.getActivityName(), is(either(equalTo("Inner User Task 1")).or(equalTo("Inner User Task 2"))));
+        }
+      } else {
+        assertThat(activityInstance.getActivityName(), is("Outer User Task"));
+      }
+    }
   }
 }
