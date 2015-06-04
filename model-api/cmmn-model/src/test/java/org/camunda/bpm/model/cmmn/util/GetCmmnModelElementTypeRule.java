5f1df1f901de789bba30e3e2f71957996f03d210
Sebastian Menski <sebastian.menski@googlemail.com>
2015-06-04 14:13:12 +0200

    chore(test): use abstract test class from xml model
    related to #CAM-4008

--- model-api/cmmn-model/src/test/java/org/camunda/bpm/model/cmmn/util/GetCmmnModelElementTypeRule.java
+++ model-api/cmmn-model/src/test/java/org/camunda/bpm/model/cmmn/util/GetCmmnModelElementTypeRule.java
@@ -17,6 +17,7 @@ import org.camunda.bpm.model.cmmn.Cmmn;
 import org.camunda.bpm.model.xml.Model;
 import org.camunda.bpm.model.xml.ModelInstance;
 import org.camunda.bpm.model.xml.instance.ModelElementInstance;
+import org.camunda.bpm.model.xml.test.GetModelElementTypeRule;
 import org.camunda.bpm.model.xml.type.ModelElementType;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
@@ -25,7 +26,7 @@ import org.junit.runner.Description;
  * @author Sebastian Menski
  * @author Roman Smirnov
  */
-public class GetCmmnModelElementTypeRule extends TestWatcher {
+public class GetCmmnModelElementTypeRule extends TestWatcher implements GetModelElementTypeRule {
 
   private ModelInstance modelInstance;
   private Model model;
