3999a8be442cc2e44223128761678485552b03a7
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2015-06-05 13:53:31 +0200

    chore(engine): add missing class
    related to CAM-4029

---
+++ engine/src/main/java/org/camunda/bpm/engine/impl/cmd/ReportDbMetricsCmd.java
@@ -0,0 +1,48 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.engine.impl.cmd;
+
+import java.io.Serializable;
+
+import org.camunda.bpm.engine.ProcessEngineException;
+import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
+import org.camunda.bpm.engine.impl.context.Context;
+import org.camunda.bpm.engine.impl.interceptor.Command;
+import org.camunda.bpm.engine.impl.interceptor.CommandContext;
+import org.camunda.bpm.engine.impl.metrics.reporter.DbMetricsReporter;
+
+/**
+ * @author Thorben Lindhauer
+ *
+ */
+public class ReportDbMetricsCmd implements Command<Void>, Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  public Void execute(CommandContext commandContext) {
+    ProcessEngineConfigurationImpl engineConfiguration = Context.getProcessEngineConfiguration();
+
+    if (!engineConfiguration.isMetricsEnabled()) {
+      throw new ProcessEngineException("Metrics reporting is disabled");
+    }
+
+    if (!engineConfiguration.isDbMetricsReporterActivate()) {
+      throw new ProcessEngineException("Metrics reporting to database is disabled");
+    }
+
+    DbMetricsReporter dbMetricsReporter = engineConfiguration.getDbMetricsReporter();
+    dbMetricsReporter.reportNow();
+    return null;
+  }
+
+}
