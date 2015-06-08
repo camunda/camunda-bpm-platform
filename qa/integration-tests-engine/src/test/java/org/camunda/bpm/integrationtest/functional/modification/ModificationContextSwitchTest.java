1a62a06346027d1e2bb7816a4fbfd654efddbd02
Daniel Meyer <daniel.meyer@camunda.com>
2015-06-08 14:53:47 +0200

    test(engine/it): context switch with modifications
    verify that context switch is performed when executing modififcations

---
+++ qa/integration-tests-engine/src/test/java/org/camunda/bpm/integrationtest/functional/modification/ModificationContextSwitchTest.java
@@ -0,0 +1,72 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.integrationtest.functional.modification;
+
+import org.camunda.bpm.engine.runtime.ProcessInstanceModificationInstantiationBuilder;
+import org.camunda.bpm.integrationtest.functional.modification.beans.ExampleDelegate;
+import org.camunda.bpm.integrationtest.util.AbstractFoxPlatformIntegrationTest;
+import org.camunda.bpm.integrationtest.util.TestContainer;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Daniel Meyer
+ *
+ */
+@RunWith(Arquillian.class)
+public class ModificationContextSwitchTest extends AbstractFoxPlatformIntegrationTest {
+
+  @Deployment
+  public static WebArchive createProcessArchiveDeplyoment() {
+    return initWebArchiveDeployment()
+            .addClass(ExampleDelegate.class)
+            .addAsResource("org/camunda/bpm/integrationtest/functional/modification/ModificationContextSwitchTest.testModificationClassloading.bpmn20.xml");
+  }
+
+  @Deployment(name="clientDeployment")
+  public static WebArchive clientDeployment() {
+    WebArchive webArchive = ShrinkWrap.create(WebArchive.class, "client.war")
+            .addClass(AbstractFoxPlatformIntegrationTest.class);
+
+    TestContainer.addContainerSpecificResources(webArchive);
+
+    return webArchive;
+
+  }
+
+  @Test
+  @OperateOnDeployment("clientDeployment")
+  public void testModificationClassloading() {
+    // given
+    // process instance is in state "waitState"
+    String pi = runtimeService.startProcessInstanceByKey("testProcess").getId();
+    Assert.assertNull(runtimeService.getVariable(pi, "executed"));
+
+    // if
+    // we modify the process instance to start the next task:
+    ProcessInstanceModificationInstantiationBuilder modification = runtimeService.createProcessInstanceModification(pi)
+      .startBeforeActivity("ServiceTask_1");
+
+    // then
+    // the modification does not fail
+    modification.execute();
+    Assert.assertTrue((Boolean)runtimeService.getVariable(pi, "executed"));
+  }
+
+}
