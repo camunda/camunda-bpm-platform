219b2838fbb00529b8b7cf23fbcfffa6320f2629
Sebastian Stamm <sebastian.stamm@camunda.com>
2015-09-11 11:04:47 +0200

    feat(dmn): add input and output variables for decision instance
    related to CAM-4535

---
+++ webapps/webapp/src/main/resources-plugin/decisionList/app/views/decisionInstance/inputTable.js
@@ -0,0 +1,30 @@
+/* global define: false, angular: false */
+define([
+  'angular',
+  'text!./variable-table.html'
+],
+function(angular, template) {
+  'use strict';
+
+  return [ 'ViewsProvider', function(ViewsProvider) {
+
+    ViewsProvider.registerDefaultView('cockpit.decisionInstance.tab', {
+      id: 'decision-input-table',
+      label: 'Inputs',
+      template: template,
+      controller: [
+               '$scope',
+      function ($scope) {
+
+        $scope.variables = $scope.decisionInstance.inputs;
+
+        // the variable widget expects the variable name in the name field, not in the clauseName field
+        for(var i = 0; i < $scope.variables.length; i++) {
+          $scope.variables[i].name = $scope.variables[i].clauseName;
+        }
+
+      }],
+      priority: 10
+    });
+  }];
+});
--- webapps/webapp/src/main/resources-plugin/decisionList/app/views/main.js
+++ webapps/webapp/src/main/resources-plugin/decisionList/app/views/main.js
@@ -10,15 +10,21 @@ define([
   './dashboard/decision-list',
 
   // decision definition
-  './decisionDefinition/decisionInstanceTable'
+  './decisionDefinition/decisionInstanceTable',
 
-], function(angular, decisionList, decisionInstanceTable) {
+  // decision instance
+  './decisionInstance/inputTable',
+  './decisionInstance/outputTable'
+
+], function(angular, decisionList, decisionInstanceTable, inputTable, outputTable) {
 
   'use strict';
   var ngModule = angular.module('cockpit.plugin.decisionList.views', []);
 
   ngModule.config(decisionList);
   ngModule.config(decisionInstanceTable);
+  ngModule.config(inputTable);
+  ngModule.config(outputTable);
 
   return ngModule;
 });
