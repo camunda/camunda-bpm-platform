34bf247d88bbae9754b6cedd19a62c64394bf526
Philipp Ossler <philipp.ossler@camunda.com>
2015-10-07 10:30:29 +0200

    fix(dmn-engine): remove illegal methods
    related to CAM-4651

--- engine-dmn/engine/src/main/java/org/camunda/bpm/dmn/engine/impl/DmnDecisionOutputImpl.java
+++ engine-dmn/engine/src/main/java/org/camunda/bpm/dmn/engine/impl/DmnDecisionOutputImpl.java
@@ -16,9 +16,6 @@ package org.camunda.bpm.dmn.engine.impl;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
-import java.util.function.Function;
 
 import org.camunda.bpm.dmn.engine.DmnDecisionOutput;
 import org.camunda.bpm.engine.impl.core.variable.VariableMapImpl;
@@ -42,6 +39,22 @@ public class DmnDecisionOutputImpl implements DmnDecisionOutput {
     } else {
       return null;
     }
+  }
+
+  public VariableMap putValue(String name, Object value) {
+    return variables.putValue(name, value);
+  }
+
+  public VariableMap putValueTyped(String name, TypedValue value) {
+    return variables.putValueTyped(name, value);
+  }
+
+  public <T> T getValue(String name, Class<T> type) {
+    return variables.getValue(name, type);
+  }
+
+  public <T extends TypedValue> T getValueTyped(String name) {
+    return variables.getValueTyped(name);
   }
 
   public int size() {
@@ -100,66 +113,6 @@ public class DmnDecisionOutputImpl implements DmnDecisionOutput {
   @Override
   public int hashCode() {
     return variables.hashCode();
-  }
-
-  public Object getOrDefault(Object key, Object defaultValue) {
-    return variables.getOrDefault(key, defaultValue);
-  }
-
-  public void forEach(BiConsumer<? super String, ? super Object> action) {
-    variables.forEach(action);
-  }
-
-  public void replaceAll(BiFunction<? super String, ? super Object, ? extends Object> function) {
-    variables.replaceAll(function);
-  }
-
-  public Object putIfAbsent(String key, Object value) {
-    return variables.putIfAbsent(key, value);
-  }
-
-  public Object computeIfAbsent(String key, Function<? super String, ? extends Object> mappingFunction) {
-    return variables.computeIfAbsent(key, mappingFunction);
-  }
-
-  public Object computeIfPresent(String key, BiFunction<? super String, ? super Object, ? extends Object> remappingFunction) {
-    return variables.computeIfPresent(key, remappingFunction);
-  }
-
-  public Object compute(String key, BiFunction<? super String, ? super Object, ? extends Object> remappingFunction) {
-    return variables.compute(key, remappingFunction);
-  }
-
-  public <T> T getValue(String arg0, Class<T> arg1) {
-    return variables.getValue(arg0, arg1);
-  }
-
-  public <T extends TypedValue> T getValueTyped(String arg0) {
-    return variables.getValueTyped(arg0);
-  }
-
-  public Object merge(String key, Object value, BiFunction<? super Object, ? super Object, ? extends Object> remappingFunction) {
-    return variables.merge(key, value, remappingFunction);
-  }
-
-  public VariableMap putValue(String arg0, Object arg1) {
-    return variables.putValue(arg0, arg1);
-  }
-
-  public VariableMap putValueTyped(String arg0, TypedValue arg1) {
-    return variables.putValueTyped(arg0, arg1);
-  }
-
-  public boolean remove(Object key, Object value) {
-    return variables.remove(key, value);
-  }
-
-  public boolean replace(String key, Object oldValue, Object newValue) {
-    return variables.replace(key, oldValue, newValue);
-  }
-
-  public Object replace(String key, Object value) {
-    return variables.replace(key, value);
   }
 
 }
