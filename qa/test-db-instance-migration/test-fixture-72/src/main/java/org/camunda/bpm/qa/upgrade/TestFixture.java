02754d914c5cd861b324c0ea1cf4a5944ebd5228
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2015-10-20 17:08:23 +0200

    refactor(migration-test): project structure
    - there is one module per previous camunda version that
      - sets up the db or migrates to its schema version
      - creates some process instance for testing
    - there is one module for the current camunda version that
      contains test cases for all the scenarios set up by the other modules
    - makes it possible to write migration tests that span more than one
      version
    - makes it possible to retain migration tests after bumping the current
      camunda version
    
    related to CAM-4354

---
+++ qa/test-db-instance-migration/test-fixture-72/src/main/java/org/camunda/bpm/qa/upgrade/TestFixture.java
@@ -0,0 +1,60 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.qa.upgrade;
+
+
+import org.camunda.bpm.engine.ManagementService;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngineConfiguration;
+import org.camunda.bpm.engine.RepositoryService;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
+import org.camunda.bpm.qa.upgrade.scenarios.sentry.OneTaskProcessScenario;
+
+/**
+ * @author Daniel Meyer
+ *
+ * Drops and creates the old database.
+ *
+ */
+public class TestFixture {
+
+  protected ProcessEngine processEngine;
+  protected RepositoryService repositoryService;
+  protected RuntimeService runtimeService;
+  protected ManagementService managementService;
+  protected TaskService taskService;
+
+  public TestFixture(ProcessEngine processEngine) {
+    this.processEngine = processEngine;
+    repositoryService = processEngine.getRepositoryService();
+    runtimeService = processEngine.getRuntimeService();
+    managementService = processEngine.getManagementService();
+    taskService = processEngine.getTaskService();
+  }
+
+  public static void main(String[] args) {
+    ProcessEngineConfigurationImpl processEngineConfiguration = (ProcessEngineConfigurationImpl) ProcessEngineConfiguration
+      .createProcessEngineConfigurationFromResource("camunda.cfg.xml");
+    ProcessEngine processEngine = processEngineConfiguration.buildProcessEngine();
+
+    // register test scenarios
+    ScenarioRunner runner = new ScenarioRunner(processEngine);
+
+    // event subprocesses
+    runner.setupScenarios(OneTaskProcessScenario.class);
+
+    processEngine.close();
+  }
+}
