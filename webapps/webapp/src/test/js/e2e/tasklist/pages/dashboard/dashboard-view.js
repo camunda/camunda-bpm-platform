8900d3416547f43db60a894ae7631f58418f23f0
Valentin Vago <zeropaper@irata.ch>
2015-10-26 14:26:41 +0100

    test(header): add tests for the tasklist navigation
    Related to CAM-4685

--- webapps/webapp/src/test/js/e2e/tasklist/pages/dashboard/dashboard-view.js
+++ webapps/webapp/src/test/js/e2e/tasklist/pages/dashboard/dashboard-view.js
@@ -4,8 +4,17 @@ var Base = require('./../base');
 
 module.exports = Base.extend({
 
-  url: '/camunda/app/tasklist/default/#/'
+  url: '/camunda/app/tasklist/default/#/',
 
+  header: function () {
+    return element(by.css('[cam-tasklist-navigation]'));
+  },
 
+  accountDropdown: function () {
+    return this.header().element(by.css('.user-account.dropdown'));
+  },
 
+  accountDropdownButton: function () {
+    return this.accountDropdown().element(by.css('.dropdown-toggle'));
+  }
 });
