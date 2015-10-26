5d61e8637058289e49e776ef4c270ca44bde1018
Valentin Vago <zeropaper@irata.ch>
2015-10-26 14:05:40 +0100

    impr(user-name): display the full user name in header
    Related to CAM-4685

--- webapps/client/scripts/controller/cam-tasklist-app-ctrl.js
+++ webapps/client/scripts/controller/cam-tasklist-app-ctrl.js
@@ -18,13 +18,37 @@ define([
   })();
 
   return [
+    'camAPI',
     '$scope',
   function(
+    camAPI,
     $scope
   ) {
 
     // create a new tasklistApp
     $scope.tasklistApp = new TasklistApp();
+
+    function getUserProfile(auth) {
+      if (!auth || !auth.name) {
+        $scope.userFullName = null;
+        return;
+      }
+
+      var userService = camAPI.resource('user');
+      userService.profile(auth.name, function (err, info) {
+        if (err) {
+          $scope.userFullName = null;
+          throw err;
+        }
+        $scope.userFullName = info.firstName + ' ' + info.lastName;
+      });
+    }
+
+    $scope.$on('authentication.changed', function (ev, auth) {
+      getUserProfile(auth);
+    });
+
+    getUserProfile($scope.authentication);
 
   }];
 
