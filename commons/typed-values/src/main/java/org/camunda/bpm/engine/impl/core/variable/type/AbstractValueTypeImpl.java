5657fb985e871404e7fe6a9d55216284dce18b05
Philipp Ossler <philipp.ossler@camunda.com>
2015-10-28 10:17:58 +0100

    chore(engine): enhanced equals check for value types
    related to #CAM-4725

--- commons/typed-values/src/main/java/org/camunda/bpm/engine/impl/core/variable/type/AbstractValueTypeImpl.java
+++ commons/typed-values/src/main/java/org/camunda/bpm/engine/impl/core/variable/type/AbstractValueTypeImpl.java
@@ -58,4 +58,29 @@ public abstract class AbstractValueTypeImpl implements ValueType {
     return new IllegalArgumentException("The type " + getName() + " supports no conversion from type: " + typeToConvertTo.getName());
   }
 
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    AbstractValueTypeImpl other = (AbstractValueTypeImpl) obj;
+    if (name == null) {
+      if (other.name != null)
+        return false;
+    } else if (!name.equals(other.name))
+      return false;
+    return true;
+  }
+
 }
