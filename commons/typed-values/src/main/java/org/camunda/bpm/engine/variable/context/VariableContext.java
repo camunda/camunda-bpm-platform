8859d55c642ee3fb588e231515ddbcf8bcdfd66d
Sebastian Menski <sebastian@menski.org>
2015-11-02 11:06:41 +0100

    chore(typed-values): align package structure with impl package
    related to #CAM-4878

--- commons/typed-values/src/main/java/org/camunda/bpm/engine/variable/context/VariableContext.java
+++ commons/typed-values/src/main/java/org/camunda/bpm/engine/variable/context/VariableContext.java
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.camunda.bpm.engine.variable;
+package org.camunda.bpm.engine.variable.context;
 
 import java.util.Set;
 
