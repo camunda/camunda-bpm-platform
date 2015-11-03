0b9bef4d0393eac0f1a8ef817e3223ddd52b24fb
Daniel Meyer <daniel.meyer@camunda.com>
2015-11-03 01:50:41 +0100

    feat(platform): support jobExecutorActivate = false
    - add possibility to start platform with job executor disabled.
    - set property 'jobExecutorActivate = false' on process engine
    - setting is honored by not force-starting job executor but rather
    letting first engine to register start the job executor if the
    configuration setting is set to true.
    - override default value if configuration setting for backwards
    compatibility.
    
    related to #CAM-4817

--- distro/wildfly/subsystem/src/main/java/org/camunda/bpm/container/impl/jboss/config/ManagedJtaProcessEngineConfiguration.java
+++ distro/wildfly/subsystem/src/main/java/org/camunda/bpm/container/impl/jboss/config/ManagedJtaProcessEngineConfiguration.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,11 +22,17 @@ import org.camunda.bpm.engine.impl.jobexecutor.FoxFailedJobCommandFactory;
 import org.camunda.bpm.engine.impl.persistence.StrongUuidGenerator;
 
 /**
- * 
+ *
  * @author Daniel Meyer
- * 
+ *
  */
 public class ManagedJtaProcessEngineConfiguration extends JtaProcessEngineConfiguration {
+
+  public ManagedJtaProcessEngineConfiguration() {
+    // override job executor auto activate: set to true in shared engine scenario
+    // if it is not specified (see #CAM-4817)
+    setJobExecutorActivate(true);
+  }
 
   @Override
   protected void init() {
@@ -40,9 +46,9 @@ public class ManagedJtaProcessEngineConfiguration extends JtaProcessEngineConfig
     if(customPostBPMNParseListeners==null) {
       customPostBPMNParseListeners = new ArrayList<BpmnParseListener>();
       setCustomPostBPMNParseListeners(customPostBPMNParseListeners);
-    }    
+    }
     customPostBPMNParseListeners.add(new FoxFailedJobParseListener());
-    
+
     setFailedJobCommandFactory(new FoxFailedJobCommandFactory());
   }
 
