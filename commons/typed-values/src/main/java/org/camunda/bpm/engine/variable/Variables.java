de755a31e7bdad5234593e494200ddfafe4fc4a6
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2015-11-13 17:03:47 +0100

    chore(typed-values): javadoc for all Variables factory methods

--- commons/typed-values/src/main/java/org/camunda/bpm/engine/variable/Variables.java
+++ commons/typed-values/src/main/java/org/camunda/bpm/engine/variable/Variables.java
@@ -36,6 +36,7 @@ import org.camunda.bpm.engine.variable.impl.value.PrimitiveTypeValueImpl.StringV
 import org.camunda.bpm.engine.variable.impl.value.builder.FileValueBuilderImpl;
 import org.camunda.bpm.engine.variable.impl.value.builder.ObjectVariableBuilderImpl;
 import org.camunda.bpm.engine.variable.impl.value.builder.SerializedObjectValueBuilderImpl;
+import org.camunda.bpm.engine.variable.type.ValueType;
 import org.camunda.bpm.engine.variable.value.BooleanValue;
 import org.camunda.bpm.engine.variable.value.BytesValue;
 import org.camunda.bpm.engine.variable.value.DateValue;
@@ -44,6 +45,7 @@ import org.camunda.bpm.engine.variable.value.FileValue;
 import org.camunda.bpm.engine.variable.value.IntegerValue;
 import org.camunda.bpm.engine.variable.value.LongValue;
 import org.camunda.bpm.engine.variable.value.NumberValue;
+import org.camunda.bpm.engine.variable.value.ObjectValue;
 import org.camunda.bpm.engine.variable.value.SerializationDataFormat;
 import org.camunda.bpm.engine.variable.value.ShortValue;
 import org.camunda.bpm.engine.variable.value.StringValue;
@@ -125,10 +127,18 @@ public class Variables {
     }
   }
 
+  /**
+   * Returns a new {@link VariableMap} instance.
+   */
   public static VariableMap createVariables() {
     return new VariableMapImpl();
   }
 
+  /**
+   * If the given map is not a variable map, adds all its entries as untyped
+   * values to a new {@link VariableMap}. If the given map is a {@link VariableMap},
+   * it is returned as is.
+   */
   public static VariableMap fromMap(Map<String, Object> map) {
     if(map instanceof VariableMap) {
       return (VariableMap) map;
@@ -139,59 +149,94 @@ public class Variables {
   }
 
   /**
-   * Shortcut for {@code Variables#createVariables()#putValue(String, Object)}
+   * Shortcut for {@code Variables.createVariables().putValue(name, value)}
    */
   public static VariableMap putValue(String name, Object value) {
     return createVariables().putValue(name, value);
   }
 
   /**
-   * Shortcut for {@code Variables#createVariables()#putValueTyped(String, Object)}
+   * Shortcut for {@code Variables.createVariables().putValueTyped(name, value)}
    */
   public static VariableMap putValueTyped(String name, TypedValue value) {
     return createVariables().putValueTyped(name, value);
   }
 
+  /**
+   * Returns a builder to create a new {@link ObjectValue} that encapsulates
+   * the given {@code value}.
+   */
   public static ObjectValueBuilder objectValue(Object value) {
     return new ObjectVariableBuilderImpl(value);
   }
 
+  /**
+   * Returns a builder to create a new {@link ObjectValue} from a serialized
+   * object representation.
+   */
   public static SerializedObjectValueBuilder serializedObjectValue() {
     return new SerializedObjectValueBuilderImpl();
   }
 
+  /**
+   * Shortcut for {@code Variables.serializedObjectValue().serializedObjectValue(value)}
+   */
   public static SerializedObjectValueBuilder serializedObjectValue(String value) {
     return serializedObjectValue().serializedValue(value);
   }
 
+  /**
+   * Creates a new {@link IntegerValue} that encapsulates the given <code>integer</code>
+   */
   public static IntegerValue integerValue(Integer integer) {
     return new IntegerValueImpl(integer);
   }
 
+  /**
+   * Creates a new {@link StringValue} that encapsulates the given <code>stringValue</code>
+   */
   public static StringValue stringValue(String stringValue) {
     return new StringValueImpl(stringValue);
   }
 
+  /**
+   * Creates a new {@link BooleanValue} that encapsulates the given <code>booleanValue</code>
+   */
   public static BooleanValue booleanValue(Boolean booleanValue) {
     return new BooleanValueImpl(booleanValue);
   }
 
+  /**
+   * Creates a new {@link BytesValue} that encapsulates the given <code>bytes</code>
+   */
   public static BytesValue byteArrayValue(byte[] bytes) {
     return new BytesValueImpl(bytes);
   }
 
+  /**
+   * Creates a new {@link DateValue} that encapsulates the given <code>date</code>
+   */
   public static DateValue dateValue(Date date) {
     return new DateValueImpl(date);
   }
 
+  /**
+   * Creates a new {@link LongValue} that encapsulates the given <code>longValue</code>
+   */
   public static LongValue longValue(Long longValue) {
     return new LongValueImpl(longValue);
   }
 
+  /**
+   * Creates a new {@link ShortValue} that encapsulates the given <code>shortValue</code>
+   */
   public static ShortValue shortValue(Short shortValue) {
     return new ShortValueImpl(shortValue);
   }
 
+  /**
+   * Creates a new {@link DoubleValue} that encapsulates the given <code>doubleValue</code>
+   */
   public static DoubleValue doubleValue(Double doubleValue) {
     return new DoubleValueImpl(doubleValue);
   }
@@ -205,10 +250,17 @@ public class Variables {
     return new NumberValueImpl(numberValue);
   }
 
+  /**
+   * Creates a {@link TypedValue} with value {@code null} and type {@link ValueType#NULL}
+   */
   public static TypedValue untypedNullValue() {
     return NullValueImpl.INSTANCE;
   }
 
+  /**
+   * Creates an untyped value, i.e. {@link TypedValue#getType()} returns <code>null</code>
+   * for the returned instance.
+   */
   public static TypedValue untypedValue(Object value) {
     if(value == null) {
       return untypedNullValue();
@@ -224,15 +276,17 @@ public class Variables {
     }
   }
 
+  /**
+   * Returns a builder to create a new {@link FileValue} with the given
+   * {@code filename}.
+   */
   public static FileValueBuilder fileValue(String filename) {
     return new FileValueBuilderImpl(filename);
   }
 
   /**
-   * Shortcut for calling fileValue(name).file(file).mimeType(type).create(). The name will be taken
-   * from the file and the type will be detected by {@link MimetypesFileTypeMap}.
-   * @param file
-   * @return
+   * Shortcut for calling {@code Variables.fileValue(name).file(file).mimeType(type).create()}.
+   * The name is set to the file name and the mime type is detected via {@link MimetypesFileTypeMap}.
    */
   public static FileValue fileValue(File file){
     String contentType = MimetypesFileTypeMap.getDefaultFileTypeMap().getContentType(file);
