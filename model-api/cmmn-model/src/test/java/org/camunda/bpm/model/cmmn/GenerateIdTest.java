64d1c0269cfc55b6c18722eb0ca4614578917d10
Sebastian Menski <sebastian@menski.org>
2015-11-16 16:36:03 +0100

    chore(cmmn): remove unused code

--- model-api/cmmn-model/src/test/java/org/camunda/bpm/model/cmmn/GenerateIdTest.java
+++ model-api/cmmn-model/src/test/java/org/camunda/bpm/model/cmmn/GenerateIdTest.java
@@ -41,10 +41,6 @@ public class GenerateIdTest {
 
     HumanTask humanTask = modelInstance.getModelElementsByType(HumanTask.class).iterator().next();
     assertThat(humanTask.getId()).isNull();
-
-
-    DefaultControl element = modelInstance.newInstance(DefaultControl.class);
-    humanTask.setDefaultControl(element);
   }
 
   @Test
