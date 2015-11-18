eb118f844534fe9fd0a06e8041bcfcbc5f1e5e1d
Sebastian Menski <sebastian@menski.org>
2015-11-18 09:37:28 +0100

    chore(qa): ignore integration test until fixed
    related to #CAM-4993

--- qa/integration-tests-engine/src/test/java/org/camunda/bpm/integrationtest/deployment/callbacks/TestPostDeployFailure_OTHERS.java
+++ qa/integration-tests-engine/src/test/java/org/camunda/bpm/integrationtest/deployment/callbacks/TestPostDeployFailure_OTHERS.java
@@ -19,6 +19,7 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.OperateOnDeployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -27,6 +28,7 @@ import org.junit.runner.RunWith;
  *
  */
 @RunWith(Arquillian.class)
+@Ignore
 public class TestPostDeployFailure_OTHERS extends AbstractFoxPlatformIntegrationTest {
   
   @Deployment(name="fail")
