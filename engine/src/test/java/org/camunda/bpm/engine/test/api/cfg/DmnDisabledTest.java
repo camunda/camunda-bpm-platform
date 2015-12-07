347692cb7b66d219ce190dba3017395858e7085b
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2015-12-07 15:18:58 +0100

    chore(engine): move dmn database test
    related to CAM-3813

--- engine/src/test/java/org/camunda/bpm/engine/test/api/cfg/DmnDisabledTest.java
+++ engine/src/test/java/org/camunda/bpm/engine/test/api/cfg/DmnDisabledTest.java
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.camunda.bpm.engine.test.db;
+package org.camunda.bpm.engine.test.api.cfg;
 
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.ProcessEngineConfiguration;
@@ -26,7 +26,7 @@ import org.junit.Test;
  * @author Roman Smirnov
  *
  */
-public class DatabaseDmnTest {
+public class DmnDisabledTest {
 
   protected static ProcessEngineImpl processEngineImpl;
 
