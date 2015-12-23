bf06190e1d9463e4806abed82630822acc01a68c
Philipp Ossler <philipp.ossler@camunda.com>
2015-12-23 10:50:30 +0100

    tests(engine): add test cases for JavaConfig
    related to #CAM-5095

--- engine-spring/src/test/java/org/camunda/bpm/engine/spring/test/SpringProcessEngineTestCaseTest.java
+++ engine-spring/src/test/java/org/camunda/bpm/engine/spring/test/SpringProcessEngineTestCaseTest.java
@@ -27,4 +27,5 @@ public class SpringProcessEngineTestCaseTest extends SpringProcessEngineTestCase
     assertNotNull(processEngine);
     assertTrue(((ProcessEngineImpl) processEngine).getProcessEngineConfiguration().isJobExecutorDeploymentAware());
   }
+
 }
