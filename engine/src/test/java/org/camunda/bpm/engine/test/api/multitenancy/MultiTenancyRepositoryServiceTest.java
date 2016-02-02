bff5b74bbdf70a9f928cff48acdac21d2985fa35
Philipp Ossler <philipp.ossler@camunda.com>
2016-02-02 09:24:22 +0100

    fix(engine): duplicate filtering respects tenantId
    - fix bug that duplicate filtering fails if deploy the same resources
    with tenant id and again without tenant id
    - move test cases to multi-tenancy package
    
    related to #CAM-5250

--- engine/src/test/java/org/camunda/bpm/engine/test/api/multitenancy/MultiTenancyRepositoryServiceTest.java
+++ engine/src/test/java/org/camunda/bpm/engine/test/api/multitenancy/MultiTenancyRepositoryServiceTest.java
@@ -13,6 +13,12 @@
 
 package org.camunda.bpm.engine.test.api.multitenancy;
 
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
 import java.util.List;
 
 import org.camunda.bpm.engine.impl.test.PluggableProcessEngineTestCase;
@@ -34,34 +40,34 @@ public class MultiTenancyRepositoryServiceTest extends PluggableProcessEngineTes
         .createDeploymentQuery()
         .singleResult();
 
-    assertNotNull(deployment);
-    assertNull(deployment.getTenantId());
+    assertThat(deployment, is(notNullValue()));
+    assertThat(deployment.getTenantId(), is(nullValue()));
   }
 
   public void testDeploymentWithTenantId() {
     createDeploymentBuilder()
-      .tenantId("tenant 1")
+      .tenantId("tenant1")
       .deploy();
 
     Deployment deployment = repositoryService
         .createDeploymentQuery()
         .singleResult();
 
-    assertNotNull(deployment);
-    assertEquals("tenant 1", deployment.getTenantId());
+    assertThat(deployment, is(notNullValue()));
+    assertThat(deployment.getTenantId(), is("tenant1"));
   }
 
   public void testProcessDefinitionVersionWithTenantId() {
     createDeploymentBuilder()
-      .tenantId("tenant 1")
+      .tenantId("tenant1")
       .deploy();
 
     createDeploymentBuilder()
-      .tenantId("tenant 1")
+      .tenantId("tenant1")
       .deploy();
 
     createDeploymentBuilder()
-      .tenantId("tenant 2")
+      .tenantId("tenant2")
       .deploy();
 
     List<ProcessDefinition> processDefinitions = repositoryService
@@ -72,12 +78,86 @@ public class MultiTenancyRepositoryServiceTest extends PluggableProcessEngineTes
         .asc()
         .list();
 
-    assertEquals(3, processDefinitions.size());
+    assertThat(processDefinitions.size(), is(3));
     // process definition was deployed twice for tenant one
-    assertEquals(1, processDefinitions.get(0).getVersion());
-    assertEquals(2, processDefinitions.get(1).getVersion());
+    assertThat(processDefinitions.get(0).getVersion(), is(1));
+    assertThat(processDefinitions.get(1).getVersion(), is(2));
     // process definition version of tenant two have to be independent from tenant one
-    assertEquals(1, processDefinitions.get(2).getVersion());
+    assertThat(processDefinitions.get(2).getVersion(), is(1));
+  }
+
+  public void testDeploymentWithDuplicateFilteringForSameTenant() {
+    // given: a deployment with tenant ID
+    createDeploymentBuilder()
+      .enableDuplicateFiltering(false)
+      .name("twice")
+      .tenantId("tenant1")
+      .deploy();
+
+    // if the same process is deployed with the same tenant ID again
+    createDeploymentBuilder()
+      .enableDuplicateFiltering(false)
+      .name("twice")
+      .tenantId("tenant1")
+      .deploy();
+
+    // then it does not create a new deployment
+    assertThat(repositoryService.createDeploymentQuery().count(), is(1L));
+  }
+
+  public void testDeploymentWithDuplicateFilteringForDifferentTenants() {
+    // given: a deployment with tenant ID
+    createDeploymentBuilder()
+      .enableDuplicateFiltering(false)
+      .name("twice")
+      .tenantId("tenant1")
+      .deploy();
+
+    // if the same process is deployed with the another tenant ID
+    createDeploymentBuilder()
+      .enableDuplicateFiltering(false)
+      .name("twice")
+      .tenantId("tenant2")
+      .deploy();
+
+    // then a new deployment is created
+    assertThat(repositoryService.createDeploymentQuery().count(), is(2L));
+  }
+
+  public void testDeploymentWithDuplicateFilteringIgnoreDeploymentForNoTenant() {
+    // given: a deployment without tenant ID
+    createDeploymentBuilder()
+      .enableDuplicateFiltering(false)
+      .name("twice")
+      .deploy();
+
+    // if the same process is deployed with tenant ID
+    createDeploymentBuilder()
+      .enableDuplicateFiltering(false)
+      .name("twice")
+      .tenantId("tenant1")
+      .deploy();
+
+    // then a new deployment is created
+    assertThat(repositoryService.createDeploymentQuery().count(), is(2L));
+  }
+
+  public void testDeploymentWithDuplicateFilteringIgnoreDeploymentForTenant() {
+    // given: a deployment with tenant ID
+    createDeploymentBuilder()
+      .enableDuplicateFiltering(false)
+      .name("twice")
+      .tenantId("tenant1")
+      .deploy();
+
+    // if the same process is deployed without tenant ID
+    createDeploymentBuilder()
+      .enableDuplicateFiltering(false)
+      .name("twice")
+      .deploy();
+
+    // then a new deployment is created
+    assertThat(repositoryService.createDeploymentQuery().count(), is(2L));
   }
 
   protected DeploymentBuilder createDeploymentBuilder() {
