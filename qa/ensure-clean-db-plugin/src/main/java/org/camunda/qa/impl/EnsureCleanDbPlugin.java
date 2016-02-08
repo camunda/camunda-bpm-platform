54ba35fea7f53a11276a5b5111e9a79835aae384
Daniel Meyer <daniel.meyer@camunda.com>
2016-02-08 15:54:26 +0100

    chore(qa): do not drop db -> log errors to file
    related to #CAM-5398

--- qa/ensure-clean-db-plugin/src/main/java/org/camunda/qa/impl/EnsureCleanDbPlugin.java
+++ qa/ensure-clean-db-plugin/src/main/java/org/camunda/qa/impl/EnsureCleanDbPlugin.java
@@ -12,26 +12,38 @@
  */
 package org.camunda.qa.impl;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.camunda.bpm.BpmPlatform;
 import org.camunda.bpm.application.ProcessApplicationInterface;
 import org.camunda.bpm.container.impl.plugin.BpmPlatformPlugin;
 import org.camunda.bpm.engine.ProcessEngine;
-import org.camunda.bpm.engine.ProcessEngineConfiguration;
+import org.camunda.bpm.engine.impl.ProcessEngineImpl;
+import org.camunda.bpm.engine.impl.SchemaOperationsProcessEngineBuild;
 import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
-import org.camunda.bpm.engine.impl.cfg.TransactionContext;
-import org.camunda.bpm.engine.impl.cfg.TransactionListener;
-import org.camunda.bpm.engine.impl.cfg.TransactionState;
+import org.camunda.bpm.engine.impl.db.PersistenceSession;
 import org.camunda.bpm.engine.impl.interceptor.Command;
 import org.camunda.bpm.engine.impl.interceptor.CommandContext;
 import org.camunda.bpm.engine.impl.test.TestHelper;
+import org.camunda.bpm.engine.impl.util.IoUtil;
 
 /**
  * @author Daniel Meyer
  *
  */
 public class EnsureCleanDbPlugin implements BpmPlatformPlugin {
+
+  public static final List<String> TABLENAMES_EXCLUDED_FROM_DB_CLEAN_CHECK = Arrays.asList(
+    "ACT_GE_PROPERTY",
+    "ACT_RU_METER_LOG"
+  );
 
   private AtomicInteger counter = new AtomicInteger();
 
@@ -40,6 +52,7 @@ public class EnsureCleanDbPlugin implements BpmPlatformPlugin {
     counter.incrementAndGet();
   }
 
+  @SuppressWarnings("resource")
   @Override
   public void postProcessApplicationUndeploy(ProcessApplicationInterface processApplication) {
 
@@ -49,7 +62,37 @@ public class EnsureCleanDbPlugin implements BpmPlatformPlugin {
       final ProcessEngine defaultProcessEngine = BpmPlatform.getDefaultProcessEngine();
       try {
         System.out.println("Ensure cleanup after integration test ");
-        TestHelper.assertAndEnsureCleanDbAndCache(defaultProcessEngine, false);
+
+        String cacheMessage = TestHelper.assertAndEnsureCleanDeploymentCache(defaultProcessEngine, false);
+        String dbMessage = assertAndEnsureCleanDb(defaultProcessEngine, false);
+        String paRegistrationMessage = TestHelper.assertAndEnsureNoProcessApplicationsRegistered(defaultProcessEngine);
+
+
+        StringBuilder message = new StringBuilder();
+        if (cacheMessage != null) {
+          message.append(cacheMessage);
+        }
+        if (dbMessage != null) {
+          message.append(dbMessage);
+        }
+        if (paRegistrationMessage != null) {
+          message.append(paRegistrationMessage);
+        }
+
+        if(message.length() > 0) {
+          System.err.println(message);
+          File tmpFile = File.createTempFile("engine-IT-db-not-clean-", ".txt");
+          if(!tmpFile.exists()) {
+            tmpFile.createNewFile();
+          }
+
+          FileOutputStream fos = new FileOutputStream(tmpFile);
+          BufferedOutputStream out = new BufferedOutputStream(fos);
+          out.write(message.toString().getBytes());
+          out.flush();
+          out.close();
+        }
+
       }
       catch(Throwable e) {
         System.err.println("Could not clean DB:");
@@ -57,6 +100,34 @@ public class EnsureCleanDbPlugin implements BpmPlatformPlugin {
       }
     }
 
+  }
+
+  public static String assertAndEnsureCleanDb(ProcessEngine processEngine, boolean fail) {
+    ProcessEngineConfigurationImpl processEngineConfiguration = ((ProcessEngineImpl) processEngine).getProcessEngineConfiguration();
+    String databaseTablePrefix = processEngineConfiguration.getDatabaseTablePrefix().trim();
+
+    // clear user operation log in case some operations are
+    // executed with an authenticated user
+    TestHelper.clearUserOperationLog(processEngineConfiguration);
+
+    Map<String, Long> tableCounts = processEngine.getManagementService().getTableCount();
+
+    StringBuilder outputMessage = new StringBuilder();
+    for (String tableName : tableCounts.keySet()) {
+      String tableNameWithoutPrefix = tableName.replace(databaseTablePrefix, "");
+      if (!TABLENAMES_EXCLUDED_FROM_DB_CLEAN_CHECK.contains(tableNameWithoutPrefix)) {
+        Long count = tableCounts.get(tableName);
+        if (count!=0L) {
+          outputMessage.append("\t").append(tableName).append(": ").append(count).append(" record(s)\n");
+        }
+      }
+    }
+
+    if (outputMessage.length() > 0) {
+      outputMessage.insert(0, "DB NOT CLEAN: \n");
+    }
+
+    return outputMessage.toString();
   }
 
 }
