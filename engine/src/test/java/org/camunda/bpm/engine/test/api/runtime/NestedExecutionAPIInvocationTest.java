cf35b5ca226a3b7876480004475317a9e80210a8
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2016-03-10 12:02:45 +0100

    test(engine): execute-until-wait-state semantics for nested API calls
    related to CAM-5618

---
+++ engine/src/test/java/org/camunda/bpm/engine/test/api/runtime/NestedExecutionAPIInvocationTest.java
@@ -0,0 +1,84 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.engine.test.api.runtime;
+
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.impl.test.PluggableProcessEngineTestCase;
+import org.camunda.bpm.engine.repository.Deployment;
+import org.camunda.bpm.engine.runtime.ActivityInstance;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Task;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.model.bpmn.Bpmn;
+import org.camunda.bpm.model.bpmn.BpmnModelInstance;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Thorben Lindhauer
+ *
+ */
+public class NestedExecutionAPIInvocationTest {
+
+  @Rule
+  public ProcessEngineRule engineRule = new ProcessEngineRule(PluggableProcessEngineTestCase.getProcessEngine(), true);
+
+  public static final String PROCESS_KEY = "process";
+
+  public static final BpmnModelInstance PROCESS_MODEL = Bpmn.createExecutableProcess(PROCESS_KEY)
+      .startEvent()
+      .serviceTask("passThrough")
+        .camundaExpression("${true}")
+      .userTask("waitState")
+      .serviceTask("startProcess")
+        .camundaClass(NestedProcessStartDelegate.class.getName())
+      .endEvent()
+      .done();
+
+  @Test
+  @Ignore("CAM-5618")
+  public void testWaitStateIsReachedOnNestedInstantiation() {
+    // given
+    Deployment deployment = engineRule
+      .getRepositoryService()
+      .createDeployment()
+      .addModelInstance("foo.bpmn", PROCESS_MODEL)
+      .deploy();
+    engineRule.manageDeployment(deployment);
+
+    engineRule.getRuntimeService().startProcessInstanceByKey(PROCESS_KEY);
+    Task task = engineRule.getTaskService().createTaskQuery().singleResult();
+
+    // when
+    engineRule.getTaskService().complete(task.getId());
+  }
+
+  public static class NestedProcessStartDelegate implements JavaDelegate {
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+      RuntimeService runtimeService = execution.getProcessEngineServices().getRuntimeService();
+      ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(PROCESS_KEY);
+
+      // then the wait state is reached immediately after instantiation
+      ActivityInstance activityInstance = runtimeService.getActivityInstance(processInstance.getId());
+      ActivityInstance[] activityInstances = activityInstance.getActivityInstances("waitState");
+      Assert.assertEquals(1, activityInstances.length);
+
+    }
+  }
+}
