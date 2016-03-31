09010c46080a440217d16e2abce1d31663df1db1
Kristin Polenz <kristin.polenz@camunda.com>
2016-03-31 14:27:32 +0200

    feat(engine/mt): create case instance by key and tenant id
    - adjust CreateCaseInstanceCmd
    
    related to #CAM-5213

--- engine/src/test/java/org/camunda/bpm/engine/test/api/multitenancy/MultiTenancyCreateCaseInstanceTest.java
+++ engine/src/test/java/org/camunda/bpm/engine/test/api/multitenancy/MultiTenancyCreateCaseInstanceTest.java
@@ -14,17 +14,24 @@
 package org.camunda.bpm.engine.test.api.multitenancy;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 import org.camunda.bpm.engine.BadUserRequestException;
+import org.camunda.bpm.engine.ProcessEngineException;
 import org.camunda.bpm.engine.impl.test.PluggableProcessEngineTestCase;
 import org.camunda.bpm.engine.repository.CaseDefinition;
+import org.camunda.bpm.engine.runtime.CaseInstanceQuery;
 
 public class MultiTenancyCreateCaseInstanceTest extends PluggableProcessEngineTestCase {
 
   protected static final String CMMN_FILE = "org/camunda/bpm/engine/test/api/cmmn/oneTaskCase.cmmn";
 
+  protected static final String CASE_DEFINITION_KEY = "oneTaskCase";
+
   protected static final String TENANT_ONE = "tenant1";
+  protected static final String TENANT_TWO = "tenant2";
 
   public void testFailToCreateCaseInstanceByIdWithoutTenantId() {
     deployment(CMMN_FILE);
@@ -54,6 +61,66 @@ public class MultiTenancyCreateCaseInstanceTest extends PluggableProcessEngineTe
     } catch(BadUserRequestException e) {
       assertThat(e.getMessage(), containsString("Cannot specify a tenant-id"));
     }
+  }
+
+  public void testFailToCreateCaseInstanceByKeyForNonExistingTenantID() {
+    deploymentForTenant(TENANT_ONE, CMMN_FILE);
+    deploymentForTenant(TENANT_TWO, CMMN_FILE);
+
+    try {
+      caseService.withCaseDefinitionByKey(CASE_DEFINITION_KEY)
+          .caseDefinitionTenantId("nonExistingTenantId")
+          .create();
+      fail("ProcessEngineException expected");
+    } catch (ProcessEngineException e) {
+      assertThat(e.getMessage(), containsString("no case definition deployed with key 'oneTaskCase' and tenant-id 'nonExistingTenantId'"));
+    }
+  }
+
+  public void testFailToCreateCaseInstanceByKeyForMultipleTenants() {
+    deploymentForTenant(TENANT_ONE, CMMN_FILE);
+    deploymentForTenant(TENANT_TWO, CMMN_FILE);
+
+    try {
+      caseService.withCaseDefinitionByKey(CASE_DEFINITION_KEY)
+          .create();
+      fail("ProcessEngineException expected");
+    } catch (ProcessEngineException e) {
+      assertThat(e.getMessage(), containsString("multiple tenants."));
+    }
+  }
+
+  public void testCreateCaseInstanceByKeyWithoutTenantId() {
+    deployment(CMMN_FILE);
+
+    caseService.withCaseDefinitionByKey(CASE_DEFINITION_KEY)
+        .caseDefinitionWithoutTenantId()
+        .create();
+
+    CaseInstanceQuery query = caseService.createCaseInstanceQuery();
+    assertThat(query.count(), is(1L));
+    assertThat(query.singleResult().getTenantId(), is(nullValue()));
+
+  }
+
+  public void testCreateCaseInstanceByKeyForAnyTenants() {
+    deploymentForTenant(TENANT_ONE, CMMN_FILE);
+
+    caseService.withCaseDefinitionByKey(CASE_DEFINITION_KEY)
+        .create();
+
+    assertThat(caseService.createCaseInstanceQuery().tenantIdIn(TENANT_ONE).count(), is(1L));
+  }
+
+  public void testCreateCaseInstanceByKeyAndTenantId() {
+    deploymentForTenant(TENANT_ONE, CMMN_FILE);
+    deploymentForTenant(TENANT_TWO, CMMN_FILE);
+
+    caseService.withCaseDefinitionByKey(CASE_DEFINITION_KEY)
+        .caseDefinitionTenantId(TENANT_ONE)
+        .create();
+
+    assertThat(caseService.createCaseInstanceQuery().tenantIdIn(TENANT_ONE).count(), is(1L));
   }
 
 }
