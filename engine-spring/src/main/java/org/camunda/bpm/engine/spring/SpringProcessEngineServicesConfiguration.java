121d4a608c6625d8ef65e9a6093a319439d7dcc4
Jan Galinski <jan.galinski@holisticon.de>
2016-04-29 15:17:07 +0200

    add explicit imports

--- engine-spring/src/main/java/org/camunda/bpm/engine/spring/SpringProcessEngineServicesConfiguration.java
+++ engine-spring/src/main/java/org/camunda/bpm/engine/spring/SpringProcessEngineServicesConfiguration.java
@@ -1,6 +1,19 @@
 package org.camunda.bpm.engine.spring;
 
-import org.camunda.bpm.engine.*;
+import org.camunda.bpm.engine.AuthorizationService;
+import org.camunda.bpm.engine.CaseService;
+import org.camunda.bpm.engine.DecisionService;
+import org.camunda.bpm.engine.ExternalTaskService;
+import org.camunda.bpm.engine.FilterService;
+import org.camunda.bpm.engine.FormService;
+import org.camunda.bpm.engine.HistoryService;
+import org.camunda.bpm.engine.IdentityService;
+import org.camunda.bpm.engine.ManagementService;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngineServices;
+import org.camunda.bpm.engine.RepositoryService;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
