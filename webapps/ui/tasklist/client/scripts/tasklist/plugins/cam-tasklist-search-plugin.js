cee2cbfaf8b7a95fc5e9e180188b3c754c501ecb
Sebastian Stamm <sebastian.stamm@camunda.com>
2016-05-10 14:28:36 +0200

    chore(search): enforce string should strinigfy everything
    related to CAM-5952

--- webapps/ui/tasklist/client/scripts/tasklist/plugins/cam-tasklist-search-plugin.js
+++ webapps/ui/tasklist/client/scripts/tasklist/plugins/cam-tasklist-search-plugin.js
@@ -11,7 +11,10 @@ var angular = require('camunda-commons-ui/vendor/angular');
   var searchConfig = JSON.parse(searchConfigJSON);
 
   var parseValue = function(value, enforceString) {
-    if(!isNaN(value) && value.trim() !== '' && !enforceString) {
+    if(enforceString) {
+      return '' + value;
+    }
+    if(!isNaN(value) && value.trim() !== '') {
       // value must be transformed to number
       return +value;
     }
