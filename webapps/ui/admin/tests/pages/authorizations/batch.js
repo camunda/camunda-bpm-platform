54ec552881cfea5f610803229492729d05481ba7
mschoe <michael.schoettes@camunda.com>
2016-05-18 14:12:29 +0200

    chore(admin): test new auth in empty list
    related to #CAM-6037

--- webapps/ui/admin/tests/pages/authorizations/batch.js
+++ webapps/ui/admin/tests/pages/authorizations/batch.js
@@ -4,6 +4,6 @@ var Page = require('./authorizations-base');
 
 module.exports = Page.extend({
 
-  url: '/camunda/app/admin/default/#/authorization?resource=11'
+  url: '/camunda/app/admin/default/#/authorization?resource=13'
 
 });
