e8a91cbde001fb26daf863a11c2b18d602acf053
Sebastian Stamm <sebastian.stamm@camunda.com>
2016-06-14 11:03:12 +0200

    feat(plugins): load custom scripts for cockpit and admin
    related to CAM-6217

--- webapps/ui/cockpit/client/scripts/camunda-cockpit-bootstrap.js
+++ webapps/ui/cockpit/client/scripts/camunda-cockpit-bootstrap.js
@@ -41,12 +41,77 @@ __define('camunda-cockpit-bootstrap', [
       return plugin.requirePackageName;
     }));
 
-    requirejs(dependencies, function() {
-      requirejs([],function() {
-        window.define = undefined;
-        window.require = undefined;
-        camundaCockpitUi(pluginDependencies);
-      });
+    requirejs(dependencies, function(angular) {
+      // we now loaded the cockpit and the plugins, great
+      // before we start initializing the cockpit though (and leave the requirejs context),
+      // lets see if we should load some custom scripts first
+
+      if (typeof window.camCockpitConf !== 'undefined' && window.camCockpitConf.customScripts) {
+        var custom = window.camCockpitConf.customScripts || {};
+
+        // copy the relevant RequireJS configuration in a empty object
+        // see: http://requirejs.org/docs/api.html#config
+        var conf = {};
+        [
+          'baseUrl',
+          'paths',
+          'bundles',
+          'shim',
+          'map',
+          'config',
+          'packages',
+          // 'nodeIdCompat',
+          'waitSeconds',
+          'context',
+          // 'deps', // not relevant in this case
+          'callback',
+          'enforceDefine',
+          'xhtml',
+          'urlArgs',
+          'scriptType'
+          // 'skipDataMain' // not relevant either
+        ].forEach(function (prop) {
+          if (custom[prop]) {
+            conf[prop] = custom[prop];
+          }
+        });
+
+        // configure RequireJS
+        requirejs.config(conf);
+
+        // load the dependencies and bootstrap the AngularJS application
+        requirejs(custom.deps || [], function() {
+
+          // create a AngularJS module (with possible AngularJS module dependencies)
+          // on which the custom scripts can register their
+          // directives, controllers, services and all when loaded
+          angular.module('cam.cockpit.custom', custom.ngDeps);
+
+          window.define = undefined;
+          window.require = undefined;
+
+          // now that we loaded the plugins and the additional modules, we can finally
+          // initialize Cockpit
+          camundaCockpitUi(pluginDependencies);
+        });
+      }
+      else {
+        // for consistency, also create a empty module
+        angular.module('cam.cockpit.custom', []);
+
+        // make sure that we are at the end of the require-js callback queue.
+        // Why? => the plugins will also execute require(..) which will place new
+        // entries into the queue.  if we bootstrap the angular app
+        // synchronously, the plugins' require callbacks will not have been
+        // executed yet and the angular modules provided by those plugins will
+        // not have been defined yet. Placing a new require call here will put
+        // the bootstrapping of the angular app at the end of the queue
+        require([], function() {
+          window.define = undefined;
+          window.require = undefined;
+          camundaCockpitUi(pluginDependencies);
+        });
+      }
     });
 
   });
