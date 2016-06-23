dee63984b682067ca602421cf2da71d24c91fa9b
Daniel Meyer <daniel.meyer@camunda.com>
2016-06-23 06:20:39 +0200

    fix(engine/spring): formatting in SpringProcessEngineServicesConfig
    related to #CAM-6279

--- engine-spring/src/main/java/org/camunda/bpm/engine/spring/SpringProcessEngineServicesConfiguration.java
+++ engine-spring/src/main/java/org/camunda/bpm/engine/spring/SpringProcessEngineServicesConfiguration.java
@@ -38,79 +38,79 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringProcessEngineServicesConfiguration implements ProcessEngineServices {
 
-    @Autowired
-    private ProcessEngine processEngine;
+  @Autowired
+  private ProcessEngine processEngine;
 
-    @Bean(name = "runtimeService")
-    @Override
-    public RuntimeService getRuntimeService() {
-        return processEngine.getRuntimeService();
-    }
+  @Bean(name = "runtimeService")
+  @Override
+  public RuntimeService getRuntimeService() {
+    return processEngine.getRuntimeService();
+  }
 
-    @Bean(name = "repositoryService")
-    @Override
-    public RepositoryService getRepositoryService() {
-        return processEngine.getRepositoryService();
-    }
+  @Bean(name = "repositoryService")
+  @Override
+  public RepositoryService getRepositoryService() {
+    return processEngine.getRepositoryService();
+  }
 
-    @Bean(name = "formService")
-    @Override
-    public FormService getFormService() {
-        return processEngine.getFormService();
-    }
+  @Bean(name = "formService")
+  @Override
+  public FormService getFormService() {
+    return processEngine.getFormService();
+  }
 
-    @Bean(name = "taskService")
-    @Override
-    public TaskService getTaskService() {
-        return processEngine.getTaskService();
-    }
+  @Bean(name = "taskService")
+  @Override
+  public TaskService getTaskService() {
+    return processEngine.getTaskService();
+  }
 
-    @Bean(name = "historyService")
-    @Override
-    public HistoryService getHistoryService() {
-        return processEngine.getHistoryService();
-    }
+  @Bean(name = "historyService")
+  @Override
+  public HistoryService getHistoryService() {
+    return processEngine.getHistoryService();
+  }
 
-    @Bean(name = "identityService")
-    @Override
-    public IdentityService getIdentityService() {
-        return processEngine.getIdentityService();
-    }
+  @Bean(name = "identityService")
+  @Override
+  public IdentityService getIdentityService() {
+    return processEngine.getIdentityService();
+  }
 
-    @Bean(name = "managementService")
-    @Override
-    public ManagementService getManagementService() {
-        return processEngine.getManagementService();
-    }
+  @Bean(name = "managementService")
+  @Override
+  public ManagementService getManagementService() {
+    return processEngine.getManagementService();
+  }
 
-    @Bean(name = "authorizationService")
-    @Override
-    public AuthorizationService getAuthorizationService() {
-        return processEngine.getAuthorizationService();
-    }
+  @Bean(name = "authorizationService")
+  @Override
+  public AuthorizationService getAuthorizationService() {
+    return processEngine.getAuthorizationService();
+  }
 
-    @Bean(name = "caseService")
-    @Override
-    public CaseService getCaseService() {
-        return processEngine.getCaseService();
-    }
+  @Bean(name = "caseService")
+  @Override
+  public CaseService getCaseService() {
+    return processEngine.getCaseService();
+  }
 
-    @Bean(name = "filterService")
-    @Override
-    public FilterService getFilterService() {
-        return processEngine.getFilterService();
-    }
+  @Bean(name = "filterService")
+  @Override
+  public FilterService getFilterService() {
+    return processEngine.getFilterService();
+  }
 
-    @Bean(name = "externalTaskService")
-    @Override
-    public ExternalTaskService getExternalTaskService() {
-        return processEngine.getExternalTaskService();
-    }
+  @Bean(name = "externalTaskService")
+  @Override
+  public ExternalTaskService getExternalTaskService() {
+    return processEngine.getExternalTaskService();
+  }
 
-    @Bean(name = "decisionService")
-    @Override
-    public DecisionService getDecisionService() {
-        return processEngine.getDecisionService();
-    }
+  @Bean(name = "decisionService")
+  @Override
+  public DecisionService getDecisionService() {
+    return processEngine.getDecisionService();
+  }
 
 }
