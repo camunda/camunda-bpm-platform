e0bb3c6243915d70dcb6fe359c3bd51dccb490a7
hawky-4s- <hawky-4s-@users.noreply.github.com>
2016-06-28 21:07:55 +0200

    fix(tests): set isExecutable default false
    * add isExecutable to process definitions
    
    related to #CAM-5084

--- qa/integration-tests-engine/src/test/java/org/camunda/bpm/integrationtest/functional/migration/MigrationContextSwitchBeansTest.java
+++ qa/integration-tests-engine/src/test/java/org/camunda/bpm/integrationtest/functional/migration/MigrationContextSwitchBeansTest.java
@@ -41,13 +41,13 @@ import org.junit.runner.RunWith;
 @RunWith(Arquillian.class)
 public class MigrationContextSwitchBeansTest extends AbstractFoxPlatformIntegrationTest {
 
-  public static final BpmnModelInstance ONE_TASK_PROCESS = Bpmn.createProcess("oneTaskProcess")
+  public static final BpmnModelInstance ONE_TASK_PROCESS = Bpmn.createExecutableProcess("oneTaskProcess")
     .startEvent()
     .userTask("userTask")
     .endEvent()
     .done();
 
-  public static final BpmnModelInstance BOUNDARY_EVENT_PROCESS = Bpmn.createProcess("boundaryProcess")
+  public static final BpmnModelInstance BOUNDARY_EVENT_PROCESS = Bpmn.createExecutableProcess("boundaryProcess")
     .startEvent()
     .userTask("userTask")
     .boundaryEvent()
