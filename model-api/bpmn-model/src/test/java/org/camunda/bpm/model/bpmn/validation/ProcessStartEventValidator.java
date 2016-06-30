f2d1298640fe621d38679307ebc8d74b6f105035
Daniel Meyer <daniel.meyer@camunda.com>
2016-06-30 06:54:17 +0200

    test(validation): simple validator example
    related to #CAM-6308

---
+++ model-api/bpmn-model/src/test/java/org/camunda/bpm/model/bpmn/validation/ProcessStartEventValidator.java
@@ -0,0 +1,43 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.model.bpmn.validation;
+
+import java.util.Collection;
+
+import org.camunda.bpm.model.bpmn.instance.Process;
+import org.camunda.bpm.model.bpmn.instance.StartEvent;
+import org.camunda.bpm.model.xml.validation.ModelElementValidator;
+import org.camunda.bpm.model.xml.validation.ValidationResultCollector;
+
+/**
+ * @author Daniel Meyer
+ *
+ */
+public class ProcessStartEventValidator implements ModelElementValidator<Process> {
+
+  @Override
+  public Class<Process> getElementType() {
+    return Process.class;
+  }
+
+  @Override
+  public void validate(Process process, ValidationResultCollector validationResultCollector) {
+    Collection<StartEvent> startEvents = process.getChildElementsByType(StartEvent.class);
+    int startEventCount = startEvents.size();
+
+    if(startEventCount != 1) {
+      validationResultCollector.addError(10, String.format("Process does not have exactly one start event. Got %d.", startEventCount));
+    }
+  }
+
+}
---
+++ model-api/bpmn-model/src/test/java/org/camunda/bpm/model/bpmn/validation/ValidateProcessTest.java
@@ -0,0 +1,66 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.model.bpmn.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.camunda.bpm.model.bpmn.Bpmn;
+import org.camunda.bpm.model.bpmn.BpmnModelInstance;
+import org.camunda.bpm.model.xml.instance.ModelElementInstance;
+import org.camunda.bpm.model.xml.validation.ModelElementValidator;
+import org.camunda.bpm.model.xml.validation.ValidationResult;
+import org.camunda.bpm.model.xml.validation.ValidationResultType;
+import org.camunda.bpm.model.xml.validation.ValidationResults;
+import org.junit.Test;
+import org.camunda.bpm.model.bpmn.instance.Process;
+
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * @author Daniel Meyer
+ *
+ */
+public class ValidateProcessTest {
+
+  @Test
+  public void validationFailsIfNoStartEventFound() {
+
+    List<ModelElementValidator<?>> validators = new ArrayList<ModelElementValidator<?>>();
+    validators.add(new ProcessStartEventValidator());
+
+    BpmnModelInstance bpmnModelInstance = Bpmn.createProcess().done();
+
+    ValidationResults validationResults = bpmnModelInstance.validate(validators);
+
+    assertThat(validationResults.hasErrors()).isTrue();
+
+    Map<ModelElementInstance, List<ValidationResult>> results = validationResults.getResults();
+    assertThat(results.size()).isEqualTo(1);
+
+    Process process = bpmnModelInstance.getDefinitions().getChildElementsByType(Process.class).iterator().next();
+    assertThat(results.containsKey(process)).isTrue();
+
+    List<ValidationResult> resultsForProcess = results.get(process);
+    assertThat(resultsForProcess.size()).isEqualTo(1);
+
+    ValidationResult validationResult = resultsForProcess.get(0);
+    assertThat(validationResult.getElement()).isEqualTo(process);
+    assertThat(validationResult.getCode()).isEqualTo(10);
+    assertThat(validationResult.getMessage()).isEqualTo("Process does not have exactly one start event. Got 0.");
+    assertThat(validationResult.getType()).isEqualTo(ValidationResultType.ERROR);
+
+  }
+
+}
