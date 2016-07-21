b713dfb801dc1213e606fa18af8e07157282b6ca
Galinski, Jan / Kuehne + Nagel / HAM GI-DB/ external <external.jan.galinski@kuehne-nagel.com>
2016-07-21 14:42:12 +0200

    CAM-6431 adjust test to new style

--- engine/src/test/java/org/camunda/bpm/engine/test/history/useroperationlog/CustomUserOperationLogTest.java
+++ engine/src/test/java/org/camunda/bpm/engine/test/history/useroperationlog/CustomUserOperationLogTest.java
@@ -1,6 +1,7 @@
 package org.camunda.bpm.engine.test.history.useroperationlog;
 
 
+import org.camunda.bpm.engine.HistoryService;
 import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
 import org.camunda.bpm.engine.impl.interceptor.Command;
 import org.camunda.bpm.engine.impl.interceptor.CommandContext;
@@ -8,6 +9,15 @@ import org.camunda.bpm.engine.impl.interceptor.CommandExecutor;
 import org.camunda.bpm.engine.impl.oplog.UserOperationLogContext;
 import org.camunda.bpm.engine.impl.oplog.UserOperationLogContextEntry;
 import org.camunda.bpm.engine.impl.persistence.entity.PropertyChange;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.engine.test.util.ProcessEngineBootstrapRule;
+import org.camunda.bpm.engine.test.util.ProcessEngineTestRule;
+import org.camunda.bpm.engine.test.util.ProvidedProcessEngineRule;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
 
 import java.util.Arrays;
 import java.util.UUID;
@@ -15,16 +25,29 @@ import java.util.UUID;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-public class CustomUserOperationLogTest extends AbstractUserOperationLogTest {
+public class CustomUserOperationLogTest  {
+
+    public static final String USER_ID = "demo";
+
+    @ClassRule
+    public static ProcessEngineBootstrapRule bootstrapRule = new ProcessEngineBootstrapRule(
+            "org/camunda/bpm/engine/test/history/useroperationlog/enable.legacy.user.operation.log.camunda.cfg.xml");
+
 
     private static final String TASK_ID = UUID.randomUUID().toString();
 
-    private CommandExecutor commandExecutor() {
-        return ((ProcessEngineConfigurationImpl)getProcessEngine().getProcessEngineConfiguration()).getCommandExecutorTxRequired();
+    private CommandExecutor commandExecutor;
+    private HistoryService historyService;
+
+    @Before
+    public void setUp() throws Exception {
+        commandExecutor = ((ProcessEngineConfigurationImpl)bootstrapRule.getProcessEngine().getProcessEngineConfiguration()).getCommandExecutorTxRequired();
+        historyService = bootstrapRule.getProcessEngine().getHistoryService();
     }
 
+    @Test
     public void testDoNotOverwriteUserId() throws Exception {
-        commandExecutor().execute(new Command<Void>(){
+        commandExecutor.execute(new Command<Void>(){
             @Override
             public Void execute(final CommandContext commandContext) {
                 final UserOperationLogContext userOperationLogContext = new UserOperationLogContext();
