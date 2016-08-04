dce65de03877f2f1953d0e340a1ebe5ba9f41a8e
Valentin Vago <zeropaper@irata.ch>
2016-08-04 10:39:13 +0200

    refactor(cockpit-dashboard-plugins): remove unneeded template files
    Related to CAM-6479

--- webapps/ui/cockpit/plugins/base/app/views/dashboard/batches.js
+++ webapps/ui/cockpit/plugins/base/app/views/dashboard/batches.js
@@ -1,16 +1,12 @@
 'use strict';
 
-var fs = require('fs');
-
-var template = fs.readFileSync(__dirname + '/batches.html', 'utf8');
-
 module.exports = [ 'ViewsProvider', function(ViewsProvider) {
   ViewsProvider.registerDefaultView('cockpit.dashboard.section', {
     id: 'batch',
     label: 'Batches',
-    template: template,
     pagePath: '#/batch',
-    controller: [function() {}],
+    template: '<!-- nothing to show, but needed -->',
+    controller: function() {},
 
     priority: -5
   });
