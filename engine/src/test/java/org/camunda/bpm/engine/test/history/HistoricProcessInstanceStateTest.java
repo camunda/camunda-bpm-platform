895a95e83e9e0f19a41f8ae29d42887784f42630
Askar Akhmerov <askar.akhmerov@camunda.com>
2016-08-04 08:42:06 +0200

    test(engine): Move multitenancy test to proper package
    related to #CAM-3477

--- engine/src/test/java/org/camunda/bpm/engine/test/history/HistoricProcessInstanceStateTest.java
+++ engine/src/test/java/org/camunda/bpm/engine/test/history/HistoricProcessInstanceStateTest.java
@@ -35,9 +35,6 @@ public class HistoricProcessInstanceStateTest {
   public static final String PROCESS_ID = "process1";
   public static final String REASON = "very important reason";
 
-  protected static final String TENANT_ONE = "tenant1";
-  protected static final String TENANT_TWO = "tenant2";
-
   public ProcessEngineRule processEngineRule = new ProvidedProcessEngineRule();
   public ProcessEngineTestRule processEngineTestRule = new ProcessEngineTestRule(processEngineRule);
 
@@ -153,74 +150,6 @@ public class HistoricProcessInstanceStateTest {
 
     assertThat(hpi1.getState(), is(HistoricProcessInstance.STATE_ACTIVE));
     assertThat(hpi2.getState(), is(HistoricProcessInstance.STATE_ACTIVE));
-  }
-
-  @Test
-  public void testSuspensionWithTenancy() throws Exception {
-    BpmnModelInstance instance = Bpmn.createExecutableProcess(PROCESS_ID)
-        .startEvent()
-        .userTask()
-        .endEvent()
-        .done();
-    ProcessDefinition processDefinition = processEngineTestRule.deployAndGetDefinition(instance);
-    ProcessDefinition processDefinition1 = processEngineTestRule.deployForTenantAndGetDefinition(TENANT_ONE, instance);
-    ProcessDefinition processDefinition2 = processEngineTestRule.deployForTenantAndGetDefinition(TENANT_TWO, instance);
-
-    ProcessInstance processInstance = processEngineRule.getRuntimeService()
-        .startProcessInstanceById(processDefinition.getId());
-    ProcessInstance processInstance1 = processEngineRule.getRuntimeService()
-        .startProcessInstanceById(processDefinition1.getId());
-    ProcessInstance processInstance2 = processEngineRule.getRuntimeService()
-        .startProcessInstanceById(processDefinition2.getId());
-
-    //suspend Tenant one
-    processEngineRule.getRuntimeService().updateProcessInstanceSuspensionState()
-        .byProcessDefinitionKey(processDefinition1.getKey())
-        .processDefinitionTenantId(processDefinition1.getTenantId()).suspend();
-
-    String[] processInstances = {
-        processInstance1.getId(),
-        processInstance2.getId(),
-        processInstance.getId()};
-
-    verifyStates(processInstances,
-        new String[]{
-            HistoricProcessInstance.STATE_SUSPENDED,
-            HistoricProcessInstance.STATE_ACTIVE,
-            HistoricProcessInstance.STATE_ACTIVE});
-
-
-    //suspend without tenant
-    processEngineRule.getRuntimeService().updateProcessInstanceSuspensionState()
-        .byProcessDefinitionKey(processDefinition.getKey())
-        .processDefinitionWithoutTenantId().suspend();
-
-    verifyStates(processInstances,
-        new String[]{
-            HistoricProcessInstance.STATE_SUSPENDED,
-            HistoricProcessInstance.STATE_ACTIVE,
-            HistoricProcessInstance.STATE_SUSPENDED});
-
-    //reactivate without tenant
-    processEngineRule.getRuntimeService().updateProcessInstanceSuspensionState()
-        .byProcessDefinitionKey(processDefinition.getKey())
-        .processDefinitionWithoutTenantId().activate();
-
-
-    verifyStates(processInstances,
-        new String[]{
-            HistoricProcessInstance.STATE_SUSPENDED,
-            HistoricProcessInstance.STATE_ACTIVE,
-            HistoricProcessInstance.STATE_ACTIVE});
-  }
-
-  protected void verifyStates(String[] processInstances, String[] states) {
-    for (int i = 0; i < processInstances.length; i++) {
-      assertThat(
-          processEngineRule.getHistoryService().createHistoricProcessInstanceQuery()
-              .processInstanceId(processInstances[i]).singleResult().getState(),
-          is(states[i]));
-    }
   }
 
 
