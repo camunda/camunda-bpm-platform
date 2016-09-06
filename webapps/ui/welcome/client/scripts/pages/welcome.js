84db309bd2e28ab1322c59a10602726b64c3cc83
Valentin Vago <zeropaper@irata.ch>
2016-09-06 14:32:38 +0200

    feat(welcome-plugin): add plugin point
    Related to CAM-6701

--- webapps/ui/welcome/client/scripts/pages/welcome.js
+++ webapps/ui/welcome/client/scripts/pages/welcome.js
@@ -7,7 +7,7 @@ var RouteConfig = [ '$routeProvider', function($routeProvider) {
   $routeProvider.when('/welcome', {
     template: template,
     // controller: Controller,
-    controller: ['$scope', function($scope) {
+    controller: ['$scope', 'Views', function($scope, Views) {
       var auth = $scope.$root.authentication;
 
       $scope.canAccessApp = function(appName) {
@@ -17,6 +17,10 @@ var RouteConfig = [ '$routeProvider', function($routeProvider) {
       $scope.columnWidth = function() {
         return 12 / (auth.authorizedApps.length - 1);
       };
+
+      $scope.plugins = Views.getProviders({
+        component: 'welcome.dashboard'
+      });
     }],
     authentication: 'required',
     reloadOnSearch: false
