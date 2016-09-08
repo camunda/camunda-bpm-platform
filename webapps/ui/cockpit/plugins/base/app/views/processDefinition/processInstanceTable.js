2c351e70e0db926b3fef49a31c0a9701f8d17edb
Mariusz Sielski <mariusz.sielski@camunda.com>
2016-09-08 07:17:24 +0200

    <feat>(process definition): add search widget to process instances on process definition page
    Related to CAM-6603

--- webapps/ui/cockpit/plugins/base/app/views/processDefinition/processInstanceTable.js
+++ webapps/ui/cockpit/plugins/base/app/views/processDefinition/processInstanceTable.js
@@ -1,9 +1,11 @@
 'use strict';
 
 var fs = require('fs');
+var angular = require('angular');
+var createSearchQueryForSearchWidget = require('../../../../../../common/scripts/util/create-search-query-for-search-widget');
 
 var template = fs.readFileSync(__dirname + '/process-instance-table.html', 'utf8');
-var angular = require('angular');
+var searchConfig = JSON.parse(fs.readFileSync(__dirname + '/process-instance-search-config.json', 'utf8'));
 
 module.exports = [ 'ViewsProvider', function(ViewsProvider) {
 
@@ -14,16 +16,14 @@ module.exports = [ 'ViewsProvider', function(ViewsProvider) {
     controller: [
       '$scope', '$location', 'search', 'routeUtil', 'PluginProcessInstanceResource',
       function($scope,   $location,   search,   routeUtil,   PluginProcessInstanceResource) {
-
         var processData = $scope.processData.newChild($scope);
-
         var processDefinition = $scope.processDefinition;
-
         var DEFAULT_PAGES = { size: 50, total: 0, current: 1 };
-
         var pages = $scope.pages = angular.copy(DEFAULT_PAGES);
 
-        var filter = null;
+        $scope.searchConfig = angular.copy(searchConfig);
+
+        processData.provide('searches', $scope.searchConfig.searches);
 
         $scope.$watch('pages.current', function(newValue, oldValue) {
           if (newValue == oldValue) {
@@ -33,19 +33,43 @@ module.exports = [ 'ViewsProvider', function(ViewsProvider) {
           search('page', !newValue || newValue == 1 ? null : newValue);
         });
 
-        processData.observe('filter', function(newFilter) {
-          pages.current = newFilter.page || 1;
+        $scope.$watch('searchConfig.searches', function(newValue, oldValue) {
+          if (newValue !== oldValue) {
+            processData.set('searches', $scope.searchConfig.searches);
+          }
+        }, true);
 
-          updateView(newFilter);
+        processData.observe('filter', function(filter) {
+          var selectedActivityIds = filter.activityIds;
+
+          if (selectedActivityIds !== null) {
+            var urlParams = search();
+            var searches = JSON.parse(urlParams.searchQuery || '[]');
+
+            searches = removeActivitySearches(searches)
+              .concat(createSearchesForActivityIds(selectedActivityIds));
+
+            search(angular.extend(urlParams, {
+              searchQuery: JSON.stringify(searches)
+            }));
+          }
         });
 
-        function updateView(newFilter) {
+        processData.observe('searches', updateView);
 
-          filter = angular.copy(newFilter);
+        processData.observe(['searches'], function(searches) {
+          if (searches) {
+            $scope.filter.activityIds = searches
+              .filter(function(search) {
+                return search.type.value.key === 'activityIdIn';
+              })
+              .map(function(search) {
+                return search.value.value;
+              });
+          }
+        });
 
-          delete filter.page;
-          delete filter.scrollToBpmnElement;
-
+        function updateView(searches) {
           var page = pages.current,
               count = pages.size,
               firstResult = (page - 1) * count;
@@ -61,25 +85,8 @@ module.exports = [ 'ViewsProvider', function(ViewsProvider) {
             sortOrder: 'desc'
           };
 
-          var countParams = angular.extend({}, filter, defaultParams);
-
-          // fix missmatch -> activityIds -> activityIdIn
-          countParams.activityIdIn = countParams.activityIds;
-          delete countParams.activityIds;
-
-          // fix missmatch -> start -> startedAfter/startedBefore
-          angular.forEach(countParams.start, function(dateFilter) {
-            if (dateFilter.value) {
-              if (dateFilter.type === 'after') {
-                countParams.startedAfter = dateFilter.value;
-              } else if (dateFilter.type === 'before') {
-                countParams.startedBefore = dateFilter.value;
-              }
-            }
-          });
-          delete countParams.start;
-
-          var params = angular.extend({}, countParams, pagingParams);
+          var query = createSearchQueryForSearchWidget(searches, ['activityIdIn']);
+          var params = angular.extend({}, query, pagingParams, defaultParams);
 
           $scope.processInstances = null;
           $scope.loadingState = 'LOADING';
@@ -89,7 +96,7 @@ module.exports = [ 'ViewsProvider', function(ViewsProvider) {
             $scope.loadingState = data.length ? 'LOADED' : 'EMPTY';
           });
 
-          PluginProcessInstanceResource.count(countParams).$promise.then(function(data) {
+          PluginProcessInstanceResource.count(query).$promise.then(function(data) {
             pages.total = data.count;
           });
         }
@@ -110,3 +117,21 @@ module.exports = [ 'ViewsProvider', function(ViewsProvider) {
     priority: 10
   });
 }];
+
+function removeActivitySearches(searches) {
+  return searches.filter(function(search) {
+    return search.type !== 'activityIdIn';
+  });
+}
+
+function createSearchesForActivityIds(activityIds) {
+  return activityIds.map(createActivitySearch);
+}
+
+function createActivitySearch(value) {
+  return {
+    type: 'activityIdIn',
+    operator: 'eq',
+    value: value
+  };
+}
--- webapps/ui/cockpit/tests/specs/process-definition-filter-spec.js
+++ webapps/ui/cockpit/tests/specs/process-definition-filter-spec.js
@@ -8,10 +8,12 @@ var processesPage = require('../pages/processes');
 var definitionPage = require('../pages/process-definition');
 
 
-describe('Cockpit Process Definition Filter Spec', function() {
+describe.only('Cockpit Process Definition Filter Spec', function() {
+  afterEach(function() {
+    definitionPage.search.clearSearch();
+  });
 
   describe('filter by variable', function() {
-
     before(function() {
       return testHelper(setupFile.setup1, function() {
         dashboardPage.navigateToWebapp('Cockpit');
@@ -21,59 +23,34 @@ describe('Cockpit Process Definition Filter Spec', function() {
       });
     });
 
-    it('should add new filter', function() {
-
+    it('should filter by number', function() {
       // when
-      definitionPage.sidebarTabClick('Filter');
-      definitionPage.filter.addFilterByVariable('test = 1.5');
+      definitionPage.search.createSearch('Variable', '=', '1.5', 'test');
 
       // then
       expect(definitionPage.processInstancesTab.table().count()).to.eventually.eql(2);
     });
 
-
-    it('should add second filter', function() {
-
+    it('should filter by string', function() {
       // when
-      definitionPage.sidebarTabClick('Filter');
-      definitionPage.filter.addFilterByVariable('myString = \"abc dfg\"');
-
-      // then
-      expect(definitionPage.processInstancesTab.table().count()).to.eventually.eql(1);
-      expect(definitionPage.processInstancesTab.businessKey(0).getText()).to.eventually.eql('Instance2');
-    });
-
-
-    it('should remove the first filter', function() {
-
-      // when
-      definitionPage.sidebarTabClick('Filter');
-      definitionPage.filter.removeVariableFilter(0);
+      definitionPage.search.createSearch('Variable', '=', 'abc dfg', 'myString');
 
       // then
       expect(definitionPage.processInstancesTab.table().count()).to.eventually.eql(2);
+      expect(definitionPage.processInstancesTab.businessKey(1).getText()).to.eventually.eql('Instance2');
     });
-
 
     it('should add like filter', function() {
-
-      // given
-      definitionPage.filter.removeVariableFilter(0);
-
       // when
-      definitionPage.sidebarTabClick('Filter');
-      definitionPage.filter.addFilterByVariable('myString like \"123%\"');
+      definitionPage.search.createSearch('Variable', 'like', '123', 'myString');
 
       // then
       expect(definitionPage.processInstancesTab.table().count()).to.eventually.eql(1);
       expect(definitionPage.processInstancesTab.businessKey(0).getText()).to.eventually.eql('Instance1');
     });
-
   });
 
-
   describe('filter by business key and variable', function() {
-
     before(function() {
       return testHelper(setupFile.setup1, function() {
         dashboardPage.navigateToWebapp('Cockpit');
@@ -83,52 +60,32 @@ describe('Cockpit Process Definition Filter Spec', function() {
       });
     });
 
-    it('should add new filter', function() {
-
+    it('should add business key filter', function() {
       // when
-      definitionPage.sidebarTabClick('Filter');
-      definitionPage.filter.addFilterByBusinessKey('myBusinessKey');
+      definitionPage.search.createSearch('Business Key',  'myBusinessKey');
 
       // then
       expect(definitionPage.processInstancesTab.table().count()).to.eventually.eql(1);
       expect(definitionPage.processInstancesTab.businessKey(0).getText()).to.eventually.eql('myBusinessKey');
     });
 
-
-    it('should remove the filter', function() {
-
-      // when
-      definitionPage.sidebarTabClick('Filter');
-      definitionPage.filter.removeBusinessKeyFilter();
-
-      // then
-      expect(definitionPage.processInstancesTab.table().count()).to.eventually.eql(3);
-    });
-
-
     it('should combine variable filter and business key filter', function() {
-
       // when
-      definitionPage.sidebarTabClick('Filter');
-      definitionPage.filter.addFilterByVariable('test>1.49');
+      definitionPage.search.createSearch('Variable', '>', '1.49', 'test');
 
       // then
       expect(definitionPage.processInstancesTab.table().count()).to.eventually.eql(2);
 
       // when
-      definitionPage.sidebarTabClick('Filter');
-      definitionPage.filter.addFilterByBusinessKey('Instance1');
+      definitionPage.search.createSearch('Business Key', 'Instance1');
 
       // then
       expect(definitionPage.processInstancesTab.table().count()).to.eventually.eql(1);
       expect(definitionPage.processInstancesTab.businessKey(0).getText()).to.eventually.eql('Instance1');
     });
-
   });
 
-
   describe.skip('filtering with long expressions', function() {
-
     before(function() {
       return testHelper(setupFile.setup1, function() {
         dashboardPage.navigateToWebapp('Cockpit');
@@ -143,10 +100,8 @@ describe('Cockpit Process Definition Filter Spec', function() {
     });
 
     it('should filter date', function() {
-
       // when
-      definitionPage.sidebarTabClick('Filter');
-      definitionPage.filter.addFilterByVariable('myDate  =  2011-11-11T11:11:11');
+      definitionPage.search.createSearch('Variable', '=', '2011-11-11T11:11:11', 'myDate');
 
       // then
       expect(definitionPage.processInstancesTab.table().count()).to.eventually.eql(1);
@@ -155,42 +110,12 @@ describe('Cockpit Process Definition Filter Spec', function() {
 
 
     it('should filter long variable', function() {
-
       // when
-      definitionPage.sidebarTabClick('Filter');
-      definitionPage.filter.addFilterByVariable('extraLong=1234567890987654321');
+      definitionPage.search.createSearch('Variable', '=', '1234567890987654321', 'extraLong');
 
       // then
       expect(definitionPage.processInstancesTab.table().count()).to.eventually.eql(1);
       expect(definitionPage.processInstancesTab.businessKey(0).getText()).to.eventually.eql('Instance1');
     });
-
   });
-
-
-  describe('filtering dates', function() {
-
-    before(function() {
-      return testHelper(setupFile.setup1, function() {
-        dashboardPage.navigateToWebapp('Cockpit');
-        dashboardPage.authentication.userLogin('admin', 'admin');
-        dashboardPage.goToSection('Processes');
-        processesPage.deployedProcessesList.selectProcess(0);
-      });
-    });
-
-    it('should add date filter', function() {
-
-      // when
-      definitionPage.sidebarTabClick('Filter');
-      definitionPage.processInstancesTab.startTime(0).getText().then(function(startTime) {
-        definitionPage.filter.addFilterByStartDate('after', startTime);
-      });
-
-      // then
-      expect(definitionPage.processInstancesTab.table().count()).to.eventually.eql(3);
-    });
-
-  });
-
 });
