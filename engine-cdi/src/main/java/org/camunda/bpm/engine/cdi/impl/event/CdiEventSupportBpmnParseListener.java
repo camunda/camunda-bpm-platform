e13f68d2faf14a2c0272e6dfaaf0483caf7509da
Christopher Zell <Zelldon91@gmail.com>
2016-09-26 09:36:35 +0200

    fix(engine): impl new BPmnParseListener methods
     related to CAM-6764

--- engine-cdi/src/main/java/org/camunda/bpm/engine/cdi/impl/event/CdiEventSupportBpmnParseListener.java
+++ engine-cdi/src/main/java/org/camunda/bpm/engine/cdi/impl/event/CdiEventSupportBpmnParseListener.java
@@ -250,4 +250,15 @@ public class CdiEventSupportBpmnParseListener implements BpmnParseListener {
   public void parseBoundaryEscalationEventDefinition(Element escalationEventDefinition, boolean interrupting, ActivityImpl boundaryEventActivity) {
   }
 
+  @Override
+  public void parseBoundaryConditionalEventDefinition(Element element, boolean interrupting, ActivityImpl conditionalActivity) {
+  }
+
+  @Override
+  public void parseIntermediateConditionalEventDefinition(Element conditionalEventDefinition, ActivityImpl conditionalActivity) {
+  }
+
+  @Override
+  public void parseConditionalStartEventForEventSubprocess(Element element, ActivityImpl conditionalActivity, boolean interrupting) {
+  }
 }
