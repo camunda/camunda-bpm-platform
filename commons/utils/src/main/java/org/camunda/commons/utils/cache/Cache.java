41394905c25f7a5ff88c9b23ea38c3d1571dfe50
Johannes Heinemann <johannes.heinemann@camunda.com>
2016-09-29 16:43:52 +0200

    chore(engine): small adjustments according to review
    * Cache+ConcurrentLruCache: fix JavaDoc
    * ConcurrentLruCache: no private methods + remove unused methods
    
    related to #CAM-6397

--- commons/utils/src/main/java/org/camunda/commons/utils/cache/Cache.java
+++ commons/utils/src/main/java/org/camunda/commons/utils/cache/Cache.java
@@ -12,22 +12,21 @@
  */
 package org.camunda.commons.utils.cache;
 
+import java.util.Set;
+
 /**
  * A Map-like data structure that stores key-value pairs and provides temporary
  * access to it.
  *
- * @param <K>
- *          the type of keys
- * @param <V>
- *          the type of mapped values
+ * @param <K> the type of keys
+ * @param <V> the type of mapped values
  */
-public interface  Cache<K, V> {
+public interface Cache<K, V> {
 
   /**
    * Gets an entry from the cache.
    *
-   * @param key
-   *          the key whose associated value is to be returned
+   * @param key the key whose associated value is to be returned
    * @return the element, or <code>null</code>, if it does not exist.
    */
   V get(K key);
@@ -35,13 +34,9 @@ public interface  Cache<K, V> {
   /**
    * Associates the specified value with the specified key in the cache.
    *
-   * @param key
-   *          key with which the specified value is to be associated
-   * @param value
-   *          value to be associated with the specified key
-   *
-   * @throws NullPointerException
-   *           if key is <code>null</code> or if value is <code>null</code>
+   * @param key   key with which the specified value is to be associated
+   * @param value value to be associated with the specified key
+   * @throws NullPointerException if key is <code>null</code> or if value is <code>null</code>
    */
   void put(K key, V value);
 
@@ -53,11 +48,23 @@ public interface  Cache<K, V> {
   /**
    * Removes an entry from the cache.
    *
-   * @param key
-   *          key with which the specified value is to be associated.
-   * @return value
-   *          removes the value with the specified key from the cache.
+   * @param key key with which the specified value is to be associated.
    */
   void remove(K key);
+
+  /**
+   * Returns a Set view of the keys contained in this cache.
+   */
+  public Set<K> keySet();
+
+  /**
+   * @return the current size of the cache
+   */
+  public int size();
+
+  /**
+   * Returns <code>true</code> if this cache contains no key-value mappings.
+   */
+  public boolean isEmpty();
 
 }
