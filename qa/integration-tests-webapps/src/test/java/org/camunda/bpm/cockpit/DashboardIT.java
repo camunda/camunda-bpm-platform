f17b03eaf9d9d935fe95fbb14672e4f53acc2f06
Valentin Vago <zeropaper@irata.ch>
2016-10-24 10:32:51 +0200

    improve(dashboardit-test): change selectors to refelect changes in HTML

--- qa/integration-tests-webapps/src/test/java/org/camunda/bpm/cockpit/DashboardIT.java
+++ qa/integration-tests-webapps/src/test/java/org/camunda/bpm/cockpit/DashboardIT.java
@@ -33,8 +33,8 @@ public class DashboardIT extends AbstractWebappUiIntegrationTest {
     WebElement submit = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("button[type=\"submit\"]")));
     submit.submit();
 
-    wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector(".prop-definitions > .value"), "1"));
-    wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector(".prop-definitions > a"), "Process definition"));
+    wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector(".deployed .processes > .value"), "1"));
+    wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector(".deployed .processes > a"), "Process definition"));
 
     wait.until(currentURIIs(new URI(appUrl + "/default/#/dashboard")));
   }
