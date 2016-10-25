d42db8063d71d49164b0d1567b39e1f19150e0c8
Valentin Vago <zeropaper@irata.ch>
2016-10-25 11:30:25 +0200

    test(cockpit-dashboard): add some tests
    Related to CAM-6676

--- webapps/ui/cockpit/tests/specs/dashboard-spec.js
+++ webapps/ui/cockpit/tests/specs/dashboard-spec.js
@@ -21,4 +21,21 @@ describe('Cockpit Dashboard Spec', function() {
     expect(dashboardPage.navbarDropDownItem(0).getText()).to.eventually.eql('Deployments');
     expect(dashboardPage.navbarDropDownItem(1).getText()).to.eventually.eql('Batches');
   });
+
+
+  it('should display information about actual state', function () {
+    expect(element(by.css('.actual .process-instances .value')).getText()).to.eventually.eql('0');
+    expect(element(by.css('.actual .open-human-tasks .value')).getText()).to.eventually.eql('0');
+    expect(element(by.css('.actual .open-cases .value')).getText()).to.eventually.eql('0');
+    expect(element(by.css('.actual .open-incidents .value')).getText()).to.eventually.eql('0');
+    browser.sleep(5000);
+  });
+
+  it('should display information about deployed resources', function () {
+    expect(element(by.css('.deployed .processes .value')).getText()).to.eventually.eql('1');
+    expect(element(by.css('.deployed .decisions .value')).getText()).to.eventually.eql('0');
+    expect(element(by.css('.deployed .cases .value')).getText()).to.eventually.eql('0');
+    expect(element(by.css('.deployed .deployments .value')).getText()).to.eventually.eql('1');
+    browser.sleep(5000);
+  });
 });
