51823729c615a024b88f703fccc331d5cbef57d2
Askar Akhmerov <askar.akhmerov@camunda.com>
2016-10-25 09:23:48 +0200

    chor(cockpit): allow hyphen in engine name
    related to #spring-engine

--- webapps/ui/welcome/client/scripts/camunda-welcome-ui.js
+++ webapps/ui/welcome/client/scripts/camunda-welcome-ui.js
@@ -57,7 +57,7 @@ module.exports = function(pluginDependencies) {
       UriProvider.replace(':engine', [ '$window', function($window) {
         var uri = $window.location.href;
 
-        var match = uri.match(/\/app\/welcome\/(\w+)(|\/)/);
+        var match = uri.match(/\/app\/welcome\/([a-zA-Z0-9-]+)(|\/)/);
         if (match) {
           return match[1];
         } else {
