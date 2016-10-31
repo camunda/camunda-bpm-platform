a36b43f9c8b74d4ff3272e6400676ad57233294a
Mariusz Sielski <mariusz.sielski@camunda.com>
2016-10-31 11:42:50 +0100

    fix(cockpit): fix errors when displaying literal expression
    related to CAM-6748, CAM-6850, CAM-6851

--- webapps/ui/cockpit/plugins/decisionList/app/views/decisionInstance/realInput.js
+++ webapps/ui/cockpit/plugins/decisionList/app/views/decisionInstance/realInput.js
@@ -13,35 +13,37 @@
           return element.isClauseRow;
         })[0];
 
-        viewer.get('eventBus').on('cell.render', function(event) {
+        if (clauseRow && clauseRow.id) {
+          viewer.get('eventBus').on('cell.render', function(event) {
 
-          var inputIdx = data.decisionInstance.inputs.map(function(inputObject) {
-            return inputObject.clauseId;
-          }).indexOf(event.data.column.id);
+            var inputIdx = data.decisionInstance.inputs.map(function(inputObject) {
+              return inputObject.clauseId;
+            }).indexOf(event.data.column.id);
 
-          if(event.data.row === clauseRow &&
+            if (event.data.row === clauseRow &&
               inputIdx !== -1) {
 
-            var input = data.decisionInstance.inputs[inputIdx];
+              var input = data.decisionInstance.inputs[inputIdx];
 
-            var realInput = document.createElement('span');
-            if(input.type !== 'Object' &&
-               input.type !== 'Bytes' &&
-               input.type !== 'File') {
+              var realInput = document.createElement('span');
+              if (input.type !== 'Object' &&
+                input.type !== 'Bytes' &&
+                input.type !== 'File') {
 
-              realInput.className = 'dmn-input';
-              realInput.textContent = ' = ' + input.value;
+                realInput.className = 'dmn-input';
+                realInput.textContent = ' = ' + input.value;
 
-            } else {
-              realInput.className = 'dmn-input-object';
-              realInput.setAttribute('title', 'Variable value of type ' + input.type + ' is not shown');
-              realInput.textContent = ' = [' + input.type + ']';
+              } else {
+                realInput.className = 'dmn-input-object';
+                realInput.setAttribute('title', 'Variable value of type ' + input.type + ' is not shown');
+                realInput.textContent = ' = [' + input.type + ']';
+              }
+              event.gfx.firstChild.appendChild(realInput);
             }
-            event.gfx.firstChild.appendChild(realInput);
-          }
-        });
+          });
 
-        viewer.get('graphicsFactory').update('row', clauseRow, elementRegistry.getGraphics(clauseRow.id));
+          viewer.get('graphicsFactory').update('row', clauseRow, elementRegistry.getGraphics(clauseRow.id));
+        }
       }
     });
   }];
