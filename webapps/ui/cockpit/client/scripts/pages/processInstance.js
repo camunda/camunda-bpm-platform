16a4ed8b2ddf5dcc5930bb60fc01e5483233865d
Mariusz Sielski <mariusz.sielski@camunda.com>
2017-01-19 11:37:44 +0100

    refactor(process-instance): move integration with search widget to separate service
    related to CAM-7126

--- webapps/ui/cockpit/client/scripts/pages/processInstance.js
+++ webapps/ui/cockpit/client/scripts/pages/processInstance.js
@@ -5,28 +5,29 @@ var fs = require('fs');
 var template = fs.readFileSync(__dirname + '/process-instance.html', 'utf8');
 
 var angular = require('camunda-commons-ui/vendor/angular');
-var searchWidgetUtils = require('../../../../common/scripts/util/search-widget-utils');
+var commonModule = require('../../../../common/scripts/module');
 
 var routeUtil = require('../../../../common/scripts/util/routeUtil');
 var camCommons = require('camunda-commons-ui/lib');
 
-var ngModule = angular.module('cam.cockpit.pages.processInstance', [camCommons.name, 'dataDepend']);
+var ngModule = angular.module('cam.cockpit.pages.processInstance',
+  [
+    camCommons.name,
+    commonModule.name,
+    'dataDepend'
+  ]
+);
 
 var Controller = [
-  '$scope', '$filter', '$rootScope', '$location', 'search', 'ProcessDefinitionResource', 'ProcessInstanceResource', 'IncidentResource', 'Views', 'Data', 'Transform', 'processInstance', 'dataDepend', 'page', 'breadcrumbTrails',
-  function($scope,   $filter,   $rootScope,   $location,   search,   ProcessDefinitionResource,   ProcessInstanceResource,   IncidentResource,   Views,   Data,   Transform,   processInstance,   dataDepend,   page,   breadcrumbTrails) {
+  '$scope', '$filter', '$rootScope', '$location', 'search', 'ProcessDefinitionResource', 'ProcessInstanceResource',
+  'IncidentResource', 'Views', 'Data', 'Transform', 'processInstance', 'dataDepend', 'page', 'breadcrumbTrails',
+  'integrateActivityInstanceFilter', 'isModuleAvailable',
+  function($scope, $filter, $rootScope, $location, search, ProcessDefinitionResource, ProcessInstanceResource,
+      IncidentResource, Views, Data, Transform, processInstance,   dataDepend, page, breadcrumbTrails,
+      integrateActivityInstanceFilter, isModuleAvailable) {
 
-    $scope.hasMigrationPlugin = false;
-    try {
-      $scope.hasMigrationPlugin = !!angular.module('cockpit.plugin.migration');
-    }
-    catch (e) {
-      // do nothing
-    }
-
+    $scope.hasMigrationPlugin = isModuleAvailable('cockpit.plugin.migration');
     $scope.processInstance = processInstance;
-
-    var filter;
 
     var processData = $scope.processData = dataDepend.create($scope);
     var pageData = $scope.pageData = dataDepend.create($scope);
@@ -38,189 +39,12 @@ var Controller = [
       $scope.hovered = id || null;
     };
 
-    $scope.$on('$locationChangeSuccess', function() {
-      var newFilter = parseFilterFromUri($scope.filter);
-
-      if ($location.path().indexOf(processInstance.id) > -1) {
-        if (searchWidgetUtils.shouldUpdateFilter(newFilter, $scope.filter, ['activityIds', 'activityInstanceIds'])) {
-          processData.set('filter', newFilter);
-        }
-
-        pageData.set('activeTab', getDefaultTab($scope.processInstanceTabs));
-      }
-    });
-
-    function parseFilterFromUri(lastFilter) {
-      var params = search();
-      var activityInstanceIds = searchWidgetUtils.getActivityIdsFromUrlParams('activityInstanceIdIn', params);
-      var activityIds = params.activityIds ? params.activityIds.split(',') : [];
-      var ignoreActivityIds = shouldActivityIdsBeIgnored(lastFilter, activityIds, activityInstanceIds);
-
-      // if activity ids haven't changed but instance ids have changed
-      // then just ignore activity ids from url and let filter auto-complete them
-      // because it means that user removed or added instance id to filter by hand
-      if (ignoreActivityIds) {
-        activityIds = [];
-      }
-
-      return {
-        activityIds: activityIds,
-        activityInstanceIds: activityInstanceIds,
-        page: parseInt(params.page, 10) || undefined,
-        replace: ignoreActivityIds || !lastFilter
-      };
-    }
-
-    function shouldActivityIdsBeIgnored(lastFilter, activityIds, activityInstanceIds) {
-      return lastFilter && angular.equals(activityIds, lastFilter.activityIds) &&
-        !angular.equals(activityInstanceIds, lastFilter.activityInstanceIds);
-    }
-
-    function serializeFilterToUri(newFilter, replace) {
-      var activityInstanceIds = angular.isArray(newFilter.activityInstanceIds) ? newFilter.activityInstanceIds : [];
-      var activityIds = angular.isArray(newFilter.activityIds) ? newFilter.activityIds : [];
-      var urlParams = search();
-      var searches = JSON.parse(urlParams.searchQuery || '[]');
-
-      //when there is no searchQuery present and there is no ids to add to searchQuery don't change anything
-      if (!urlParams.searchQuery && !activityInstanceIds.length && !activityIds.length) {
-        searches = null;
-      } else {
-        searches = searchWidgetUtils.replaceActivitiesInSearchQuery(searches, 'activityInstanceIdIn', activityInstanceIds);
-      }
-
-      search.updateSilently({
-        searchQuery: searches? JSON.stringify(searches) : null,
-        activityIds: activityIds.length ? activityIds.join(',') : null
-      }, replace);
-
-      $scope.filter = filter = newFilter;
-    }
-
-    /**
-     * Auto complete a filter based on the given filter data.
-     *
-     * It performs the following logic
-     *
-     *   - If activity instances are selected, select the associated activities unless they are explicitly specified.
-     *   - If an activity is selected, select the associated activity instances unless they are explicitly specified.
-     *
-     * @param  {Object} filter the filter to auto complete
-     * @param  {Object} instanceIdToInstanceMap a activity instance id -> activity instance map
-     * @param  {*} activityIdToInstancesMap a activity id -> activity instance map
-     */
-    function autoCompleteFilter(newFilter, instanceIdToInstanceMap, activityIdToInstancesMap) {
-      var activityIds = newFilter.activityIds || [],
-          activityInstanceIds = newFilter.activityInstanceIds || [],
-          page = parseInt(newFilter.page, 10) || null,
-          scrollToBpmnElement = newFilter.scrollToBpmnElement,
-          // if filter has been changed from outside this component,
-          // newFilter is different from cached filter
-          externalUpdate = newFilter !== filter,
-          changed,
-          completedFilter,
-          replace = newFilter.replace;
-
-      delete newFilter.replace;
-
-      angular.forEach(activityInstanceIds, function(instanceId) {
-        var instance = instanceIdToInstanceMap[instanceId] || {},
-            activityId = instance.activityId || instance.targetActivityId,
-            idx = activityIds.indexOf(activityId);
-
-        if (idx === -1) {
-          activityIds.push(activityId);
-          changed = true;
-        }
-      });
-
-      angular.forEach(activityIds, function(activityId) {
-        var instanceList = activityIdToInstancesMap[activityId],
-            foundOne = false,
-            instanceIds = [];
-
-        if (instanceList) {
-
-          for (var i = 0, instance; (instance = instanceList[i]); i++) {
-            var idx = activityInstanceIds.indexOf(instance.id);
-
-            if (idx !== -1) {
-              foundOne = true;
-              break;
-            }
-
-            instanceIds.push(instance.id);
-          }
-
-          if (!foundOne) {
-            activityInstanceIds = activityInstanceIds.concat(instanceIds);
-            changed = true;
-          }
-
-        }
-      });
-
-      // delete activity and activity instances which do not exist
-      for(var i = 0; i < activityIds.length; i++) {
-        if(!activityIdToInstancesMap[activityIds[i]]) {
-          activityIds.splice(i, 1);
-          i--;
-        }
-      }
-      for(i = 0; i < activityInstanceIds.length; i++) {
-        if(!instanceIdToInstanceMap[activityInstanceIds[i]]) {
-          activityInstanceIds.splice(i, 1);
-          i--;
-        }
-      }
-
-      if (activityIds.length > 0) {
-        var newScrollTo = activityIds[activityIds.length - 1];
-
-        if (newScrollTo !== scrollToBpmnElement) {
-          scrollToBpmnElement = newScrollTo;
-          changed = true;
-        }
-      }
-
-      completedFilter = {
-        activityIds: activityIds,
-        activityInstanceIds: activityInstanceIds,
-        scrollToBpmnElement: scrollToBpmnElement,
-        page: page
-      };
-
-      changed = !angular.equals(completedFilter, newFilter);
-
-      // update filter only if actual changes happened above
-      // (auto completion took place)
-      if (changed) {
-
-        // update cached filters
-        $scope.filter = filter = completedFilter;
-
-        // notify external components of filter change
-        processData.set('filter', filter);
-      }
-
-      // update uri only if filter change is triggered from
-      // external view component
-      if (externalUpdate) {
-
-        // serialize filter to url
-        serializeFilterToUri(filter, replace);
-      }
-    }
-
     // end utilities ///////////////////////
 
     // /////// Begin definition of process data
 
     // processInstance
     processData.provide('processInstance', processInstance);
-
-    // filter
-    processData.provide('filter', parseFilterFromUri());
 
     // processDefinition
     processData.provide('processDefinition', ['processInstance', function(processInstance) {
@@ -400,10 +224,11 @@ var Controller = [
 
     // /////// End definition of process data
 
-
+    integrateActivityInstanceFilter($scope, function(tabs) {
+      pageData.set('activeTab', getDefaultTab(tabs));
+    });
+    
     // /////// Begin usage of definied process data
-
-    processData.observe([ 'filter', 'instanceIdToInstanceMap', 'activityIdToInstancesMap'], autoCompleteFilter);
 
     $scope.processDefinition = processData.observe('processDefinition', function(processDefinition) {
       $scope.processDefinition = processDefinition;
@@ -553,8 +378,8 @@ var Controller = [
       }
 
       var ctrlKey = $event.ctrlKey,
-          activityIds = angular.copy(filter.activityIds) || [],
-          activityInstanceIds = angular.copy(filter.activityInstanceIds) || [],
+          activityIds = angular.copy($scope.filter.activityIds) || [],
+          activityInstanceIds = angular.copy($scope.filter.activityInstanceIds) || [],
           idx = activityIds.indexOf(id),
           instanceList = angular.copy($scope.activityIdToInstancesMap[id]) || [],
           multiInstance = $scope.activityIdToInstancesMap[id+'#multiInstanceBody'],
@@ -625,8 +450,8 @@ var Controller = [
       }
 
       var ctrlKey = $event.ctrlKey,
-          activityIds = angular.copy(filter.activityIds) || [],
-          activityInstanceIds = angular.copy(filter.activityInstanceIds) || [],
+          activityIds = angular.copy($scope.filter.activityIds) || [],
+          activityInstanceIds = angular.copy($scope.filter.activityInstanceIds) || [],
           idx = activityInstanceIds.indexOf(id),
           instanceList = $scope.activityIdToInstancesMap[activityId],
           newFilter;
@@ -710,7 +535,7 @@ var Controller = [
     var initData = {
       processInstance : processInstance,
       processData     : processData,
-      filter          : filter,
+      filter          : $scope.filter,
       pageData        : pageData
     };
 
