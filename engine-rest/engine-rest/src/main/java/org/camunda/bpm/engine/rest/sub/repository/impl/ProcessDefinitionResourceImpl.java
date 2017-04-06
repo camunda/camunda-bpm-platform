8df1211880458f7c780d04d97787eb42f08ec28b
Tassilo Weidner <mail@tas.so>
2017-04-06 13:04:39 +0200

    feat(engine-rest): start a process instance on post with an empty body
    related to #CAM-7593

--- engine-rest/engine-rest/src/main/java/org/camunda/bpm/engine/rest/sub/repository/impl/ProcessDefinitionResourceImpl.java
+++ engine-rest/engine-rest/src/main/java/org/camunda/bpm/engine/rest/sub/repository/impl/ProcessDefinitionResourceImpl.java
@@ -101,6 +101,7 @@ public class ProcessDefinitionResourceImpl implements ProcessDefinitionResource 
 
   @Override
   public ProcessInstanceDto startProcessInstance(UriInfo context, StartProcessInstanceDto parameters) {
+    parameters = parameters == null ? new StartProcessInstanceDto() : parameters;
     ProcessInstanceWithVariables instance = null;
     try {
       instance = startProcessInstanceAtActivities(parameters);
