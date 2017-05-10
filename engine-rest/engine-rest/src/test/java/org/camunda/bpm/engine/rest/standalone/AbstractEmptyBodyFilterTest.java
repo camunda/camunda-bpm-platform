f867b65581c823321ffc869e53ee16d3a2c9eabc
Tassilo Weidner <tassilo.weidner@camunda.com>
2017-05-10 14:31:45 +0200

    chore(engine-rest): improve and cleanup EmptyBodyFilter
    related to #CAM-7593

--- engine-rest/engine-rest/src/test/java/org/camunda/bpm/engine/rest/standalone/AbstractEmptyBodyFilterTest.java
+++ engine-rest/engine-rest/src/test/java/org/camunda/bpm/engine/rest/standalone/AbstractEmptyBodyFilterTest.java
@@ -12,16 +12,17 @@
  */
 package org.camunda.bpm.engine.rest.standalone;
 
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.json.JSONConfiguration;
-import com.sun.jersey.client.apache4.ApacheHttpClient4;
-import com.sun.jersey.client.apache4.config.DefaultApacheHttpClient4Config;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.entity.ContentType;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.camunda.bpm.engine.RepositoryService;
 import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.impl.util.json.JSONObject;
 import org.camunda.bpm.engine.repository.ProcessDefinition;
 import org.camunda.bpm.engine.repository.ProcessDefinitionQuery;
 import org.camunda.bpm.engine.rest.AbstractRestServiceTest;
@@ -29,14 +30,14 @@ import org.camunda.bpm.engine.rest.helper.MockProvider;
 import org.camunda.bpm.engine.rest.util.container.TestContainerRule;
 import org.camunda.bpm.engine.runtime.ProcessInstanceWithVariables;
 import org.camunda.bpm.engine.runtime.ProcessInstantiationBuilder;
-import org.codehaus.jettison.json.JSONException;
-import org.codehaus.jettison.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
@@ -62,12 +63,14 @@ public abstract class AbstractEmptyBodyFilterTest extends AbstractRestServiceTes
   protected ProcessInstantiationBuilder mockInstantiationBuilder;
   protected RuntimeService runtimeServiceMock;
 
-  protected ApacheHttpClient4 client;
+  protected CloseableHttpClient client;
+  protected RequestConfig reqConfig;
   protected final String BASE_URL = "http://localhost:38080";
 
   @Before
   public void setUpRuntimeData() {
-    setUpApacheHTTPClient();
+    client = HttpClients.createDefault();
+    reqConfig = RequestConfig.custom().setConnectTimeout(3 * 60 * 1000).setSocketTimeout(10 * 60 * 1000).build();
 
     ProcessDefinition mockDefinition = MockProvider.createMockDefinition();
 
@@ -94,73 +97,80 @@ public abstract class AbstractEmptyBodyFilterTest extends AbstractRestServiceTes
     when(repositoryServiceMock.createProcessDefinitionQuery()).thenReturn(processDefinitionQueryMock);
   }
 
-  private void setUpApacheHTTPClient() {
-    DefaultApacheHttpClient4Config clientConfig = new DefaultApacheHttpClient4Config();
-    clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
-    client = ApacheHttpClient4.create(clientConfig);
-
-    DefaultHttpClient defaultHttpClient = (DefaultHttpClient) client.getClientHandler().getHttpClient();
-    HttpParams params = defaultHttpClient.getParams();
-    HttpConnectionParams.setConnectionTimeout(params, 3 * 60 * 1000);
-    HttpConnectionParams.setSoTimeout(params, 10 * 60 * 1000);
-  }
-
   @After
-  public void destroyApacheHttpClient() {
-    client.destroy();
+  public void tearDown() throws IOException {
+    client.close();
   }
 
   @Test
-  public void testBodyIsEmpty() throws JSONException {
-    ClientResponse response = client.resource(BASE_URL + START_PROCESS_INSTANCE_BY_KEY_URL)
-      .accept(MediaType.APPLICATION_JSON)
-      .entity("", MediaType.APPLICATION_JSON)
-      .post(ClientResponse.class);
+  public void testBodyIsEmpty() throws IOException {
+    HttpPost post = new HttpPost(BASE_URL + START_PROCESS_INSTANCE_BY_KEY_URL);
+    post.setConfig(reqConfig);
+    post.setHeader(HttpHeaders.CONTENT_TYPE, ContentType.create(MediaType.APPLICATION_JSON).toString());
+    post.setEntity(new ByteArrayEntity("".getBytes("UTF-8")));
 
-    assertEquals(MockProvider.EXAMPLE_PROCESS_INSTANCE_ID, response.getEntity(JSONObject.class).get("id"));
+    CloseableHttpResponse response = client.execute(post);
+
+    assertEquals(200, response.getStatusLine().getStatusCode());
+    assertEquals(MockProvider.EXAMPLE_PROCESS_INSTANCE_ID,
+      new JSONObject(EntityUtils.toString(response.getEntity(), "UTF-8")).get("id"));
     response.close();
   }
 
   @Test
-  public void testBodyIsNull() throws JSONException {
-    ClientResponse response = client.resource(BASE_URL + START_PROCESS_INSTANCE_BY_KEY_URL)
-      .accept(MediaType.APPLICATION_JSON)
-      .entity(null, MediaType.APPLICATION_JSON)
-      .post(ClientResponse.class);
+  public void testBodyIsNull() throws IOException {
+    HttpPost post = new HttpPost(BASE_URL + START_PROCESS_INSTANCE_BY_KEY_URL);
+    post.setConfig(reqConfig);
+    post.setHeader(HttpHeaders.CONTENT_TYPE, ContentType.create(MediaType.APPLICATION_JSON).toString());
+    post.setEntity(null);
 
-    assertEquals(MockProvider.EXAMPLE_PROCESS_INSTANCE_ID, response.getEntity(JSONObject.class).get("id"));
+    CloseableHttpResponse response = client.execute(post);
+
+    assertEquals(200, response.getStatusLine().getStatusCode());
+    assertEquals(MockProvider.EXAMPLE_PROCESS_INSTANCE_ID,
+      new JSONObject(EntityUtils.toString(response.getEntity(), "UTF-8")).get("id"));
     response.close();
   }
 
   @Test
-  public void testBodyIsNullAndMediaTypeIsNull() throws JSONException {
-    ClientResponse response = client.resource(BASE_URL + START_PROCESS_INSTANCE_BY_KEY_URL)
-      .entity(null, (MediaType) null)
-      .post(ClientResponse.class);
+  public void testBodyIsNullAndContentTypeIsNull() throws IOException {
+    HttpPost post = new HttpPost(BASE_URL + START_PROCESS_INSTANCE_BY_KEY_URL);
+    post.setConfig(reqConfig);
+    post.setEntity(null);
 
-    assertEquals(415, response.getStatus());
+    CloseableHttpResponse response = client.execute(post);
+
+    assertEquals(415, response.getStatusLine().getStatusCode());
     response.close();
   }
 
   @Test
-  public void testBodyIsNullAndMediaTypeHasISOCharset() throws JSONException {
-    ClientResponse response = client.resource(BASE_URL + START_PROCESS_INSTANCE_BY_KEY_URL)
-      .accept(MediaType.APPLICATION_JSON)
-      .entity(null, ContentType.create(MediaType.APPLICATION_JSON, "iso-8859-1").toString())
-      .post(ClientResponse.class);
+  public void testBodyIsNullAndContentTypeHasISOCharset() throws IOException {
+    HttpPost post = new HttpPost(BASE_URL + START_PROCESS_INSTANCE_BY_KEY_URL);
+    post.setConfig(reqConfig);
+    post.setHeader(HttpHeaders.CONTENT_TYPE, ContentType.create(MediaType.APPLICATION_JSON, "iso-8859-1").toString());
+    post.setEntity(null);
 
-    assertEquals(MockProvider.EXAMPLE_PROCESS_INSTANCE_ID, response.getEntity(JSONObject.class).get("id"));
+    CloseableHttpResponse response = client.execute(post);
+
+    assertEquals(200, response.getStatusLine().getStatusCode());
+    assertEquals(MockProvider.EXAMPLE_PROCESS_INSTANCE_ID,
+      new JSONObject(EntityUtils.toString(response.getEntity(), "UTF-8")).get("id"));
     response.close();
   }
 
   @Test
-  public void testBodyIsEmptyJSONObject() throws JSONException {
-    ClientResponse response = client.resource(BASE_URL + START_PROCESS_INSTANCE_BY_KEY_URL)
-      .accept(MediaType.APPLICATION_JSON)
-      .entity(EMPTY_JSON_OBJECT, POST_JSON_CONTENT_TYPE)
-      .post(ClientResponse.class);
+  public void testBodyIsEmptyJSONObject() throws IOException {
+    HttpPost post = new HttpPost(BASE_URL + START_PROCESS_INSTANCE_BY_KEY_URL);
+    post.setConfig(reqConfig);
+    post.setHeader(HttpHeaders.CONTENT_TYPE, ContentType.create(MediaType.APPLICATION_JSON, "iso-8859-1").toString());
+    post.setEntity(new ByteArrayEntity(EMPTY_JSON_OBJECT.getBytes("UTF-8")));
 
-    assertEquals(MockProvider.EXAMPLE_PROCESS_INSTANCE_ID, response.getEntity(JSONObject.class).get("id"));
+    CloseableHttpResponse response = client.execute(post);
+
+    assertEquals(200, response.getStatusLine().getStatusCode());
+    assertEquals(MockProvider.EXAMPLE_PROCESS_INSTANCE_ID,
+      new JSONObject(EntityUtils.toString(response.getEntity(), "UTF-8")).get("id"));
     response.close();
   }
 
