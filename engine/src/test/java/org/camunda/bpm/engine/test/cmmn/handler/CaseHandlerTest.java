de0c51d99099d05c36d53597d7416c8914afe60a
Yana Vasileva <yana.vasileva@camunda.com>
2017-05-16 08:56:11 +0200

    fix(engine): fix a test case
    Related to #CAM-7697

--- engine/src/test/java/org/camunda/bpm/engine/test/cmmn/handler/CaseHandlerTest.java
+++ engine/src/test/java/org/camunda/bpm/engine/test/cmmn/handler/CaseHandlerTest.java
@@ -14,7 +14,10 @@ package org.camunda.bpm.engine.test.cmmn.handler;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import org.camunda.bpm.engine.exception.NotValidException;
 import org.camunda.bpm.engine.impl.cmmn.behavior.CmmnActivityBehavior;
 import org.camunda.bpm.engine.impl.cmmn.entity.repository.CaseDefinitionEntity;
 import org.camunda.bpm.engine.impl.cmmn.handler.CaseHandler;
@@ -134,11 +137,13 @@ public class CaseHandlerTest extends CmmnElementHandlerTest {
     Integer historyTimeToLive = -6;
     caseDefinition.setCamundaHistoryTimeToLive(historyTimeToLive);
 
-    // when
-    CaseDefinitionEntity activity = (CaseDefinitionEntity) handler.handleElement(caseDefinition, context);
-
-    // then
-    assertEquals(Integer.valueOf(historyTimeToLive), activity.getHistoryTimeToLive());
+    try {
+      // when
+      handler.handleElement(caseDefinition, context);
+      fail("Exception is expected, that negative value is not allowed.");
+    } catch (NotValidException ex) {
+      assertTrue(ex.getMessage().contains("greater than"));
+    }
   }
 
 }
