2a09bf1deb2f3bee75375a84858f29e7e801229f
Tassilo Weidner <tassilo.weidner@camunda.com>
2017-07-19 15:14:09 +0200

    feat(admin): add sorting for users, groups & tenants
    related to CAM-8000

--- webapps/ui/admin/client/scripts/pages/tenants.js
+++ webapps/ui/admin/client/scripts/pages/tenants.js
@@ -8,29 +8,54 @@ var searchConfig = JSON.parse(fs.readFileSync(__dirname + '/tenants-search-plugi
 var angular = require('camunda-commons-ui/vendor/angular');
 
 var Controller = ['$scope', '$location', 'search', 'TenantResource', 'camAPI', 'page', function($scope, $location, search, TenantResource, camAPI, pageService) {
-
   $scope.searchConfig = angular.copy(searchConfig);
   $scope.onSearchChange = updateView;
 
+  $scope.query = $scope.pages = $scope.sortBy = $scope.sortOrder = null;
+
+  $scope.orderClass = function(forColumn) {
+    forColumn = forColumn || $scope.sortBy;
+    return 'glyphicon-' + ({
+        none: 'minus',
+        desc: 'chevron-down',
+        asc:  'chevron-up'
+      }[forColumn === $scope.sortBy ? $scope.sortOrder : 'none']);
+  };
+
+  $scope.changeOrder = function(column) {
+    $scope.sortBy = column;
+    $scope.sortOrder = $scope.sortOrder === 'desc' ? 'asc' : 'desc';
+
+    updateView();
+  };
+
   function updateView(query, pages) {
-    var page = pages.current,
-        count = pages.size,
+    if (query && pages) {
+      $scope.query = query;
+      $scope.pages = pages;
+    }
+
+    $scope.sortBy = $scope.sortBy || 'id';
+    $scope.sortOrder = $scope.sortOrder || 'asc';
+
+    var page = $scope.pages.current,
+        count = $scope.pages.size,
         firstResult = (page - 1) * count;
 
-    var pagingParams = {
+    var queryParams = {
       firstResult: firstResult,
-      maxResults: count
+      maxResults: count,
+      sortBy: $scope.sortBy,
+      sortOrder: $scope.sortOrder
     };
-
-    var params = angular.extend({}, query, pagingParams);
 
     $scope.tenantList = null;
     $scope.loadingState = 'LOADING';
 
-    return TenantResource.count(query).$promise.then(function(data) {
+    return TenantResource.count(Object.assign({}, $scope.query)).$promise.then(function(data) {
       var total = data.count;
 
-      return TenantResource.query(params).$promise.then(function(data) {
+      return TenantResource.query(Object.assign({}, $scope.query, queryParams)).$promise.then(function(data) {
         $scope.tenantList = data;
         $scope.loadingState = data.length ? 'LOADED' : 'EMPTY';
 
