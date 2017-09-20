21ebe49c63ac4fd781e484db1513c581154f1b42
siffogh <siffogh3@gmail.com>
2017-09-20 11:36:40 +0200

    chore(maintain): remove unnecessary moment-precise-range plugin

--- webapps/ui/cockpit/client/scripts/camunda-cockpit-ui.js
+++ webapps/ui/cockpit/client/scripts/camunda-cockpit-ui.js
@@ -8,7 +8,6 @@ var sdk = require('camunda-commons-ui/vendor/camunda-bpm-sdk-angular');
 var dataDepend = require('angular-data-depend');
 var camCommon = require('../../../common/scripts/module');
 var moment = require('camunda-commons-ui/vendor/moment');
-var momentPreciseRangePlugin = require('moment-precise-range-plugin');
 var events = require('events');
 var lodash = require('camunda-commons-ui/vendor/lodash');
 
@@ -134,7 +133,6 @@ module.exports.exposePackages = function(container) {
   container['camunda-bpm-sdk-js'] = sdk;
   container['angular-data-depend'] = dataDepend;
   container['moment'] = moment;
-  container['moment-precise-range-plugin'] = momentPreciseRangePlugin;
   container['events'] = events;
   container['cam-common'] = camCommon;
   container['lodash'] = lodash;
