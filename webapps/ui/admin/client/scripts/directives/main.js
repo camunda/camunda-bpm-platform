fb4869a03b7a6bc2ac83a017253a5e7a1bffb168
siffogh <siffogh3@gmail.com>
2017-10-16 11:30:18 +0200

    chore(maintain): move date directive from cockpit to common
    Related to CAM-8285

--- webapps/ui/admin/client/scripts/directives/main.js
+++ webapps/ui/admin/client/scripts/directives/main.js
@@ -2,7 +2,9 @@
 
 var angular = require('camunda-commons-ui/vendor/angular'),
 
-    breadcrumbs = require('./../../../../common/scripts/directives/breadcrumbs');
+    breadcrumbs = require('./../../../../common/scripts/directives/breadcrumbs'),
+    date = require('./../../../../common/scripts/directives/date');
 
 var directivesModule = module.exports = angular.module('cam.admin.directives', []);
 directivesModule.directive('camBreadcrumbsPanel', breadcrumbs);
+directivesModule.directive('date', date);
