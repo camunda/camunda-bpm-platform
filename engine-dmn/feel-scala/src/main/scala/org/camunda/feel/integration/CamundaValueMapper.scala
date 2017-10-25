95f71cae75f177363d50e4dad72121c2910a9253
Philipp Ossler <philipp.ossler@camunda.com>
2017-10-25 23:27:00 +0200

    feat(timezone): add support for date-time and time with zone offset
    related to #28

--- engine-dmn/feel-scala/src/main/scala/org/camunda/feel/integration/CamundaValueMapper.scala
+++ engine-dmn/feel-scala/src/main/scala/org/camunda/feel/integration/CamundaValueMapper.scala
@@ -4,15 +4,28 @@ import org.camunda.feel._
 import org.camunda.feel.interpreter._
 import org.camunda.feel.spi._
 import scala.collection.JavaConverters._
-import java.time.ZoneId
+import java.time._
 
 class CamundaValueMapper extends CustomValueMapper {
 
+  override def toVal(x: Any): Val = x match {
+    // joda-time
+    case x: org.joda.time.LocalDate => ValDate(LocalDate.of(x.getYear, x.getMonthOfYear, x.getDayOfMonth))
+    case x: org.joda.time.LocalTime => ValLocalTime(LocalTime.of(x.getHourOfDay, x.getMinuteOfHour, x.getSecondOfMinute))
+    case x: org.joda.time.LocalDateTime => ValLocalDateTime(LocalDateTime.of(x.getYear, x.getMonthOfYear, x.getDayOfMonth, x.getHourOfDay, x.getMinuteOfHour, x.getSecondOfMinute))
+    case x: org.joda.time.Duration => ValDayTimeDuration( Duration.ofMillis( x.getMillis ) )
+    case x: org.joda.time.Period => ValYearMonthDuration( Period.of(x.getYears, x.getMonths, 0) )
+    // else
+    case _ => super.toVal(x)
+  }
+
   override def unpackVal(value: Val): Any = value match {
     case ValNumber(number) => number.doubleValue: java.lang.Double
-    case ValDateTime(dateTime) => java.util.Date.from(dateTime.atZone(ZoneId.systemDefault).toInstant): java.util.Date
+    case ValLocalDateTime(dateTime) => java.util.Date.from(dateTime.atZone(ZoneId.systemDefault).toInstant): java.util.Date
+    case ValDateTime(dateTime) => java.util.Date.from(dateTime.toInstant): java.util.Date
     case ValList(list) => (list map unpackVal).asJava: java.util.List[Any]
     case ValContext(dc: DefaultContext) => (dc.variables map { case (key, value) => key -> unpackVal(toVal(value)) } toMap).asJava: java.util.Map[String, Any]
+    // else
     case _ => super.unpackVal(value)
   }
 
--- engine-dmn/feel-scala/src/test/scala/org/camunda/feel/integration/CamundaFeelEngineIntegrationTest.scala
+++ engine-dmn/feel-scala/src/test/scala/org/camunda/feel/integration/CamundaFeelEngineIntegrationTest.scala
@@ -79,4 +79,44 @@ class CamundaFeelEngineIntegrationTest extends FlatSpec with Matchers {
     camundaFeelEngine.evaluateSimpleExpression[Any](""" {a:"b"} """, context) should be (map)
   }
 
+  it should "map joda-local-date as Date" in {
+
+    val camundaFeelEngine = camundaFeelEngineFactory.createInstance()
+    val context = new SimpleTestContext(Map("x" -> org.joda.time.LocalDate.parse("2017-04-02")))
+
+    camundaFeelEngine.evaluateSimpleExpression[Boolean](""" x = date("2017-04-02") """, context) should be (true)
+  }
+
+  it should "map joda-local-time as Date" in {
+
+    val camundaFeelEngine = camundaFeelEngineFactory.createInstance()
+    val context = new SimpleTestContext(Map("x" -> org.joda.time.LocalTime.parse("12:04:30")))
+
+    camundaFeelEngine.evaluateSimpleExpression[Boolean](""" x = time("12:04:30") """, context) should be (true)
+  }
+
+  it should "map joda-local-date-time as Date" in {
+
+    val camundaFeelEngine = camundaFeelEngineFactory.createInstance()
+    val context = new SimpleTestContext(Map("x" -> org.joda.time.LocalDateTime.parse("2017-04-02T12:04:30")))
+
+    camundaFeelEngine.evaluateSimpleExpression[Boolean](""" x = date and time("2017-04-02T12:04:30") """, context) should be (true)
+  }
+
+  it should "map joda-period as Year-Month-Duration" in {
+
+    val camundaFeelEngine = camundaFeelEngineFactory.createInstance()
+    val context = new SimpleTestContext(Map("x" -> org.joda.time.Period.parse("P2Y4M")))
+
+    camundaFeelEngine.evaluateSimpleExpression[Boolean](""" x = duration("P2Y4M") """, context) should be (true)
+  }
+
+  it should "map joda-duration as Day-Time-Duration" in {
+
+    val camundaFeelEngine = camundaFeelEngineFactory.createInstance()
+    val context = new SimpleTestContext(Map("x" -> org.joda.time.Duration.standardHours(4)))
+
+    camundaFeelEngine.evaluateSimpleExpression[Boolean](""" x = duration("PT4H") """, context) should be (true)
+  }
+
 }
