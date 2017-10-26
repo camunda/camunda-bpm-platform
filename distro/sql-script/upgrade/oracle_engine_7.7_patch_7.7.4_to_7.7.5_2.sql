8835d30f0652fc76453340c0138fe253c378a7a6
Anna Pazola <pazolka@gmail.com>
2017-10-26 13:08:17 +0200

    fix(oracle): drop indices before changing field length in patch script
    related to CAM-8177

--- distro/sql-script/upgrade/oracle_engine_7.7_patch_7.7.4_to_7.7.5_2.sql
+++ distro/sql-script/upgrade/oracle_engine_7.7_patch_7.7.4_to_7.7.5_2.sql
@@ -1,3 +1,18 @@
 -- increase the field length https://app.camunda.com/jira/browse/CAM-8177 --
+DROP INDEX ACT_UNIQ_AUTH_USER;
+DROP INDEX ACT_UNIQ_AUTH_GROUP;
+
 ALTER TABLE ACT_RU_AUTHORIZATION 
   MODIFY RESOURCE_ID_ nvarchar2(255);
+
+create unique index ACT_UNIQ_AUTH_USER on ACT_RU_AUTHORIZATION
+   (case when USER_ID_ is null then null else TYPE_ end,
+    case when USER_ID_ is null then null else RESOURCE_TYPE_ end,
+    case when USER_ID_ is null then null else RESOURCE_ID_ end,
+    case when USER_ID_ is null then null else USER_ID_ end);
+
+create unique index ACT_UNIQ_AUTH_GROUP on ACT_RU_AUTHORIZATION
+   (case when GROUP_ID_ is null then null else TYPE_ end,
+    case when GROUP_ID_ is null then null else RESOURCE_TYPE_ end,
+    case when GROUP_ID_ is null then null else RESOURCE_ID_ end,
+    case when GROUP_ID_ is null then null else GROUP_ID_ end);
