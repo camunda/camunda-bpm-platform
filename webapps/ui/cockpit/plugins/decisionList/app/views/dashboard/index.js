05c0dbadcab75272ff10a98a3030640436ddb168
Tassilo Weidner <tassilo.weidner@camunda.com>
2017-11-03 18:13:13 +0100

    chore(cockpit): remove drds table
    related to CAM-7900

--- webapps/ui/cockpit/plugins/decisionList/app/views/dashboard/index.js
+++ webapps/ui/cockpit/plugins/decisionList/app/views/dashboard/index.js
@@ -6,7 +6,6 @@ var decisionList = require('./views/decision-list');
 var DecisionListController = require('./controllers/decision-list');
 var decisionListService = require('./services/decision-list');
 var decisionsTableComponent = require('./components/decisions-table');
-var drdsTableComponent = require('./components/drds-table');
 
 var ngModule = angular.module('cockpit.plugin.decisionList.views.dashboard', [camCommon.name]);
 
@@ -17,6 +16,5 @@ ngModule.factory('decisionList', decisionListService);
 ngModule.controller('DecisionListController', DecisionListController);
 
 ngModule.directive('decisionsTable', decisionsTableComponent);
-ngModule.directive('drdsTable', drdsTableComponent);
 
 module.exports = ngModule;
--- webapps/ui/cockpit/plugins/drd/app/plugins/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var angular = require('camunda-commons-ui/vendor/angular');
-
-var ngModule = angular.module('cockpit.plugin.drd.dashboard', []);
-
-module.exports = ngModule;
