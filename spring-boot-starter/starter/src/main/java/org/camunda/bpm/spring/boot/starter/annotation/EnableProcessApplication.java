61ab3f03c9b2ec4c39ec603d5d29ecdfeebab01d
Falko Menge <falko.menge@camunda.com>
2017-11-14 20:40:38 +0100

    docs(EnableProcessApplication): add JavaDoc to annotation

--- spring-boot-starter/starter/src/main/java/org/camunda/bpm/spring/boot/starter/annotation/EnableProcessApplication.java
+++ spring-boot-starter/starter/src/main/java/org/camunda/bpm/spring/boot/starter/annotation/EnableProcessApplication.java
@@ -1,5 +1,6 @@
 package org.camunda.bpm.spring.boot.starter.annotation;
 
+import org.camunda.bpm.engine.spring.SpringProcessEngineConfiguration;
 import org.camunda.bpm.spring.boot.starter.SpringBootProcessApplication;
 import org.springframework.context.annotation.Import;
 
@@ -10,6 +11,24 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation that disables the {@link SpringProcessEngineConfiguration}
+ * auto-deploy feature and instead uses the required META-INF/processes.xml
+ * as an indicator for resource scanning. This also allows all processes.xml
+ * configuration features described here:
+ * <a href="https://docs.camunda.org/manual/latest/user-guide/process-applications/the-processes-xml-deployment-descriptor/">The processes.xml Deployment Descriptor</a>
+ * 
+ * <p>To use it, just add the annotation to your Spring Boot application class:</p>
+ * <pre>
+ * &#64;SpringBootApplication
+ * &#64;EnableProcessApplication("myProcessApplicationName")
+ * public class MyApplication {
+ * 
+ * ...
+ * 
+ * }
+ * </pre>
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Import(SpringBootProcessApplication.class)
