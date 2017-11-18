fd9f0e5b06e759e1208cd838b41a039e8a43a76f
Svetlana Dorokhova <svetlana.dorokhova@camunda.com>
2017-11-18 23:32:23 +0100

    fix(test): cleanup history level after test run

--- spring-boot-starter/starter/src/test/java/org/camunda/bpm/spring/boot/starter/CamundaBpmActuatorConfigurationIT.java
+++ spring-boot-starter/starter/src/test/java/org/camunda/bpm/spring/boot/starter/CamundaBpmActuatorConfigurationIT.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = { TestApplication.class }, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class CamundaBpmActuatorConfigurationIT {
+public class CamundaBpmActuatorConfigurationIT extends AbstractCamundaAutoConfigurationIT{
 
   @Autowired
   private TestRestTemplate testRestTemplate;
