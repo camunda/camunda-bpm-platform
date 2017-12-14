6b38e5b041e117e62e5daeec010a153a27b2e64f
Roman Smirnov <roman.smirnov@camunda.com>
2017-12-14 16:08:18 +0100

    feat(incident): add sorting of incidents
    related to CAM-8623

--- webapps/src/main/java/org/camunda/bpm/cockpit/impl/plugin/base/dto/query/IncidentQueryDto.java
+++ webapps/src/main/java/org/camunda/bpm/cockpit/impl/plugin/base/dto/query/IncidentQueryDto.java
@@ -14,6 +14,11 @@ package org.camunda.bpm.cockpit.impl.plugin.base.dto.query;
 
 import javax.ws.rs.core.MultivaluedMap;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.camunda.bpm.cockpit.impl.plugin.base.dto.IncidentDto;
 import org.camunda.bpm.cockpit.rest.dto.AbstractRestQueryParametersDto;
 import org.camunda.bpm.engine.rest.dto.CamundaQueryParam;
@@ -25,6 +30,32 @@ import org.camunda.bpm.engine.rest.dto.converter.StringArrayConverter;
 public class IncidentQueryDto extends AbstractRestQueryParametersDto<IncidentDto> {
 
   private static final long serialVersionUID = 1L;
+
+  private static final String SORT_BY_INCIDENT_TIMESTAMP = "incidentTimestamp";
+  private static final String SORT_BY_INCIDENT_TYPE = "incidentType";
+  private static final String SORT_BY_ACTIVITY_ID = "activityId";
+  private static final String SORT_BY_CAUSE_INCIDENT_PROCESS_INSTANCE_ID = "causeIncidentProcessInstanceId";
+  private static final String SORT_BY_ROOT_CAUSE_INCIDENT_PROCESS_INSTANCE_ID = "rootCauseIncidentProcessInstanceId";
+
+  private static final List<String> VALID_SORT_BY_VALUES;
+  static {
+    VALID_SORT_BY_VALUES = new ArrayList<String>();
+    VALID_SORT_BY_VALUES.add(SORT_BY_INCIDENT_TIMESTAMP);
+    VALID_SORT_BY_VALUES.add(SORT_BY_INCIDENT_TYPE);
+    VALID_SORT_BY_VALUES.add(SORT_BY_ACTIVITY_ID);
+    VALID_SORT_BY_VALUES.add(SORT_BY_CAUSE_INCIDENT_PROCESS_INSTANCE_ID);
+    VALID_SORT_BY_VALUES.add(SORT_BY_ROOT_CAUSE_INCIDENT_PROCESS_INSTANCE_ID);
+  }
+
+  private static final Map<String, String> ORDER_BY_VALUES;
+  static {
+    ORDER_BY_VALUES = new HashMap<String, String>();
+    ORDER_BY_VALUES.put(SORT_BY_INCIDENT_TIMESTAMP, "RES.INCIDENT_TIMESTAMP_");
+    ORDER_BY_VALUES.put(SORT_BY_INCIDENT_TYPE, "RES.INCIDENT_TYPE_");
+    ORDER_BY_VALUES.put(SORT_BY_ACTIVITY_ID, "RES.ACTIVITY_ID_");
+    ORDER_BY_VALUES.put(SORT_BY_CAUSE_INCIDENT_PROCESS_INSTANCE_ID, "RES.CAUSE_PROC_INST_ID_");
+    ORDER_BY_VALUES.put(SORT_BY_ROOT_CAUSE_INCIDENT_PROCESS_INSTANCE_ID, "RES.ROOT_PROC_INST_ID_");
+  }
 
   protected String[] processDefinitionIdIn;
   protected String[] processInstanceIdIn;
@@ -64,10 +95,10 @@ public class IncidentQueryDto extends AbstractRestQueryParametersDto<IncidentDto
   }
 
   protected boolean isValidSortByValue(String value) {
-    return false;
+    return VALID_SORT_BY_VALUES.contains(value);
   }
 
   protected String getOrderByValue(String sortBy) {
-    return super.getOrderBy();
+    return ORDER_BY_VALUES.get(sortBy);
   }
 }
