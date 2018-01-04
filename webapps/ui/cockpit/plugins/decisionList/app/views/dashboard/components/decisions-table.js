2511559c3c14145155c2e553fd87c9585c809b24
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2018-01-04 11:53:23 +0100

    feat(cockpit): add persistent sorting to decision definition table
    related to CAM-8550

--- webapps/ui/cockpit/plugins/decisionList/app/views/dashboard/components/decisions-table.js
+++ webapps/ui/cockpit/plugins/decisionList/app/views/dashboard/components/decisions-table.js
@@ -12,6 +12,39 @@ module.exports = function() {
       decisionCount: '=',
       decisions: '=',
       isDrdAvailable: '='
-    }
+    },
+    controller: ['$scope', 'localConf','$translate',
+      function($scope, localConf, $translate) {
+
+        $scope.headColumns = [
+          { class: 'name',     request: 'key'          , sortable: true, content: $translate.instant('PLUGIN_DECISION_TABLE_NAME')},
+          { class: 'tenant-id', request: 'tenantId'     , sortable: true, content: $translate.instant('PLUGIN_DECISION_TABLE_TENANT_ID')},
+          { class: 'tenant-id',  request: 'drd.key', sortable: true, content: $translate.instant('PLUGIN_DECISION_TABLE_DECISION_REQUIREMENTS'), condition: $scope.isDrdAvailable}
+        ];
+
+        // Default sorting
+        var defaultValue = { sortBy: 'key', sortOrder: 'asc', sortReverse: false};
+        $scope.sortObj   = loadLocal(defaultValue);
+
+        // Update Table
+        $scope.onSortChange = function(sortObj) {
+          sortObj = sortObj || $scope.sortObj;
+          // transforms sortOrder to boolean required by anqular-sorting;
+          sortObj.sortReverse = sortObj.sortOrder !== 'asc';
+          saveLocal(sortObj);
+          $scope.sortObj = sortObj;
+        };
+
+        function saveLocal(sortObj) {
+          localConf.set('sortProcessDefTab', sortObj);
+
+        }
+        function loadLocal(defaultValue) {
+          return localConf.get('sortProcessDefTab', defaultValue);
+        }
+
+
+
+      }]
   };
 };
