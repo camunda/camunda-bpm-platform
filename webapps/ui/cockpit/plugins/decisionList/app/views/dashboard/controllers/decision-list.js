c3ed1df5eb87d117bcb68cede401c503faeab459
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2018-01-04 15:05:27 +0100

    feat(cockpit): move DRD sorting logic to platform-ee
    related to CAM-8552

--- webapps/ui/cockpit/plugins/decisionList/app/views/dashboard/controllers/decision-list.js
+++ webapps/ui/cockpit/plugins/decisionList/app/views/dashboard/controllers/decision-list.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = [
-  '$scope', 'decisionList', 'Views', 'localConf', '$translate',
-  function($scope, decisionList, Views, localConf, $translate) {
+  '$scope', 'decisionList', 'Views', //'localConf', '$translate',
+  function($scope, decisionList, Views /*, localConf, $translate*/) {
     $scope.loadingState = 'LOADING';
     $scope.drdDashboard = Views.getProvider({ component: 'cockpit.plugin.drd.dashboard' });
     $scope.isDrdDashboardAvailable = !!$scope.drdDashboard;
@@ -25,38 +25,6 @@ module.exports = [
         throw err;
       });
 
-
-    // DRD table
-    $scope.headColumns = [
-      { class: 'name',     request: 'key'          , sortable: true, content: $translate.instant('PLGN_DRD_NAME')},
-      { class: 'tenant-id', request: 'tenantId'     , sortable: true, content: $translate.instant('PLGN_DRD_TENANT_ID_COL')}
-    ];
-
-    // Default sorting
-    var defaultValue = { sortBy: 'key', sortOrder: 'asc', sortReverse: false};
-    $scope.sortObj   = loadLocal(defaultValue);
-
-    // Update Table
-    $scope.onSortChange = function(sortObj) {
-      sortObj = sortObj || $scope.sortObj;
-      // transforms sortOrder to boolean required by anqular-sorting;
-      sortObj.sortReverse = sortObj.sortOrder !== 'asc';
-      saveLocal(sortObj);
-      $scope.sortObj = sortObj;
-    };
-
-    function saveLocal(sortObj) {
-      localConf.set('sortDRDTab', sortObj);
-
-    }
-    function loadLocal(defaultValue) {
-      return localConf.get('sortDRDTab', defaultValue);
-    }
-
-
-
-
-
-    $scope.drdDashboardVars = { read: [ 'drdsCount', 'drds','headColumns', 'sortObj','onSortChange' ] };
+    $scope.drdDashboardVars = { read: [ 'drdsCount', 'drds'] };
   }
 ];
