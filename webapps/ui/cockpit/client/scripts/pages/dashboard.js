6182d327e46390c5b91301c3a43c3529bf06865e
Seif Eddine Ghezala <siffogh3@gmail.com>
2018-01-09 11:14:32 +0100

    maintain(cockpit): refactor code for aggregating process instances count in dashboard
    Related to CAM-8604

--- webapps/ui/cockpit/client/scripts/pages/dashboard.js
+++ webapps/ui/cockpit/client/scripts/pages/dashboard.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var angular = require('camunda-commons-ui/vendor/angular');
 var fs = require('fs');
 var template = fs.readFileSync(__dirname + '/dashboard.html', 'utf8');
 var series = require('camunda-bpm-sdk-js').utils.series;
@@ -194,85 +193,6 @@ var Controller = [
       $scope.data.actual.openIncidents = totalIncidents;
     }
 
-    var aggregateStatistics = function(statistics) {
-      // represents a map which contains to definition key and tenant id
-      // the corresponding statistics.
-      var statisticsResult = [];
-      // the result to return
-      var result = [];
-
-      // iterate over assigned statistics
-      angular.forEach(statistics, function(currentStatistic) {
-
-        // get the statistics to the definition key of the current item
-        var statisticsForDefinition = statisticsResult[currentStatistic.definition.key];
-
-        if(!statisticsForDefinition) {
-          // create an array for tenants if not exists
-          statisticsResult[currentStatistic.definition.key] = [];
-        }
-
-        // get the statistic for the tenant id of the current item
-        var statistic = statisticsResult[currentStatistic.definition.key][currentStatistic.definition.tenantId];
-
-        if (!statistic) {
-          // if there does not exists a statistic to the definition key
-          // then create a copy of the current item (currentStatistic).
-          statistic = angular.copy(currentStatistic);
-
-          if (!statistic.definition.name) {
-            // if there does not exist a name then set definition key
-            // as the name of the definition.
-            statistic.definition.name = statistic.definition.key;
-          }
-
-          // put the statistic into the map of statistics
-          statisticsResult[statistic.definition.key][currentStatistic.definition.tenantId] = statistic;
-
-          // add the statistic to the result set
-          result.push(statistic);
-
-        } else {
-          if (currentStatistic.definition.version > statistic.definition.version) {
-            // if the version of the current statistic, then create copy from them.
-            statistic.definition = currentStatistic.definition;
-            statistic.id = currentStatistic.id;
-
-            // if there does not exist a name then set definition key
-            // as the name of the definition.
-            statistic.definition.name = statistic.definition.name || statistic.definition.key;
-          }
-
-          // Add the saved values to the corresponding values of the current statistic
-          statistic.instances += currentStatistic.instances;
-          statistic.failedJobs += currentStatistic.failedJobs;
-
-          angular.forEach(currentStatistic.incidents, function(incident) {
-            var incidentType = incident.incidentType;
-            var incidentCount = incident.incidentCount;
-
-            var newIncident = true;
-            for(var i = 0; i < statistic.incidents.length; i++) {
-              var statisticIncident = statistic.incidents[i];
-              if (statisticIncident.incidentType == incidentType) {
-                statisticIncident.incidentCount = incidentCount + statisticIncident.incidentCount;
-                newIncident = false;
-              }
-            }
-
-            if (newIncident) {
-              // merge the incidents
-              statistic.incidents.push(incident);
-            }
-
-          });
-
-        }
-      });
-
-      return result;
-    };
-
 
     var taskResource = camAPI.resource('task');
     $scope.$watch('actualActive', function() {
@@ -290,12 +210,10 @@ var Controller = [
 
             $scope.processDefinitionData = data.items;
 
-            camAPI.resource('process-definition').statistics({ rootIncidents: true })
-              .then(function(processDefinitionStatistics) {
-                var aggregatedStatistics = aggregateStatistics(processDefinitionStatistics);
-                aggregateInstances(aggregatedStatistics);
-                aggregateIncidents(aggregatedStatistics);
-              });
+            processData.observe('processDefinitionWithRootIncidentsStatistics', function(processDefinitionStatistics) {
+              aggregateInstances(processDefinitionStatistics);
+              aggregateIncidents(processDefinitionStatistics);
+            });
 
             next();
           });
