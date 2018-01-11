97046e902d40371d8b5e296b855b390b8784d475
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2018-01-11 16:09:23 +0100

    fix(cockpit): fix test cases
    related to CAM-8572

--- webapps/ui/common/unit-tests/tests-module.js
+++ webapps/ui/common/unit-tests/tests-module.js
@@ -2,6 +2,7 @@ var angular = require('camunda-commons-ui/vendor/angular');
 var createCamApiMock = require('./create-cam-api-mock');
 var ViewsProvider = require('./views-provider-mock');
 var $routeProvider = require('./route-provider-mock');
+var localConfMock = require('./local-conf-mock');
 
 var ngModule = angular.module('common-tests-module', []);
 
@@ -11,6 +12,10 @@ ngModule.provider('Views', function() {
 });
 ngModule.provider('$route', function() {
   return $routeProvider;
+});
+
+ngModule.provider('localConf', function() {
+  return localConfMock;
 });
 
 module.exports = ngModule;
