3e093665fe82c82efda22966cdc94d385619a690
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2018-01-16 11:56:55 +0100

    feat(cockpit): add sorting to decision instance inputs
    related to CAM-8560

--- webapps/ui/cockpit/plugins/decisionList/app/views/decisionInstance/outputTable.js
+++ webapps/ui/cockpit/plugins/decisionList/app/views/decisionInstance/outputTable.js
@@ -12,7 +12,10 @@ module.exports = [ 'ViewsProvider', function(ViewsProvider) {
     template: template,
     controller: [
       '$scope',
-      function($scope) {
+      '$translate',
+      'localConf',
+      'orderByFilter',
+      function($scope, $translate, localConf, orderBy) {
 
         $scope.variables = $scope.decisionInstance.outputs.map(function(variable) {
           return {
@@ -24,6 +27,33 @@ module.exports = [ 'ViewsProvider', function(ViewsProvider) {
             }
           };
         });
+
+        $scope.headColumns = [
+          { class: 'name',  request: 'variable.name', sortable: true, content: $translate.instant('PLUGIN_VARIABLE_NAME')},
+          { class: 'type',  request: 'variable.type', sortable: true, content: $translate.instant('PLUGIN_VARIABLE_TYPE')},
+          { class: 'value', request: '', sortable: false, content: $translate.instant('PLUGIN_VARIABLE_VALUE')}
+        ];
+
+        // Default sorting
+        $scope.sortObj   = loadLocal({ sortBy: 'variable.name', sortOrder: 'asc', sortReverse: false });
+
+        $scope.onSortChange = function(sortObj) {
+          sortObj = sortObj || $scope.sortObj;
+          sortObj.sortReverse = sortObj.sortOrder !== 'asc';
+          saveLocal(sortObj);
+          // Angular filter function
+          $scope.variables = orderBy($scope.variables, sortObj.sortBy, sortObj.sortReverse);
+        };
+
+        function saveLocal(sortObj) {
+          localConf.set('sortDecisionInputTab', sortObj);
+
+        }
+
+        function loadLocal(defaultValue) {
+          return localConf.get('sortDecisionInputTab', defaultValue);
+        }
+
       }],
     priority: 10
   });
