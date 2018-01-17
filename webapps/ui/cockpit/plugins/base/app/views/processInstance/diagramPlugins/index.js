5b0a5a101805bca75f730145e4a506d28f1ecd58
Seif Eddine Ghezala <siffogh3@gmail.com>
2018-01-17 15:33:21 +0100

    feat(cockpit): add drill down to call activity in runtime view of process instance
    Related to CAM-8676

--- webapps/ui/cockpit/plugins/base/app/views/processInstance/diagramPlugins/index.js
+++ webapps/ui/cockpit/plugins/base/app/views/processInstance/diagramPlugins/index.js
@@ -4,11 +4,13 @@ var angular = require('angular');
 var camCommon = require('cam-common');
 
 var instanceCount = require('./instanceCount');
+var callActivity = require('./callActivity');
 
 var ngModule = angular.module('cockpit.plugin.base.views.instance.diagram-plugins', [
   camCommon.name
 ]);
 
 ngModule.config(instanceCount);
+ngModule.config(callActivity);
 
 module.exports = ngModule;
