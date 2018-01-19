4a4176d6dfecd5e47dd1223c7e8c97d86fef4c9c
Philipp Ossler <philipp.ossler@camunda.com>
2018-01-19 12:09:33 +0100

    chore(engine): add support for time/date-time with zone id

--- engine-dmn/feel-scala/src/main/scala/org/camunda/feel/integration/transformer/FeelDateTimeTypeTransformer.scala
+++ engine-dmn/feel-scala/src/main/scala/org/camunda/feel/integration/transformer/FeelDateTimeTypeTransformer.scala
@@ -5,6 +5,7 @@ import org.camunda.bpm.dmn.engine.impl.spi.`type`.DmnDataTypeTransformer
 import org.camunda.bpm.engine.variable.value.TypedValue
 import org.camunda.bpm.engine.variable.Variables
 import java.time.ZoneId
+import java.time.OffsetDateTime
 
 class FeelDateTimeTypeTransformer extends DmnDataTypeTransformer {
   
@@ -12,8 +13,9 @@ class FeelDateTimeTypeTransformer extends DmnDataTypeTransformer {
     
     val dateTime: DateTime = value match {
       case x: DateTime => x
-      case x: LocalDateTime => x.atZone(ZoneId.systemDefault).toOffsetDateTime
-      case x: java.util.Date => x.toInstant().atZone(ZoneId.systemDefault()).toOffsetDateTime
+      case x: LocalDateTime => x.atZone(ZoneId.systemDefault)
+      case x: OffsetDateTime => x.toZonedDateTime()
+      case x: java.util.Date => x.toInstant().atZone(ZoneId.systemDefault())
       case other => throw new IllegalArgumentException(s"Cannot transform '$other' to FEEL date-time.")
     }
     
--- engine-dmn/feel-scala/src/main/scala/org/camunda/feel/integration/transformer/FeelLocalDateTimeTypeTransformer.scala
+++ engine-dmn/feel-scala/src/main/scala/org/camunda/feel/integration/transformer/FeelLocalDateTimeTypeTransformer.scala
@@ -5,6 +5,7 @@ import org.camunda.bpm.dmn.engine.impl.spi.`type`.DmnDataTypeTransformer
 import org.camunda.bpm.engine.variable.value.TypedValue
 import org.camunda.bpm.engine.variable.Variables
 import java.time.ZoneId
+import java.time.OffsetDateTime
 
 class FeelLocalDateTimeTypeTransformer extends DmnDataTypeTransformer {
   
@@ -13,6 +14,7 @@ class FeelLocalDateTimeTypeTransformer extends DmnDataTypeTransformer {
     val localDateTime: LocalDateTime = value match {
       case x: LocalDateTime => x
       case x: DateTime => x.toLocalDateTime
+      case x: OffsetDateTime => x.toLocalDateTime()
       case x: java.util.Date => x.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime
       case other => throw new IllegalArgumentException(s"Cannot transform '$other' to FEEL local-date-time.")
     }
