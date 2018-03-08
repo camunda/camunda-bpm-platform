3494585f45c42077de3ab1f2af932c10643b767b
Tassilo Weidner <tassilo.weidner@camunda.com>
2018-03-08 17:36:47 +0100

    test(webapps-rest-it): perform 500 concurrent fetch and lock requests
    related to CAM-8849

--- qa/integration-tests-webapps/src/test/java/org/camunda/bpm/rest/RestJaxRs2IT.java
+++ qa/integration-tests-webapps/src/test/java/org/camunda/bpm/rest/RestJaxRs2IT.java
@@ -1,15 +1,34 @@
 package org.camunda.bpm.rest;
 
 import com.sun.jersey.api.client.ClientResponse;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.util.EntityUtils;
 import org.camunda.bpm.AbstractWebappIntegrationTest;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -29,8 +48,8 @@ public class RestJaxRs2IT extends AbstractWebappIntegrationTest {
     Thread.sleep(5 * 1000);
   }
 
-  @Test
-  public void testJaxRsTwoArtifactIsUsed() throws JSONException {
+  @Test(timeout=10000)
+  public void shouldUseJaxRs2Artifact() throws JSONException {
     Map<String, Object> payload = new HashMap<String, Object>();
     payload.put("workerId", "aWorkerId");
     payload.put("asyncResponseTimeout", 1000 * 60 * 30 + 1);
@@ -43,6 +62,54 @@ public class RestJaxRs2IT extends AbstractWebappIntegrationTest {
     String responseMessage = response.getEntity(JSONObject.class).get("message").toString();
     assertTrue(responseMessage.equals("The asynchronous response timeout cannot be set to a value greater than 1800000 milliseconds"));
     response.close();
+  }
+
+  @Test
+  public void shouldPerform500ConcurrentRequests() throws InterruptedException, ExecutionException {
+    PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
+    final CloseableHttpClient httpClient = HttpClients.custom()
+      .setConnectionManager(cm)
+      .build();
+
+    Callable<String> performRequest = new Callable<String>() {
+
+      @Override
+      public String call() throws IOException {
+        HttpPost request = new HttpPost(APP_BASE_PATH + FETCH_AND_LOCK_PATH);
+        request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        StringEntity stringEntity = new StringEntity("{ \"workerId\": \"aWorkerId\", \"asyncResponseTimeout\": 1000 }");
+        request.setEntity(stringEntity);
+
+        CloseableHttpResponse response = httpClient.execute(request, HttpClientContext.create());
+        String responseBody = null;
+        try {
+          HttpEntity entity = response.getEntity();
+          responseBody = EntityUtils.toString(entity);
+          request.releaseConnection();
+        } finally {
+          response.close();
+        }
+
+        return responseBody;
+      }
+
+    };
+
+    int requestsCount = 500;
+    ExecutorService service = Executors.newFixedThreadPool(requestsCount);
+
+    List<Callable<String>> requests = new ArrayList<Callable<String>>();
+    for (int i = 0; i < requestsCount; i++) {
+      requests.add(performRequest);
+    }
+
+    List<Future<String>> futures = service.invokeAll(requests);
+    service.shutdown();
+    service.awaitTermination(1, TimeUnit.DAYS);
+
+    for (Future<String> future : futures) {
+      assertEquals(future.get(), "[]");
+    }
   }
 
 }
