52ab004cbcce75cd032a2863a1bb107cfe7d632f
Nikola Koevski <koevskin@gmail.com>
2018-03-09 10:30:19 +0100

    fix(db): add indexes on process definition id and end time
    * Add statements in create and drop SQL scripts
    * Add patch SQL scripts for 7.7 and 7.8
    
    Related to CAM-8833

---
+++ distro/sql-script/upgrade/postgres_engine_7.7_patch_7.7.8_to_7.7.9.sql
@@ -0,0 +1,3 @@
+--https://app.camunda.com/jira/browse/CAM-8833
+create index ACT_IDX_HI_PROCINST_PROC_DEF_ID_END_TIME_ on ACT_HI_PROCINST(PROC_DEF_ID_, END_TIME_);
+create index ACT_IDX_HI_ACT_INST_PROC_DEF_ID_END_TIME_ on ACT_HI_ACTINST(PROC_DEF_ID_, END_TIME_);
