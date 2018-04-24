3b50608bf513a853f7b9d1925f9229cdcd72fb3b
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2018-04-24 10:58:47 +0200

    fix(cockpit): add class to selected row in drd view
    related to CAM-8982

--- webapps/ui/cockpit/plugins/decisionList/app/views/decisionInstance/highlightRules.js
+++ webapps/ui/cockpit/plugins/decisionList/app/views/decisionInstance/highlightRules.js
@@ -1,15 +1,15 @@
-  'use strict';
+'use strict';
 
-  module.exports = [ 'ViewsProvider', function(ViewsProvider) {
+module.exports = [ 'ViewsProvider', function(ViewsProvider) {
 
-    ViewsProvider.registerDefaultView('cockpit.decisionInstance.table', {
-      id: 'highlightRules',
-      initialize: function(data) {
-        data.decisionInstance.outputs.forEach(function(output) {
-          if (output.ruleId) {
-            data.tableControl.highlightRow(output.ruleId, 'fired');
-          }
-        });
-      }
-    });
-  }];
+  ViewsProvider.registerDefaultView('cockpit.decisionInstance.table', {
+    id: 'highlightRules',
+    initialize: function(data) {
+      data.decisionInstance.outputs.forEach(function(output) {
+        if (output.ruleId) {
+          data.tableControl.highlightRow(output.ruleId, 'highlight');
+        }
+      });
+    }
+  });
+}];
