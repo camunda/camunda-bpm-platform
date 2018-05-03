108133a738b2920327f910cf033239273e82e0df
Roman <roman.smirnov@camunda.com>
2018-05-03 12:00:12 +0200

    fix(admin): add UPDATE permission to process definition resource
    related to CAM-8986

--- webapps/ui/admin/client/scripts/pages/authorizations.js
+++ webapps/ui/admin/client/scripts/pages/authorizations.js
@@ -52,7 +52,7 @@ module.exports = [ '$routeProvider', function($routeProvider) {
           3: [ 'CREATE', 'DELETE' ],
           4: [ 'READ', 'UPDATE', 'CREATE', 'DELETE' ],
           5: [ 'READ', 'UPDATE', 'DELETE' ],
-          6: [ 'READ', 'CREATE_INSTANCE', 'READ_INSTANCE', 'UPDATE_INSTANCE', 'DELETE_INSTANCE', 'MIGRATE_INSTANCE', 'READ_TASK', 'UPDATE_TASK', 'TASK_ASSIGN', 'TASK_WORK', 'READ_HISTORY', 'DELETE_HISTORY' ],
+          6: [ 'READ', 'UPDATE', 'CREATE_INSTANCE', 'READ_INSTANCE', 'UPDATE_INSTANCE', 'DELETE_INSTANCE', 'MIGRATE_INSTANCE', 'READ_TASK', 'UPDATE_TASK', 'TASK_ASSIGN', 'TASK_WORK', 'READ_HISTORY', 'DELETE_HISTORY' ],
           7: [ 'CREATE', 'READ', 'UPDATE', 'DELETE', 'TASK_ASSIGN', 'TASK_WORK' ],
           8: [ 'CREATE', 'READ', 'UPDATE', 'DELETE' ],
           9: [ 'CREATE', 'READ', 'DELETE' ],
