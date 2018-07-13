2609222cd2fe0aa1a92b7c2f6cdefcee40622b72
Yana Vasileva <yana.vasileva@camunda.com>
2018-07-13 08:46:16 +0200

    fix(db-scripts): adjust scripts
    * according to patch naming scheme
    * add the index to mysql and mariadb
    
    Related to CAM-9165

--- engine/src/main/resources/org/camunda/bpm/engine/db/drop/activiti.mysql.drop.case.engine.sql
+++ engine/src/main/resources/org/camunda/bpm/engine/db/drop/activiti.mysql.drop.case.engine.sql
@@ -29,6 +29,9 @@ alter table ACT_RU_CASE_SENTRY_PART
 alter table ACT_RU_CASE_SENTRY_PART
     drop foreign key ACT_FK_CASE_SENTRY_CASE_EXEC;
 
+-- https://app.camunda.com/jira/browse/CAM-9165
+drop index ACT_IDX_CASE_EXE_CASE_INST on ACT_RU_CASE_EXECUTION;
+
 drop table if exists ACT_RE_CASE_DEF;
 drop table if exists ACT_RU_CASE_EXECUTION;
 drop table if exists ACT_RU_CASE_SENTRY_PART;
