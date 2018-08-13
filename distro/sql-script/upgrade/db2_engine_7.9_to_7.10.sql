d7456d5dc6df9553a1bca14d5e4a2e0d992e8dcb
Yana Vasileva <yana.vasileva@camunda.com>
2018-08-13 11:32:03 +0200

    fix(db-scripts): remove redundant removal of ACT_IDX_JOB_HANDLER index
    Related to CAM-7676

--- distro/sql-script/upgrade/db2_engine_7.9_to_7.10.sql
+++ distro/sql-script/upgrade/db2_engine_7.9_to_7.10.sql
@@ -1,6 +1,3 @@
--- Related to https://app.camunda.com/jira/browse/CAM-7676
-drop index ACT_IDX_JOB_HANDLER;
-
 -- https://app.camunda.com/jira/browse/CAM-9084
 ALTER TABLE ACT_RE_PROCDEF
   ADD STARTABLE_ smallint DEFAULT 1 not null check(STARTABLE_ in (1,0));
