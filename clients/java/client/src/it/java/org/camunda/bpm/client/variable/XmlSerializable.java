cadae0ece68d29227c8c3bad009824ba21be64ee
Nikola Koevski <koevskin@gmail.com>
2018-11-15 20:35:47 +0100

    chore(engine): add & reformat license headers to files
    * Add missing license headers to java source files;
    * Format existing license headers in java source files to get a consitent pattern.
    
    Related to CAM-9524

--- clients/java/client/src/it/java/org/camunda/bpm/client/variable/XmlSerializable.java
+++ clients/java/client/src/it/java/org/camunda/bpm/client/variable/XmlSerializable.java
@@ -1,4 +1,6 @@
 /*
+ * Copyright © 2018 camunda services GmbH and various authors (info@camunda.com)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
--- clients/java/client/src/it/java/org/camunda/bpm/client/variable/XmlSerializableNoAnnotation.java
+++ clients/java/client/src/it/java/org/camunda/bpm/client/variable/XmlSerializableNoAnnotation.java
@@ -1,4 +1,6 @@
 /*
+ * Copyright © 2018 camunda services GmbH and various authors (info@camunda.com)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -11,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.camunda.bpm.client.variable;
 
 public class XmlSerializableNoAnnotation {
