54a11d12fb457d46abc0cb9ba5bfadf7bb61005c
Nikola Koevski <koevskin@gmail.com>
2018-11-15 18:40:56 +0100

    chore(engine): add & reformat license headers to files
    * Add missing license headers to java source files;
    * Format existing license headers in java source files to get a consitent pattern.
    
    Related to CAM-9524

--- model-api/cmmn-model/src/test/java/org/camunda/bpm/model/cmmn/instance/camunda/CamundaOutTest.java
+++ model-api/cmmn-model/src/test/java/org/camunda/bpm/model/cmmn/instance/camunda/CamundaOutTest.java
@@ -1,4 +1,7 @@
-/* Licensed under the Apache License, Version 2.0 (the "License");
+/*
+ * Copyright © 2014 - 2018 camunda services GmbH and various authors (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -10,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.camunda.bpm.model.cmmn.instance.camunda;
 
 import static org.camunda.bpm.model.cmmn.impl.CmmnModelConstants.CAMUNDA_NS;
