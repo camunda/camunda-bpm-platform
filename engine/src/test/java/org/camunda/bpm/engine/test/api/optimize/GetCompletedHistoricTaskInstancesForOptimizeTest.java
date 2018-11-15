e96cef8c89050591f38d2e1e3e8a5cbd5b23d18c
Nikola Koevski <koevskin@gmail.com>
2018-11-15 10:39:36 +0100

    chore(engine): update license and test cases
    Related to CAM-9523

--- engine/src/test/java/org/camunda/bpm/engine/test/api/optimize/GetCompletedHistoricTaskInstancesForOptimizeTest.java
+++ engine/src/test/java/org/camunda/bpm/engine/test/api/optimize/GetCompletedHistoricTaskInstancesForOptimizeTest.java
@@ -1,8 +1,11 @@
-/* Licensed under the Apache License, Version 2.0 (the "License");
+/*
+ * Copyright © 2012 - 2018 camunda services GmbH and various authors (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -109,7 +112,7 @@ public class GetCompletedHistoricTaskInstancesForOptimizeTest {
 
     // when
     List<HistoricTaskInstance> completedHistoricTaskInstances =
-      optimizeService.getCompletedHistoricTaskInstances(pastDate(), null, 10);
+      optimizeService.getCompletedHistoricTaskInstances(null, null, 10);
 
     // then
     assertThat(completedHistoricTaskInstances.size(), is(1));
