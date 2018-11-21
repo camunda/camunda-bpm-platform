a968ef09cdd24af9877085acdc0616d2743b1600
Nikola Koevski <koevskin@gmail.com>
2018-11-21 10:22:18 +0100

    chore(engine): update license header
    Related to CAM-9523

--- engine-rest/engine-rest/src/test/java/org/camunda/bpm/engine/rest/optimize/OptimizeUserOperationLogRestServiceTest.java
+++ engine-rest/engine-rest/src/test/java/org/camunda/bpm/engine/rest/optimize/OptimizeUserOperationLogRestServiceTest.java
@@ -1,4 +1,7 @@
-/* Licensed under the Apache License, Version 2.0 (the "License");
+/*
+ * Copyright © 2012 - 2018 camunda services GmbH and various authors (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
