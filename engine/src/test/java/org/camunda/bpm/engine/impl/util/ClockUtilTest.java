38cfa4f63c6b9f2f813c59292d15572d390b7785
Stefan Zilske <stefan.zilske@holisticon.de>
2019-01-04 13:16:40 +0100

    (chore) Add license header, enhance time assertions

--- engine/src/test/java/org/camunda/bpm/engine/impl/util/ClockUtilTest.java
+++ engine/src/test/java/org/camunda/bpm/engine/impl/util/ClockUtilTest.java
@@ -5,10 +5,25 @@ import org.junit.Test;
 
 import java.util.Date;
 
-import static org.exparity.hamcrest.date.DateMatchers.sameSecondOfMinute;
+import static org.exparity.hamcrest.date.DateMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
+/*
+ * Copyright © 2013-2019 camunda services GmbH and various authors (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 public class ClockUtilTest {
 
     @Test
@@ -28,7 +43,7 @@ public class ClockUtilTest {
 
         ClockUtil.offset(duration.getMillis());
 
-        assertThat(ClockUtil.now(), sameSecondOfMinute(target));
+        assertTimesAreInSameSecond(ClockUtil.now(), target);
     }
 
     @Test
@@ -38,7 +53,7 @@ public class ClockUtilTest {
 
         ClockUtil.setCurrentTime(target);
 
-        assertThat(ClockUtil.now(), sameSecondOfMinute(target));
+        assertTimesAreInSameSecond(ClockUtil.now(), target);
     }
 
     @Test
@@ -48,10 +63,10 @@ public class ClockUtilTest {
 
         ClockUtil.offset(duration.getMillis());
 
-        assertThat(ClockUtil.now(), sameSecondOfMinute(target));
+        assertTimesAreInSameSecond(ClockUtil.now(), target);
 
-        assertThat(ClockUtil.resetClock(), sameSecondOfMinute(new Date()));
-        assertThat(ClockUtil.getCurrentTime(), sameSecondOfMinute(new Date()));
+        assertTimesAreInSameSecond(ClockUtil.resetClock(), new Date());
+        assertTimesAreInSameSecond(ClockUtil.getCurrentTime(), new Date());
     }
 
     @Test
@@ -61,11 +76,11 @@ public class ClockUtilTest {
 
         ClockUtil.offset(duration.getMillis());
 
-        assertThat(ClockUtil.now(), sameSecondOfMinute(target));
+        assertTimesAreInSameSecond(ClockUtil.now(), target);
 
         ClockUtil.reset();
 
-        assertThat(ClockUtil.getCurrentTime(), sameSecondOfMinute(new Date()));
+        assertTimesAreInSameSecond(ClockUtil.getCurrentTime(), new Date());
     }
 
     @Test
@@ -76,11 +91,11 @@ public class ClockUtilTest {
 
         ClockUtil.offset(duration.getMillis());
 
-        assertThat(ClockUtil.now(), sameSecondOfMinute(target));
+        assertTimesAreInSameSecond(ClockUtil.now(), target);
 
         Thread.sleep(10000);
 
-        assertThat(ClockUtil.now(), sameSecondOfMinute(new Date(target.getTime() + Duration.standardSeconds(10).getMillis())));
+        assertTimesAreInSameSecond(ClockUtil.now(), new Date(target.getTime() + Duration.standardSeconds(10).getMillis()));
     }
 
     @Test
@@ -90,9 +105,17 @@ public class ClockUtilTest {
         Date target = new Date(now.getTime() + duration.getMillis());
         ClockUtil.setCurrentTime(target);
 
-
         Thread.sleep(10000);
 
-        assertThat(ClockUtil.now(), sameSecondOfMinute(target));
+        assertTimesAreInSameSecond(ClockUtil.now(), target);
+    }
+
+    private static void assertTimesAreInSameSecond(Date first, Date second) {
+        assertThat(first, sameSecondOfMinute(second));
+        assertThat(first, sameMinuteOfHour(second));
+        assertThat(first, sameHourOfDay(second));
+        assertThat(first, sameDayOfMonth(second));
+        assertThat(first, sameMonthOfYear(second));
+        assertThat(first, sameMonthOfYear(second));
     }
 }
