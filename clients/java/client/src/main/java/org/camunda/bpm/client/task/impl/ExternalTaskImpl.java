707f5fdbf3dd6350598d24ceb6947f38613a1bea
Tobias Metzke <tobias.metzke@camunda.com>
2019-01-09 15:32:03 +0100

    chore(client): adjust license headers
    related to CAM-9453

--- clients/java/client/src/main/java/org/camunda/bpm/client/task/impl/ExternalTaskImpl.java
+++ clients/java/client/src/main/java/org/camunda/bpm/client/task/impl/ExternalTaskImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2018 camunda services GmbH and various authors (info@camunda.com)
+ * Copyright © 2018-2019 camunda services GmbH and various authors (info@camunda.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
