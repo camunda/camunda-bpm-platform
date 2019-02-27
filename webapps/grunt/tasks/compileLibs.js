86aaa54611a7dab0f3339068959ed40ad19f1922
Tassilo Weidner <tassilo.weidner@camunda.com>
2019-02-27 20:53:42 +0100

    chore(deps): add transpilation as grunt task
    related to CAM-9844

---
+++ webapps/grunt/tasks/compileLibs.js
@@ -0,0 +1,40 @@
+let { promisify } = require('util');
+let exec = promisify(require('child_process').exec);
+let path = require('path');
+
+module.exports = function(grunt, isCeEdition) {
+  grunt.registerTask('compileLibs', function() {
+    let libs = [];
+
+    let libDir = null;
+    if (!isCeEdition) {
+      libDir = '..';
+
+      libs.push('../camunda-commons-ui/node_modules/camunda-bpm-sdk-js/vendor/superagent');
+    } else {
+      libDir = 'node_modules';
+    }
+
+    libs = libs.concat(['node_modules/camunda-bpm-sdk-js/vendor/superagent',
+      libDir + '/camunda-commons-ui/bpmn-js',
+      libDir + '/camunda-commons-ui/dmn-js',
+      libDir + '/camunda-commons-ui/cmmn-js'
+    ]);
+
+    let done = this.async();
+
+    let cmd = 'npm run build';
+    if (process.platform === 'win32') {
+      cmd = cmd.replace(/\//g, '\\');
+    }
+
+    let builds = libs.map(lib => {
+      let libPath = path.join(__dirname, `../../${lib}/`);
+    return exec(cmd, { maxBuffer: 1024 * 500, cwd: libPath });
+  });
+
+    Promise.all(builds)
+      .then(() => done())
+  .catch(console.error);
+  });
+};
