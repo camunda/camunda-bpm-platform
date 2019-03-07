4b5f42991be3322e4dab4b79dba9488c96b8063a
Yana Vasileva <yana.vasileva@camunda.com>
2019-03-07 17:30:55 +0100

    fix(rest): query for variables in filters with authorization
    Related to CAM-9843

--- engine-rest/engine-rest/src/main/java/org/camunda/bpm/engine/rest/sub/runtime/impl/FilterResourceImpl.java
+++ engine-rest/engine-rest/src/main/java/org/camunda/bpm/engine/rest/sub/runtime/impl/FilterResourceImpl.java
@@ -18,12 +18,10 @@ package org.camunda.bpm.engine.rest.sub.runtime.impl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.camunda.bpm.engine.EntityTypes;
 import org.camunda.bpm.engine.FilterService;
-import org.camunda.bpm.engine.IdentityService;
 import org.camunda.bpm.engine.ProcessEngineException;
 import org.camunda.bpm.engine.exception.NotValidException;
 import org.camunda.bpm.engine.exception.NullValueException;
 import org.camunda.bpm.engine.filter.Filter;
-import org.camunda.bpm.engine.impl.identity.Authentication;
 import org.camunda.bpm.engine.impl.persistence.entity.VariableInstanceEntity;
 import org.camunda.bpm.engine.query.Query;
 import org.camunda.bpm.engine.rest.FilterRestService;
@@ -516,21 +514,13 @@ public class FilterResourceImpl extends AbstractAuthorizedRestResource implement
 
   protected List<VariableInstance> queryVariablesInstancesByVariableScopeIds(Collection<String> variableNames, Collection<String> variableScopeIds) {
 
-    IdentityService identityService = processEngine.getIdentityService();
-    Authentication currentAuthentication = identityService.getCurrentAuthentication();
-
-    try {
-      identityService.clearAuthentication();
-      return getProcessEngine().getRuntimeService()
-          .createVariableInstanceQuery()
-          .disableBinaryFetching()
-          .disableCustomObjectDeserialization()
-          .variableNameIn(variableNames.toArray(new String[variableNames.size()]))
-          .variableScopeIdIn(variableScopeIds.toArray(new String[variableScopeIds.size()]))
-          .list();
-    } finally {
-      identityService.setAuthentication(currentAuthentication);
-    }
+    return getProcessEngine().getRuntimeService()
+        .createVariableInstanceQuery()
+        .disableBinaryFetching()
+        .disableCustomObjectDeserialization()
+        .variableNameIn(variableNames.toArray(new String[variableNames.size()]))
+        .variableScopeIdIn(variableScopeIds.toArray(new String[variableScopeIds.size()]))
+        .list();
 
   }
 
