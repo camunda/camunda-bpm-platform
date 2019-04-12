aa911619306cb3fdddd897b9f14994166686140b
Philipp Ossler <philipp.ossler@gmail.com>
2019-04-12 14:14:13 +0200

    feat(factory): load function providers
    * load function providers via service loader SPI

--- engine-dmn/feel-scala/src/main/scala/org/camunda/feel/integration/CamundaFeelEngine.scala
+++ engine-dmn/feel-scala/src/main/scala/org/camunda/feel/integration/CamundaFeelEngine.scala
@@ -4,7 +4,6 @@ import org.camunda.bpm.engine.variable.context.VariableContext
 import org.camunda.feel.{EvalValue, EvalFailure, ParseFailure}
 import org.camunda.feel.interpreter._
 import org.camunda.feel.spi._
-import scala.collection.JavaConversions._
 import org.camunda.bpm.dmn.feel.impl.FeelException
 
 /**
@@ -13,27 +12,27 @@ import org.camunda.bpm.dmn.feel.impl.FeelException
 class CamundaFeelEngine extends org.camunda.bpm.dmn.feel.impl.FeelEngine {
 
   private lazy val engine =
-    new org.camunda.feel.FeelEngine(valueMapper = new CamundaValueMapper)
+    new org.camunda.feel.FeelEngine(
+      valueMapper = new CamundaValueMapper,
+      functionProvider = SpiServiceLoader.loadFunctionProvider
+    )
 
   private def asVariableProvider(ctx: VariableContext,
-                                 valueMapper: ValueMapper): VariableProvider =
-    new VariableProvider {
-      override def getVariable(name: String): Option[Val] = {
-        if (ctx.containsVariable(name)) {
-          Some(valueMapper.toVal(ctx.resolve(name).getValue))
-        } else {
-          None
-        }
-      }
+                                 valueMapper: ValueMapper): VariableProvider = (name: String) => {
+    if (ctx.containsVariable(name)) {
+      Some(valueMapper.toVal(ctx.resolve(name).getValue))
+    } else {
+      None
     }
+  }
 
   override def evaluateSimpleExpression[T](expression: String,
                                            ctx: VariableContext): T = {
     val context = new RootContext(
       variableProvider = asVariableProvider(ctx, engine.valueMapper))
     engine.evalExpression(expression, context) match {
-      case EvalValue(value)    => value.asInstanceOf[T]
-      case EvalFailure(error)  => throw new FeelException(error)
+      case EvalValue(value) => value.asInstanceOf[T]
+      case EvalFailure(error) => throw new FeelException(error)
       case ParseFailure(error) => throw new FeelException(error)
     }
   }
@@ -45,8 +44,8 @@ class CamundaFeelEngine extends org.camunda.bpm.dmn.feel.impl.FeelEngine {
       Map(RootContext.inputVariableKey -> inputVariable),
       variableProvider = asVariableProvider(ctx, engine.valueMapper))
     engine.evalUnaryTests(expression, context) match {
-      case EvalValue(value)    => value.asInstanceOf[Boolean]
-      case EvalFailure(error)  => throw new FeelException(error)
+      case EvalValue(value) => value.asInstanceOf[Boolean]
+      case EvalFailure(error) => throw new FeelException(error)
       case ParseFailure(error) => throw new FeelException(error)
     }
   }
