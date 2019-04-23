109bfab77765bd6991109f68a0e6220cc659b1f3
Martin Stamm <martin.stamm@camunda.com>
2019-04-23 11:01:39 +0200

    fix(cockpit): escape instance names
    related to CAM-10194

--- webapps/ui/cockpit/client/scripts/services/main.js
+++ webapps/ui/cockpit/client/scripts/services/main.js
@@ -27,7 +27,8 @@ var angular = require('camunda-commons-ui/vendor/angular'),
     camAPI = require('./../../../../common/scripts/services/cam-api'),
     hasPlugin = require('./../../../../common/scripts/services/has-plugin'),
     localConf = require('camunda-commons-ui/lib/services/cam-local-configuration'),
-    typeUtils = require('./../../../../common/scripts/services/typeUtils');
+    typeUtils = require('./../../../../common/scripts/services/typeUtils'),
+    escapeHtml = require('./escapeHtml');
 
 var servicesModule = angular.module('cam.cockpit.services', []);
 
@@ -40,5 +41,6 @@ servicesModule.factory('camAPI', camAPI);
 servicesModule.factory('hasPlugin', hasPlugin);
 servicesModule.factory('localConf', localConf);
 servicesModule.factory('typeUtils', typeUtils);
+servicesModule.factory('escapeHtml', escapeHtml);
 
 module.exports = servicesModule;
