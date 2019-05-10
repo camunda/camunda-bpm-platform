63fda2f5883e50b88137b289214f5acec1d7f2ed
Yana Vasileva <yana.vasileva@camunda.com>
2019-05-10 16:15:44 +0200

    chore(upgrade-scripts): fix license header

--- engine/src/main/resources/org/camunda/bpm/engine/db/upgrade/mssql_engine_7.3_to_7.4.sql
+++ engine/src/main/resources/org/camunda/bpm/engine/db/upgrade/mssql_engine_7.3_to_7.4.sql
@@ -3,8 +3,8 @@
 -- under one or more contributor license agreements. See the NOTICE file
 -- distributed with this work for additional information regarding copyright
 -- ownership. Camunda licenses this file to you under the Apache License,
--- Version 2.0; you may not use this file except in compliance with the
--- License. You may obtain a copy of the License at
+-- Version 2.0; you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
 --
 --     http://www.apache.org/licenses/LICENSE-2.0
 --
