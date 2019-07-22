09fb059c820abad98b0b5d4ef9ab7abdbbb1cf3e
Miklas Boskamp <miklas.boskamp@camunda.com>
2019-07-22 17:25:04 +0200

    chore(engine): add tests for additional case-insensitive queries
    * CaseExecutionQuery
    * CaseInstanceQuery
    * ExecutionQuery
    * HistoricCaseInstanceQuery
    * HistoricProcessInstanceQuery
    * VariableInstanceQuery
    
    Related to CAM-10647

---
+++ engine/src/test/java/org/camunda/bpm/engine/test/api/variables/CaseExecutionQueryVariableIgnoreCaseTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.engine.test.api.variables;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.camunda.bpm.engine.CaseService;
+import org.camunda.bpm.engine.RepositoryService;
+import org.camunda.bpm.engine.impl.cmmn.entity.runtime.CaseExecutionQueryImpl;
+import org.camunda.bpm.engine.repository.Deployment;
+import org.camunda.bpm.engine.runtime.CaseExecution;
+import org.junit.After;
+import org.junit.Before;
+
+public class CaseExecutionQueryVariableIgnoreCaseTest extends AbstractVariableIgnoreCaseTest<CaseExecutionQueryImpl, CaseExecution> {
+
+  CaseService caseService;
+  RepositoryService repositoryService;
+
+  @Before
+  public void init() {
+    caseService = engineRule.getCaseService();
+    repositoryService = engineRule.getRepositoryService();
+
+    repositoryService.createDeployment().addClasspathResource("org/camunda/bpm/engine/test/api/cmmn/oneTaskCase.cmmn").deploy();
+    instance = caseService.withCaseDefinitionByKey("oneTaskCase").setVariables(VARIABLES).businessKey("oneTaskCase").create();
+  }
+
+  @After
+  public void tearDown() {
+    for (Deployment deployment : repositoryService.createDeploymentQuery().list()) {
+      repositoryService.deleteDeployment(deployment.getId(), true);
+    }
+  }
+
+  @Override
+  protected CaseExecutionQueryImpl createQuery() {
+    return (CaseExecutionQueryImpl) caseService.createCaseExecutionQuery();
+  }
+
+  @Override
+  protected void assertThatTwoInstancesAreEqual(CaseExecution one, CaseExecution two) {
+    assertThat(one.getId()).isEqualTo(two.getId());
+  }
+}
