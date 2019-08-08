dff6c3747e08185c873a708ef8968ec21789a10e
Miklas Boskamp <miklas.boskamp@camunda.com>
2019-08-08 17:02:19 +0200

    feature(script): improve start script for windows
     Related to CAM-10401

--- distro/run/assembly/src/start.bat
+++ distro/run/assembly/src/start.bat
@@ -1,1 +1,14 @@
-java -Dloader.path="..\lib\webapps\camunda-rest-distro-webapps-1.0-SNAPSHOT.jar, ..\lib\db\*" -jar ..\lib\camunda-rest-distro-1.0-SNAPSHOT.jar --spring.config.location=file:..\config\application.yml
+@echo off
+
+SET webappsPath=..\lib\webapps\camunda-rest-distro-webapps-1.0-SNAPSHOT.jar, 
+SET libPath=..\lib\db\*
+
+IF "%1"=="--webapps" (
+	SET classPath=%webappsPath%%libPath%
+	ECHO starting with webapps
+	) else (
+	SET classPath=%libPath%
+	ECHO starting without webapps
+	)
+	
+start java -Dloader.path="%classPath%" -jar ..\lib\camunda-rest-distro-1.0-SNAPSHOT.jar --spring.config.location=file:..\config\application.yml
