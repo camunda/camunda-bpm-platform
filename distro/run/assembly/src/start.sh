bdcdd4a72b4c52ebbf98da8817b980f463d6ece9
Nikola Koevski <nikola.koevski>
2019-08-08 17:01:35 +0200

    feat(project): make webapps optional
    Related to CAM-10401

--- distro/run/assembly/src/start.sh
+++ distro/run/assembly/src/start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-java -Dloader.path="../lib/webapps/camunda-rest-distro-webapps-1.0-SNAPSHOT.jar, ../lib/db/*" \
-     -jar ../lib/camunda-rest-distro-1.0-SNAPSHOT.jar \
-     --spring.config.location=file:../config/application.yml
+if [ ! -z ${1+x} ] && [ $1 = "--webapps" ]; then
+   java -Dloader.path="../lib/webapps/camunda-rest-distro-webapps-1.0-SNAPSHOT.jar, ../lib/db/*" \
+        -jar ../lib/camunda-rest-distro-1.0-SNAPSHOT.jar \
+        --spring.config.location=file:../config/application.yml
+else
+   java -Dloader.path="../lib/db/*" \
+        -jar ../lib/camunda-rest-distro-1.0-SNAPSHOT.jar \
+        --spring.config.location=file:../config/application.yml
+fi
