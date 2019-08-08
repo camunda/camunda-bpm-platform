63a811c016e9cb67a1f3284b270c51c8666bc088
Nikola Koevski <nikola.koevski>
2019-08-08 14:17:39 +0200

    feat(config): set external config file

--- distro/run/assembly/src/start.sh
+++ distro/run/assembly/src/start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-java -Dloader.path="../lib/webapps/camunda-rest-distro-webapps-1.0-SNAPSHOT.jar" -jar ../lib/camunda-rest-distro-1.0-SNAPSHOT.jar
+java -Dloader.path="../lib/webapps/camunda-rest-distro-webapps-1.0-SNAPSHOT.jar" \
+     -jar ../lib/camunda-rest-distro-1.0-SNAPSHOT.jar \
+     --spring.config.location=file:../config/application.yml
--- distro/run/modules/rest/src/main/java/org/camunda/camunda/rest/distro/rest/CamundaRestDistro.java
+++ distro/run/modules/rest/src/main/java/org/camunda/camunda/rest/distro/rest/CamundaRestDistro.java
@@ -1,5 +1,7 @@
 package org.camunda.camunda.rest.distro.rest;
 
+import org.camunda.bpm.spring.boot.starter.property.CamundaBpmProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
