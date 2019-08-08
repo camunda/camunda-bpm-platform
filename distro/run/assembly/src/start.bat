0ab20cf42f3c22a6be67c4ad88f9309886df508b
Miklas Boskamp <miklas.boskamp@camunda.com>
2019-08-08 16:19:28 +0200

    feature(db): configurable databases
    Related to CAM-10401

---
+++ distro/run/assembly/src/start.bat
@@ -0,0 +1,1 @@
+java -Dloader.path="..\lib\webapps\camunda-rest-distro-webapps-1.0-SNAPSHOT.jar, ..\lib\db\*" -jar ..\lib\camunda-rest-distro-1.0-SNAPSHOT.jar --spring.config.location=file:..\config\application.yml
--- distro/run/assembly/src/start.sh
+++ distro/run/assembly/src/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-java -Dloader.path="../lib/webapps/camunda-rest-distro-webapps-1.0-SNAPSHOT.jar" \
+java -Dloader.path="../lib/webapps/camunda-rest-distro-webapps-1.0-SNAPSHOT.jar, ../lib/db/*" \
      -jar ../lib/camunda-rest-distro-1.0-SNAPSHOT.jar \
      --spring.config.location=file:../config/application.yml
