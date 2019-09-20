5365daf906d1e9852c7c0f739c59dd9ae1d13aa2
Laszlo Palossy <palossy.laszlo@gmail.com>
2019-09-20 18:37:30 +0200

    fix(engine): filtering by candidate user in an OR query
    * resolve the user's candidate groups when filtering by candidateUser in an OR query
    * cache result when getting the groups of the candidateUser
    
    related to CAM-10801, closes #473

--- engine/src/main/java/org/camunda/bpm/engine/impl/TaskQueryImpl.java
+++ engine/src/main/java/org/camunda/bpm/engine/impl/TaskQueryImpl.java
@@ -23,8 +23,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.camunda.bpm.engine.ProcessEngineException;
@@ -124,6 +126,7 @@ public class TaskQueryImpl extends AbstractQuery<TaskQuery, Task> implements Tas
   protected String caseExecutionId;
 
   protected List<String> cachedCandidateGroups;
+  protected Map<String, List<String>> cachedUserGroups;
 
   // or query /////////////////////////////
   protected List<TaskQueryImpl> queries = new ArrayList<TaskQueryImpl>(Arrays.asList(this));
@@ -927,42 +930,39 @@ public class TaskQueryImpl extends AbstractQuery<TaskQuery, Task> implements Tas
   }
 
   public List<String> getCandidateGroups() {
-
     if (cachedCandidateGroups != null) {
       return cachedCandidateGroups;
     }
 
-    if (isOrQueryActive) {
-
-      if (candidateGroup != null) {
-        cachedCandidateGroups = new ArrayList<String>();
-        cachedCandidateGroups.add(candidateGroup);
-
-        if (candidateGroups != null) {
-          cachedCandidateGroups.addAll(candidateGroups);
-        }
-
-      }
-      else if (candidateGroups != null) {
-        cachedCandidateGroups = candidateGroups;
-      }
-
-      return cachedCandidateGroups;
-    }
-
     if (candidateGroup != null && candidateGroups != null) {
-      //get intersection of candidateGroups and candidateGroup
-      cachedCandidateGroups = new ArrayList<String>(candidateGroups);
-      cachedCandidateGroups.retainAll(Arrays.asList(candidateGroup));
-    }
-    else if (candidateGroup != null) {
+      cachedCandidateGroups = new ArrayList<>(candidateGroups);
+      if (!isOrQueryActive) {
+        // get intersection of candidateGroups and candidateGroup
+        cachedCandidateGroups.retainAll(Arrays.asList(candidateGroup));
+      } else {
+        // get union of candidateGroups and candidateGroup
+        if (!candidateGroups.contains(candidateGroup)) {
+          cachedCandidateGroups.add(candidateGroup);
+        }
+      }
+    } else if (candidateGroup != null) {
       cachedCandidateGroups = Arrays.asList(candidateGroup);
-    }
-    else if (candidateUser != null) {
-      cachedCandidateGroups = getGroupsForCandidateUser(candidateUser);
-    }
-    else if (candidateGroups != null) {
+    } else if (candidateGroups != null) {
       cachedCandidateGroups = candidateGroups;
+    }
+
+    if (candidateUser != null) {
+      List<String> groupsForCandidateUser = getGroupsForCandidateUser(candidateUser);
+
+      if (cachedCandidateGroups == null) {
+        cachedCandidateGroups = groupsForCandidateUser;
+      } else {
+        for (String group : groupsForCandidateUser) {
+          if (!cachedCandidateGroups.contains(group)) {
+            cachedCandidateGroups.add(group);
+          }
+        }
+      }
     }
 
     return cachedCandidateGroups;
@@ -1013,17 +1013,33 @@ public class TaskQueryImpl extends AbstractQuery<TaskQuery, Task> implements Tas
   }
 
   protected List<String> getGroupsForCandidateUser(String candidateUser) {
-    List<Group> groups = Context
-      .getCommandContext()
-      .getReadOnlyIdentityProvider()
-      .createGroupQuery()
-      .groupMember(candidateUser)
-      .list();
-    List<String> groupIds = new ArrayList<String>();
+    Map<String, List<String>> cachedUserGroups = getCachedUserGroups();
+    if (cachedUserGroups.containsKey(candidateUser)) {
+      return cachedUserGroups.get(candidateUser);
+    }
+
+    List<Group> groups = Context.getCommandContext()
+        .getReadOnlyIdentityProvider()
+        .createGroupQuery()
+        .groupMember(candidateUser)
+        .list();
+    
+    List<String> groupIds = new ArrayList<>();
     for (Group group : groups) {
       groupIds.add(group.getId());
     }
+
+    cachedUserGroups.put(candidateUser, groupIds);
+
     return groupIds;
+  }
+
+  protected Map<String, List<String>> getCachedUserGroups() {
+    // store and retrieve cached user groups always from the first query
+    if (queries.get(0).cachedUserGroups == null) {
+      queries.get(0).cachedUserGroups = new HashMap<>();
+    }
+    return queries.get(0).cachedUserGroups;
   }
 
   protected void ensureOrExpressionsEvaluated() {
