d4927c76d406e2a6521e07e74295274d3ad91c83
Nikola Koevski <nikola.koevski@camunda.com>
2019-11-06 09:33:28 +0100

    feat(engine): historic case activity instance query w/o tenant id
    Related to CAM-6554

--- engine/src/test/java/org/camunda/bpm/engine/test/api/multitenancy/cmmn/query/history/MultiTenancyHistoricCaseActivityInstanceQueryTest.java
+++ engine/src/test/java/org/camunda/bpm/engine/test/api/multitenancy/cmmn/query/history/MultiTenancyHistoricCaseActivityInstanceQueryTest.java
@@ -16,8 +16,7 @@
  */
 package org.camunda.bpm.engine.test.api.multitenancy.cmmn.query.history;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
@@ -35,121 +34,168 @@ public class MultiTenancyHistoricCaseActivityInstanceQueryTest extends Pluggable
 
   protected final static String CMMN_FILE = "org/camunda/bpm/engine/test/api/cmmn/oneTaskCase.cmmn";
 
+  protected final static String TENANT_NULL = null;
   protected final static String TENANT_ONE = "tenant1";
   protected final static String TENANT_TWO = "tenant2";
 
   @Override
   protected void setUp() {
+    // given
+    deploymentForTenant(TENANT_NULL, CMMN_FILE);
     deploymentForTenant(TENANT_ONE, CMMN_FILE);
     deploymentForTenant(TENANT_TWO, CMMN_FILE);
 
+    createCaseInstance(TENANT_NULL);
     createCaseInstance(TENANT_ONE);
     createCaseInstance(TENANT_TWO);
   }
 
   public void testQueryWithoutTenantId() {
-    HistoricCaseActivityInstanceQuery query = historyService.
-        createHistoricCaseActivityInstanceQuery();
+    // when
+    HistoricCaseActivityInstanceQuery query = historyService
+        .createHistoricCaseActivityInstanceQuery();
 
-    assertThat(query.count(), is(2L));
+    // then
+    assertThat(query.count()).isEqualTo(3L);
+  }
+
+  public void testQueryFilterWithoutTenantId() {
+    // when
+    HistoricCaseActivityInstanceQuery query = historyService
+        .createHistoricCaseActivityInstanceQuery()
+        .withoutTenantId();
+
+    // then
+    assertThat(query.count()).isEqualTo(1L);
   }
 
   public void testQueryByTenantId() {
-    HistoricCaseActivityInstanceQuery query = historyService
+    // when
+    HistoricCaseActivityInstanceQuery queryTenantOne = historyService
         .createHistoricCaseActivityInstanceQuery()
         .tenantIdIn(TENANT_ONE);
 
-    assertThat(query.count(), is(1L));
-
-    query = historyService
+    HistoricCaseActivityInstanceQuery queryTenantTwo = historyService
         .createHistoricCaseActivityInstanceQuery()
         .tenantIdIn(TENANT_TWO);
 
-    assertThat(query.count(), is(1L));
+    // then
+    assertThat(queryTenantOne.count()).isEqualTo(1L);
+    assertThat(queryTenantTwo.count()).isEqualTo(1L);
   }
 
   public void testQueryByTenantIds() {
+    // when
     HistoricCaseActivityInstanceQuery query = historyService
         .createHistoricCaseActivityInstanceQuery()
         .tenantIdIn(TENANT_ONE, TENANT_TWO);
 
-    assertThat(query.count(), is(2L));
+    // then
+    assertThat(query.count()).isEqualTo(2L);
   }
 
   public void testQueryByNonExistingTenantId() {
+    // when
     HistoricCaseActivityInstanceQuery query = historyService
         .createHistoricCaseActivityInstanceQuery()
         .tenantIdIn("nonExisting");
 
-    assertThat(query.count(), is(0L));
+    // then
+    assertThat(query.count()).isEqualTo(0L);
   }
 
   public void testFailQueryByTenantIdNull() {
     try {
+      // when
       historyService.createHistoricCaseActivityInstanceQuery()
         .tenantIdIn((String) null);
 
       fail("expected exception");
+
+      // then
     } catch (NullValueException e) {
     }
   }
 
   public void testQuerySortingAsc() {
-    List<HistoricCaseActivityInstance> historicCaseActivityInstances = historyService.createHistoricCaseActivityInstanceQuery()
+    // when
+    List<HistoricCaseActivityInstance> historicCaseActivityInstances = historyService
+        .createHistoricCaseActivityInstanceQuery()
         .orderByTenantId()
         .asc()
         .list();
 
-    assertThat(historicCaseActivityInstances.size(), is(2));
-    assertThat(historicCaseActivityInstances.get(0).getTenantId(), is(TENANT_ONE));
-    assertThat(historicCaseActivityInstances.get(1).getTenantId(), is(TENANT_TWO));
+    // then
+    assertThat(historicCaseActivityInstances.size()).isEqualTo(3);
+    assertThat(historicCaseActivityInstances.get(0).getTenantId()).isEqualTo(TENANT_NULL);
+    assertThat(historicCaseActivityInstances.get(1).getTenantId()).isEqualTo(TENANT_ONE);
+    assertThat(historicCaseActivityInstances.get(2).getTenantId()).isEqualTo(TENANT_TWO);
   }
 
   public void testQuerySortingDesc() {
+    // when
     List<HistoricCaseActivityInstance> historicCaseActivityInstances = historyService.createHistoricCaseActivityInstanceQuery()
         .orderByTenantId()
         .desc()
         .list();
 
-    assertThat(historicCaseActivityInstances.size(), is(2));
-    assertThat(historicCaseActivityInstances.get(0).getTenantId(), is(TENANT_TWO));
-    assertThat(historicCaseActivityInstances.get(1).getTenantId(), is(TENANT_ONE));
+    // then
+    assertThat(historicCaseActivityInstances.size()).isEqualTo(3);
+    assertThat(historicCaseActivityInstances.get(0).getTenantId()).isEqualTo(TENANT_TWO);
+    assertThat(historicCaseActivityInstances.get(1).getTenantId()).isEqualTo(TENANT_ONE);
+    assertThat(historicCaseActivityInstances.get(2).getTenantId()).isEqualTo(TENANT_NULL);
   }
 
   public void testQueryNoAuthenticatedTenants() {
+    // given
     identityService.setAuthentication("user", null, null);
 
+    // when
     HistoricCaseActivityInstanceQuery query = historyService.createHistoricCaseActivityInstanceQuery();
-    assertThat(query.count(), is(0L));
+
+    // then
+    assertThat(query.count()).isEqualTo(1L); // null-tenant instances are still included
   }
 
   public void testQueryAuthenticatedTenant() {
+    // given
     identityService.setAuthentication("user", null, Arrays.asList(TENANT_ONE));
 
+    // when
     HistoricCaseActivityInstanceQuery query = historyService.createHistoricCaseActivityInstanceQuery();
 
-    assertThat(query.count(), is(1L));
-    assertThat(query.tenantIdIn(TENANT_ONE).count(), is(1L));
-    assertThat(query.tenantIdIn(TENANT_TWO).count(), is(0L));
-    assertThat(query.tenantIdIn(TENANT_ONE, TENANT_TWO).count(), is(1L));
+    // then
+    assertThat(query.count()).isEqualTo(2L);  // null-tenant instances are still included
+    assertThat(query.tenantIdIn(TENANT_ONE).count()).isEqualTo(1L);
+    assertThat(query.withoutTenantId().count()).isEqualTo(1L);
+    assertThat(query.tenantIdIn(TENANT_TWO).count()).isEqualTo(0L);
+    assertThat(query.tenantIdIn(TENANT_ONE, TENANT_TWO).count()).isEqualTo(1L);
   }
 
   public void testQueryAuthenticatedTenants() {
+    // given
     identityService.setAuthentication("user", null, Arrays.asList(TENANT_ONE, TENANT_TWO));
 
+    // when
     HistoricCaseActivityInstanceQuery query = historyService.createHistoricCaseActivityInstanceQuery();
 
-    assertThat(query.count(), is(2L));
-    assertThat(query.tenantIdIn(TENANT_ONE).count(), is(1L));
-    assertThat(query.tenantIdIn(TENANT_TWO).count(), is(1L));
+    // then
+    assertThat(query.count()).isEqualTo(3L); // null-tenant instances are still included
+    assertThat(query.tenantIdIn(TENANT_ONE).count()).isEqualTo(1L);
+    assertThat(query.tenantIdIn(TENANT_TWO).count()).isEqualTo(1L);
+    assertThat(query.withoutTenantId().count()).isEqualTo(1L);
   }
 
   public void testQueryDisabledTenantCheck() {
+    // given
     processEngineConfiguration.setTenantCheckEnabled(false);
     identityService.setAuthentication("user", null, null);
 
+    // when
     HistoricCaseActivityInstanceQuery query = historyService.createHistoricCaseActivityInstanceQuery();
-    assertThat(query.count(), is(2L));
+
+    // then
+    assertThat(query.count()).isEqualTo(3L);
   }
 
   protected void createCaseInstance(String tenantId) {
