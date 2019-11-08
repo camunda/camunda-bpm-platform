0834446dae0e44a9fff621cf9cc86c5619fea17c
Tobias Metzke <tobias.metzke@camunda.com>
2019-11-08 10:22:22 +0100

    test(spin-plugin): handle multiple engines

--- engine-plugins/spin-plugin/src/test/java/org/camunda/spin/plugin/variables/JsonSerializationWithValidationOnMultipleEnginesTest.java
+++ engine-plugins/spin-plugin/src/test/java/org/camunda/spin/plugin/variables/JsonSerializationWithValidationOnMultipleEnginesTest.java
@@ -26,7 +26,6 @@ import static org.mockito.Mockito.when;
 import org.camunda.bpm.engine.ProcessEngineConfiguration;
 import org.camunda.bpm.engine.ProcessEngineException;
 import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
-import org.camunda.bpm.engine.repository.Deployment;
 import org.camunda.bpm.engine.runtime.DeserializationTypeValidator;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
@@ -55,9 +54,10 @@ public class JsonSerializationWithValidationOnMultipleEnginesTest {
     public ProcessEngineConfiguration configureEngine(ProcessEngineConfigurationImpl configuration) {
       DeserializationTypeValidator validatorMock = mock(DeserializationTypeValidator.class);
       when(validatorMock.validate(anyString())).thenReturn(true);
-      configuration.setDeserializationTypeValidator(validatorMock);
-      configuration.setDeserializationTypeValidationEnabled(true);
-      return configuration;
+      return configuration
+          .setDeserializationTypeValidator(validatorMock)
+          .setDeserializationTypeValidationEnabled(true)
+          .setJdbcUrl("jdbc:h2:mem:positive");
     };
   };
 
@@ -66,9 +66,10 @@ public class JsonSerializationWithValidationOnMultipleEnginesTest {
     public ProcessEngineConfiguration configureEngine(ProcessEngineConfigurationImpl configuration) {
       DeserializationTypeValidator validatorMock = mock(DeserializationTypeValidator.class);
       when(validatorMock.validate(anyString())).thenReturn(false);
-      configuration.setDeserializationTypeValidator(validatorMock);
-      configuration.setDeserializationTypeValidationEnabled(true);
-      return configuration;
+      return configuration
+          .setDeserializationTypeValidator(validatorMock)
+          .setDeserializationTypeValidationEnabled(true)
+          .setJdbcUrl("jdbc:h2:mem:negative");
     };
   };
 
@@ -84,51 +85,43 @@ public class JsonSerializationWithValidationOnMultipleEnginesTest {
   @Test
   public void shouldUsePositiveValidator() {
     // given
-    Deployment deployment = engineRulePositive.getRepositoryService().createDeployment()
+    engineRulePositive.manageDeployment(engineRulePositive.getRepositoryService().createDeployment()
         .addModelInstance("foo.bpmn", getOneTaskModel())
-        .deploy();
-    try {
-      ProcessInstance instance = engineRulePositive.getRuntimeService().startProcessInstanceByKey("oneTaskProcess");
+        .deploy());
+    ProcessInstance instance = engineRulePositive.getRuntimeService().startProcessInstanceByKey("oneTaskProcess");
 
-      // add serialized values to both instances
-      JsonSerializable bean = new JsonSerializable("a String", 42, true);
-      engineRulePositive.getRuntimeService().setVariable(instance.getId(), "simpleBean",
-          objectValue(bean).serializationDataFormat(DataFormats.JSON_DATAFORMAT_NAME).create());
+    // add serialized value
+    JsonSerializable bean = new JsonSerializable("a String", 42, true);
+    engineRulePositive.getRuntimeService().setVariable(instance.getId(), "simpleBean",
+        objectValue(bean).serializationDataFormat(DataFormats.JSON_DATAFORMAT_NAME).create());
 
-      // when
-      Object value = engineRulePositive.getRuntimeService().getVariable(instance.getId(), "simpleBean");
+    // when
+    Object value = engineRulePositive.getRuntimeService().getVariable(instance.getId(), "simpleBean");
 
-      // then
-      assertEquals(bean, value);
-    } finally {
-      engineRulePositive.getRepositoryService().deleteDeployment(deployment.getId(), true);
-    }
+    // then
+    assertEquals(bean, value);
   }
 
   @Test
   public void shouldUseNegativeValidator() {
     // given
-    Deployment deployment = engineRuleNegative.getRepositoryService().createDeployment()
+    engineRuleNegative.manageDeployment(engineRuleNegative.getRepositoryService().createDeployment()
         .addModelInstance("foo.bpmn", getOneTaskModel())
-        .deploy();
-    try {
-      ProcessInstance instance = engineRuleNegative.getRuntimeService().startProcessInstanceByKey("oneTaskProcess");
+        .deploy());
+    ProcessInstance instance = engineRuleNegative.getRuntimeService().startProcessInstanceByKey("oneTaskProcess");
 
-      // add serialized values to both instances
-      JsonSerializable bean = new JsonSerializable("a String", 42, true);
-      engineRuleNegative.getRuntimeService().setVariable(instance.getId(), "simpleBean",
-          objectValue(bean).serializationDataFormat(DataFormats.JSON_DATAFORMAT_NAME).create());
+    // add serialized value
+    JsonSerializable bean = new JsonSerializable("a String", 42, true);
+    engineRuleNegative.getRuntimeService().setVariable(instance.getId(), "simpleBean",
+        objectValue(bean).serializationDataFormat(DataFormats.JSON_DATAFORMAT_NAME).create());
 
-      // then
-      thrown.expect(ProcessEngineException.class);
-      thrown.expectMessage("Cannot deserialize");
-      thrown.expectCause(isA(SpinJsonException.class));
+    // then
+    thrown.expect(ProcessEngineException.class);
+    thrown.expectMessage("Cannot deserialize");
+    thrown.expectCause(isA(SpinJsonException.class));
 
-      // when
-      engineRuleNegative.getRuntimeService().getVariable(instance.getId(), "simpleBean");
-    } finally {
-      engineRuleNegative.getRepositoryService().deleteDeployment(deployment.getId(), true);
-    }
+    // when
+    engineRuleNegative.getRuntimeService().getVariable(instance.getId(), "simpleBean");
   }
 
   protected BpmnModelInstance getOneTaskModel() {
