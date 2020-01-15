98ffde6d9a86b2498185e4863c902a1d297d2693
Tobias Metzke <tobias.metzke@camunda.com>
2020-01-15 16:17:05 +0100

    feat(distro): separate CE and EE distros

---
+++ distro/run/distro/enterprise/src/start.bat
@@ -0,0 +1,14 @@
+@echo off
+
+SET webappsPath=..\lib\webapps\camunda-rest-distro-webapps-ee-1.0-SNAPSHOT.jar, 
+SET libPath=..\lib\db\*
+
+IF "%1"=="--webapps" (
+	SET classPath=%webappsPath%%libPath%
+	ECHO starting with webapps
+	) else (
+	SET classPath=%libPath%
+	ECHO starting without webapps
+	)
+	ECHO %classPath%
+start java -Dloader.path="%classPath%" -jar ..\lib\camunda-rest-distro-rest-1.0-SNAPSHOT.jar --spring.config.location=file:..\config\application.yml 
