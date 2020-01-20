3c19c231f320543ee3cf2813eedc0789209692f8
Emma Emma Emma <epollum@gmail.com>
2020-01-20 13:06:12 +0100

    auto format code

--- engine/src/main/java/org/camunda/bpm/engine/impl/cmd/GetTopicNamesCmd.java
+++ engine/src/main/java/org/camunda/bpm/engine/impl/cmd/GetTopicNamesCmd.java
@@ -1,39 +1,28 @@
 package org.camunda.bpm.engine.impl.cmd;
 
-import org.camunda.bpm.engine.exception.NotValidException;
 import org.camunda.bpm.engine.externaltask.ExternalTaskQuery;
 import org.camunda.bpm.engine.impl.ExternalTaskQueryImpl;
-import org.camunda.bpm.engine.impl.QueryOrderingProperty;
 import org.camunda.bpm.engine.impl.interceptor.Command;
 import org.camunda.bpm.engine.impl.interceptor.CommandContext;
+import org.camunda.bpm.engine.impl.util.EnsureUtil;
 
 import java.io.Serializable;
 import java.util.List;
 
-import static org.camunda.bpm.engine.impl.util.EnsureUtil.ensureNotNull;
-
 public class GetTopicNamesCmd implements Command<List<String>>, Serializable {
 
-    ExternalTaskQuery externalTaskQuery;
+  protected ExternalTaskQuery externalTaskQuery;
 
-    private GetTopicNamesCmd(){
-    }
+  public GetTopicNamesCmd() {
+  }
 
-    public GetTopicNamesCmd(ExternalTaskQuery externalTaskQuery){
-        this.externalTaskQuery = externalTaskQuery;
-    }
+  public GetTopicNamesCmd(ExternalTaskQuery externalTaskQuery) {
+    this.externalTaskQuery = externalTaskQuery;
+  }
 
-    //todo: note that ordering by default is process id [asc]
-    @Override
-    public List<String> execute(CommandContext commandContext) {
-        if (externalTaskQuery == null) throw new NullPointerException("ExternalTasKQuery cannot be null.");
-        else {
-            ExternalTaskQueryImpl impl = (ExternalTaskQueryImpl) externalTaskQuery;
-            commandContext.getAuthorizationManager().configureExternalTaskQuery(impl);
-            commandContext.getTenantManager().configureQuery(impl);
-            return commandContext
-                    .getExternalTaskManager()
-                    .selectTopicNamesByQuery(impl);
-        }
-    }
+  @Override public List<String> execute(CommandContext commandContext) {
+    EnsureUtil.ensureNotNull("externalTaskQuery", externalTaskQuery);
+    ExternalTaskQueryImpl externalTaskQueryImpl = (ExternalTaskQueryImpl) externalTaskQuery;
+    return commandContext.getExternalTaskManager().selectTopicNamesByQuery(externalTaskQueryImpl);
+  }
 }
