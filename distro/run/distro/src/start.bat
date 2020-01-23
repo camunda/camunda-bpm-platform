f080ecfc04d0416e6481c4791fa3e8f267c7474f
Tobias Metzke <tobias.metzke@camunda.com>
2020-01-23 15:52:59 +0100

    chore(start): set up JVM in start scripts

--- distro/run/distro/src/start.bat
+++ distro/run/distro/src/start.bat
@@ -1,9 +1,32 @@
 @echo off
 
+REM setup the JVM
+IF "x%JAVA_HOME%" == "x" (
+  SET JAVA=java
+  ECHO JAVA_HOME is not set. Unexpected results may occur.
+  ECHO Set JAVA_HOME to the directory of your local JDK to avoid this message.
+) ELSE (
+  IF NOT EXIST "%JAVA_HOME%" (
+    ECHO JAVA_HOME "%JAVA_HOME%" path doesn't exist
+    GOTO :EOF
+  ) ELSE (
+    IF NOT EXIST "%JAVA_HOME%\bin\java.exe" (
+      ECHO "%JAVA_HOME%\bin\java.exe" does not exist
+      GOTO :EOF
+    )
+    ECHO Setting JAVA property to "%JAVA_HOME%\bin\java"
+    SET JAVA="%JAVA_HOME%\bin\java"
+  )
+)
+
+
+REM set environment parameters
 SET webappsPath=../lib/webapps/
 SET restPath=../lib/rest/
 SET classPath=../lib/db/
 
+
+REM inspect arguments
 :Loop
 IF [%~1]==[] GOTO Continue
 
@@ -23,4 +46,6 @@ GOTO Loop
 
 ECHO classpath: %classPath%
 
-call java -Dloader.path="%classPath%" -jar "../lib/camunda-bpm-rest-distro-1.0-SNAPSHOT.jar" --spring.config.location=file:../config/application.yml
+
+REM start the application
+call %JAVA% -Dloader.path="%classPath%" -jar "../lib/camunda-bpm-rest-distro-1.0-SNAPSHOT.jar" --spring.config.location=file:../config/application.yml
--- distro/run/distro/src/start.sh
+++ distro/run/distro/src/start.sh
@@ -11,10 +11,26 @@ usage()
 
 #### Main
 
+# setup the JVM
+if [ "x$JAVA" = "x" ]; then
+  if [ "x$JAVA_HOME" != "x" ]; then
+    echo Setting JAVA property to "$JAVA_HOME/bin/java"
+    JAVA="$JAVA_HOME/bin/java"
+  else
+    echo JAVA_HOME is not set. Unexpected results may occur.
+    echo Set JAVA_HOME to the directory of your local JDK to avoid this message.
+    JAVA="java"
+  fi
+fi
+
+
+# set environment parameters
 webappsPath=../lib/webapps/
 restPath=../lib/rest/
 classPath=../lib/db/
 
+
+# inspect arguments
 while [ "$1" != "" ]; do
   case $1 in 
     --webapps ) classPath=$webappsPath,$classPath
@@ -31,4 +47,5 @@ done
 
 echo classpath: $classPath
 
-java -Dloader.path="$classPath" -jar "../lib/camunda-bpm-rest-distro-1.0-SNAPSHOT.jar" --spring.config.location=file:../config/application.yml
+# start the application
+"$JAVA" -Dloader.path="$classPath" -jar "../lib/camunda-bpm-rest-distro-1.0-SNAPSHOT.jar" --spring.config.location=file:../config/application.yml
