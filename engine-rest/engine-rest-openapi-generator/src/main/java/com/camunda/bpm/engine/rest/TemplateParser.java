d75f0eebbd1ed49d5f54911bd320cbc4d675cd92
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2020-01-31 16:39:26 +0100

    chore(engine-rest): generate JSON as part of build

--- engine-rest/engine-rest-openapi-generator/src/main/java/com/camunda/bpm/engine/rest/TemplateParser.java
+++ engine-rest/engine-rest-openapi-generator/src/main/java/com/camunda/bpm/engine/rest/TemplateParser.java
@@ -3,8 +3,11 @@ package com.camunda.bpm.engine.rest;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -20,13 +23,21 @@ public class TemplateParser {
 
   public static void main(String[] args) throws ParseException, IOException, TemplateException {
 
+    if (args.length != 3) {
+      throw new RuntimeException("Must provide two arguments: <source template directory> <main template> <output directory>");
+    }
+
+    String sourceDirectory = args[0];
+    String mainTemplate = args[1];
+    String outputFile = args[2];
+
     Configuration cfg = new Configuration();
 
     System.getProperties();
-    cfg.setDirectoryForTemplateLoading(new File("./src/main/templates/"));
+    cfg.setDirectoryForTemplateLoading(new File(sourceDirectory));
     cfg.setDefaultEncoding("UTF-8");
 
-    Template template = cfg.getTemplate("main.ftl");
+    Template template = cfg.getTemplate(mainTemplate);
 
     Map<String, Object> templateData = new HashMap<>();
     // templateData.put("msg", "Today is a beautiful day");
@@ -41,9 +52,11 @@ public class TemplateParser {
       String jsonString = out.getBuffer().toString();
       String prettyJson = formatJsonString(jsonString);
 
-      System.out.println(prettyJson);
+//      System.out.println(prettyJson);
 
-      out.flush();
+      File outFile = new File(outputFile);
+      FileUtils.forceMkdirParent(outFile);
+      FileUtils.write(outFile, prettyJson, StandardCharsets.UTF_8);
     }
   }
 
