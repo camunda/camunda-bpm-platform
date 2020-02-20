d0eae6a94d611dd6a510e35f8626edba17a4629e
Tassilo Weidner <tassilo.weidner@camunda.com>
2020-02-20 17:28:53 +0100

    test(qa/engine): add tests to verify feel-scala spin integration
    related to CAM-11454

---
+++ qa/integration-tests-engine/src/test/java/org/camunda/bpm/integrationtest/functional/spin/JsonListSerializable.java
@@ -0,0 +1,55 @@
+  
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.integrationtest.functional.spin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonListSerializable<T> {
+
+  private List<T> listProperty;
+
+  public JsonListSerializable() {
+    this.listProperty = new ArrayList<T>();
+  }
+
+  public void setListProperty(List<T> listProperty) {
+    this.listProperty = listProperty;
+  }
+
+  public String toExpectedJsonString() {
+    StringBuilder jsonBuilder = new StringBuilder();
+
+    jsonBuilder.append("{\"listProperty\":[");
+    for (int i = 0; i < listProperty.size(); i++) {
+      jsonBuilder.append(listProperty.get(i));
+      if (i < listProperty.size() - 1) {
+        jsonBuilder.append(",");
+      }
+    }
+    jsonBuilder.append("]}");
+
+    return jsonBuilder.toString();
+  }
+
+  public String toString() {
+    return toExpectedJsonString();
+  }
+
+
+}
