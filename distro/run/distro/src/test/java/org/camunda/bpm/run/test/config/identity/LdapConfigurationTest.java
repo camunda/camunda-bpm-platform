f973d5eb5095f22d9458a7249ff101f9cf5d741e
Miklas Boskamp <miklas.boskamp@camunda.com>
2020-02-20 10:31:04 +0100

    chore(run): Add additional LDAP related test
    Related to CAM-11293

--- distro/run/distro/src/test/java/org/camunda/bpm/run/test/config/identity/LdapConfigurationTest.java
+++ distro/run/distro/src/test/java/org/camunda/bpm/run/test/config/identity/LdapConfigurationTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.camunda.bpm.run.test.config.ldap;
+package org.camunda.bpm.run.test.config.identity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -30,7 +30,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = { CamundaBpmRun.class })
-@ActiveProfiles(profiles = { "test-ldap-enabled" })
+@ActiveProfiles(profiles = { "test-auth-disabled" , "test-ldap-enabled" })
 public class LdapConfigurationTest {
   
   @Autowired
