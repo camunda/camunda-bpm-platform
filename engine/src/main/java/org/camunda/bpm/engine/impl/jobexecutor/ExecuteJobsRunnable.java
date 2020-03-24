72ef90a7bd6c15fa4504867d0eda4b5f3511e33c
Miklas Boskamp <miklas.boskamp@camunda.com>
2020-03-24 09:35:52 +0100

    fix(engine): re-integrate executeJob hook method for job execution
    Related to CAM-11679

--- engine/src/main/java/org/camunda/bpm/engine/impl/jobexecutor/ExecuteJobsRunnable.java
+++ engine/src/main/java/org/camunda/bpm/engine/impl/jobexecutor/ExecuteJobsRunnable.java
@@ -68,7 +68,7 @@ public class ExecuteJobsRunnable implements Runnable {
         if (jobExecutor.isActive()) {
           JobFailureCollector jobFailureCollector = new JobFailureCollector(nextJobId);
           try {
-            ExecuteJobHelper.executeJob(nextJobId, commandExecutor, jobFailureCollector, new ExecuteJobsCmd(nextJobId, jobFailureCollector), engineConfiguration);
+            executeJob(nextJobId, commandExecutor, jobFailureCollector);
           } catch(Throwable t) {
             if (ProcessEngineLogger.shouldLogJobException(engineConfiguration, jobFailureCollector.getJob())) {
               ExecuteJobHelper.LOGGING_HANDLER.exceptionWhileExecutingJob(nextJobId, t);
@@ -106,8 +106,8 @@ public class ExecuteJobsRunnable implements Runnable {
    * Note: this is a hook to be overridden by
    * org.camunda.bpm.container.impl.threading.ra.inflow.JcaInflowExecuteJobsRunnable.executeJob(String, CommandExecutor)
    */
-  protected void executeJob(String nextJobId, CommandExecutor commandExecutor) {
-    ExecuteJobHelper.executeJob(nextJobId, commandExecutor);
+  protected void executeJob(String nextJobId, CommandExecutor commandExecutor, JobFailureCollector jobFailureCollector) {
+    ExecuteJobHelper.executeJob(nextJobId, commandExecutor, jobFailureCollector, new ExecuteJobsCmd(nextJobId, jobFailureCollector), processEngine.getProcessEngineConfiguration());
   }
 
   protected void unlockJob(String nextJobId, CommandExecutor commandExecutor) {
