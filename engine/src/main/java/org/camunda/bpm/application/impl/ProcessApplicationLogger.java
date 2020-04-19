64dd13fc962bc16b74a67c6b2bd6a47c3a2fe73d
David Hodges <david.hodges@ecogyenergy.com>
2020-04-19 19:23:50 +0200

    fix(engine): improve exception message
    related to CAM-11819

--- engine/src/main/java/org/camunda/bpm/application/impl/ProcessApplicationLogger.java
+++ engine/src/main/java/org/camunda/bpm/application/impl/ProcessApplicationLogger.java
@@ -49,7 +49,7 @@ public class ProcessApplicationLogger extends ProcessEngineLogger {
   public ProcessEngineException exceptionWhileNotifyingPaTaskListener(Exception e) {
     return new ProcessEngineException(exceptionMessage(
         "002",
-        "Exception while notifying process application task listener."), e);
+        "Exception while notifying process application task listener: " + e.getMessage()), e);
   }
 
   public void noTargetProcessApplicationForExecution(DelegateExecution execution) {
