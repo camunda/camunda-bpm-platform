50d19af46e4824f9d3165a496c6369fea6e79277
Tassilo Weidner <tassilo.weidner@camunda.com>
2020-04-21 10:07:00 +0200

    feat(engine): enforce hist. process auth. for ext. task log query
    related to CAM-11645

--- engine/src/main/java/org/camunda/bpm/engine/HistoryService.java
+++ engine/src/main/java/org/camunda/bpm/engine/HistoryService.java
@@ -656,7 +656,16 @@ public interface HistoryService {
   HistoricDecisionInstanceStatisticsQuery createHistoricDecisionInstanceStatisticsQuery(String decisionRequirementsDefinitionId);
 
   /**
-   * Creates a new programmatic query to search for {@link HistoricExternalTaskLog historic external task logs}.
+   * <p>Creates a new programmatic query to search for
+   * {@link HistoricExternalTaskLog historic external task logs}.
+   *
+   * <p>The result of the query is empty in the following cases:
+   * <ul>
+   *   <li>The user has no {@link Permissions#READ_HISTORY} permission on
+   *   {@link Resources#PROCESS_DEFINITION} OR
+   *   <li>The user has no {@link HistoricProcessInstancePermissions#READ} permission on
+   *       {@link Resources#HISTORIC_PROCESS_INSTANCE} ({@code enableHistoricInstancePermissions} in
+   *       {@link ProcessEngineConfigurationImpl} must be set to {@code true})
    *
    * @since 7.7
    */
