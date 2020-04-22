62b88c4bd11d4d4294449ef5d9989b9ee5fd19d2
Miklas Boskamp <miklas.boskamp@camunda.com>
2020-04-22 13:16:15 +0200

    Apply suggestions from code review
    Co-Authored-By: Nikola Koevski <nikola.koevski@camunda.com>

--- spring-boot-starter/starter/src/test/java/org/camunda/bpm/spring/boot/starter/configuration/impl/DefaultJobConfigurationTest.java
+++ spring-boot-starter/starter/src/test/java/org/camunda/bpm/spring/boot/starter/configuration/impl/DefaultJobConfigurationTest.java
@@ -39,6 +39,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(
   classes = {TestApplication.class},
