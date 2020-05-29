90acf9e9ec5eabd154b4e4871062bf4d5fa92ad6
Miklas Boskamp <miklas.boskamp@camunda.com>
2020-05-29 07:16:10 +0200

    chore(release): adjust create scripts for 7.14
    Related to CAM-11980

--- engine/src/main/resources/org/camunda/bpm/engine/db/create/activiti.mysql.create.engine.sql
+++ engine/src/main/resources/org/camunda/bpm/engine/db/create/activiti.mysql.create.engine.sql
@@ -63,7 +63,7 @@ create table ACT_GE_SCHEMA_LOG (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
 
 insert into ACT_GE_SCHEMA_LOG
-values ('0', CURRENT_TIMESTAMP, '7.13.0');
+values ('0', CURRENT_TIMESTAMP, '7.14.0');
 
 create table ACT_RE_DEPLOYMENT (
     ID_ varchar(64),
