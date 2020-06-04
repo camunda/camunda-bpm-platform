bb74038419038e88a19f537c6d19683586b4b991
Tassilo Weidner <tassilo.weidner@camunda.com>
2020-06-04 15:31:58 +0200

    fix(rest): add historic instance permission check
    related to CAM-11954

--- webapps/src/main/java/org/camunda/bpm/cockpit/impl/db/QueryServiceImpl.java
+++ webapps/src/main/java/org/camunda/bpm/cockpit/impl/db/QueryServiceImpl.java
@@ -23,8 +23,8 @@ import org.camunda.bpm.cockpit.db.QueryParameters;
 import org.camunda.bpm.cockpit.db.QueryService;
 import org.camunda.bpm.engine.ProcessEngineException;
 import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
+import org.camunda.bpm.engine.impl.db.AuthorizationCheck;
 import org.camunda.bpm.engine.impl.db.ListQueryParameterObject;
-import org.camunda.bpm.engine.impl.interceptor.Command;
 import org.camunda.bpm.engine.impl.interceptor.CommandContext;
 import org.camunda.bpm.engine.impl.util.QueryMaxResultsLimitUtil;
 
@@ -37,48 +37,33 @@ public class QueryServiceImpl implements QueryService {
   }
 
   public <T> List<T> executeQuery(final String statement, final QueryParameters<T> parameter) {
-    List<T> queryResult = commandExecutor.executeCommand(new Command<List<T>>() {
+    return commandExecutor.executeCommand(commandContext -> {
+      ProcessEngineConfigurationImpl engineConfig = getProcessEngineConfiguration(commandContext);
 
-      @SuppressWarnings("unchecked")
-      public List<T> execute(CommandContext commandContext) {
-        commandContext.getAuthorizationManager()
-          .enableQueryAuthCheck(parameter.getAuthCheck());
+      configureAuthCheck(parameter, engineConfig, commandContext);
 
-        if (parameter.isMaxResultsLimitEnabled()) {
-          QueryMaxResultsLimitUtil.checkMaxResultsLimit(parameter.getMaxResults(),
-            getProcessEngineConfiguration(commandContext));
-        }
-
-        return (List<T>) commandContext.getDbSqlSession().selectList(statement, parameter);
+      if (parameter.isMaxResultsLimitEnabled()) {
+        QueryMaxResultsLimitUtil.checkMaxResultsLimit(parameter.getMaxResults(), engineConfig);
       }
 
+      return (List<T>) commandContext.getDbSqlSession()
+        .selectList(statement, parameter);
     });
-    return queryResult;
   }
 
   public <T> T executeQuery(final String statement, final Object parameter, final Class<T> clazz) {
-      T queryResult = commandExecutor.executeCommand(new Command<T>() {
-
-      @SuppressWarnings("unchecked")
-      public T execute(CommandContext commandContext) {
-        return (T) commandContext.getDbSqlSession().selectOne(statement, parameter);
-      }
-
-    });
-    return queryResult;
+    return commandExecutor.executeCommand(commandContext -> (T) commandContext.getDbSqlSession()
+      .selectOne(statement, parameter));
   }
 
   public Long executeQueryRowCount(final String statement, final ListQueryParameterObject parameter) {
-    Long queryResult = commandExecutor.executeCommand(new Command<Long>() {
+    return commandExecutor.executeCommand(commandContext -> {
+      ProcessEngineConfigurationImpl engineConfig = getProcessEngineConfiguration(commandContext);
 
-      public Long execute(CommandContext commandContext) {
-        commandContext.getAuthorizationManager()
-          .enableQueryAuthCheck(parameter.getAuthCheck());
-        return (Long) commandContext.getDbSqlSession().selectOne(statement, parameter);
-      }
+      configureAuthCheck(parameter, engineConfig, commandContext);
 
+      return (Long) commandContext.getDbSqlSession().selectOne(statement, parameter);
     });
-    return queryResult;
   }
 
   protected ProcessEngineConfigurationImpl getProcessEngineConfiguration(
@@ -96,6 +81,18 @@ public class QueryServiceImpl implements QueryService {
     }
 
     return processEngineConfiguration;
+  }
+
+  protected  <T> void configureAuthCheck(ListQueryParameterObject parameter,
+                                         ProcessEngineConfigurationImpl engineConfig,
+                                         CommandContext commandContext) {
+    AuthorizationCheck authCheck = parameter.getAuthCheck();
+
+    commandContext.getAuthorizationManager()
+      .enableQueryAuthCheck(authCheck);
+
+    boolean isEnableHistoricInstancePermissions = engineConfig.isEnableHistoricInstancePermissions();
+    authCheck.setHistoricInstancePermissionsEnabled(isEnableHistoricInstancePermissions);
   }
 
 }
