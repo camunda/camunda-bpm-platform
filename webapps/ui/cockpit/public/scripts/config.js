9afef686bed7c4688d8e7eb5ee7f33e3ddd07414
Tassilo Weidner <tassilo.weidner@camunda.com>
2020-07-29 16:51:37 +0200

    fix(cockpit): add default configurations
    related to CAM-12222

--- webapps/ui/cockpit/public/scripts/config.js
+++ webapps/ui/cockpit/public/scripts/config.js
@@ -16,53 +16,56 @@
  */
 
 export default {
-  customScripts: [
-    // If you have a folder called 'my-custom-script' (in the 'cockpit' folder)
-    // with a file called 'customScript.js' in it
-    // 'my-custom-script/customScript'
-  ],
-  locales: ['en'],
-  csrfCookieName: 'XSRF-TOKEN',
-  bpmnJs: {
-    additionalModules: [
-      // If you have a folder called 'my-custom-module' (in the 'cockpit' folder)
-      // with a file called 'module.js' in it
-      // 'my-custom-module/module'
-    ],
-    moddleExtensions: {
-      // If you have a folder called 'my-custom-moddle' (in the 'cockpit' folder)
-      // with a file called 'camunda.json' in it defining the 'camunda' moddle extension
-      // camunda: 'my-custom-moddle/camunda'
-    }
-  },
-  skipCustomListeners: {
-    default: true,
-    hidden: false
-  },
-  skipIoMappings: {
-    default: true,
-    hidden: false
-  },
-  runtimeActivityInstanceMetrics: {
-    display: true
-  },
-  historicActivityInstanceMetrics: {
-    adjustablePeriod: true,
-    // Select from the default time period: day, week, month, complete
-    period: {
-      unit: 'day'
-    }
-  },
-  batchOperation: {
-    // Select mode of query for process instances or decision instances
-    // possible values: filter, search
-    mode: 'filter',
-    // Select if Historic Batches should be loaded automatically when navigating to #/batch
-    autoLoadEnded: true
-  },
-  defaultFilter: {
-    historicProcessDefinitionInstancesSearch: {}
-  },
-  disableWelcomeMessage: false,
-  userOperationLogAnnotationLength: 4000
+  // customScripts: [
+  //   // If you have a folder called 'my-custom-script' (in the 'cockpit' folder)
+  //   // with a file called 'customScript.js' in it
+  //   // 'my-custom-script/customScript'
+  // ],
+  // locales: ['en'],
+  // csrfCookieName: 'XSRF-TOKEN',
+  // bpmnJs: {
+  //   additionalModules: [
+  //     // If you have a folder called 'my-custom-module' (in the 'cockpit' folder)
+  //     // with a file called 'module.js' in it
+  //     // 'my-custom-module/module'
+  //   ],
+  //   moddleExtensions: {
+  //     // If you have a folder called 'my-custom-moddle' (in the 'cockpit' folder)
+  //     // with a file called 'camunda.json' in it defining the 'camunda' moddle extension
+  //     // camunda: 'my-custom-moddle/camunda'
+  //   }
+  // },
+  // skipCustomListeners: {
+  //   default: true,
+  //   hidden: false
+  // },
+  // skipIoMappings: {
+  //   default: true,
+  //   hidden: false
+  // },
+  // runtimeActivityInstanceMetrics: {
+  //   display: true
+  // },
+  // historicActivityInstanceMetrics: {
+  //   adjustablePeriod: true,
+  //   // Select from the default time period: day, week, month, complete
+  //   period: {
+  //     unit: 'week'
+  //   }
+  // },
+  // batchOperation: {
+  //   // Select mode of query for process instances or decision instances
+  //   // possible values: filter, search
+  //   mode: 'filter',
+  //   // Select if Historic Batches should be loaded automatically when navigating to #/batch
+  //   autoLoadEnded: true
+  // },
+  // defaultFilter: {
+  //   historicProcessDefinitionInstancesSearch: {
+  //     lastDays: 5,
+  //     event: 'started'
+  //   }
+  // },
+  // disableWelcomeMessage: false,
+  // userOperationLogAnnotationLength: 4000
 };
