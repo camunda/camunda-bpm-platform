8d550e4911a913538c97c1642378671a2de30afb
Yana Vasileva <yana.vasileva@camunda.com>
2020-09-02 14:43:36 +0200

    fix default value

--- engine/src/test/java/org/camunda/bpm/engine/test/concurrency/ConcurrentTelemetryConfigurationTest.java
+++ engine/src/test/java/org/camunda/bpm/engine/test/concurrency/ConcurrentTelemetryConfigurationTest.java
@@ -72,7 +72,7 @@ public class ConcurrentTelemetryConfigurationTest extends ConcurrencyTestCase {
 
     assertNull(thread1.exception);
     assertNull(thread2.exception);
-    assertThat(managementService.isTelemetryEnabled()).isFalse();
+    assertThat(managementService.isTelemetryEnabled()).isNull();
   }
 
   protected static class ControllableUpdateTelemetrySetupCommand extends ControllableCommand<Void> {
