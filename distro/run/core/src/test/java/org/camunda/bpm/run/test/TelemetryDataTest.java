3e4cfadae7a97d1114e17108520f7b372e7dda32
Yana Vasileva <yana.vasileva@camunda.com>
2020-09-16 08:58:45 +0200

    remove unnecessary web config

--- distro/run/core/src/test/java/org/camunda/bpm/run/test/TelemetryDataTest.java
+++ distro/run/core/src/test/java/org/camunda/bpm/run/test/TelemetryDataTest.java
@@ -29,11 +29,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = { CamundaBpmRun.class }, webEnvironment = WebEnvironment.DEFINED_PORT)
+@SpringBootTest(classes = { CamundaBpmRun.class })
 public class TelemetryDataTest {
 
   @Autowired
