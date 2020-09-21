20ad025cd8a9e4f6b21674c563b4c73f1b460f79
Tassilo Weidner <tassilo.weidner@camunda.com>
2020-09-21 12:12:05 +0200

    test(engine): fix failing mariadb test
    * MariaDB 10.0 does not throw exception when column size is exceeded
    * Add missing required history level for test
    
    Related to CAM-12466

--- engine/src/test/java/org/camunda/bpm/engine/test/persistence/SuppressSqlExceptionsTest.java
+++ engine/src/test/java/org/camunda/bpm/engine/test/persistence/SuppressSqlExceptionsTest.java
@@ -37,6 +37,7 @@ import org.camunda.bpm.engine.impl.db.sql.DbSqlSessionFactory;
 import org.camunda.bpm.engine.impl.history.HistoryLevel;
 import org.camunda.bpm.engine.impl.test.RequiredDatabase;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.engine.test.RequiredHistoryLevel;
 import org.camunda.bpm.engine.test.util.ProcessEngineTestRule;
 import org.camunda.bpm.engine.test.util.ProvidedProcessEngineRule;
 import org.camunda.bpm.engine.variable.Variables;
@@ -44,7 +45,6 @@ import org.camunda.bpm.model.bpmn.Bpmn;
 import org.camunda.bpm.model.bpmn.BpmnModelInstance;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
@@ -172,7 +172,8 @@ public class SuppressSqlExceptionsTest {
         .isInstanceOf(PersistenceException.class);
   }
 
-  @Ignore("https://jira.camunda.com/browse/CAM-12466")
+  @RequiredHistoryLevel(ProcessEngineConfigurationImpl.HISTORY_ACTIVITY)
+  @RequiredDatabase(excludes = DbSqlSessionFactory.MARIADB)
   @Test
   public void shouldThrowExceptionOnInsert_ColumnSizeExceeded() {
     // given
@@ -316,6 +317,7 @@ public class SuppressSqlExceptionsTest {
   }
 
   @Test
+  @RequiredDatabase(excludes = DbSqlSessionFactory.MARIADB)
   public void shouldThrowExceptionOnUpdate() {
     // given
     HistoryLevel historyLevel = engineConfig.getHistoryLevel();
