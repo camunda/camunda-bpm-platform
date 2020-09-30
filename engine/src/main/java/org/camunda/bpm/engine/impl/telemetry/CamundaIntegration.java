46807dcde8bdde68f4c678cc5e55446384ca8153
yanavasileva <yanavasileva@users.noreply.github.com>
2020-09-30 17:02:57 +0200

    fix(engine): fix typo in integration fields for telemetry
    Related to CAM-12522

--- engine/src/main/java/org/camunda/bpm/engine/impl/telemetry/CamundaIntegration.java
+++ engine/src/main/java/org/camunda/bpm/engine/impl/telemetry/CamundaIntegration.java
@@ -20,8 +20,8 @@ public class CamundaIntegration {
 
   public static final String SPRING_BOOT_STARTER = "spring-boot-starter";
   public static final String CAMUNDA_BPM_RUN = "camunda-bpm-run";
-  public static final String WILDFLY_SUBSYSTEM = "wildfly-sybsystem";
-  public static final String JBOSS_SUBSYSTEM = "jboss-sybsystem";
+  public static final String WILDFLY_SUBSYSTEM = "wildfly-subsystem";
+  public static final String JBOSS_SUBSYSTEM = "jboss-subsystem";
   public static final String CAMUNDA_EJB_SERVICE = "camunda-ejb-service";
 
 }
