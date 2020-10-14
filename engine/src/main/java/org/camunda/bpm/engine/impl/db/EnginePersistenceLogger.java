0e510fc48d51d75801e322b4e910eb8b9317ffee
yanavasileva <yanavasileva@users.noreply.github.com>
2020-10-14 09:37:51 +0200

    chore(engine): adjust duplicated code in persistence logger
    Related to CAM-12601

--- engine/src/main/java/org/camunda/bpm/engine/impl/db/EnginePersistenceLogger.java
+++ engine/src/main/java/org/camunda/bpm/engine/impl/db/EnginePersistenceLogger.java
@@ -790,13 +790,6 @@ public class EnginePersistenceLogger extends ProcessEngineLogger {
         failedOperation.toString());
   }
 
-  public void logTaskWithoutExecution(String taskId) {
-	  logDebug("091",
-			  "Execution of external task {} is null. This indicates that the task was concurrently completed or deleted. "
-			  + "It is not returned by the current fetch and lock command.",
-			  taskId);
-  }
-
   public CrdbTransactionRetryException crdbTransactionRetryException(DbOperation operation) {
     return new CrdbTransactionRetryException(exceptionMessage(
         "102",
@@ -844,4 +837,11 @@ public class EnginePersistenceLogger extends ProcessEngineLogger {
         "Error while fetching the telemetry initial message status property from the database: {}", exception.getMessage());
   }
 
+	
+  public void logTaskWithoutExecution(String taskId) {
+    logDebug("108",
+      "Execution of external task {} is null. This indicates that the task was concurrently completed or deleted. "
+      + "It is not returned by the current fetch and lock command.",
+      taskId);
+  }
 }
