a43e3462f4da347661cca95822cf89bf6924691f
Martin Stamm <martin.stamm@camunda.com>
2021-02-11 09:56:33 +0100

    chore(plugins): add compatibility for legacy plugins
    related to CAM-12978, closes #1301

--- webapps/ui/cockpit/client/scripts/config.js
+++ webapps/ui/cockpit/client/scripts/config.js
@@ -26,6 +26,18 @@ export default {
   //   // with a file called 'customScript.js' in it
   //   // 'my-custom-script/customScript'
   // ],
+  // requireJsConfig: {
+  //   // AngularJS module names
+  //   ngDeps: ['ui.bootstrap'],
+  //   // RequireJS configuration for a complete configuration documentation see:
+  //   // http://requirejs.org/docs/api.html#config
+  //   deps: ['jquery', 'custom-ui'],
+  //   paths: {
+  //     // if you have a folder called `custom-ui` (in the `cockpit` folder)
+  //     // with a file called `scripts.js` in it and defining the `custom-ui` AMD module
+  //     'custom-ui': 'custom-ui/scripts'
+  //   }
+  // },
   // historicActivityInstanceMetrics: {
   //   adjustablePeriod: true,
   //   //select from the default time period: day, week, month, complete
