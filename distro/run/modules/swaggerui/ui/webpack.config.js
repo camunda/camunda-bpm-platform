cf90242186cad1ad7ee5318d0be3bcb378343483
Anton von Weltzien <anton.weltzien@outlook.com>
2021-03-12 16:19:13 +0100

    feat(run): add license book generation for swagger-ui
    Related to CAM-13194

--- distro/run/modules/swaggerui/ui/webpack.config.js
+++ distro/run/modules/swaggerui/ui/webpack.config.js
@@ -1,6 +1,8 @@
+const writeThirdPartyNotice = require('./tasks/license').writeThirdPartyNotice;
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const TerserPlugin = require("terser-webpack-plugin");
+const LicenseCheckerWebpackPlugin = require("license-checker-webpack-plugin");
 
 
 const config = {
@@ -23,6 +25,15 @@ const config = {
     plugins: [
         new CopyPlugin({
             patterns: [{from: 'src/index.html'}, {from: 'src/favicon.ico'}],
+        }),
+        new LicenseCheckerWebpackPlugin({
+            outputFilename: path.join( "..", "..", "THIRD-PARTY-NOTICE.json"),
+            outputWriter: writeThirdPartyNotice,
+            override: {
+                "swagger-ui@~3.43.0": { repository: "https://github.com/swagger-api/swagger-ui" },
+                "css-loader@~5.1.1": { repository: "https://github.com/webpack-contrib/css-loader" },
+                "style-loader@~2.0.0": { repository: "https://github.com/webpack-contrib/style-loader" }
+            },
         })
     ],
     optimization: {
