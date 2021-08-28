4fdfde4c73c58db4b44e9c16af9496149c365da8
t.arian <taha.aarian@gmail.com>
2021-08-28 15:17:03 +0430

    preparing for pull request
    JIRA Issue link
    https://jira.camunda.com/browse/CAM-13774
    
    please assign to Tassilo Weidner

--- clients/java/client/src/test/java/org/camunda/bpm/client/task/ChargeCardWorkerTest.java
+++ clients/java/client/src/test/java/org/camunda/bpm/client/task/ChargeCardWorkerTest.java
@@ -11,6 +11,8 @@ public class ChargeCardWorkerTest {
     private final static Logger LOGGER = Logger.getLogger(ChargeCardWorkerTest.class.getName());
 
     public static void main(String[] args) {
+
+        // Personal Test
         ExternalTaskClient client = ExternalTaskClient.create()
                 .baseUrl("http://10.1.2.51:8080/engine-rest")
                 .asyncResponseTimeout(10000) // long polling timeout
@@ -19,15 +21,10 @@ public class ChargeCardWorkerTest {
         client.subscribe("charge-card")
                 .lockDuration(1000)
                 .handler((externalTask, externalTaskService) -> {
-
-
-
                     Map<String, Object> vars = new HashMap<String, Object>();
                     vars.put("progress",10);
 
                     externalTaskService.setVariables(externalTask,vars);
-
-
                     externalTaskService.complete(externalTask);
                 })
                 .open();
