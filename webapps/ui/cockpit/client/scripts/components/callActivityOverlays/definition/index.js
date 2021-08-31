bfcee47e376b23015fba6c8dc912e4de32ca3583
Anton von Weltzien <anton.weltzien@camunda.com>
2021-08-31 17:03:55 +0200

    add missing semicolon

--- webapps/ui/cockpit/client/scripts/components/callActivityOverlays/definition/index.js
+++ webapps/ui/cockpit/client/scripts/components/callActivityOverlays/definition/index.js
@@ -74,7 +74,7 @@ module.exports = function(viewContext) {
                 'PLUGIN_ACTIVITY_DEFINITION_CALLED_NOT_RESOLVABLE'
               );
               for (const activity of callActivityFlowNodes) {
-                const calledProcess = callActivityToProcessMap[activity]?.id
+                const calledProcess = callActivityToProcessMap[activity]?.id;
 
                 const toolTipTitle = calledProcess ? resolvable : notResolvable;
 
