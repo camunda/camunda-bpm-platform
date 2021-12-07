a8bfa033987293cb776ca5e3e7491ef977f42941
Tassilo Weidner <tassilo.weidner@camunda.com>
2021-12-07 12:58:40 +0100

    Update SessionCookieIT.java

--- spring-boot-starter/starter-webapp-core/src/test/java/org/camunda/bpm/spring/boot/starter/webapp/filter/session/it/SessionCookieIT.java
+++ spring-boot-starter/starter-webapp-core/src/test/java/org/camunda/bpm/spring/boot/starter/webapp/filter/session/it/SessionCookieIT.java
@@ -25,11 +25,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = { TestApplication.class }, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
     properties = {"server.error.include-message=always"})
+@DirtiesContext
 public class SessionCookieIT {
 
   @Rule
