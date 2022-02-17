3d2926673351e380b01153e7bec7f74d9f56267a
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2022-02-17 04:09:43 +0100

    chore(engine-cdi): ensure engine-cdi runs with Java 17
    - our CDI module tests with an old version of Weld (required to
      test compatibility with Java EE 6), which doesn't run with JDK 17
    - as a solution, we create an additional module that tests compatibility
      with Weld 3, reusing the tests from the core module. On JDK 16+, we
      then only execute the compatibility test suite
    
    related to CAM-13816, CAM-14368

---
+++ engine-cdi/core/src/test/java/org/camunda/bpm/engine/cdi/test/api/annotation/ExecutionIdTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.engine.cdi.test.api.annotation;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Set;
+
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.spi.Bean;
+
+import org.camunda.bpm.engine.cdi.BusinessProcess;
+import org.camunda.bpm.engine.cdi.annotation.ExecutionIdLiteral;
+import org.camunda.bpm.engine.cdi.test.CdiProcessEngineTestCase;
+import org.camunda.bpm.engine.test.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * 
+ * @author Daniel Meyer
+ */
+@RunWith(Arquillian.class)
+public class ExecutionIdTest extends CdiProcessEngineTestCase {
+  
+  @Test
+  @Deployment
+  public void testExecutionIdInjectableByName() {
+    getBeanInstance(BusinessProcess.class).startProcessByKey("keyOfTheProcess");
+    String processInstanceId = (String) getBeanInstance("processInstanceId");
+    Assert.assertNotNull(processInstanceId);
+    String executionId = (String) getBeanInstance("executionId");
+    Assert.assertNotNull(executionId);
+    
+    assertEquals(processInstanceId, executionId);
+  }
+  
+  @Test
+  @Deployment
+  public void testExecutionIdInjectableByQualifier() {
+    getBeanInstance(BusinessProcess.class).startProcessByKey("keyOfTheProcess");
+    
+    Set<Bean<?>> beans = beanManager.getBeans(String.class, new ExecutionIdLiteral());    
+    Bean<String> bean = (Bean<java.lang.String>) beanManager.resolve(beans);
+    
+    CreationalContext<String> ctx = beanManager.createCreationalContext(bean);
+    String executionId = (String) beanManager.getReference(bean, String.class, ctx);   
+    Assert.assertNotNull(executionId);
+    
+    String processInstanceId = (String) getBeanInstance("processInstanceId");
+    Assert.assertNotNull(processInstanceId);
+    
+    assertEquals(processInstanceId, executionId);
+  }
+  
+}
