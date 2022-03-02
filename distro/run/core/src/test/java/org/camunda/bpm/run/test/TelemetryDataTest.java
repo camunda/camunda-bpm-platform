9774f25cb7f24a8563cb7d327dfc91fb0bd98477
Nikola Koevski <nikola.koevski@camunda.com>
2022-03-02 15:05:56 +0100

    chore(run): clean up application contexts in the core tests
    * Remove cached application contexts that are not reused by applutin the
      @DirtiesContext annotation. The tests started failing on JDK8 due to the increased heap size by the application contexts.
    
    Related to CAM-14412

--- distro/run/core/src/test/java/org/camunda/bpm/run/test/TelemetryDataTest.java
+++ distro/run/core/src/test/java/org/camunda/bpm/run/test/TelemetryDataTest.java
@@ -29,8 +29,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = { CamundaBpmRun.class })
 public class TelemetryDataTest {
