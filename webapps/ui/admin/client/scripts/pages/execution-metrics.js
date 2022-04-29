2d1ced8f7dd956a82893496b0161abecbd57b290
Clifton Davies <20408665+cliftondavies@users.noreply.github.com>
2022-04-29 14:41:20 +0100

    fix(admin): display telemetry settings and execution metrics
    related to CAM-14509, closes #1890

--- webapps/ui/admin/client/scripts/pages/execution-metrics.js
+++ webapps/ui/admin/client/scripts/pages/execution-metrics.js
@@ -214,23 +214,7 @@ module.exports = [
       label: 'EXECUTION_METRICS',
       template: template,
       controller: Controller,
-      priority: 900,
-      access: [
-        'AuthorizationResource',
-        function(AuthorizationResource) {
-          return function(cb) {
-            AuthorizationResource.check({
-              permissionName: 'ALL',
-              resourceName: 'authorization',
-              resourceType: 4
-            })
-              .$promise.then(function(response) {
-                cb(null, response.authorized);
-              })
-              .catch(cb);
-          };
-        }
-      ]
+      priority: 900
     });
   }
 ];
