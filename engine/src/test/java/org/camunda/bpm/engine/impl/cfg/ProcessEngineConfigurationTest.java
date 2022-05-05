c200833cd9edc6c1d29dd52638a4f73c44c1d801
Jan Cheng <zhengj.cn@gmail.com>
2022-05-05 16:50:43 +0800

    test(engine): tests for init dmn elProvider

--- engine/src/test/java/org/camunda/bpm/engine/impl/cfg/ProcessEngineConfigurationTest.java
+++ engine/src/test/java/org/camunda/bpm/engine/impl/cfg/ProcessEngineConfigurationTest.java
@@ -17,7 +17,14 @@
 package org.camunda.bpm.engine.impl.cfg;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+
+import org.camunda.bpm.dmn.engine.DmnEngine;
+import org.camunda.bpm.dmn.engine.impl.DefaultDmnEngineConfiguration;
+import org.camunda.bpm.dmn.engine.impl.spi.el.ElExpression;
+import org.camunda.bpm.dmn.engine.impl.spi.el.ElProvider;
 import org.camunda.bpm.engine.ProcessEngineConfiguration;
+import org.camunda.bpm.engine.impl.el.ElProviderCompatible;
 import org.junit.Test;
 
 public class ProcessEngineConfigurationTest {
@@ -25,9 +32,46 @@ public class ProcessEngineConfigurationTest {
   @Test
   public void shouldEnableStandaloneTasksByDefault() {
     // when
-    ProcessEngineConfigurationImpl engineConfiguration = (ProcessEngineConfigurationImpl) ProcessEngineConfiguration.createStandaloneProcessEngineConfiguration();
+    ProcessEngineConfigurationImpl engineConfiguration = (ProcessEngineConfigurationImpl) ProcessEngineConfiguration
+        .createStandaloneProcessEngineConfiguration();
 
     // then
     assertThat(engineConfiguration.isStandaloneTasksEnabled()).isTrue();
+  }
+
+  @Test
+  public void shouldUseDefaultElProviderByJuelExpressionManager() {
+    // when
+    ProcessEngineConfigurationImpl engineConfiguration = (ProcessEngineConfigurationImpl) ProcessEngineConfiguration
+        .createStandaloneProcessEngineConfiguration();
+
+    engineConfiguration.init();
+    DmnEngine dmnEngine = engineConfiguration.getDmnEngine();
+
+    // then
+    ElProvider expected = ((ElProviderCompatible) engineConfiguration.getExpressionManager()).toElProvider();
+    assertEquals(expected, ((DefaultDmnEngineConfiguration) dmnEngine.getConfiguration()).getElProvider());
+  }
+
+  @Test
+  public void shouldUseProvidedElProvider() {
+    // when
+    ProcessEngineConfigurationImpl engineConfiguration = (ProcessEngineConfigurationImpl) ProcessEngineConfiguration
+        .createStandaloneProcessEngineConfiguration();
+
+    ElProvider elProvider = new ElProvider() {
+      @Override
+      public ElExpression createExpression(String expression) {
+        throw new UnsupportedOperationException();
+      }
+    };
+
+    engineConfiguration.setElProvider(elProvider);
+
+    engineConfiguration.init();
+    DmnEngine dmnEngine = engineConfiguration.getDmnEngine();
+
+    // then
+    assertEquals(elProvider, ((DefaultDmnEngineConfiguration) dmnEngine.getConfiguration()).getElProvider());
   }
 }
