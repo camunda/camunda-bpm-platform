10e2e1d14877bee114c2bd62edccfebaaff4f1ff
Miklas Boskamp <miklas.boskamp@camunda.com>
2022-08-30 10:43:48 +0200

    chore(demo): produce OOM

---
+++ engine/src/test/java/FailingTest.java
@@ -0,0 +1,35 @@
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Test;
+
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class FailingTest {
+
+  @Test
+  public void produceOOM() {
+    List<byte[]> list = new LinkedList<>();
+    int index = 1;
+
+    while (true) {
+        byte[] b = new byte[1000 * 1024 * 1024]; // 1GB byte object
+        list.add(b);
+    }
+  }
+}
