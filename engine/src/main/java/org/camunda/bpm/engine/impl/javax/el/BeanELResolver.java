0ab17a2110fec72b6e9aedb9b88e14708e0bdb63
Yana Vasileva <yana.vasileva@camunda.com>
2022-09-29 15:42:16 +0200

    remove unused imports

--- engine/src/main/java/org/camunda/bpm/engine/impl/javax/el/BeanELResolver.java
+++ engine/src/main/java/org/camunda/bpm/engine/impl/javax/el/BeanELResolver.java
@@ -26,12 +26,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
