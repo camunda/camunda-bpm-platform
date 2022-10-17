6f09751aaaacb1b9b82dfa5692de96cbc15284a3
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2022-10-17 16:41:13 +0200

    chore(project): add workflow that copies issues
    related to CAMTEAM-273

--- .github/actions/common/dist/index.js
+++ .github/actions/common/dist/index.js
@@ -8299,6 +8299,95 @@ function wrappy (fn, cb) {
 
 /***/ }),
 
+/***/ 8685:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+const core = __nccwpck_require__(2186);
+const github = __nccwpck_require__(5438);
+
+
+module.exports = async function() {
+  try {
+    const issueNumber = core.getInput('issue');
+    const deepCopy = core.getInput('deep-copy');
+    const repoToken = core.getInput('repo-token');
+    const newVersion = core.getInput('version');
+    const versionPrefix = core.getInput('version-prefix');
+    
+    const octokit = github.getOctokit(repoToken);
+    const repo = github.context.payload.repository;
+    
+    const { data: issue } = await octokit.rest.issues.get({
+      owner: repo.owner.login,
+      repo: repo.name,
+      issue_number: issueNumber,
+    });
+    
+    const issueBody = issue.body;
+    var newIssueBody = issue.body;
+    
+    const newVersionLabel = {name: `${versionPrefix}${newVersion}`};
+    const createNewIssueLabels = function (previousLabels) {
+      const newLabels = previousLabels.filter(label => !label.name.startsWith(versionPrefix));
+      newLabels.push(newVersionLabel);
+      return newLabels;
+    }
+    
+    if (deepCopy && issue.body) {
+      // match #<digits> and only when there is space/new line characters around it
+      const issueRegex = /(\s+#)(\d+)(\s+)/g;
+      
+      const matches = issueBody.matchAll(issueRegex);
+      
+      newIssueBody = '';
+      var previousMatchOffset = 0;
+      
+      for (const match of matches) {
+        const { data: referencedIssue } = await octokit.rest.issues.get({
+          owner: repo.owner.login,
+          repo: repo.name,
+          issue_number: match[2],
+        });
+        
+        const newReferencedIssueLabels = createNewIssueLabels(referencedIssue.labels);
+        
+        const { data: newReferencedIssue } = await octokit.rest.issues.create({
+          owner: repo.owner.login,
+          repo: repo.name,
+          title: referencedIssue.title,
+          body: referencedIssue.body,
+          labels: newReferencedIssueLabels
+        });
+        
+        core.info(`Copied referenced issue ${referencedIssue.number} into new issue ${newReferencedIssue.number}.`);
+        
+        newIssueBody = newIssueBody + issueBody.substring(previousMatchOffset, match.index)
+          + match[1] + newReferencedIssue.number + match[3];
+          
+        previousMatchOffset = match.index + match[0].length;
+          
+      }
+      newIssueBody = newIssueBody + issueBody.substring(previousMatchOffset);
+    }
+    
+    const newIssueLabels = createNewIssueLabels(issue.labels);
+    
+    const { data: newIssue } = await octokit.rest.issues.create({
+      owner: repo.owner.login,
+      repo: repo.name,
+      title: issue.title,
+      body: newIssueBody,
+      labels: newIssueLabels
+    });
+    
+    core.info(`Copied issue ${issueNumber} into new issue ${newIssue.number}.`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+/***/ }),
+
 /***/ 6453:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
@@ -8772,6 +8861,7 @@ var exports = __webpack_exports__;
 exports.extractLinks = __nccwpck_require__(6453);
 exports.formatJiraLinks = __nccwpck_require__(2607);
 exports.generateReleaseNotes = __nccwpck_require__(970);
+exports.copyIssue = __nccwpck_require__(8685);
 })();
 
 module.exports = __webpack_exports__;
--- .github/actions/common/index.js
+++ .github/actions/common/index.js
@@ -1,3 +1,4 @@
 exports.extractLinks = require('./src/extract-links.js');
 exports.formatJiraLinks = require('./src/format-jira-links.js');
 exports.generateReleaseNotes = require('./src/generate-release-notes.js');
+exports.copyIssue = require('./src/copy-issue.js');
