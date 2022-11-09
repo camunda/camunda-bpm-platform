03a3760167ba9864632fb6d829e75b632f16ef8f
Tassilo Weidner <3015690+tasso94@users.noreply.github.com>
2022-11-09 11:13:50 +0100

    chore(webapps): enforce translation string sorting
    related to #2893

--- webapps/ui/admin/grunt/config/eslint.js
+++ webapps/ui/admin/grunt/config/eslint.js
@@ -19,7 +19,10 @@ module.exports = function(config, eslintConf) {
   'use strict';
 
   eslintConf.admin_scripts = {
-    src: ['<%= pkg.gruntConfig.adminSourceDir %>/scripts/**/*.js']
+    src: [
+      '<%= pkg.gruntConfig.adminSourceDir %>/en.json',
+      '<%= pkg.gruntConfig.adminSourceDir %>/scripts/**/*.js'
+    ]
   };
 
   eslintConf.admin_plugins = {
