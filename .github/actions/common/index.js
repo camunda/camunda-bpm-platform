65c27224508f56f79aa966e63e4a3f8a35add148
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2022-11-30 17:44:30 +0100

    chore(project): externalize github actions and workflows
    related to CAMTEAM-282

--- .github/actions/common/index.js
+++
@@ -1,5 +0,0 @@
-exports.extractLinks = require('./src/extract-links.js');
-exports.formatJiraLinks = require('./src/format-jira-links.js');
-exports.generateReleaseNotes = require('./src/generate-release-notes.js');
-exports.synchronizeLabelColors = require('./src/synchronize-label-colors.js');
-exports.copyIssue = require('./src/copy-issue.js');
