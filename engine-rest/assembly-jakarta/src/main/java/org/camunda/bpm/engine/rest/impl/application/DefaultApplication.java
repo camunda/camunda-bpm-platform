3d1a3aa31853a77fc6958f0d01dbe1b76739fbb9
Daniel Kelemen <daniel.kelemen@camunda.com>
2023-02-13 17:21:48 +0100

    feat(engine-rest): add support for Jakarta APIs & Wildfly27

---
+++ engine-rest/assembly-jakarta/src/main/java/org/camunda/bpm/engine/rest/impl/application/DefaultApplication.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.engine.rest.impl.application;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+import org.camunda.bpm.engine.rest.impl.CamundaRestResources;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * <p>Default {@link Application} registering all resources.</p>
+ *
+ * <p><strong>NOTE</strong> This class is excluded from the classes-jar,
+ * such that users that want to embed the REST API as a JAR file into a
+ * custom JAX-RS application are able to build a deployment based on their
+ * requirements.</p>
+ *
+ * @author Daniel Meyer
+ *
+ */
+@ApplicationPath("/")
+public class DefaultApplication extends Application {
+
+  @Override
+  public Set<Class<?>> getClasses() {
+    Set<Class<?>> classes = new HashSet<Class<?>>();
+
+    classes.addAll(CamundaRestResources.getResourceClasses());
+    classes.addAll(CamundaRestResources.getConfigurationClasses());
+
+    return classes;
+  }
+
+}
