ed6066698671ce18e6501d1db8fe86ba781d3278
Tassilo Weidner <3015690+tasso94@users.noreply.github.com>
2023-04-14 17:57:27 +0200

    change back reverted changes

--- webapps/frontend/camunda-commons-ui/lib/services/index.js
+++ webapps/frontend/camunda-commons-ui/lib/services/index.js
@@ -30,6 +30,7 @@ var angular = require('../../../camunda-bpm-sdk-js/vendor/angular'),
   HttpClient = require('./HttpClient'),
   unescape = require('./unescape'),
   fixDate = require('./fixDate'),
+  ifUnauthorizedForwardToWelcomeApp = require('./ifUnauthorizedForwardToWelcomeApp'),
   unfixDate = require('./unfixDate'),
   shouldDisplayAuthenticationError = require('./shouldDisplayAuthenticationError');
 
@@ -46,6 +47,10 @@ ngModule.factory('ResourceResolver', ResourceResolver);
 ngModule.factory('camAPIHttpClient', HttpClient);
 ngModule.factory('unescape', unescape);
 ngModule.factory('fixDate', fixDate);
+ngModule.factory(
+  'ifUnauthorizedForwardToWelcomeApp',
+  ifUnauthorizedForwardToWelcomeApp
+);
 ngModule.factory('unfixDate', unfixDate);
 ngModule.factory(
   'shouldDisplayAuthenticationError',
@@ -62,12 +67,19 @@ ngModule.config([
       '$rootScope',
       '$q',
       'RequestLogger',
-      function($rootScope, $q, RequestLogger) {
+      'ifUnauthorizedForwardToWelcomeApp',
+      function(
+        $rootScope,
+        $q,
+        RequestLogger,
+        ifUnauthorizedForwardToWelcomeApp
+      ) {
         RequestLogger.logStarted();
 
         return {
           response: function(response) {
             RequestLogger.logFinished();
+            ifUnauthorizedForwardToWelcomeApp(response.headers());
 
             return response;
           },
@@ -94,7 +106,8 @@ ngModule.config([
   '$httpProvider',
   '$windowProvider',
   function($httpProvider, $windowProvider) {
-    if (!DEV_MODE) {// eslint-disable-line
+    // eslint-disable-next-line
+    if (!DEV_MODE) {
       var window = $windowProvider.$get();
       var uri = window.location.href;
 
