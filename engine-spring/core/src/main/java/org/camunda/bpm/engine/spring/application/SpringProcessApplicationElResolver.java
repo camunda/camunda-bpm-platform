d211d3dd61f1843d2253719628f0a3c3fe45ddfe
Yana Vasileva <yana.vasileva@camunda.com>
2023-05-05 11:29:50 +0200

    revert elresolver

--- engine-spring/core/src/main/java/org/camunda/bpm/engine/spring/application/SpringProcessApplicationElResolver.java
+++ engine-spring/core/src/main/java/org/camunda/bpm/engine/spring/application/SpringProcessApplicationElResolver.java
@@ -24,6 +24,7 @@ import org.camunda.bpm.application.ProcessApplicationElResolver;
 import org.camunda.bpm.application.impl.EjbProcessApplication;
 import org.camunda.bpm.application.impl.ServletProcessApplication;
 import org.camunda.bpm.engine.spring.ApplicationContextElResolver;
+import org.camunda.bpm.impl.juel.jakarta.el.ELResolver;
 import org.springframework.util.ClassUtils;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
