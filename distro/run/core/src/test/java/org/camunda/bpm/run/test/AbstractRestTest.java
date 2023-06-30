807e223cf4ff2184cef4974ceb90959d2dcc03c3
psavidis <69160690+psavidis@users.noreply.github.com>
2023-06-30 11:08:23 +0300

    feat(run): Upgrade to Spring Boot 3.1
    * This commit includes maven-enforcer-plugin in camunda-run to enforce java 17 during build time
    
    Related to: #3442

--- distro/run/core/src/test/java/org/camunda/bpm/run/test/AbstractRestTest.java
+++ distro/run/core/src/test/java/org/camunda/bpm/run/test/AbstractRestTest.java
@@ -26,7 +26,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
