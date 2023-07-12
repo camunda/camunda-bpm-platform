48356052a6bfd15f88ec15a433ecf7847f282426
Yana Vasileva <yana.vasileva@camunda.com>
2023-07-12 11:22:08 +0200

    sh: move open a browser to earlier step

--- distro/run/assembly/resources/run.sh
+++ distro/run/assembly/resources/run.sh
@@ -111,6 +111,30 @@ if [ "$1" = "start" ] ; then
 
   echo classpath: $classPath
 
+  # open a browser (must be done first)
+  UNAME=`which uname`
+  if [ -n "$UNAME" -a "`$UNAME`" = "Darwin" ]
+  then
+    BROWSERS="open"
+  else
+    BROWSERS="xdg-open gnome-www-browser x-www-browser firefox chromium chromium-browser google-chrome"
+  fi
+  
+  if [ -z "$BROWSER" ]; then
+    for executable in $BROWSERS; do
+      BROWSER=`which $executable 2> /dev/null`
+      if [ -n "$BROWSER" ]; then
+        break;
+      fi
+    done
+  fi
+  
+  if [ -z "$BROWSER" ]; then
+    (sleep 5; echo -e "We are sorry... We tried all we could do but we couldn't locate your default browser... \nIf you want to see our default website please open your browser and insert this URL:\nhttp://localhost:8080/camunda-welcome/index.html";) &
+  else
+    (sleep 10; $BROWSER "http://localhost:8080/camunda-welcome/index.html";) &
+  fi
+
   # start the application
   if [ "$detachProcess" = "true" ]; then
 
@@ -121,30 +145,6 @@ A Camunda Run instance is already in operation (process id $(cat $PID_PATH)).
 
 Please stop it or remove the file $PID_PATH."
       exit 1
-    fi
-
-    # open a browser (must be done first)
-    UNAME=`which uname`
-    if [ -n "$UNAME" -a "`$UNAME`" = "Darwin" ]
-    then
-      BROWSERS="open"
-    else
-      BROWSERS="xdg-open gnome-www-browser x-www-browser firefox chromium chromium-browser google-chrome"
-    fi
-    
-    if [ -z "$BROWSER" ]; then
-      for executable in $BROWSERS; do
-        BROWSER=`which $executable 2> /dev/null`
-        if [ -n "$BROWSER" ]; then
-          break;
-        fi
-      done
-    fi
-    
-    if [ -z "$BROWSER" ]; then
-      (sleep 5; echo -e "We are sorry... We tried all we could do but we couldn't locate your default browser... \nIf you want to see our default website please open your browser and insert this URL:\nhttp://localhost:8080/camunda-welcome/index.html";) &
-    else
-      (sleep 10; $BROWSER "http://localhost:8080/camunda-welcome/index.html";) &
     fi
 
     # start Camunda Run detached
