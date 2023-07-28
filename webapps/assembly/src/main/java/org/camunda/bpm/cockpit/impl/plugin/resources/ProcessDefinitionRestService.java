d8ad3a04aa456711fa6266bddc9572530ca5c11e
Daniel Kelemen <daniel.kelemen@camunda.com>
2023-07-28 18:13:40 +0200

    feat(engine): extend process definition statistics api & query
    related to camunda/camunda-bpm-platform#3611

--- webapps/assembly/src/main/java/org/camunda/bpm/cockpit/impl/plugin/resources/ProcessDefinitionRestService.java
+++ webapps/assembly/src/main/java/org/camunda/bpm/cockpit/impl/plugin/resources/ProcessDefinitionRestService.java
@@ -16,11 +16,26 @@
  */
 package org.camunda.bpm.cockpit.impl.plugin.resources;
 
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-
+import org.camunda.bpm.cockpit.db.QueryParameters;
+import org.camunda.bpm.cockpit.impl.plugin.base.dto.ProcessDefinitionStatisticsDto;
+import org.camunda.bpm.cockpit.impl.plugin.base.dto.query.ProcessDefinitionStatisticsQueryDto;
 import org.camunda.bpm.cockpit.impl.plugin.base.sub.resources.ProcessDefinitionResource;
 import org.camunda.bpm.cockpit.plugin.resource.AbstractPluginResource;
+import org.camunda.bpm.engine.rest.dto.CountResultDto;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+import java.util.List;
+
+import static org.camunda.bpm.engine.authorization.Permissions.READ;
+import static org.camunda.bpm.engine.authorization.Resources.PROCESS_DEFINITION;
 
 public class ProcessDefinitionRestService extends AbstractPluginResource {
 
@@ -33,6 +48,37 @@ public class ProcessDefinitionRestService extends AbstractPluginResource {
   @Path("/{id}")
   public ProcessDefinitionResource getProcessDefinition(@PathParam("id") String id) {
     return new ProcessDefinitionResource(getProcessEngine().getName(), id);
+  }
+
+  @GET
+  @Path("/statistics-count")
+  @Produces(MediaType.APPLICATION_JSON)
+  public CountResultDto getStatisticsCount(@Context UriInfo uriInfo) {
+    QueryParameters queryDto = new ProcessDefinitionStatisticsQueryDto(uriInfo.getQueryParameters());
+    configureExecutionQuery(queryDto);
+    long count = getQueryService().executeQueryRowCount("selectPDStatisticsCount", queryDto);
+    return new CountResultDto(count);
+  }
+
+  @GET
+  @Path("/statistics")
+  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes(MediaType.APPLICATION_JSON)
+  public List<ProcessDefinitionStatisticsDto> queryStatistics(@Context UriInfo uriInfo,
+                                                              @QueryParam("firstResult") Integer firstResult,
+                                                              @QueryParam("maxResults") Integer maxResults) {
+    QueryParameters queryDto = new ProcessDefinitionStatisticsQueryDto(uriInfo.getQueryParameters());
+    configureExecutionQuery(queryDto);
+    queryDto.setFirstResult(firstResult != null && firstResult >= 0 ? firstResult : 0);
+    queryDto.setMaxResults(maxResults != null && maxResults > 0 ? maxResults : Integer.MAX_VALUE);
+
+    return getQueryService().executeQuery("selectPDStatistics", queryDto);
+  }
+
+  protected void configureExecutionQuery(QueryParameters query) {
+    configureAuthorizationCheck(query);
+    configureTenantCheck(query);
+    addPermissionCheck(query, PROCESS_DEFINITION, "RES.KEY_", READ);
   }
 
 }
