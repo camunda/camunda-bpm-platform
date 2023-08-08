16ea1829da2b98c9f25ace25fa25964801ac8ab2
Yana Vasileva <yana.vasileva@camunda.com>
2023-08-08 11:50:06 +0200

    move the java check and var to run scripts

--- distro/run/assembly/resources/start.bat
+++ distro/run/assembly/resources/start.bat
@@ -3,43 +3,6 @@
 SET BASEDIR=%~dp0
 SET EXECUTABLE=%BASEDIR%internal\run.bat
 
-REM setup the JVM
-IF "x%JAVA_HOME%" == "x" (
-  SET JAVA=java
-  ECHO JAVA_HOME is not set. Unexpected results may occur.
-  ECHO Set JAVA_HOME to the directory of your local JDK to avoid this message.
-) ELSE (
-  IF NOT EXIST "%JAVA_HOME%" (
-    ECHO JAVA_HOME "%JAVA_HOME%" path doesn't exist
-    GOTO Done
-  ) ELSE (
-    IF NOT EXIST "%JAVA_HOME%\bin\java.exe" (
-      ECHO "%JAVA_HOME%\bin\java.exe" does not exist
-      GOTO Done
-    )
-    ECHO Setting JAVA property to "%JAVA_HOME%\bin\java"
-    SET JAVA="%JAVA_HOME%\bin\java"
-  )
-)
-
-IF NOT "x%JAVA_OPTS%" == "x" (
-  ECHO JAVA_OPTS: %JAVA_OPTS%
-)
-
-REM check Java version >= 17
-SET EXPECTED_JAVA_VERSION=17
-FOR /f "tokens=3" %%g IN ('JAVA -version 2^>^&1 ^| findstr /i "version"') DO (
-  SET JAVA_VERSION=%%g
-)
-SET JAVA_VERSION=%JAVA_VERSION:"=%
-ECHO Java version is %JAVA_VERSION%
-FOR /f "delims=. tokens=1" %%v in ("%JAVA_VERSION%") do (
-  IF %%v LSS %EXPECTED_JAVA_VERSION% (
-    ECHO You mus use at least JDK 17 to start Camunda Platform Run.
-    GOTO Done
-  )
-)
-
 REM Start detached if no arguments
 IF [%~1]==[] GOTO StartDetached
 
--- distro/run/assembly/resources/start.sh
+++ distro/run/assembly/resources/start.sh
@@ -5,31 +5,6 @@ runScript=$BASEDIR/internal/run.sh
 
 if [ $# -eq 0 ]; then
 
-  # setup the JVM
-  if [ "x$JAVA" = "x" ]; then
-    if [ "x$JAVA_HOME" != "x" ]; then
-      echo Setting JAVA property to "$JAVA_HOME/bin/java"
-      JAVA="$JAVA_HOME/bin/java"
-    else
-      echo JAVA_HOME is not set. Unexpected results may occur.
-      echo Set JAVA_HOME to the directory of your local JDK to avoid this message.
-      JAVA="java"
-    fi
-  fi
-
-  # check Java version >= 17
-  EXPECTED_JAVA_VERSION=17
-  JAVA_VERSION=$("$JAVA" -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^0\./s///' | cut -d'.' -f1)
-  echo Java version is $("$JAVA" -version 2>&1 | head -1 | cut -d'"' -f2)
-  if [[ "$JAVA_VERSION" -lt "$EXPECTED_JAVA_VERSION" ]]; then
-    echo You must use at least JDK 17 to start Camunda Platform Run.
-    exit 1
-  fi
-
-  if [ "x$JAVA_OPTS" != "x" ]; then
-    echo JAVA_OPTS: $JAVA_OPTS
-  fi
-
   # open a browser (must be done first)
   UNAME=`which uname`
   if [ -n "$UNAME" -a "`$UNAME`" = "Darwin" ]
