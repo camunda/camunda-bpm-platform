5fc4d9144a8fcaa2ad238b500007ddd714c8ec0d
Petros Savvidis <petros.savvidis@camunda.com>
2023-09-28 16:29:22 +0300

    chore(engine): Adjust create scripts
    Replace `7.20.0` with `7.21.0` in engine create sql scripts of different dbs

--- engine/src/main/resources/org/camunda/bpm/engine/db/create/activiti.mysql.create.engine.sql
+++ engine/src/main/resources/org/camunda/bpm/engine/db/create/activiti.mysql.create.engine.sql
@@ -69,7 +69,7 @@ create table ACT_GE_SCHEMA_LOG (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
 
 insert into ACT_GE_SCHEMA_LOG
-values ('0', CURRENT_TIMESTAMP, '7.20.0');
+values ('0', CURRENT_TIMESTAMP, '7.21.0');
 
 create table ACT_RE_DEPLOYMENT (
     ID_ varchar(64),
