fc8f3292788686067defb6e12584b00340b23f09
Miklas Boskamp <20189772+mboskamp@users.noreply.github.com>
2024-01-22 13:46:43 +0100

    fix(engine): correct execution order of task listeners
    Related to https://github.com/camunda/camunda-bpm-platform/issues/4042

---
+++ engine/src/test/java/org/camunda/bpm/engine/test/bpmn/tasklistener/builtin/PostParseListener.java
@@ -0,0 +1,49 @@
+
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.bpm.engine.test.bpmn.tasklistener.builtin;
+
+import org.camunda.bpm.engine.delegate.DelegateTask;
+import org.camunda.bpm.engine.delegate.TaskListener;
+import org.camunda.bpm.engine.impl.ProcessEngineLogger;
+import org.camunda.bpm.engine.impl.bpmn.behavior.UserTaskActivityBehavior;
+import org.camunda.bpm.engine.impl.bpmn.parser.AbstractBpmnParseListener;
+import org.camunda.bpm.engine.impl.pvm.process.ActivityImpl;
+import org.camunda.bpm.engine.impl.pvm.process.ScopeImpl;
+import org.camunda.bpm.engine.impl.task.TaskDefinition;
+import org.camunda.bpm.engine.impl.util.xml.Element;
+import org.slf4j.Logger;
+
+public class PostParseListener extends AbstractBpmnParseListener {
+
+  Logger LOG = ProcessEngineLogger.TEST_LOGGER.getLogger();
+
+  @Override
+  public void parseUserTask(final Element userTaskElement, final ScopeImpl scope, final ActivityImpl activity) {
+    getTaskDefinition(activity).addBuiltInTaskListener("create", new TaskListener() {
+
+      @Override
+      public void notify(DelegateTask delegateTask) {
+        LOG.info("Executed task listener: PostParseListener");
+      }
+    });
+  }
+
+  private TaskDefinition getTaskDefinition(final ActivityImpl activity) {
+    return ((UserTaskActivityBehavior) activity.getActivityBehavior()).getTaskDefinition();
+  }
+}
