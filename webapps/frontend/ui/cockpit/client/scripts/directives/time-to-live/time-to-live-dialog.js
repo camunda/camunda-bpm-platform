d8e376d2bd40808d60eeece6fcba68d9cfb3975a
venetrius <gergely.juhasz@camunda.com>
2024-03-14 08:14:15 +0100

    feat(frontend): Link to SET_REMOVAL_TIME batch should pre-set values (#4149)
    related to: camunda/camunda-bpm-platform/issues/4096

--- webapps/frontend/ui/cockpit/client/scripts/directives/time-to-live/time-to-live-dialog.js
+++ webapps/frontend/ui/cockpit/client/scripts/directives/time-to-live/time-to-live-dialog.js
@@ -51,8 +51,39 @@ const Controller = [
     $scope.ttl = $scope.definition.historyTimeToLive;
 
     $scope.openBatchOperation = () => {
+      let batchSearchQuery = null;
+      let operation = null;
+
+      if ($scope.$parent?.resource === 'process-definition') {
+        operation = 'PROCESS_SET_REMOVAL_TIME';
+        batchSearchQuery = JSON.stringify([
+          {
+            type: 'PIprocessDefinitionKey',
+            operator: 'eq',
+            value: $scope.definition.key
+          },
+          {
+            type: 'PIfinished',
+            operator: 'eq',
+            value: ''
+          }
+        ]);
+      } else {
+        operation = 'DECISION_SET_REMOVAL_TIME';
+        batchSearchQuery = JSON.stringify([
+          {
+            type: 'decisionDefinitionKeyIn',
+            operator: 'In',
+            value: [$scope.definition.key]
+          }
+        ]);
+      }
+
       $scope.$dismiss();
-      $location.path('/batch/operation');
+      $location.path('/batch/operation').search({
+        batchSearchQuery,
+        operation
+      });
     };
 
     $scope.isValid = () => {
