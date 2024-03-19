52c98fa0ea1fb47d28f32a01d5efd060f5be9901
Daniel Kelemen <danielkelemen@users.noreply.github.com>
2024-03-19 14:57:36 +0100

    fix(webapp): fix execution metrics errors
    related to https://github.com/camunda/camunda-bpm-platform/issues/4158

--- webapps/assembly/src/main/java/org/camunda/bpm/cockpit/db/QueryService.java
+++ webapps/assembly/src/main/java/org/camunda/bpm/cockpit/db/QueryService.java
@@ -16,20 +16,10 @@
  */
 package org.camunda.bpm.cockpit.db;
 
-import java.util.List;
-
-import org.camunda.bpm.engine.impl.db.ListQueryParameterObject;
-
 /**
  *
  * @author Nico Rehwaldt
  */
-public interface QueryService {
+public interface QueryService extends org.camunda.bpm.webapp.db.QueryService {
 
-  public <T> List<T> executeQuery(final String statement, final QueryParameters parameter);
-
-  @Deprecated
-  public <T> T executeQuery(final String statement, final Object parameter, final Class<T> clazz);
-
-  public Long executeQueryRowCount(final String statement, final ListQueryParameterObject parameter);
 }
