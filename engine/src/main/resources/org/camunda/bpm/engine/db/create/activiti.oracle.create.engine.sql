96c1bf3a59a1792deb40add0a56ef4ae956c483f
venetrius <gergely.juhasz@camunda.com>
2024-04-08 11:39:55 +0200

    chore(project): Prepare Codebase for 7.22.0 Release (#4242)
    Related-to: #3860

--- engine/src/main/resources/org/camunda/bpm/engine/db/create/activiti.oracle.create.engine.sql
+++ engine/src/main/resources/org/camunda/bpm/engine/db/create/activiti.oracle.create.engine.sql
@@ -69,7 +69,7 @@ create table ACT_GE_SCHEMA_LOG (
 );
 
 insert into ACT_GE_SCHEMA_LOG
-values ('0', CURRENT_TIMESTAMP, '7.21.0');
+values ('0', CURRENT_TIMESTAMP, '7.22.0');
 
 create table ACT_RE_DEPLOYMENT (
     ID_ NVARCHAR2(64),
