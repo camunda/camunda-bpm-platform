e1f6ee56101fad8904078bf238e3a3d42b2f6725
Tassilo Weidner <3015690+tasso94@users.noreply.github.com>
2024-05-24 10:26:40 +0200

    Exclude transitive dependencies

--- qa/integration-tests-engine-jakarta/src/test/java/org/camunda/bpm/integrationtest/util/DeploymentHelper.java
+++ qa/integration-tests-engine-jakarta/src/test/java/org/camunda/bpm/integrationtest/util/DeploymentHelper.java
@@ -16,7 +16,6 @@
  */
 package org.camunda.bpm.integrationtest.util;
 
-import org.apache.commons.lang.ArrayUtils;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 
@@ -35,51 +34,32 @@ public class DeploymentHelper extends AbstractDeploymentHelper {
   }
 
   public static JavaArchive[] getWeld() {
-    return getWeld(CAMUNDA_ENGINE_CDI);
+    return getWeldShaded(CAMUNDA_ENGINE_CDI);
   }
 
   public static JavaArchive[] getEngineSpring() {
     return getEngineSpring(CAMUNDA_ENGINE_SPRING);
   }
 
-  protected static JavaArchive[] getWeld(String engineCdiArtifactName) {
-    if (CACHED_WELD_ASSETS != null) {
+  protected static JavaArchive[] getWeldShaded(String engineCdiArtifactName) {
+    if(CACHED_WELD_ASSETS != null) {
       return CACHED_WELD_ASSETS;
     } else {
 
-      JavaArchive[] archives = resolveDependenciesFromPomXml(engineCdiArtifactName,
-              "org.jboss.weld.servlet:weld-servlet-core",
-              "jakarta.enterprise.concurrent:jakarta.enterprise.concurrent-api",
-              "jakarta.transaction:jakarta.transaction-api",
-              "jakarta.inject:jakarta.inject-api"
-      );
+      JavaArchive[] resolvedArchives = Maven.configureResolver()
+              .workOffline()
+              .loadPomFromFile("pom.xml")
+              .resolve(engineCdiArtifactName, "org.jboss.weld.servlet:weld-servlet-shaded")
+              .withoutTransitivity()
+              .as(JavaArchive.class);
 
-      if(archives.length == 0) {
-        throw new RuntimeException("could not resolve the weld implementation and jakarta API dependencies");
+      if(resolvedArchives.length == 0) {
+        throw new RuntimeException("could not resolve org.jboss.weld.servlet:weld-servlet-shaded");
       } else {
-        CACHED_WELD_ASSETS = archives;
+        CACHED_WELD_ASSETS = resolvedArchives;
         return CACHED_WELD_ASSETS;
       }
     }
-  }
-
-  protected static JavaArchive[] resolveDependenciesFromPomXml(String engineCdiArtifactName, String... dependencyNames) {
-    JavaArchive[] result = new JavaArchive[0];
-    for (String dependencyName : dependencyNames) {
-      JavaArchive[] archive = resolveDependenciesFromPomXml(engineCdiArtifactName, dependencyName);
-      result = (JavaArchive[]) ArrayUtils.addAll(result, archive);
-    }
-
-    return result;
-  }
-
-  protected static JavaArchive[] resolveDependenciesFromPomXml(String engineCdiArtifactName, String dependencyName) {
-      return Maven.configureResolver()
-              .workOffline()
-              .loadPomFromFile("pom.xml")
-              .resolve(engineCdiArtifactName, dependencyName)
-              .withTransitivity()
-              .as(JavaArchive.class);
   }
 
 }
