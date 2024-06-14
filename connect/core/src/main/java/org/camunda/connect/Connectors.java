a61222b6015afb77b1654069641fdf7869dcc14d
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2024-06-14 10:34:07 +0200

    feat(connect): upgrade to apache httpclient 5.x
    * preserve compatibility
    
    Co-authored-by: tasso94 <3015690+tasso94@users.noreply.github.com>

--- connect/core/src/main/java/org/camunda/connect/Connectors.java
+++ connect/core/src/main/java/org/camunda/connect/Connectors.java
@@ -40,7 +40,7 @@ public class Connectors {
   public static String SOAP_HTTP_CONNECTOR_ID = "soap-http-connector";
 
   /** The global instance of the manager */
-  private static final Connectors INSTANCE = new Connectors();
+  static final Connectors INSTANCE = new Connectors();
 
   /**
    * Provides the global instance of the Connectors manager.
