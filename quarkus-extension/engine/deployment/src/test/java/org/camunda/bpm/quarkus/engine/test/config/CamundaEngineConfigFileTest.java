816b58fd91e578d1ec7e4992300cdcd114ac3e5b
Joaquín <165814090+joaquinfelici@users.noreply.github.com>
2024-09-09 10:34:28 -0300

    chore(quarkus): Bump quarkus version to 3.14
    Related-to: https://github.com/camunda/camunda-bpm-platform/issues/4163

--- quarkus-extension/engine/deployment/src/test/java/org/camunda/bpm/quarkus/engine/test/config/CamundaEngineConfigFileTest.java
+++ quarkus-extension/engine/deployment/src/test/java/org/camunda/bpm/quarkus/engine/test/config/CamundaEngineConfigFileTest.java
@@ -67,10 +67,10 @@ public class CamundaEngineConfigFileTest {
     assertThat(jobExecutor.getMaxWait()).isEqualTo(65000);
     assertThat(jobExecutor.getBackoffTimeInMillis()).isEqualTo(5);
     // assert correct thread pool config
-    assertThat(config.jobExecutor.threadPool.maxPoolSize).isEqualTo(12);
-    assertThat(config.jobExecutor.threadPool.queueSize).isEqualTo(5);
+    assertThat(config.jobExecutor().threadPool().maxPoolSize()).isEqualTo(12);
+    assertThat(config.jobExecutor().threadPool().queueSize()).isEqualTo(5);
     // assert correct datasource
-    assertThat(config.datasource).hasValue("camunda");
+    assertThat(config.datasource()).hasValue("camunda");
     assertThat(configuration.getDataSource().getConnection()).asString().contains("h2:mem:camunda");
   }
 }
