f03855ba4a7a78160cd9dfc8e477f7a99e26063a
Petros Savvidis <petros.savvidis@camunda.com>
2024-09-10 13:11:30 +0300

    chore(engine): Add Column and Index to Migration Scripts
    Related-to: https://github.com/camunda/camunda-bpm-platform/issues/4214

--- engine/src/main/resources/org/camunda/bpm/engine/db/upgrade/mariadb_engine_7.21_to_7.22.sql
+++ engine/src/main/resources/org/camunda/bpm/engine/db/upgrade/mariadb_engine_7.21_to_7.22.sql
@@ -21,3 +21,6 @@ values ('1100', CURRENT_TIMESTAMP, '7.22.0');
 alter table ACT_RU_TASK add column TASK_STATE_ varchar(64);
 
 alter table ACT_HI_TASKINST add column TASK_STATE_ varchar(64);
+
+alter table ACT_HI_PROCINST add RESTARTED_PROC_INST_ID_ varchar(64);
+create index ACT_IDX_HI_PRO_RST_PRO_INST_ID on ACT_HI_PROCINST(RESTARTED_PROC_INST_ID_);
