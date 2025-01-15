93319af0a4816e8e2fd797507a5471ad7a1369dc
Helene <21290204+PHWaechtler@users.noreply.github.com>
2025-01-15 12:25:34 +0100

    feat(baseUrl): add UrlResolver (#4857)
    related to #4888 
    
    ---------
    
    Co-authored-by: xmgdtc <ma.xiaolei@sye-info.com>
    Co-authored-by: yanavasileva <yanavasileva@users.noreply.github.com>

--- clients/java/client/src/main/java/org/camunda/bpm/client/impl/EngineClient.java
+++ clients/java/client/src/main/java/org/camunda/bpm/client/impl/EngineClient.java
@@ -19,9 +19,9 @@ package org.camunda.bpm.client.impl;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-
-import org.camunda.bpm.client.task.OrderingConfig;
+import org.camunda.bpm.client.UrlResolver;
 import org.camunda.bpm.client.task.ExternalTask;
+import org.camunda.bpm.client.task.OrderingConfig;
 import org.camunda.bpm.client.task.impl.ExternalTaskImpl;
 import org.camunda.bpm.client.task.impl.dto.BpmnErrorRequestDto;
 import org.camunda.bpm.client.task.impl.dto.CompleteRequestDto;
@@ -46,7 +46,8 @@ public class EngineClient {
   protected static final String ID_RESOURCE_PATH = EXTERNAL_TASK_RESOURCE_PATH + "/" + ID_PATH_PARAM;
   public static final String LOCK_RESOURCE_PATH = ID_RESOURCE_PATH + "/lock";
   public static final String EXTEND_LOCK_RESOURCE_PATH = ID_RESOURCE_PATH + "/extendLock";
-  public static final String SET_VARIABLES_RESOURCE_PATH = EXTERNAL_TASK__PROCESS_RESOURCE_PATH + "/" + ID_PATH_PARAM + "/variables";
+  public static final String SET_VARIABLES_RESOURCE_PATH =
+      EXTERNAL_TASK__PROCESS_RESOURCE_PATH + "/" + ID_PATH_PARAM + "/variables";
   public static final String UNLOCK_RESOURCE_PATH = ID_RESOURCE_PATH + "/unlock";
   public static final String COMPLETE_RESOURCE_PATH = ID_RESOURCE_PATH + "/complete";
   public static final String FAILURE_RESOURCE_PATH = ID_RESOURCE_PATH + "/failure";
@@ -56,7 +57,7 @@ public class EngineClient {
   public static final String PROCESS_INSTANCE_ID_RESOURCE_PATH = PROCESS_INSTANCE_RESOURCE_PATH + "/" + ID_PATH_PARAM;
   public static final String GET_BINARY_VARIABLE =
       PROCESS_INSTANCE_ID_RESOURCE_PATH + "/variables/" + NAME_PATH_PARAM + "/data";
-  protected String baseUrl;
+  protected UrlResolver urlResolver;
   protected String workerId;
   protected int maxTasks;
   protected boolean usePriority;
@@ -65,89 +66,128 @@ public class EngineClient {
   protected RequestExecutor engineInteraction;
   protected TypedValues typedValues;
 
-  public EngineClient(String workerId, int maxTasks, Long asyncResponseTimeout, String baseUrl, RequestExecutor engineInteraction) {
+  public EngineClient(String workerId,
+                      int maxTasks,
+                      Long asyncResponseTimeout,
+                      String baseUrl,
+                      RequestExecutor engineInteraction) {
     this(workerId, maxTasks, asyncResponseTimeout, baseUrl, engineInteraction, true, OrderingConfig.empty());
   }
 
-  public EngineClient(String workerId, int maxTasks, Long asyncResponseTimeout, String baseUrl, RequestExecutor engineInteraction,
-                      boolean usePriority, OrderingConfig orderingConfig) {
+  public EngineClient(String workerId,
+                      int maxTasks,
+                      Long asyncResponseTimeout,
+                      String baseUrl,
+                      RequestExecutor engineInteraction,
+                      boolean usePriority,
+                      OrderingConfig orderingConfig) {
     this.workerId = workerId;
     this.asyncResponseTimeout = asyncResponseTimeout;
     this.maxTasks = maxTasks;
     this.usePriority = usePriority;
     this.engineInteraction = engineInteraction;
-    this.baseUrl = baseUrl;
+    this.urlResolver = new PermanentUrlResolver(baseUrl);
     this.orderingConfig = orderingConfig;
   }
 
-  public List<ExternalTask> fetchAndLock(List<TopicRequestDto> topics)  {
-    FetchAndLockRequestDto payload = new FetchAndLockRequestDto(workerId, maxTasks, asyncResponseTimeout,
-        topics, usePriority, orderingConfig);
+  public EngineClient(String workerId,
+                      int maxTasks,
+                      Long asyncResponseTimeout,
+                      UrlResolver urlResolver,
+                      RequestExecutor engineInteraction) {
+    this(workerId, maxTasks, asyncResponseTimeout, urlResolver, engineInteraction, true, OrderingConfig.empty());
+  }
 
-    String resourceUrl = baseUrl + FETCH_AND_LOCK_RESOURCE_PATH;
+  public EngineClient(String workerId,
+                      int maxTasks,
+                      Long asyncResponseTimeout,
+                      UrlResolver urlResolver,
+                      RequestExecutor engineInteraction,
+                      boolean usePriority,
+                      OrderingConfig orderingConfig) {
+    this.workerId = workerId;
+    this.asyncResponseTimeout = asyncResponseTimeout;
+    this.maxTasks = maxTasks;
+    this.usePriority = usePriority;
+    this.engineInteraction = engineInteraction;
+    this.urlResolver = urlResolver;
+    this.orderingConfig = orderingConfig;
+  }
+
+  public List<ExternalTask> fetchAndLock(List<TopicRequestDto> topics) {
+    FetchAndLockRequestDto payload = new FetchAndLockRequestDto(workerId, maxTasks, asyncResponseTimeout, topics,
+        usePriority, orderingConfig);
+
+    String resourceUrl = getBaseUrl() + FETCH_AND_LOCK_RESOURCE_PATH;
     ExternalTask[] externalTasks = engineInteraction.postRequest(resourceUrl, payload, ExternalTaskImpl[].class);
     return Arrays.asList(externalTasks);
   }
 
-  public void lock(String taskId, long lockDuration)  {
+  public void lock(String taskId, long lockDuration) {
     LockRequestDto payload = new LockRequestDto(workerId, lockDuration);
     String resourcePath = LOCK_RESOURCE_PATH.replace("{id}", taskId);
-    String resourceUrl = baseUrl + resourcePath;
+    String resourceUrl = getBaseUrl() + resourcePath;
     engineInteraction.postRequest(resourceUrl, payload, Void.class);
   }
 
-  public void unlock(String taskId)  {
+  public void unlock(String taskId) {
     String resourcePath = UNLOCK_RESOURCE_PATH.replace("{id}", taskId);
-    String resourceUrl = baseUrl + resourcePath;
+    String resourceUrl = getBaseUrl() + resourcePath;
     engineInteraction.postRequest(resourceUrl, null, Void.class);
   }
 
-  public void complete(String taskId, Map<String, Object> variables, Map<String, Object> localVariables)  {
+  public void complete(String taskId, Map<String, Object> variables, Map<String, Object> localVariables) {
     Map<String, TypedValueField> typedValueDtoMap = typedValues.serializeVariables(variables);
     Map<String, TypedValueField> localTypedValueDtoMap = typedValues.serializeVariables(localVariables);
 
     CompleteRequestDto payload = new CompleteRequestDto(workerId, typedValueDtoMap, localTypedValueDtoMap);
     String resourcePath = COMPLETE_RESOURCE_PATH.replace("{id}", taskId);
-    String resourceUrl = baseUrl + resourcePath;
+    String resourceUrl = getBaseUrl() + resourcePath;
     engineInteraction.postRequest(resourceUrl, payload, Void.class);
   }
 
-  public void setVariables(String processId, Map<String, Object> variables)  {
+  public void setVariables(String processId, Map<String, Object> variables) {
     Map<String, TypedValueField> typedValueDtoMap = typedValues.serializeVariables(variables);
     SetVariablesRequestDto payload = new SetVariablesRequestDto(workerId, typedValueDtoMap);
     String resourcePath = SET_VARIABLES_RESOURCE_PATH.replace("{id}", processId);
-    String resourceUrl = baseUrl + resourcePath;
+    String resourceUrl = getBaseUrl() + resourcePath;
     engineInteraction.postRequest(resourceUrl, payload, Void.class);
   }
 
-
-  public void failure(String taskId, String errorMessage, String errorDetails, int retries, long retryTimeout, Map<String, Object> variables, Map<String, Object> localVariables)  {
+  public void failure(String taskId,
+                      String errorMessage,
+                      String errorDetails,
+                      int retries,
+                      long retryTimeout,
+                      Map<String, Object> variables,
+                      Map<String, Object> localVariables) {
     Map<String, TypedValueField> typedValueDtoMap = typedValues.serializeVariables(variables);
     Map<String, TypedValueField> localTypedValueDtoMap = typedValues.serializeVariables(localVariables);
 
-    FailureRequestDto payload = new FailureRequestDto(workerId, errorMessage, errorDetails, retries, retryTimeout, typedValueDtoMap, localTypedValueDtoMap);
+    FailureRequestDto payload = new FailureRequestDto(workerId, errorMessage, errorDetails, retries, retryTimeout,
+        typedValueDtoMap, localTypedValueDtoMap);
     String resourcePath = FAILURE_RESOURCE_PATH.replace("{id}", taskId);
-    String resourceUrl = baseUrl + resourcePath;
+    String resourceUrl = getBaseUrl() + resourcePath;
     engineInteraction.postRequest(resourceUrl, payload, Void.class);
   }
 
-  public void bpmnError(String taskId, String errorCode, String errorMessage, Map<String, Object> variables)  {
+  public void bpmnError(String taskId, String errorCode, String errorMessage, Map<String, Object> variables) {
     Map<String, TypedValueField> typeValueDtoMap = typedValues.serializeVariables(variables);
     BpmnErrorRequestDto payload = new BpmnErrorRequestDto(workerId, errorCode, errorMessage, typeValueDtoMap);
     String resourcePath = BPMN_ERROR_RESOURCE_PATH.replace("{id}", taskId);
-    String resourceUrl = baseUrl + resourcePath;
+    String resourceUrl = getBaseUrl() + resourcePath;
     engineInteraction.postRequest(resourceUrl, payload, Void.class);
   }
 
   public void extendLock(String taskId, long newDuration)  {
     ExtendLockRequestDto payload = new ExtendLockRequestDto(workerId, newDuration);
     String resourcePath = EXTEND_LOCK_RESOURCE_PATH.replace("{id}", taskId);
-    String resourceUrl = baseUrl + resourcePath;
+    String resourceUrl = getBaseUrl() + resourcePath;
     engineInteraction.postRequest(resourceUrl, payload, Void.class);
   }
 
   public byte[] getLocalBinaryVariable(String variableName, String executionId)  {
-    String resourcePath = baseUrl + GET_BINARY_VARIABLE
+    String resourcePath =  getBaseUrl()  + GET_BINARY_VARIABLE
             .replace(ID_PATH_PARAM, executionId)
             .replace(NAME_PATH_PARAM, variableName);
 
@@ -155,7 +195,7 @@ public class EngineClient {
   }
 
   public String getBaseUrl() {
-    return baseUrl;
+    return urlResolver.getBaseUrl();
   }
 
   public String getWorkerId() {
