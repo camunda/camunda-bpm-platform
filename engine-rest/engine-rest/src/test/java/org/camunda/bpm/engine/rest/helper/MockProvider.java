a0ab1fb03da0303d0d47481e6516f6b4013321e2
Jan Rohwer <jan.rohwer@holisticon.de>
2025-05-29 22:20:03 +0200

    add documentation and test cases for createTime in ExternalTask (#5150)

--- engine-rest/engine-rest/src/test/java/org/camunda/bpm/engine/rest/helper/MockProvider.java
+++ engine-rest/engine-rest/src/test/java/org/camunda/bpm/engine/rest/helper/MockProvider.java
@@ -919,6 +919,7 @@ public abstract class MockProvider {
   public static final String EXTERNAL_TASK_ID_LIST = EXTERNAL_TASK_ID + "," + EXTERNAL_TASK_ANOTHER_ID;
   public static final String EXTERNAL_TASK_ERROR_MESSAGE = "some error";
   public static final String EXTERNAL_TASK_LOCK_EXPIRATION_TIME = withTimezone("2015-10-05T13:25:00");
+  public static final String EXTERNAL_TASK_CREATE_TIME = withTimezone("2015-10-05T12:25:00");
   public static final Integer EXTERNAL_TASK_RETRIES = 5;
   public static final boolean EXTERNAL_TASK_SUSPENDED = true;
   public static final String EXTERNAL_TASK_TOPIC_NAME = "aTopic";
@@ -3137,6 +3138,7 @@ public abstract class MockProvider {
       .errorMessage(EXTERNAL_TASK_ERROR_MESSAGE)
       .executionId(EXAMPLE_EXECUTION_ID)
       .lockExpirationTime(DateTimeUtil.parseDate(EXTERNAL_TASK_LOCK_EXPIRATION_TIME))
+      .createTime(DateTimeUtil.parseDate(EXTERNAL_TASK_CREATE_TIME))
       .processDefinitionId(EXAMPLE_PROCESS_DEFINITION_ID)
       .processDefinitionKey(EXAMPLE_PROCESS_DEFINITION_KEY)
       .processDefinitionVersionTag(EXAMPLE_PROCESS_DEFINITION_VERSION_TAG)
