27d280e3fcae6cb7027821d6b31587c71254405a
Yana Vasileva <yana.vasileva@camunda.com>
2025-07-10 16:48:15 +0200

    switch to com.cronutils.model.Cron

--- engine/src/main/java/org/camunda/bpm/engine/impl/calendar/CronTimer.java
+++ engine/src/main/java/org/camunda/bpm/engine/impl/calendar/CronTimer.java
@@ -16,20 +16,29 @@
  */
 package org.camunda.bpm.engine.impl.calendar;
 
+import com.cronutils.model.Cron;
+import com.cronutils.model.CronType;
+import com.cronutils.model.definition.CronDefinitionBuilder;
+import com.cronutils.model.time.ExecutionTime;
+import com.cronutils.parser.CronParser;
 import java.text.ParseException;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeParseException;
 import java.util.Date;
+import java.util.Objects;
 
 /**
- * A cron timer implementation that wraps the existing CronExpression functionality
- * and provides an interface similar to the Zeebe CronTimer.
+ * A cron timer implementation that uses cronutils library for parsing and evaluation.
  */
 public class CronTimer {
 
-  private final CronExpression cronExpression;
+  private final Cron cron;
   private int repetitions;
 
-  public CronTimer(final CronExpression cronExpression) {
-    this.cronExpression = cronExpression;
+  public CronTimer(final Cron cron) {
+    this.cron = cron;
   }
 
   public int getRepetitions() {
@@ -40,25 +49,29 @@ public class CronTimer {
     // set default value to -1
     repetitions = -1;
 
-    final Date fromDate = new Date(fromEpochMilli);
-    final Date nextExecution = cronExpression.getTimeAfter(fromDate);
+    final var next =
+        ExecutionTime.forCron(cron)
+            .nextExecution(
+                ZonedDateTime.ofInstant(
+                    Instant.ofEpochMilli(fromEpochMilli), ZoneId.systemDefault()))
+            .map(ZonedDateTime::toInstant)
+            .map(Instant::toEpochMilli);
 
     // set `repetitions` to 0 when the next execution time does not exist
-    if (nextExecution == null) {
+    if (next.isEmpty()) {
       repetitions = 0;
-      return fromEpochMilli;
     }
 
-    return nextExecution.getTime();
+    return next.orElse(fromEpochMilli);
   }
 
   public static CronTimer parse(final String text) throws ParseException {
     try {
-      final CronExpression cronExpression = new CronExpression(text);
-      return new CronTimer(cronExpression);
-    } catch (final ParseException ex) {
-      throw ex;
-    } catch (final Exception ex) {
+      final var cron =
+          new CronParser(CronDefinitionBuilder.instanceDefinitionFor(CronType.SPRING53))
+              .parse(text);
+      return new CronTimer(cron);
+    } catch (final IllegalArgumentException | NullPointerException ex) {
       throw new ParseException(ex.getMessage(), 0);
     }
   }
