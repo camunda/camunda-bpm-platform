f1ce74d70c93f568308db9f10fb4adc0b15201f7
yanavasileva <yanavasileva@users.noreply.github.com>
2025-07-14 15:44:11 +0200

    Update CycleBusinessCalendarTest.java

--- engine/src/test/java/org/camunda/bpm/engine/test/standalone/calendar/CycleBusinessCalendarTest.java
+++ engine/src/test/java/org/camunda/bpm/engine/test/standalone/calendar/CycleBusinessCalendarTest.java
@@ -17,12 +17,14 @@
 package org.camunda.bpm.engine.test.standalone.calendar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.Assert.assertEquals;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.camunda.bpm.engine.ProcessEngineException;
 import org.camunda.bpm.engine.impl.calendar.CycleBusinessCalendar;
 import org.camunda.bpm.engine.impl.util.ClockUtil;
 import org.junit.After;
@@ -94,6 +96,43 @@ public class CycleBusinessCalendarTest {
   }
 
   @Test
+  public void testResolveDueDate() throws Exception {
+    CycleBusinessCalendar cbc = new CycleBusinessCalendar();
+
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy MM dd HH:mm");
+    Date startDate = sdf.parse("2010 02 11 17:23");
+
+    assertThat(sdf.format(cbc.resolveDuedate("0 0 * * * ?", startDate))).isEqualTo("2010 02 11 18:00");
+    assertThat(sdf.format(cbc.resolveDuedate("*/10 * * * 2 ?", startDate))).isEqualTo("2010 02 11 17:23");
+    assertThat(sdf.format(cbc.resolveDuedate("0 0 8-10 * * ?", startDate))).isEqualTo("2010 02 12 08:00");
+    assertThat(sdf.format(cbc.resolveDuedate("0 0/30 8-10 * * ?", startDate))).isEqualTo("2010 02 12 08:00");
+    assertThat(sdf.format(cbc.resolveDuedate("0 0 9-17 * * ?", startDate))).isEqualTo("2010 02 12 09:00");
+    assertThat(sdf.format(cbc.resolveDuedate("0 0 0 25 12 ?", startDate))).isEqualTo("2010 12 25 00:00");
+    assertThat(sdf.format(cbc.resolveDuedate("0 0 0 L 12 ?", startDate))).isEqualTo("2010 12 31 00:00");
+    assertThat(sdf.format(cbc.resolveDuedate("0 0 * 1|2 * ?", startDate))).isEqualTo("2010 03 01 00:00");
+    assertThat(sdf.format(cbc.resolveDuedate("0 0 6,19 * * ?", startDate))).isEqualTo("2010 02 11 19:00");
+  }
+
+  @Test
+  public void testSpecialCharactersResolveDueDate() throws Exception {
+    CycleBusinessCalendar cbc = new CycleBusinessCalendar();
+
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy MM dd HH:mm");
+    Date startDate = sdf.parse("2010 02 11 17:23");
+
+    assertThat(sdf.format(cbc.resolveDuedate("0 0 0 * * THUL", startDate))).isEqualTo("2010 02 25 00:00");
+    assertThat(sdf.format(cbc.resolveDuedate("0 0 0 1W * *", startDate))).isEqualTo("2010 03 01 00:00");
+    assertThat(sdf.format(cbc.resolveDuedate("0 0 0 ? * 5#2", startDate))).isEqualTo("2010 02 12 00:00");
+    assertThat(sdf.format(cbc.resolveDuedate("@monthly", startDate))).isEqualTo("2010 03 01 00:00");
+    assertThat(sdf.format(cbc.resolveDuedate("@annually", startDate))).isEqualTo("2011 01 01 00:00");
+    assertThat(sdf.format(cbc.resolveDuedate("@yearly", startDate))).isEqualTo("2011 01 01 00:00");
+    assertThat(sdf.format(cbc.resolveDuedate("@weekly", startDate))).isEqualTo("2010 02 14 00:00");
+    assertThat(sdf.format(cbc.resolveDuedate("@daily", startDate))).isEqualTo("2010 02 12 00:00");
+    assertThat(sdf.format(cbc.resolveDuedate("@midnight", startDate))).isEqualTo("2010 02 12 00:00");
+    assertThat(sdf.format(cbc.resolveDuedate("@hourly", startDate))).isEqualTo("2010 02 11 18:00");
+  }
+
+  @Test
   public void testEndOfMonthRelativeExpressions() throws ParseException {
     SimpleDateFormat sdf = new SimpleDateFormat("yyyy MM dd HH:mm");
     CycleBusinessCalendar cbc = new CycleBusinessCalendar();
@@ -106,6 +145,25 @@ public class CycleBusinessCalendarTest {
     assertThat(sdf.format(cbc.resolveDuedate("0 37 8 L-1 * ?", startDate))).isEqualTo("2025 02 27 08:37");
     assertThat(sdf.format(cbc.resolveDuedate("0 0 12 L-15 * ?", startDate))).isEqualTo("2025 03 16 12:00");
     assertThat(sdf.format(cbc.resolveDuedate("0 0 12 L-27 * ?", startDate))).isEqualTo("2025 03 04 12:00");
+
+    // leap year
+    startDate = sdf.parse("2000 02 26 10:00");
+    assertThat(sdf.format(cbc.resolveDuedate("0 15 10 L-3 2 ?", startDate))).isEqualTo("2000 02 26 10:15");
+    startDate = sdf.parse("2000 02 27 10:00");
+    assertThat(sdf.format(cbc.resolveDuedate("0 15 10 L-3 2 ?", startDate))).isEqualTo("2000 02 26 10:15");
+  }
+
+  @Test
+  public void testTooManyArgumentExpressions() throws ParseException {
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy MM dd HH:mm");
+    CycleBusinessCalendar cbc = new CycleBusinessCalendar();
+
+    Date startDate = sdf.parse("2025 02 14 12:00");
+
+    assertThatThrownBy(() -> cbc.resolveDuedate("0 15 10 * * ? 2025 *", startDate))
+        .isInstanceOf(ProcessEngineException.class)
+        .hasMessageContaining("Exception while parsing cycle expression");
+
   }
 
 }
