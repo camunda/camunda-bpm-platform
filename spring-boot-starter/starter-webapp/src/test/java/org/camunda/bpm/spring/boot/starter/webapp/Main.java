1986c67f94fa3b947d41fab842597ce97c8ad1be
Punit Darira <punitdarira@gmail.com>
2025-07-15 01:31:53 -0400

    Formatting PC. Pushing everything

---
+++ spring-boot-starter/starter-webapp/src/test/java/org/camunda/bpm/spring/boot/starter/webapp/Main.java
@@ -0,0 +1,11 @@
+package org.camunda.bpm.spring.boot.starter.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String... args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
