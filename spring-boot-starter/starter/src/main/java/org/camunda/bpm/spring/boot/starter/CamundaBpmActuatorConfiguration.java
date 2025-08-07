a0cd0373cb5218e74c921d60ed4c5a2a8dc9bea6
Punit Darira <punitdarira@gmail.com>
2025-08-06 20:11:04 -0400

    feat(rest): Fixing typo

--- spring-boot-starter/starter/src/main/java/org/camunda/bpm/spring/boot/starter/CamundaBpmActuatorConfiguration.java
+++ spring-boot-starter/starter/src/main/java/org/camunda/bpm/spring/boot/starter/CamundaBpmActuatorConfiguration.java
@@ -26,7 +26,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
